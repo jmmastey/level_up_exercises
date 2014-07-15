549fe5acdd1ab9728f403f138a304c7e193a5568
Ashton Thomas <athomas5@enova.com>
2014-07-15 17:10:15 -0500

    did I fix the tab debacle..?

--- dino_catalog/dino_dex.rb
+++ dino_catalog/dino_dex.rb
@@ -1,89 +1,90 @@
 require './dino'
 require 'hirb'
+require 'json'
 
 class DinoDex
 
-	def initialize(*files)
+  def initialize(*files)
 
-		@dex_array = []
+    @dex_array = []
 
-		if files.nil? || files.size == 0
-			raise 'We have no files to initialize our Dino Dex!'
-		else
-			files.each do |file|
-				process_file(file)
-			end
-		end
+    if files.nil? || files.size == 0
+      raise 'We have no files to initialize our Dino Dex!'
+    else
+      files.each do |file|
+        process_file(file)
+      end
+    end
 
-	end
-
-
-	def filter
-
-		return DinoDexSearch.new(@dex_array)
-
-	end
+  end
 
 
+  def filter
 
-	def process_file(file)
+    return DinoDexSearch.new(@dex_array)
 
-		File.open(file, "r") do |source|
-
-			enum = source.each
-
-			header = enum.next
-
-			config = process_header(header)
-
-			enum.each do |c|
-
-				# split em out and remove the \n and stuffz
-				items = c.split(',').map!{|i|i.gsub(/\s\z/,"")}
-
-				# make the default nil
-				# this is used in the Dino initialize
-				dino_hash = Hash.new(nil)
-
-				config.each_with_index do |key, index|
-
-					if key == :special_diet_carnivore
-						if(items[index] == "Yes")
-							dino_hash[:diet] = "Carnivore"
-						else
-							# Leave this field nil?
-							dino_hash[:diet] = nil
-						end
-					else
-						dino_hash[key] = items[index]
-					end
-
-				end
-
-				@dex_array << Dino.new(dino_hash)
-
-			end
-
-		end
+  end
 
 
-	end
 
-	def process_header header
+  def process_file(file)
 
-		# split header items
-		# make lowercase
-		# remove whitespace
-		columns = header.split(',')
-		columns.map!{|h|h.gsub(/\s+/,"").downcase}
-		return columns.map{|h|Dino.get_symbol_key(h)}
+    File.open(file, "r") do |source|
+
+      enum = source.each
+
+      header = enum.next
+
+      config = process_header(header)
+
+      enum.each do |c|
+
+        # split em out and remove the \n and stuffz
+        items = c.split(',').map!{|i|i.gsub(/\s\z/,"")}
+
+        # make the default nil
+        # this is used in the Dino initialize
+        dino_hash = Hash.new(nil)
+
+        config.each_with_index do |key, index|
+
+          if key == :special_diet_carnivore
+            if(items[index] == "Yes")
+              dino_hash[:diet] = "Carnivore"
+            else
+              # Leave this field nil?
+              dino_hash[:diet] = nil
+            end
+          else
+            dino_hash[key] = items[index]
+          end
+
+        end
+
+        @dex_array << Dino.new(dino_hash)
+
+      end
+
+    end
 
 
-	end
+  end
+
+  def process_header header
+
+    # split header items
+    # make lowercase
+    # remove whitespace
+    columns = header.split(',')
+    columns.map!{|h|h.gsub(/\s+/,"").downcase}
+    return columns.map{|h|Dino.get_symbol_key(h)}
 
 
-	# Not sure what the defacto is here for access (?)
-	private :process_file, :process_header
+  end
+
+
+  # Not sure what the defacto is here for access (?)
+  private :process_file, :process_header
 
 
 end
@@ -91,77 +92,85 @@ end
 
 class DinoDexSearch
 
-	def initialize(dex)
-		@dex = dex
-	end
+  def initialize(dex)
+    @dex = dex
+  end
 
-	def where(key, value)
+  def where(key, value)
 
-		@dex.keep_if do |h|
-			h.instance_variable_get("@#{key}") == value
-		end
+    @dex.keep_if do |h|
+      h.instance_variable_get("@#{key}") == value
+    end
 
-		self
-	end
-
-	def where_in(key, *values)
-
-		@dex.keep_if do |h|
-
-			keep = false
-
-			values.each do |value|
-				if h.instance_variable_get("@#{key}") == value
-					keep = true
-				end
-			end
+    self
+  end
 
 
-			keep
-		end
+  def where_in(key, *values)
 
-		self
-	end
+    @dex.keep_if do |h|
 
-	def more_than(key, value)
-		@dex.keep_if do |h|
-			h.instance_variable_get("@#{key}").to_i > value
-		end
+      keep = false
 
-		self
-	end
-
-	def print
+      values.each do |value|
+        if h.instance_variable_get("@#{key}") == value
+          keep = true
+        end
+      end
 
 
-		@dex.sort! { |a,b| a.name.downcase <=> b.name.downcase }
+      keep
+    end
 
-		display_array = []
-
-
-		@dex.each do |dino|
-			display_array <<
-				[
-					dino.name, dino.period, dino.continent,
-					dino.diet, dino.weight, dino.walking,
-					dino.description
-				]
-		end
+    self
+  end
 
 
-		puts Hirb::Helpers::AutoTable.render(display_array,
-			:headers =>
-				[
-					'NAME','PERIOD', 'CONTINENT', 'DIET',
-					'WEIGHT', 'WALKING', 'DESCRIPTION'
-				])
+
+  def more_than(key, value)
+    @dex.keep_if do |h|
+      h.instance_variable_get("@#{key}").to_i > value
+    end
+
+    self
+  end
 
 
-	end
 
-	def export
-		#TODO
-	end
+  def print
+
+
+    @dex.sort! { |a,b| a.name.downcase <=> b.name.downcase }
+
+    display_array = []
+
+
+    @dex.each do |dino|
+      display_array <<
+        [
+          dino.name, dino.period, dino.continent,
+          dino.diet, dino.weight, dino.walking,
+          dino.description
+        ]
+    end
+
+
+    puts Hirb::Helpers::AutoTable.render(display_array,
+      :headers =>
+        [
+          'NAME','PERIOD', 'CONTINENT', 'DIET',
+          'WEIGHT', 'WALKING', 'DESCRIPTION'
+        ])
+
+
+  end
+
+  def export_json
+
+
+
+
+  end
 
 end
 
@@ -179,8 +188,8 @@ dex = DinoDex.new('dinodex.csv','african_dinosaur_export.csv')
 
 
 dex.filter().where(:walking, "Biped")
-	.where_in(:diet, "Carnivore", "Insectivore", "Piscivore")
-	.print()
+  .where_in(:diet, "Carnivore", "Insectivore", "Piscivore")
+  .print()
 
 
 # dex.filter().where(:walking, "Biped")

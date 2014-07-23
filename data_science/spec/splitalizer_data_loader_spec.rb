1e9c86cbe3e099a3a99bfac75c07fd57d8d8f41f
Ashton Thomas <athomas5@enova.com>
2014-07-23 09:46:44 -0500

    adding test to Splitalizer for DataScience

---
+++ data_science/spec/splitalizer_data_loader_spec.rb
@@ -0,0 +1,21 @@
+require './lib/splitalizer_data_loader.rb'
+
+describe SplitalizerDataLoader do
+
+	context 'when there is no data' do
+		# data = SplitalizerDataLoader.parse("./spec/data/test_no_data.json")
+		# splital = Splitalizer.new(data)
+
+	end
+
+	context 'when there is no file' do
+
+	end
+
+	context 'when there are no soruce parameters' do
+
+	end
+
+
+
+end

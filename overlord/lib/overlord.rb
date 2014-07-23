92b10ceb7fd26b57867b121d7860b9d058b1b37e
Ashton Thomas <athomas5@enova.com>
2014-07-23 16:47:01 -0500

    adding bootstrap

--- overlord/lib/overlord.rb
+++ overlord/lib/overlord.rb
@@ -1,9 +1,18 @@
 module Overlord
   class Bomber < Sinatra::Base
+
     use Rack::Session::Pool, :expire_after => 360000
+    set :public, 'public'
+
+    helpers do
+      def username
+        session[:identity] ? session[:identity] : 'Hello stranger'
+      end
+    end
 
     get '/' do
-      "Time to build an app around here. Start time: " + start_time
+      # "Time to build an app around here. Start time: " + start_time
+      erb(:off)
     end
 
     get '/boom' do

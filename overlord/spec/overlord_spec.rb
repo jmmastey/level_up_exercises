33f720e6f8111201869b110820bc5959730210e3
Ashton Thomas <athomas5@enova.com>
2014-07-25 11:10:28 -0500

    gitignore coverage and add capybara

--- overlord/spec/overlord_spec.rb
+++ overlord/spec/overlord_spec.rb
@@ -1,9 +1,20 @@
 require 'spec_helper'
+# require 'capybara'
+# require 'capybara/dsl'
+
+RSpec.configure do |config|
+  config.include Capybara::DSL
+end
 
 describe Overlord::Bomber do
 
+  # https://gist.github.com/zhengjia/428105
+  # (Slide 44) http://www.slideshare.net/bsbodden/rspec-and-capybara
+  # http://youtu.be/BG_DDUD4M9E?t=47m18s
+  # https://learn.thoughtbot.com/test-driven-rails-resources/capybara.pdf
+
   def app
-    @app ||= Overlord::Bomber
+    Capybara.app = Overlord::Bomber
   end
 
   describe "GET '/'" do
@@ -12,4 +23,12 @@ describe Overlord::Bomber do
       expect(last_response).to be_ok
     end
   end
+
+  describe "foo capybara screwery" do
+    it "should do something" do
+      visit '/'
+      expect(page.has_content?("BOOT DA BOMB")).to be_truthy
+    end
+  end
+
 end

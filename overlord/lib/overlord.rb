8f544f66d14fc8ce239f4f4909d8644beaea63e9
Ashton Thomas <athomas5@enova.com>
2014-07-25 12:50:45 -0500

    adding state pages

--- overlord/lib/overlord.rb
+++ overlord/lib/overlord.rb
@@ -2,7 +2,10 @@ module Overlord
   class Bomber < Sinatra::Base
 
     use Rack::Session::Pool, :expire_after => 360000
+    # set :root, File.dirname(__FILE__)
+    # set :public_folder, '../public'
     set :public, 'public'
+
 
     helpers do
       def username
@@ -12,12 +15,32 @@ module Overlord
 
     get '/' do
       # "Time to build an app around here. Start time: " + start_time
-      erb(:off)
+      erb :off
+    end
+
+    get '/boot_process' do
+      erb :boot_process
+    end
+
+    get '/stand_by' do
+      erb :stand_by
+    end
+
+    get '/activating' do
+      erb :activating
+    end
+
+    get '/activated' do
+      erb :activated
+    end
+
+    get '/deactivating' do
+      erb :deactivating
     end
 
     get '/boom' do
-      puts "Sorry, we're dead..."
-      halt 521
+      # status 521
+      erb :boom
     end
 
     # we can shove stuff into the session cookie YAY!

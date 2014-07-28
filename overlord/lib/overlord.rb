b926f7cceac139dc377a62f9565b9d52ea2e281e
Ashton Thomas <athomas5@enova.com>
2014-07-28 14:04:15 -0500

    update to enihilation machine. bulk of functionality implemented. needs improvement

--- overlord/lib/overlord.rb
+++ overlord/lib/overlord.rb
@@ -1,56 +1,218 @@
+require 'logger'
+
 module Overlord
   class Bomber < Sinatra::Base
 
+    LOG = Logger.new(STDOUT)
+
     use Rack::Session::Pool, :expire_after => 360000
-    # set :root, File.dirname(__FILE__)
-    # set :public_folder, '../public'
     set :public_folder, 'public'
 
-
     helpers do
-      def username
-        session[:identity] ? session[:identity] : 'Hello stranger'
+
+      def is_boomed?
+        boom_time = session[:boom_time]
+
+        return false if boom_time.nil?
+
+        boom_time < Time.now
+
       end
+
+      def booted?
+        !session[:activation_code].nil?
+      end
+
+      def activated?
+        !session[:boom_time].nil?
+      end
+
+      def valid_code?(code)
+        code.match(/^[0-9]{4}$/).size == 1
+      end
+
+      def allow_deactivation?(code)
+        session[:deactivation_code].eql?(code)
+      end
+
+      def allow_activation?(code)
+        session[:activation_code].eql?(code)
+      end
+
+      def inc_deact_count
+        if session[:bad_deact_count].nil?
+          session[:bad_deact_count] = 1
+        else
+          session[:bad_deact_count] += 1
+        end
+
+      end
+
+      def inc_act_count
+        if session[:bad_act_count].nil?
+          session[:bad_act_count] = 1
+        else
+          session[:bad_act_count] += 1
+        end
+      end
+
+      def clear_out
+        session[:deactivation_code] = nil
+        session[:activation_code] = nil
+        session[:bad_deact_count] = 0
+        session[:bad_act_count] = 0
+        session[:boom_time] = nil
+      end
+
+      def template
+        "<script type=\"text/template\" id=\"main-example-template\">
+        <div class=\"time <%= label %>\">
+        <span class=\"count curr top\"><%= curr %></span>
+        <span class=\"count next top\"><%= next %></span>
+        <span class=\"count next bottom\"><%= next %></span>
+        <span class=\"count curr bottom\"><%= curr %></span>
+        <span class=\"label\"><%= label.length < 6 ? label : label.substr(0, 3)  %></span>
+        </div>
+        </script>"
+      end
+
     end
 
     get '/' do
-      # "Time to build an app around here. Start time: " + start_time
+      redirect to('/activated') if activated?
+      redirect to('/stand_by') if booted?
       erb :off
     end
 
     get '/boot_process' do
-      # https://gist.github.com/havvg/3226804
+      redirect to('/activated') if activated?
+      redirect to('/stand_by') if booted?
+
       erb :boot_process
+
     end
 
     post '/stand_by' do
 
-      puts params
+      # validations?
+      activation = params[:post]["activation-code"]
+      deactivation = params[:post]["deactivation-code"]
 
+      if !valid_code?(activation) || !valid_code?(deactivation)
+        redirect to('/boot_process')
+      end
+
+      session[:activation_code] = activation
+      session[:deactivation_code] = deactivation
+
+      erb :stand_by
+
+    end
+
+    get '/stand_by' do
+      redirect to('/activated') if activated?
+      redirect to('/') if !booted?
       erb :stand_by
     end
 
     get '/activate' do
+
+      if session[:bad_act_count].nil? || session[:bad_act_count] == 0
+        @remaining_attempts = nil
+      else
+        @remaining_attempts = 3 - session[:bad_act_count]
+      end
+
       erb :activate
     end
 
     post '/activated' do
+      redirect to('/') if !booted?
+
+      if !activated?
+        activation = params[:post]["activation-code"]
+        seconds = params[:post]["seconds-to-boom"].to_i #exceptions?
+
+        if allow_activation?(activation)
+          session[:bad_act_count] = 0
+          session[:boom_time] = Time.now + seconds
+        else
+          inc_act_count
+          if session[:bad_act_count] >= 3
+            redirect to('/boom')
+          else
+            redirect back
+          end
+        end
+      end
+
+      @template = template
+      @seconds = session[:boom_time] - Time.now
+      @boom_time = session[:boom_time]
+
       erb :activated
+
     end
 
+    get '/activated' do
+      redirect to('/stand_by') if !activated?
+      redirect to('/') if !booted?
+
+      if is_boomed?
+        puts "We are all dead"
+        redirect to('/boom')
+      else
+        @template = template
+        @seconds = session[:boom_time] - Time.now
+        @boom_time = session[:boom_time]
+        erb :activated
+      end
+    end
+
+
     get '/deactivate' do
+      redirect to('/') if !booted?
+
+      if session[:bad_deact_count].nil? || session[:bad_deact_count] == 0
+        @remaining_attempts = nil
+      else
+        @remaining_attempts = 3 - session[:bad_deact_count]
+      end
+
       erb :deactivate
     end
 
-    get '/boom' do
-      # status 521
-      erb :boom
+    post '/deactivated' do
+      # back to Standby?
+      # if valid deactivation code
+      redirect to('/stand_by') # Will need to make stand_by a get request as well
     end
 
-    # we can shove stuff into the session cookie YAY!
-    def start_time
-      session[:start_time] ||= (Time.now).to_s
+    get '/boom' do
+      clear_out
+      erb :boom, :layout => :boom_layout
     end
+
+    post '/power_off' do
+      deactivation = params[:post]["deactivation-code"]
+
+      if allow_deactivation?(deactivation)
+        clear_out
+        redirect to('/')
+      else
+        inc_deact_count
+
+        if session[:bad_deact_count] >= 3
+          redirect to('/boom')
+        else
+          redirect back
+        end
+
+      end
+
+    end
+
+
 
   end
 end
--- overlord/public/js/app.js
+++ overlord/public/js/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
   $('#bootForm').bootstrapValidator({
     feedbackIcons: {
       valid: 'glyphicon glyphicon-ok',
@@ -17,7 +18,7 @@ $(document).ready(function(){
             message: 'Activation code required and cannot be empty'
           },
           regexp: {
-            regexp: /[0-9]{4}/,
+            regexp: /^[0-9]{4}$/,
             message: 'Activation code must be 4 numbers'
           }
         }
@@ -29,11 +30,46 @@ $(document).ready(function(){
             message: 'Deactivation code required and cannot be empty'
           },
           regexp: {
-            regexp: /[0-9]{4}/,
+            regexp: /^[0-9]{4}$/,
             message: 'Activation code must be 4 numbers'
           }
         }
       }
     }
   });
+
+  $('#activateForm').bootstrapValidator({
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',
+      invalid: 'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
+    },
+    live: 'enabled',
+    message: 'This value is not valid',
+    submitButtons: 'button[type="submit"]',
+    trigger: null,
+    fields: {
+      'post[seconds-to-boom]': {
+        message: 'You are probably going to kill yourself.',
+        validators: {
+          notEmpty: {
+            message: 'Do you really want to kill yourself? Give yourself at least one second.'
+          },
+          regexp: {
+            regexp: /^([0-9]{1,5})$/,
+            message: 'We need to see fireworks sooner.'
+          }
+        }
+      },
+      'post[activation-code]': {
+        message: 'The activation code is not valid',
+        validators: {
+          notEmpty: {
+            message: 'activation code required and cannot be empty'
+          }
+        }
+      }
+    }
+  });
+
 });

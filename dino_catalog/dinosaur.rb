880cd991d8ed01489dfd7292e717a884ad169352
Ramya Kannan <ramyakannan@dhcp-10-224-159-57.enova.com>
2014-08-25 16:25:25 -0500

    some more refactored code

--- dino_catalog/dinosaur.rb
+++ dino_catalog/dinosaur.rb
@@ -1,7 +1,6 @@
 class Dinosaur
 
-  
-  def initialize(dino_data=[], @options={})
+  def initialize(dino_data=[])
     @dinosaur_data = dino_data
   end
 
@@ -33,12 +32,19 @@ class Dinosaur
     criteria.all?{ |method_name, param_value| filter_criteria(method_name, criteria) }
   end
 
+  #def criteria_not_matches?(method_name)
+  # !Dinosaur.method_defined?(:"#{method_name.downcase}?") && !(method_name.downcase).eql?("walking")
+  #end
+
   def filter_criteria(method_name, criteria)
-    puts Dinosaur.methods
-    if ["Walking"].include?(method_name)
-      send("#{method_name.downcase}", criteria[method_name]) if ["Walking"].include?(method_name)
-    else
-      send("#{method_name.downcase}"+"?", criteria[method_name])
+    begin
+      if ["Walking"].include?(method_name)
+        send("#{method_name.downcase}", criteria[method_name]) if ["Walking"].include?(method_name)
+      else
+        send("#{method_name.downcase}"+"?", criteria[method_name])
+      end
+    rescue Exception => e
+      raise "No method error! Please provide valid input like this: Walking-Biped, Diet-Carnivore, etc.."
     end
   end
 

75e60257d9722dd4a70b0947d62689e5a27eebf7
Cedrick Johnson <cjohnson4@enova.com>
2014-08-27 12:11:24 -0500

    More formatting and removing if else blocks to replace with ternary operators

--- style_guide/triangle_facts.rb
+++ style_guide/triangle_facts.rb
@@ -6,60 +6,53 @@ class Triangle
     end
 
     def recite_facts
-	puts 'This triangle is equilateral!' if equilateral?
-	puts 'This triangle is isosceles! Also, that word is hard to type.' if isosceles?
-	puts 'This triangle is scalene and mathematically boring.' if scalene?
+	"This triangle is equilateral!" if equilateral?
+	puts "This triangle is isosceles! Also, that word is hard to type." if isosceles?
+	puts "This triangle is scalene and mathematically boring." if scalene?
 
 	angles = calculate_angles(@side1, @side2, @side3)
-	puts 'The angles of this triangle are ' + angles.join(',')
+	puts "The angles of this triangle are " + angles.join(',')
 
-	puts 'This triangle is also a right triangle!' if angles.include? 90
+	puts "This triangle is also a right triangle!" if angles.include? 90
     end
 
 	private
 
-    def calculate_angles(a,b,c)
-	angleA = radians_to_degrees(Math.acos((b**2 + c**2 - a**2) / (2.0 * b * c)))
-	angleB = radians_to_degrees(Math.acos((a**2 + c**2 - b**2) / (2.0 * a * c)))
-	angleC = radians_to_degrees(Math.acos((a**2 + b**2 - c**2) / (2.0 * a * b)))
+    def calculate_angles(side_a, side_b, side_c)
+	angle_a = radians_to_degrees(Math.acos((side_b**2 + side_c**2 - side_a**2) / 
+					       (2.0 * side_b * side_c)))
 
-	return [angleA, angleB, angleC]
+	angle_b = radians_to_degrees(Math.acos((side_a**2 + side_c**2 - side_b**2) / 
+					       (2.0 * side_a * side_c)))
+
+	angle_c = radians_to_degrees(Math.acos((side_a**2 + side_b**2 - side_c**2) / 
+					       (2.0 * side_a * side_b)))
+
+	[angle_a, angle_b, angle_c]
     end
 
     def radians_to_degrees(rads)
-	return (rads * 180 / Math::PI).round
+	(rads * 180 / Math::PI).round
     end
 
     def equilateral?
-	if @side1 == @side2 && @side2 == @side3
-	    true
-	else
-	    false
-	end
+	@side1 == @side2 && @side2 == @side3 ? true : false
     end
 
     def isosceles?
-	if [@side1, @side2, @side3].uniq.length == 2
-	    true
-	else
-	    false
-	end
+	[@side1, @side2, @side3].uniq.length == 2 ? true : false
     end
 
     def scalene?
-	if ! (equilateral? || isosceles?)
-	    true 
-	else
-	    false 
-	end
+	equilateral? || isosceles? ? false : true
     end
 end
 
 
 triangles = [
-    [5,5,5],
-    [5,12,13],
-    [10,12,10]
+    [5, 5, 5],
+    [5, 12, 13],
+    [10, 12, 10]
 ]
 triangles.each { |sides|
     tri = Triangle.new(*sides)

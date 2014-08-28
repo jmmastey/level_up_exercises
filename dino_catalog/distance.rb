11cad79c54d1841fb4c02cb7e638d9cb6e1d43e9
johnmcconnell <johnnyillinois@gmail.com>
2014-08-28 17:19:04 -0500

    cleaned up and added similarity

--- dino_catalog/distance.rb
+++ dino_catalog/distance.rb
@@ -1,11 +1,19 @@
+module Distance
 
+# the idea is to a normalized value
+# for how close a string is to a
+# valid string
+def Distance::similarity(unknown,valid)
+  distance = edit_distance(unknown,valid)
+  return distance.to_f / unknown.size
+end
+  
 
-def edit_distance(s1,s2)
+def Distance::edit_distance(s1,s2)
   edit_distance_rec(s1, s1.size, s2, s2.size)
 end
 
-
-def edit_distance_rec(str1,len1,str2,len2)
+def Distance::edit_distance_rec(str1,len1,str2,len2)
   return len2 if len1 == 0 # must add or delete the other letters
   return len1 if len2 == 0
 
@@ -17,4 +25,6 @@ def edit_distance_rec(str1,len1,str2,len2)
 		edit_distance_rec(str1, len1, str2, len2 - 1) + 1,
 		edit_distance_rec(str1, len1 - 1, str2, len2 - 1) + cost
 	].min
+end
+
 end

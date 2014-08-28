52558176bd935b7122022fe3a4414db5a21321bf
Cedrick Johnson <cjohnson4@enova.com>
2014-08-28 17:29:43 -0500

    started working on the short methods exercise

--- short_methods/robot_name.rb
+++ short_methods/robot_name.rb
@@ -3,29 +3,37 @@ class NameCollisionError < RuntimeError; end;
 class Robot
   attr_accessor :name
 
-  @@registry
-
-  def initialize(args = {})
+  def initialize(args = nil)
     @@registry ||= []
-    @name_generator = args[:name_generator]
 
+    generate_name(args)
 
-    if @name_generator
-      @name = @name_generator.call
-    else
-      generate_char = -> { ('A'..'Z').to_a.sample }
-      generate_num = -> { rand(10) }
-
-      @name = "#{generate_char.call}#{generate_char.call}#{generate_num.call}#{generate_num.call}#{generate_num.call}"
-    end
-
-    raise NameCollisionError, 'There was a problem generating the robot name!' if !(name =~ /[[:alpha:]]{2}[[:digit:]]{3}/) || @@registry.include?(name)
-    @@registry << @name
   end
+
+  def generate_name(params)
+    params = nil ? @name = name_generator : @name = name 
+
+    @@registry << @name if ! @@registry.include? @name
+
+  end
+
+  private
+
+  def name_generator
+    generate_char = -> { ('A'..'Z').to_a.sample }
+    generate_num = -> { rand(10) }
+
+    @name = "#{generate_char.call}#{generate_char.call}#{generate_num.call}
+      #{generate_num.call}#{generate_num.call}"
+  
+    raise NameCollisionError, 'There was a problem generating the robot name!' if 
+      !(@name =~ /[[:alpha:]]{2}[[:digit:]]{3}/) || @@registry.include?(name)
+  end
+
 
 end
 
-robot = Robot.new
+robot = Robot.new()
 puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
 
 # Errors!

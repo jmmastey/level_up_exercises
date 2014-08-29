341fb05da56b08a7e5305474e7ec1bb7ac0ec937
johnmcconnell <johnnyillinois@gmail.com>
2014-08-29 11:42:06 -0500

    created the dinosaur class

---
+++ dino_catalog/dino.rb
@@ -0,0 +1,20 @@
+
+class Dinosaur
+  attr_reader :name
+  attr_reader :period
+  attr_reader :continent
+  attr_reader :diet
+  attr_reader :weight
+  attr_reader :walk
+  attr_reader :desc
+  def initialize(name,period,continent,diet,weight,walk,desc)
+    @name = name
+    @period = period
+    @continent = continent
+    @diet = diet
+    @weight = weight
+    @walk = walk
+    @desc = desc
+  end
+end
+

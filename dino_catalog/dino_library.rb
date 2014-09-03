e157e41e369b50f49fb698ed6c73e1c408002aa9
Ramya Kannan <ramyakannan@dhcp-10-224-109-203.enova.com>
2014-09-03 16:21:42 -0500

    some more refactoring from code review comments

--- dino_catalog/dino_library.rb
+++ dino_catalog/dino_library.rb
@@ -1,39 +1,41 @@
 require 'csv'
-require './parse_files'
+require './file_parser'
 require './dinosaur'
+require 'pp'
 
 class DinoLibrary
-
-  attr_accessor :dinos, :dino_library
-
   def initialize
-    parser = ParseFiles.new
-    self.dino_library = parser.parse_files(["dinodex.csv", "african_dinosaur_export.csv"])
-     @dinos = dino_library.collect do |line|
+    dino_library = FileParser.parse(["dinodex.csv", "african_dinosaur_export.csv"])
+    @dinos = dino_library.collect do |line|
       Dinosaur.new(line)
-     end
+    end
   end
 
   def prompt_user
-    puts "Hello. Welcome to DinoDex Catalog. Please choose any or all of the following options-"
-    puts "Walking-Biped or Quadriped\nDiet-Insectivore or Carnivore or Piscivore\nWeight-4400 (or specify weight in tonnes)\nContinent-North America, Europe, Asia
+    print "Hello. Welcome to DinoDex Catalog.\nPlease choose any or all of the following options- Walking-Biped or Quadriped\nDiet-Insectivore or Carnivore or Piscivore\nWeight-4400 (or specify weight in tonnes)\nContinent-North America, Europe, Asia
 Period-Jurassic or Early or Late.\nElse if you simply know the name of the dinosaur and want all details about it, type in: Information-name_of_dinosaur\n"
   end
 
   def user_input
-    prompt_user
-    user_inp = gets.chomp.split(/[,-]/).each(&:lstrip!)
     begin
-      criteria = Hash[*user_inp]
-      @dinos.select! { |dino| dino.match_criteria(criteria)}
-    rescue Exception => e
+      prompt_user
+      tokenized_inp = tokenize_user_input(gets)
+      criteria = Hash[*tokenized_inp]
+      @dinos.select { |dino| dino.match_criteria(criteria)}
+    rescue
       raise "Please enter valid criteria like this: Walking-Biped, Diet-Carnivore, Period-Jurassic, etc.."
+      retry
     end
-    @dinos.each do |dino| puts dino.inspect
+    @dinos.each do |dino| puts dino.to_s
     end
   end
 
-dinosaurs = DinoLibrary.new
-dinosaurs.user_input
+  def tokenize_user_input(input_string)
+    tokenized_string = input_string.split(/[,-]/).each(&:strip!)
+    tokenized_string
+  end
+
+  dinosaurs = DinoLibrary.new
+  dinosaurs.user_input
 
 end

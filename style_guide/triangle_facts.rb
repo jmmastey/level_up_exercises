49a919c540a0f78df2485b7e6b213c7bf566789c
Colleen Sain <csain@enova.com>
2014-09-03 12:58:41 -0500

    More style changes to triangles

--- style_guide/triangle_facts.rb
+++ style_guide/triangle_facts.rb
@@ -5,18 +5,6 @@ class Triangle
     @side1,@side2,@side3 = side1,side2,side3
   end
 
-  def equilateral
-    side1 == side2 && side2 == side3
-  end
-
-  def isosceles
-    [side1, side2, side3].uniq.length == 2
-  end
-
-  def scalene
-    equilateral || isosceles ? false : true
-  end
-
   def recite_facts
     puts 'This triangle is equilateral!' if equilateral
     puts 'This triangle is isosceles! Also, that word is hard to type.' if isosceles 
@@ -28,17 +16,30 @@ class Triangle
     puts 'This triangle is also a right triangle!' if angles.include? 90
   end
 
-  def calculate_angles(a, b, c)
-    angleA = radians_to_degrees(Math.acos((b**2 + c**2 - a**2) / (2.0 * b * c)))
-    angleB = radians_to_degrees(Math.acos((a**2 + c**2 - b**2) / (2.0 * a * c)))
-    angleC = radians_to_degrees(Math.acos((a**2 + b**2 - c**2) / (2.0 * a * b)))
+  private
+      def equilateral
+	side1 == side2 && side2 == side3
+      end
 
-    [angleA, angleB, angleC]
-  end
+      def isosceles
+	[side1, side2, side3].uniq.length == 2
+      end
 
-  def radians_to_degrees(rads)
-    (rads * 180 / Math::PI).round
-  end
+      def scalene
+	equilateral || isosceles ? false : true
+      end
+
+      def calculate_angles(a, b, c)
+	angleA = radians_to_degrees(Math.acos((b**2 + c**2 - a**2) / (2.0 * b * c)))
+	angleB = radians_to_degrees(Math.acos((a**2 + c**2 - b**2) / (2.0 * a * c)))
+	angleC = radians_to_degrees(Math.acos((a**2 + b**2 - c**2) / (2.0 * a * b)))
+
+	[angleA, angleB, angleC]
+      end
+
+      def radians_to_degrees(rads)
+	(rads * 180 / Math::PI).round
+      end
 end
 
 

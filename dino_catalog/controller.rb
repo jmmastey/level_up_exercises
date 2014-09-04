71b98d0b353e72c822fb71e5694eb56606c18a15
Colleen Sain <csain@enova.com>
2014-09-04 13:20:28 -0500

    Fixed json issue with included file now -_-

--- dino_catalog/controller.rb
+++ dino_catalog/controller.rb
@@ -42,12 +42,9 @@ class Controller
 		def search_by_params_hash
 			# I can pass in a hash, and I'd like to get the proper list of dinos back out
 			puts "Please enter a hash of search params:"
-			puts "example: { :period => 'cretaceous', :diet => 'carnivore' }"
-
+			puts "example: { 'period' : 'cretaceous', 'diet' : 'carnivore' }"
 			
-			#hash = { period: 'cretaceous', diet: 'carnivore' }
 			filter_params = JSON.parse(gets.chomp)
-			puts filter_params
 			puts "\nSearch results:"
 			puts find_dinos_by_params(filter_params).map(&:to_s)	
 		end

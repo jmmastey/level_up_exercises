40f4a7484f023a6a1953ec02dc6ee41065229808
Cedrick Johnson <cjohnson4@enova.com>
2014-09-08 16:18:01 -0500

    Initial checking for short methods exercise

--- short_methods/robot_name.rb
+++ short_methods/robot_name.rb
@@ -2,6 +2,64 @@ class NameCollisionError < RuntimeError; end;
 
 class Robot
   attr_accessor :name
+  @@registry
+
+  def initialize(args = {})
+    @@registry ||= []
+    @name_generator = args[:name_generator]
+    generate_name
+  end
+
+  private
+
+  def generate_name
+
+    generated_name = ""
+    #generate_num = -> { rand(10) }
+    #generate_char = -> { ('A'..'Z').to_a.sample }
+
+    2.times.collect { generated_name << generate_char }
+    3.times.collect { generated_name << generate_num.to_s }
+
+    if is_name_valid?(generated_name)
+      raise NameCollisionError, 'There was a problem generating the robot name'
+    else
+      set_name(generated_name)
+    end
+  end
+
+  def set_name(name)
+    @name = name
+    @@registry << name
+  end
+
+  def generate_num
+    rand(10)
+  end
+
+  def generate_char
+    ('A'..'Z').to_a.sample
+  end
+
+  def is_name_valid?(name)
+    !(name =~ /[[:alpha:]]{2}[[:digit:]]{3}/) || @@registry.include?(name)
+  end
+end
+
+# Errors!
+generator = -> { 'AA111' }
+robot = Robot.new
+puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
+
+
+=begin
+
+This is the OLD WAY... Need to do this thing
+
+class NameCollisionError < RuntimeError; end;
+
+class Robot
+  attr_accessor :name
 
   @@registry
 
@@ -32,3 +90,5 @@ puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
 # generator = -> { 'AA111' }
 # Robot.new(name_generator: generator)
 # Robot.new(name_generator: generator)
+=end
+

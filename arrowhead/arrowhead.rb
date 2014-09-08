be06af4bc5afd9936b49e09e9859177bd29cb24c
Cedrick Johnson <cjohnson4@enova.com>
2014-09-08 15:06:22 -0500

    Made changes to arrowhead exercise to make it easier to read

--- arrowhead/arrowhead.rb
+++ arrowhead/arrowhead.rb
@@ -1,31 +1,48 @@
 class Arrowhead
-
   CLASSIFICATIONS = {
     far_west: {
-      notched: "Archaic Side Notch",
-      stemmed: "Archaic Stemmed",
-      lanceolate: "Agate Basin",
-      bifurcated: "Cody",
+      notched: 'Archaic Side Notch',
+      stemmed: 'Archaic Stemmed',
+      lanceolate: 'Agate Basin',
+      bifurcated: 'Cody',
     },
     northern_plains: {
-      notched: "Besant",
-      stemmed: "Archaic Stemmed",
-      lanceolate: "Humboldt Constricted Base",
-      bifurcated: "Oxbow",
+      notched: 'Besant',
+      stemmed: 'Archaic Stemmed',
+      lanceolate: 'Humboldt Constricted Base',
+      bifurcated: 'Oxbow',
     },
   }
 
+  attr_reader :region, :shape
+
   def self.classify(region, shape)
 
-    raise "Unknown region, please provide a valid
-      region." if CLASSIFICATIONS[region].nil?
+    @region = region
+    @shape = shape
 
-    raise "Unknown shape value. Are you sure you know
-      what you're talking about?" if !CLASSIFICATIONS[region].key? shape
+    unless valid_region?
+      raise "Unknown region, please provide a valid region."
+    end
+
+    unless valid_shape?
+      raise "Unknown shape value. Are you sure you know what you're talking about?"
+    end
 
     arrowhead = CLASSIFICATIONS[region].fetch shape
     puts "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
   end
+
+  def self.valid_region?
+    !CLASSIFICATIONS[@region].nil?
+  end
+
+  def self.valid_shape?
+    CLASSIFICATIONS[@region].key? @shape
+  end
+
+
+
 end
 
 Arrowhead::classify(:northern_plains, :lanceolate)

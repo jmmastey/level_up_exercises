5557c43498bfa2ac017e34fb0cc2b00d4a6bbb9d
Lawrence Walters <lawrencewalters@users.noreply.github.com>
2014-09-09 13:36:19 -0500

    loader and tests fulling working and refactored sandy metz style

--- 02_basic_ruby/dinodex/lib/dinodex_loader.rb
+++ 02_basic_ruby/dinodex/lib/dinodex_loader.rb
@@ -2,8 +2,13 @@ require "CSV"
 require_relative "dinosaur"
 
 class DinodexLoader
+  ALT_COLUMN_HANDLING = {
+      genus: "name",
+      carnivore: {field: "diet", processor: :normalized_diet},
+      weight_in_lbs: "weight"
+  }
 
-  def self.find_csv_files(directory)
+  def find_csv_files(directory)
     csv_files = []
     Dir.entries(directory).select { |f| File.extname(f) == '.csv' }.each do |f|
       csv_files.push f
@@ -11,12 +16,10 @@ class DinodexLoader
     csv_files
   end
 
-  def self.load_csv_file(file)
-    newDinosaurs = []
-    if !File.exists?(file)
-      raise("File #{file} not found")
-    end
+  def load_csv_file(file)
+    raise("File #{file} not found") unless File.exists?(file)
 
+    newDinosaurs = []
     table = CSV.read(file, {headers: true})
     table.each do |row|
       dinosaur = Dinosaur.new
@@ -29,31 +32,19 @@ class DinodexLoader
     newDinosaurs
   end
 
-  def self.parse_csv_field(dinosaur, field, column_name)
-    if !field.nil?
-      #TODO: change this to just a map? dinosaur.send(fieldLookup[field] +"=", value)
-      if ["genus", "name"].include? column_name
-        dinosaur.name = field
-      elsif ["period"].include? column_name
-        dinosaur.period = field
-      elsif ["diet"].include? column_name
-        dinosaur.diet = field
-      elsif ["carnivore"].include? column_name
-        dinosaur.diet = normalized_diet(field)
-      elsif ["weight_in_lbs", "weight"].include? column_name
-        dinosaur.weight = field
-      elsif ["walking"].include? column_name
-        dinosaur.walking = field
-      elsif ["description"].include? column_name
-        dinosaur.description = field
-      elsif ["continent"].include? column_name
-        dinosaur.continent = field
-      end
+  private
+
+  def parse_csv_field(dinosaur, value, column_name)
+    field_name = ALT_COLUMN_HANDLING[column_name.to_sym] || column_name
+    if field_name.is_a?(Hash) && self.respond_to?(field_name[:processor], true)
+      value = self.send(field_name[:processor], value)
+      field_name = field_name[:field]
     end
+    dinosaur.send("#{field_name}=", value) if dinosaur.respond_to? ("#{field_name}=")
   end
 
-  def self.normalized_diet(diet)
-    diet.downcase == "yes" ? "Carnivore" : "Herbivore"
+  def normalized_diet(carnivore)
+    carnivore.downcase == "yes" ? "Carnivore" : "Herbivore"
   end
 
 end
--- 02_basic_ruby/dinodex/spec/dinodex_loader_spec.rb
+++ 02_basic_ruby/dinodex/spec/dinodex_loader_spec.rb
@@ -5,44 +5,65 @@ require "dinosaur"
 
 describe DinodexLoader, "#DinodexLoad" do
   before(:each) do
-    @output = double('output').as_null_object
     @directory = "./inputs"
+    @dinodex_loader = DinodexLoader.new
   end
 
   it "finds 2 files in our csv directory" do
-    expect(DinodexLoader.find_csv_files(@directory).length).to be(2)
+    expect(@dinodex_loader.find_csv_files(@directory).length).to be(2)
   end
 
   it "find african_dinosaur_export.csv" do
-    expect(DinodexLoader.find_csv_files(@directory).to_s).to include("african_dinosaur_export.csv")
+    expect(@dinodex_loader.find_csv_files(@directory).to_s).to include("african_dinosaur_export.csv")
   end
 
   it "find dinodex.csv" do
-    expect(DinodexLoader.find_csv_files(@directory).to_s).to include("dinodex.csv")
+    expect(@dinodex_loader.find_csv_files(@directory).to_s).to include("dinodex.csv")
   end
 
   it "raises an error when file not found" do
     file = "/asdv.csv"
-    expect { DinodexLoader.load_csv_file(@directory + file) }.to raise_error("File #{@directory}#{file} not found")
+    expect { @dinodex_loader.load_csv_file(@directory + file) }.to raise_error("File #{@directory}#{file} not found")
   end
 
   it "loads the dinodex CSV file with 9 dinosaurs" do
-    expect(DinodexLoader.load_csv_file("#{@directory}/dinodex.csv").length).to be(9)
+    expect(@dinodex_loader.load_csv_file("#{@directory}/dinodex.csv").length).to be(9)
   end
 
-  #TODO check for a specific dinosaur in one of these files and validate it's data?
-
-  it "parses weight_in_lbs and weight to dinosaur.weight" do
-    dinosaur = Dinosaur.new
-    dinosaur.weight="0"
-    expect(DinodexLoader.parse_csv_field(dinosaur, "1000", "weight_in_lbs"))
-    expect(dinosaur.weight).to eq("1000")
+  it "parses name field to dinosaur.name" do
+    @dinosaurs = @dinodex_loader.load_csv_file("#{@directory}/dinodex.csv")
+    expect(@dinosaurs.select { |dino| dino.name == "Megalosaurus" }.length).to eq(1)
   end
 
-  it "parses carnivore = no to herbivore" do
-    dinosaur = Dinosaur.new
-    expect(DinodexLoader.parse_csv_field(dinosaur, "no", "carnivore"))
-    expect(dinosaur.diet).to eq("Herbivore")
+  it "parses period field to dinosaur.period" do
+    @dinosaurs = @dinodex_loader.load_csv_file("#{@directory}/dinodex.csv")
+    expect(@dinosaurs.select { |dino| dino.name == "Megalosaurus" }[0].period).to eq("Jurassic")
+  end
+
+end
+
+describe DinodexLoader, "African CSV Parsing" do
+  before(:each) do
+    @directory = "./inputs"
+    @dinodex_loader = DinodexLoader.new
+    @dinosaurs = @dinodex_loader.load_csv_file("#{@directory}/african_dinosaur_export.csv")
+  end
+
+
+  it "parses oddity field genus to name" do
+    expect(@dinosaurs.select { |dino| dino.name == "Afrovenator" }.length).to eq(1)
+  end
+
+  it "parses alt field weight_in_lbs and weight to dinosaur.weight" do
+    expect(@dinosaurs.select { |dino| dino.name == "Abrictosaurus" }[0].weight).to eq("100")
+  end
+
+  it "parses alt field carnivore = no to herbivore" do
+    expect(@dinosaurs.select { |dino| dino.name == "Abrictosaurus" }[0].diet).to eq("Herbivore")
+  end
+
+  it "parses alt field carnivore = yes to carnivore" do
+    expect(@dinosaurs.select { |dino| dino.name == "Afrovenator" }[0].diet).to eq("Carnivore")
   end
 
 end

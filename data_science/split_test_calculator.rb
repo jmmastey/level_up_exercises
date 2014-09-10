b23c7781b008ed954ef8ea757267441fbba09134
Dan Kotowski <dkotowski@enova.com>
2014-09-10 14:50:11 -0500

    Expanded split test application.

--- data_science/split_test_calculator.rb
+++ data_science/split_test_calculator.rb
@@ -1,3 +1,24 @@
 class SplitTestCalculator
+  attr_reader :control_group, :variation_group
 
+  def initialize(stats = {})
+    assign_fields(stats)
+    run_calculations
+  end
+
+  private
+
+  def assign_fields(stats)
+    @control_group = SplitTestGroup.new(
+      conversions: stats[:control_conversions],
+      views: stats[:control_views]
+    )
+    @variation_group = SplitTestGroup.new(
+      conversions: stats[:variation_conversions],
+      views: stats[:variation_views]
+    )
+  end
+
+  def run_calculations
+  end
 end

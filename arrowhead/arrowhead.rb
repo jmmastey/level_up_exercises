f40b9c3d26827d32af650e9794013a6ca8f2f36e
johnmcconnell <johnnyillinois@gmail.com>
2014-09-10 11:29:39 -0500

    return to original message

--- arrowhead/arrowhead.rb
+++ arrowhead/arrowhead.rb
@@ -1,5 +1,5 @@
 require "./arrowdb"
 
-arrowhead = ArrowDb.find_arrowhead(region: :random, type: :bifurcated)
+arrowhead = ArrowDb.find_arrowhead(region: :northern_plains, type: :bifurcated)
 
 puts "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."

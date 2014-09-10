9fdfd17cb6e4aab42d908d15ba6ec3a18063f6f8
Dan Kotowski <dkotowski@enova.com>
2014-09-10 15:17:21 -0500

    Removed 'calculate_' methods and refactored calculations.

--- data_science/split_test_group.rb
+++ data_science/split_test_group.rb
@@ -1,36 +1,31 @@
 class SplitTestGroup
-  attr_reader :conversions, :views, :conversion_rate, \
-    :conversion_rate_interval, :conversion_rate_range
+  attr_reader :conversions, :views
 
   CONFIDENCE_STD_ERROR_FACTOR = 1.96
 
   def initialize(data)
     @conversions = data[:conversions]
     @views = data[:views]
-
-    perform_calculations
   end
 
-  private
-
-  def perform_calculations
-    calculate_conversion_rate
-    calculate_conversion_rate_interval
-    calculate_conversion_rate_range
+  def conversion_rate
+    @conversion_rate ||= @conversions.to_f / @views
   end
 
-  def calculate_conversion_rate
-    @conversion_rate = @conversions.to_f / @views
+  def conversion_rate_interval
+    unless @conversion_rate_interval
+      standard_error = Math.sqrt(@conversion_rate * (1 - @conversion_rate) / @views)
+      @conversion_rate_interval = CONFIDENCE_STD_ERROR_FACTOR * standard_error
+    end
+
+    @conversion_rate_interval
   end
 
-  def calculate_conversion_rate_interval
-    standard_error = Math.sqrt(@conversion_rate * (1 - @conversion_rate) / @views)
-    @conversion_rate_interval = CONFIDENCE_STD_ERROR_FACTOR * standard_error
-  end
-
-  def calculate_conversion_rate_range
-    min = @conversion_rate - @conversion_rate_interval
-    max = @conversion_rate + @conversion_rate_interval
+  def conversion_rate_range
+    unless @conversion_rate_range
+      min = @conversion_rate - @conversion_rate_interval
+      max = @conversion_rate + @conversion_rate_interval
+    end
 
     @conversion_rate_range = [min, max]
   end

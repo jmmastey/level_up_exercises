bc605255736f387fc55e7ba483989e1dde618b08
Dan Kotowski <dkotowski@enova.com>
2014-09-10 15:41:02 -0500

    Fixed issue where unassigned variables were used in calculations.

--- data_science/split_test_calculator.rb
+++ data_science/split_test_calculator.rb
@@ -5,6 +5,16 @@ class SplitTestCalculator
     assign_fields(stats)
   end
 
+  def better_group
+    if @control_group.conversion_rate == @variation_group.conversion_rate
+      nil
+    elsif @control_group.conversion_rate < @variation_group.conversion_rate
+      :variation_group
+    else
+      :control_group
+    end
+  end
+
   private
 
   def assign_fields(stats)

fab7b68a072abb946e93c6ca735769ffe743fdbc
Dan Kotowski <dkotowski@enova.com>
2014-09-10 15:41:02 -0500

    Fixed issue where unassigned variables were used in calculations.

--- data_science/spec/split_test_calculator_spec.rb
+++ data_science/spec/split_test_calculator_spec.rb
@@ -2,14 +2,20 @@ require_relative '../split_test_calculator.rb'
 
 describe SplitTestCalculator do
   it "can be created using a hash of data" do
-    calc = SplitTestCalculator.new(control_views: 1349,
+    @calc = SplitTestCalculator.new(control_views: 1349,
                                    control_conversions: 47,
                                    variation_views: 1543,
                                    variation_conversions: 79)
 
-    expect(calc.control_group.views).to eq(1349)
-    expect(calc.control_group.conversions).to eq(47)
-    expect(calc.variation_group.views).to eq(1543)
-    expect(calc.variation_group.conversions).to eq(79)
+    expect(@calc.control_group.views).to eq(1349)
+    expect(@calc.control_group.conversions).to eq(47)
+    expect(@calc.variation_group.views).to eq(1543)
+    expect(@calc.variation_group.conversions).to eq(79)
+  end
+
+  describe "#better_group" do
+    it "can determine the group with the bigger conversion rate" do
+      expect(@calc.better_group).to eq(:variation_group)
+    end
   end
 end

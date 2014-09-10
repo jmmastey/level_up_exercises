45e2da7f2c04ce221c6b9ceaa4865a2647a615fa
Lawrence Walters <lawrencewalters@users.noreply.github.com>
2014-09-10 14:27:12 -0500

    fixed the sins!!!

--- arrowhead/arrowhead.rb
+++ arrowhead/arrowhead.rb
@@ -1,50 +1,45 @@
 class Arrowhead
-
   # This seriously belongs in a database.
   CLASSIFICATIONS = {
     far_west: {
       notched: "Archaic Side Notch",
       stemmed: "Archaic Stemmed",
       lanceolate: "Agate Basin",
-      bifurcated: "Cody",
+      bifurcated: "Cody"
     },
     northern_plains: {
       notched: "Besant",
       stemmed: "Archaic Stemmed",
       lanceolate: "Humboldt Constricted Base",
-      bifurcated: "Oxbow",
-    },
+      bifurcated: "Oxbow"
+    }
   }
 
-  # FIXME: I don't have time to deal with this.
   def self.classify(region, shape)
-    if CLASSIFICATIONS.include? region
-      shapes = CLASSIFICATIONS[region]
-      if shapes.include? shape
-        arrowhead = shapes[shape]
-        puts "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
-      else
-        raise "Unknown shape value. Are you sure you know what you're talking about?"
-      end
-    else
-      raise "Unknown region, please provide a valid region."
-    end
+    validate_input(CLASSIFICATIONS, region, "region")
+    validate_input(CLASSIFICATIONS[region], shape, "shape in region #{region}")
+
+    arrowhead = CLASSIFICATIONS[region][shape]
+    puts "You have a(n) #{arrowhead} arrowhead. Probably priceless."
   end
 
+  def self.validate_input(map, value, name)
+    map.include?(value) || raise(ArgumentError, "Unknown #{name} #{value}")
+  end
 end
 
+inputs = [
+  [:northern_plains, :bifurcated],
+  [:far_west, :lanceolate],
+  [:northern_plains, :lanceolate],
+  [:northern_plains, :asdf],
+  [:hello, :bifurcated]
+]
 
-def self.classify
-  raise 'Region doesn\'t exist' unless self.region_present?(region)
-  raise "Classification doesn't exist" unless self.classication_present?
-
-  arrowhead = CLASSFICATIONS[region][shape]
-  puts "You have a(n) #{arrowhead} arrowhead. Probably priceless."
+inputs.each do |region_and_shape|
+  begin
+    Arrowhead.classify(region_and_shape[0], region_and_shape[1])
+  rescue ArgumentError => ae
+    puts "Argument error: #{ae.message}"
+  end
 end
-
-def self.region_present?(region)
-  CLASSIFICATIONS.include? region
-end
-
-
-puts Arrowhead::classify(:northern_plains, :bifurcated)

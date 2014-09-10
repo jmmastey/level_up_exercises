038a385591ed0c7d695ae3c66cfe8b6276d19378
johnmcconnell <johnnyillinois@gmail.com>
2014-09-10 10:21:30 -0500

    Added named parameters to cosine func

--- style_guide/triangle_facts.rb
+++ style_guide/triangle_facts.rb
@@ -42,14 +42,21 @@ class Triangle
   end
 
   def angles
-    angle_a = radians_to_degrees(Math.acos(cosine(@side2, @side3, @side1)))
-    angle_b = radians_to_degrees(Math.acos(cosine(@side1, @side3, @side2)))
-    angle_c = radians_to_degrees(Math.acos(cosine(@side1, @side2, @side3)))
+    cos_a = cosine(adjacent1:@side2, adjacent2:@side3, opposite:@side1)
+    cos_b = cosine(adjacent1:@side1, adjacent2:@side3, opposite:@side2)
+    cos_c = cosine(adjacent1:@side1, adjacent2:@side2, opposite:@side3)
+
+    angle_a = radians_to_degrees(Math.acos(cos_a))
+    angle_b = radians_to_degrees(Math.acos(cos_b))
+    angle_c = radians_to_degrees(Math.acos(cos_c))
 
     [angle_a, angle_b, angle_c]
   end
 
-  def cosine(a, b, c)
+  def cosine(sides)
+    a = sides[:adjacent1]
+    b = sides[:adjacent2]
+    c = sides[:opposite]
     (a**2 + b**2 - c**2) / (2.0 * a * b)
   end
 

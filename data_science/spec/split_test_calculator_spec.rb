7bea4afaccd7010babf5c9f162c5c27d64759d72
Dan Kotowski <dkotowski@enova.com>
2014-09-11 09:11:58 -0500

    Added :name field to test group. Modified split test calculator to require groups as input rather than conversion and view counts.

--- data_science/spec/split_test_calculator_spec.rb
+++ data_science/spec/split_test_calculator_spec.rb
@@ -3,34 +3,33 @@ require_relative '../split_test_calculator.rb'
 describe SplitTestCalculator do
 
   it "can be created using a hash of data" do
-    @calc = SplitTestCalculator.new(control_views: 1349,
-                                    control_conversions: 47,
-                                    variation_views: 1543,
-                                    variation_conversions: 79)
-    expect(@calc.control_group.views).to eq(1349)
-    expect(@calc.control_group.conversions).to eq(47)
-    expect(@calc.variation_group.views).to eq(1543)
-    expect(@calc.variation_group.conversions).to eq(79)
+    control_group = SplitTestGroup.new(name: "A", views: 1349, conversions: 47)
+    variation_group = SplitTestGroup.new(name: "B", views:1543, conversions: 79)
+    SplitTestCalculator.new(control_group: control_group,
+                            variation_group: variation_group)
   end
 
-  context "Created using hash" do
-    let(:calc) do
-      SplitTestCalculator.new(control_views: 1349,
-                              control_conversions: 47,
-                              variation_views: 1543,
-                              variation_conversions: 79)
-    end
+  it "requires that the control and variation groups are assigned" do
+    expect { SplitTestCalculator.new() }.to raise_error(ArgumentError)
+    expect { SplitTestCalculator.new(variation_group: SplitTestGroup.new) }
+  end
 
-    describe "#better_group" do
-      it "can determine the group with the bigger conversion rate" do
-        expect(calc.better_group).to eq(:variation_group)
-      end
-    end
+  let(:calc) do
+    control_group = SplitTestGroup.new(name: "A", views: 1349, conversions: 47)
+    variation_group = SplitTestGroup.new(name: "B", views:1543, conversions: 79)
+    SplitTestCalculator.new(control_group: control_group,
+                            variation_group: variation_group)
+  end
 
-    describe "#confident?" do
-      it "determines whether it is 95% confident in a difference between the variations" do
-        expect(calc).to be_confident
-      end
+  describe "#better_group" do
+    it "can determine the group with the bigger conversion rate" do
+      expect(calc.better_group).to eq(:variation_group)
+    end
+  end
+
+  describe "#confident?" do
+    it "determines whether it is 95% confident in a difference between the variations" do
+      expect(calc).to be_confident
     end
   end
 end

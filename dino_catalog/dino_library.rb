43bec109054b8a1cbb640fdcdcddbe259458e159
Ramya Kannan <ramyakannan@dhcp-10-224-109-95.enova.com>
2014-09-11 10:32:19 -0500

    seperated out user input to a seperate class

--- dino_catalog/dino_library.rb
+++ dino_catalog/dino_library.rb
@@ -2,6 +2,8 @@ require 'csv'
 require './file_parser'
 require './dinosaur'
 require 'pp'
+require './user_input'
+require 'pry'
 
 class DinoLibrary
   def initialize
@@ -11,28 +13,25 @@ class DinoLibrary
     end
   end
 
-  def prompt_user
-    print "Hello. Welcome to DinoDex Catalog.\nPlease choose any or all of the following options- Walking-Biped or Quadriped\nDiet-Insectivore or Carnivore or Piscivore\nWeight-4400 (or specify weight in tonnes)\nContinent-North America, Europe, Asia
-Period-Jurassic or Early or Late.\nElse if you simply know the name of the dinosaur and want all details about it, type in: Information-name_of_dinosaur\n"
-  end
-
   def user_input
     begin
-      prompt_user
-      tokenized_inp = tokenize_user_input(gets)
+      dino_list = nil
+      UserInput.prompt_user
+      user_response = gets.chomp
+      tokenized_inp = UserInput.tokenize_user_input(user_response)
       criteria = Hash[*tokenized_inp]
-      @dinos.select { |dino| dino.match_criteria(criteria)}
+      dino_list ||= @dinos
+      dino_list = dino_list.select { |dino| dino.match_criteria(criteria)}
     rescue
       raise "Please enter valid criteria like this: Walking-Biped, Diet-Carnivore, Period-Jurassic, etc.."
-      retry
     end
-    @dinos.each do |dino| puts dino.to_s
-    end
+    print_dinos(dino_list)
+    UserInput.search_again
   end
 
-  def tokenize_user_input(input_string)
-    tokenized_string = input_string.split(/[,-]/).each(&:strip!)
-    tokenized_string
+  def print_dinos(dino_list)
+    dino_list.each do |dino| puts dino.to_s
+    end
   end
 
   dinosaurs = DinoLibrary.new

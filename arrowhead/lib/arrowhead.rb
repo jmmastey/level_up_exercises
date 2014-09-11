ac296a26285725f38133f0529203b4b4bb195fc0
Cedrick Johnson <cjohnson4@enova.com>
2014-09-11 14:24:29 -0500

    refactored solution based on feedback

--- arrowhead/lib/arrowhead.rb
+++ arrowhead/lib/arrowhead.rb
@@ -17,30 +17,26 @@ class Arrowhead
   attr_reader :region, :shape, :arrowhead_name
 
   def self.classify(region, shape)
-    @region = region
-    @shape = shape
-
-    unless valid_region?
-      fail "Unknown region, please provide a valid region."
+    unless valid_region?(region)
+      fail 'Unknown region, please provide a valid region.'
     end
 
-    unless valid_shape?
+    unless valid_shape?(region,shape)
       fail "Unknown shape value. Are you sure you know what you're talking about?"
     end
 
     arrowhead_name = CLASSIFICATIONS[region].fetch shape
   end
 
-  def self.valid_region?
-    !CLASSIFICATIONS[@region].nil?
+  def self.valid_region?(region)
+    !CLASSIFICATIONS[region].nil?
   end
 
-  def self.valid_shape?
-    CLASSIFICATIONS[@region].key? @shape
+  def self.valid_shape?(region, shape)
+    CLASSIFICATIONS[region].key? shape
   end
 
   def to_s
-    "You have a(n) '#{arrowhead_name}' arrowhead. Probably priceless."
+    arrowhead_name
   end
 end
-
--- arrowhead/spec/arrowhead_spec.rb
+++ arrowhead/spec/arrowhead_spec.rb
@@ -2,29 +2,25 @@ require 'spec_helper.rb'
 require_relative '../lib/arrowhead.rb'
 
 describe Arrowhead do
-  before(:all) do
-  end
-
-  after(:all) do
-  end
-
   context 'arrowhead' do
-    describe "valid" do
-      it "should return a valid arrowhead" do
+    describe 'valid' do
+      it 'should return a valid arrowhead' do
         arrowhead = Arrowhead.classify(:far_west, :stemmed)
         expect(arrowhead).to match("Stemmed")
       end
     end
 
-    describe "invalid" do
-      it "should throw a proper error for a invalid region" do
-        expect { Arrowhead.classify(:far_wessst, :stemmed) }.to raise_error
-        (RuntimeError)
+    describe 'invalid' do
+      it 'should throw a proper error for a invalid region' do
+        expect do
+          Arrowhead.classify(:far_wessst, :stemmed)
+        end.to raise_error (RuntimeError)
       end
 
-      it "should throw a proper error for a invalid shape" do
-        expect { Arrowhead.classify(:far_west, :stemmers) }.to raise_error
-        (RuntimeError)
+      it 'should throw a proper error for a invalid shape' do
+        expect do
+          Arrowhead.classify(:far_west, :stemmers)
+        end.to raise_error (RuntimeError)
       end
     end
   end

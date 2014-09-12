c98f69574effeb0800b3093463de9de01c2778d8
Juan bonfante <jbonfante@enova.com>
2014-09-12 13:48:43 -0500

    Fixes to satiate rubocop

--- arrowhead/arrowhead.rb
+++ arrowhead/arrowhead.rb
@@ -2,34 +2,30 @@
 class Arrowhead
   # This seriously belongs in a database.
   CLASSIFICATIONS = {
-      far_west: {
-          notched: 'Archaic Side Notch',
-          stemmed: 'Archaic Stemmed',
-          lanceolate: 'Agate Basin',
-          bifurcated: 'Cody'
-      },
-      northern_plains: {
-          notched: 'Besant',
-          stemmed: 'Archaic Stemmed',
-          lanceolate: 'Humboldt Constricted Base',
-          bifurcated: 'Oxbow'
-      }
+    far_west: {
+      notched: 'Archaic Side Notch',
+      stemmed: 'Archaic Stemmed',
+      lanceolate: 'Agate Basin',
+      bifurcated: 'Cody'
+    },
+    northern_plains: {
+      notched: 'Besant',
+      stemmed: 'Archaic Stemmed',
+      lanceolate: 'Humboldt Constricted Base',
+      bifurcated: 'Oxbow'
+    }
   }
 
   # FIXME: I don't have time to deal with this.
   def self.classify(region, shape)
-    begin
-      arrowhead = get_arrowhead(get_shapes(region), shape)
-      puts "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
-    rescue KeyError
-      raise "Unknown. Are you sure you know what you're talking about?"
-
-    end
+    arrowhead = get_arrowhead(get_shapes(region), shape)
+    puts "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
+  rescue KeyError
+    raise "Unknown. Are you sure you know what you're talking about?"
   end
 
   def self.get_arrowhead(shapes, shape)
     shapes.fetch(shape)
-
   end
 
   def self.get_shapes(region)

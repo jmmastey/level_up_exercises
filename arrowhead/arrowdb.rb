52e141c6361ef200eeab82146780ed688c34fdd1
johnmcconnell <johnnyillinois@gmail.com>
2014-09-12 11:14:22 -0500

    initialized ARROWHEADS with for loop

--- arrowhead/arrowdb.rb
+++ arrowhead/arrowdb.rb
@@ -1,23 +1,26 @@
 require "./arrow.rb"
 
 module ArrowDb
-  ARROWHEADS = [
-    ArrowHead.new(type: :notched, name: "Archaic Side Notch", region: :far_west),
-    ArrowHead.new(type: :stemmed, name: "Archaic Stemmed", region: :far_west),
-    ArrowHead.new(type: :lanceolate, name: "Agate Basin", region: :far_west),
-    ArrowHead.new(type: :bifurcated, name: "Cody", region: :far_west),
-    ArrowHead.new(type: :notched, name: "Besant", region: :northern_plains),
-    ArrowHead.new(type: :stemmed, name: "Archaic Stemmed", region: :northern_plains),
-    ArrowHead.new(type: :lanceolate, name: "Humboldt Constricted Base", region: :northern_plains),
-    ArrowHead.new(type: :bifurcated, name: "Oxbow", region: :northern_plains)
+  RAWDATA = [
+    [:notched, "Archaic Side Notch", :far_west],
+    [:stemmed, "Archaic Stemmed", :far_west],
+    [:lanceolate, "Agate Basin", :far_west],
+    [:bifurcated, "Cody", :far_west],
+    [:notched, "Besant", :northern_plains],
+    [:stemmed, "Archaic Stemmed", :northern_plains],
+    [:lanceolate, "Humboldt Constricted Base", :northern_plains],
+    [:bifurcated, "Oxbow", :northern_plains]
   ]
 
-  def self.find_arrowhead(values)
-    arrowheads = ARROWHEADS.to_enum
-    found_arrowhead = arrowheads.find do |arrowhead|
-      arrowhead.type?(values[:type]) && arrowhead.from?(values[:region])
+  ARROWHEADS = RAWDATA.map do |info|
+    ArrowHead.new(type: info[0], name: info[1], region: info[2])
+  end
+
+  def self.find_arrowhead(params)
+    found_arrowhead = ARROWHEADS.find do |arrowhead|
+      arrowhead.type?(params[:type]) && arrowhead.from?(params[:region])
     end
-    found_arrowhead || raise_not_found_error(values)
+    found_arrowhead || raise_not_found_error(params)
   end
 
   def self.raise_not_found_error(values)

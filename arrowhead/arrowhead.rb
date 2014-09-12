55d2b0561a7d90dbceabbdcb89aad85fea106798
Juan bonfante <jbonfante@enova.com>
2014-09-12 13:47:10 -0500

    Put begin and rescue in right method, and only looking for KeyErrors.

--- arrowhead/arrowhead.rb
+++ arrowhead/arrowhead.rb
@@ -2,30 +2,34 @@
 class Arrowhead
   # This seriously belongs in a database.
   CLASSIFICATIONS = {
-    far_west: {
-      notched: 'Archaic Side Notch',
-      stemmed: 'Archaic Stemmed',
-      lanceolate: 'Agate Basin',
-      bifurcated: 'Cody'
-    },
-    northern_plains: {
-      notched: 'Besant',
-      stemmed: 'Archaic Stemmed',
-      lanceolate: 'Humboldt Constricted Base',
-      bifurcated: 'Oxbow'
-    }
+      far_west: {
+          notched: 'Archaic Side Notch',
+          stemmed: 'Archaic Stemmed',
+          lanceolate: 'Agate Basin',
+          bifurcated: 'Cody'
+      },
+      northern_plains: {
+          notched: 'Besant',
+          stemmed: 'Archaic Stemmed',
+          lanceolate: 'Humboldt Constricted Base',
+          bifurcated: 'Oxbow'
+      }
   }
 
   # FIXME: I don't have time to deal with this.
   def self.classify(region, shape)
-    get_arrowhead(get_shapes(region), shape)
+    begin
+      arrowhead = get_arrowhead(get_shapes(region), shape)
+      puts "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
+    rescue KeyError
+      raise "Unknown. Are you sure you know what you're talking about?"
+
+    end
   end
 
   def self.get_arrowhead(shapes, shape)
-    arrowhead = shapes[shape]
-    puts "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
-  rescue
-    raise "Unknown. Are you sure you know what you're talking about?"
+    shapes.fetch(shape)
+
   end
 
   def self.get_shapes(region)

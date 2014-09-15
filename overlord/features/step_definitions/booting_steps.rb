d8e1d005359e52bd5220eba5026b39d8ff0bc8f4
Dan Kotowski <dkotowski@enova.com>
2014-09-15 16:05:02 -0500

    Filled booting steps.

--- overlord/features/step_definitions/booting_steps.rb
+++ overlord/features/step_definitions/booting_steps.rb
@@ -1,36 +1,43 @@
+require "capybara/cucumber"
+require "capybara/dsl"
+require "capybara/rspec"
+require_relative "../../overlord"
+
+Capybara.app = Overlord
+
 Given(/^my bomb is not booted$/) do
-  pending # express the regexp above with the code you wish you had
+  visit("/")
+  expect(page).to have_content("Someone set up us the bomb")
 end
 
 When(/^I boot my bomb$/) do
-  pending # express the regexp above with the code you wish you had
+  click_on("Boot")
 end
 
 Then(/^my bomb should be inactive$/) do
-  pending # express the regexp above with the code you wish you had
+  expect(find(".activation_status")).to have_content("INACTIVE")
 end
 
-Then(/^my bomb's activation code should be (\d+)$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+Then(/^my bomb's activation code should be (\d+)$/) do |code|
+  expect(@bomb.activation_code).to eq(code)
 end
 
-Then(/^my bomb's deactivation code should be (\d+)$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+Then(/^my bomb's deactivation code should be (\d+)$/) do |code|
+  expect(@bomb.deactivation_code).to eq(code)
 end
 
-Given(/^I set my activation code to (\d+)$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+Given(/^I set my activation code to (\d+)$/) do |code|
+  fill_in("activation_code", with: code)
 end
 
-Given(/^I set my deactivation code to (\d+)$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+Given(/^I set my deactivation code to (\d+)$/) do |code|
+  fill_in("deactivation_code", with: code)
 end
 
-When(/^I set my activation code to "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+When(/^I set my activation code to "(.*?)"$/) do |code|
+  fill_in("activation_code", with: code)
 end
 
 Then(/^my bomb should reject the code$/) do
-  pending # express the regexp above with the code you wish you had
+  expect(page).to have_content("invalid code")
 end
-

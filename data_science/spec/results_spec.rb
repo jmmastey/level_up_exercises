dc8852268d77e2e76fa7feb128da11cff00e5148
Colleen Sain <csain@enova.com>
2014-09-15 17:04:49 -0500

    DRY-ed up results spec

--- data_science/spec/results_spec.rb
+++ data_science/spec/results_spec.rb
@@ -23,20 +23,10 @@ describe Results do
       results = Results.new
       expect { results.add_data }.to raise_error
     end
+  end
 
-    it "parses JSON data and stores as cohort objects" do
-      mock_json = "[{\"date\":\"2014-03-20\",\"cohort\":\"B\",\"result\":1},"
-      mock_json += "{\"date\":\"2014-03-20\",\"cohort\":\"A\",\"result\":0}]"
-      allow(File).to receive(:read).with("some_file").and_return(mock_json)
-
-      results = Results.new
-      results.add_data("some_file")
-
-      expect(results.data.first).to be_a DataPoint
-      expect(results.data.last).to be_a DataPoint
-    end
-
-    it "should create data object for each element in json input" do
+  context "after data has been added" do
+    let(:results) do
       mock_json = "[{\"date\":\"2014-03-20\",\"cohort\":\"B\",\"result\":1},"
       mock_json += "{\"date\":\"2014-03-20\",\"cohort\":\"A\",\"result\":1},"
       mock_json += "{\"date\":\"2014-03-20\",\"cohort\":\"A\",\"result\":0}]"
@@ -44,8 +34,30 @@ describe Results do
 
       results = Results.new
       results.add_data("some_file")
+      results
+    end
 
+    it "parses JSON data and stores as cohort objects" do
+      expect(results.data.first).to be_a DataPoint
+      expect(results.data.last).to be_a DataPoint
+    end
+
+    it "should create data object for each element in json input" do
       expect(results.data.count).to eq(3)
+    end
+
+    it "should add a key for each cohort" do
+      expect(results.cohorts.has_key? "A").to eq(true)
+    end
+
+    describe "#cohorts" do
+      it "should have an accurate count of cohort data" do
+        expect(results.cohorts["A"].visitors).to eq(2)
+        expect(results.cohorts["A"].conversions).to eq(1)
+
+        expect(results.cohorts["B"].visitors).to eq(1)
+        expect(results.cohorts["B"].conversions).to eq(1)
+      end
     end
   end
 end

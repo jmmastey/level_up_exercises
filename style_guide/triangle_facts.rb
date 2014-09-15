ed6477f4781b1e3ef1c62e7b72abf3dc7f680c9f
Lawrence Walters <lawrencewalters@users.noreply.github.com>
2014-09-15 15:48:57 -0500

    updates based on joe's comments

--- style_guide/triangle_facts.rb
+++ style_guide/triangle_facts.rb
@@ -9,27 +9,23 @@ class Triangle
   end
 
   def equilateral?
-    return side1 == side2 && side2 == side3
+    side1 == side2 && side2 == side3
   end
 
   def isosceles?
-    return [side1, side2, side3].uniq.length == 2
+    [side1, side2, side3].uniq.length == 2
   end
 
   def scalene?
-    if !(equilateral? || isosceles?)
-      false
-    else
-      true
-    end
+    !equilateral? && !isosceles?
   end
 
   def recite_facts
     puts 'This triangle is equilateral!' if equilateral?
-    puts 'This triangle is isosceles! Also, that word is hard to type.' if isosceles?
+    puts 'This triangle is isosceles!' if isosceles?
     puts 'This triangle is scalene and mathematically boring.' if scalene?
 
-    angles = self.calculate_angles(side1, side2, side3)
+    angles = calculate_angles(side1, side2, side3)
     puts 'The angles of this triangle are ' + angles.join(',')
 
     puts 'This triangle is also a right triangle!' if angles.include? 90
@@ -37,11 +33,11 @@ class Triangle
   end
 
   def calculate_angles(a, b, c)
-    angleA = radians_to_degrees(Math.acos((b**2 + c**2 - a**2) / (2.0 * b * c)))
-    angleB = radians_to_degrees(Math.acos((a**2 + c**2 - b**2) / (2.0 * a * c)))
-    angleC = radians_to_degrees(Math.acos((a**2 + b**2 - c**2) / (2.0 * a * b)))
+    angl_a = radians_to_degrees(Math.acos((b**2 + c**2 - a**2) / (2.0 * b * c)))
+    angl_b = radians_to_degrees(Math.acos((a**2 + c**2 - b**2) / (2.0 * a * c)))
+    angl_c = radians_to_degrees(Math.acos((a**2 + b**2 - c**2) / (2.0 * a * b)))
 
-    [angleA, angleB, angleC]
+    [angl_a, angl_b, angl_c]
   end
 
   def radians_to_degrees(rads)
@@ -49,10 +45,9 @@ class Triangle
   end
 end
 
-
 triangles = [
-    [5, 5, 5],
-    [5, 12, 13],
+  [5, 5, 5],
+  [5, 12, 13]
 ]
 triangles.each do |sides|
   tri = Triangle.new(*sides)

3d7ff64b1790a5c58e9b8cdb8471658691377fde
Lawrence Walters <lawrencewalters@users.noreply.github.com>
2014-09-16 10:12:59 -0500

    done.... some hound issues, like "new hash format" that I can't get to work....

--- short_methods/robot_name.rb
+++ short_methods/robot_name.rb
@@ -1,35 +1,55 @@
 class Robot
   attr_accessor :name
 
-  @@registry
+  @@registry ||= []
 
   def initialize(args = {})
-    @@registry ||= []
-    @name_generator = args[:name_generator]
-
-
-    if @name_generator
-      @name = @name_generator.call
-    else
-      generate_char = -> { ('A'..'Z').to_a.sample }
-      generate_num = -> { rand(10) }
-
-      @name = "#{generate_char.call}#{generate_char.call}#{generate_num.call}#{generate_num.call}#{generate_num.call}"
-    end
-
-    raise ArgumentError, 'There was a problem generating the robot name!' if !(name =~ /[[:alpha:]]{2}[[:digit:]]{3}/) || @@registry.include?(name)
+    @name = create_name(args)
+    validate_name(@name)
     @@registry << @name
   end
 
-end
+  private
 
+  def validate_name(name)
+    validate_pattern(name)
+    check_for_duplicate(name)
+  end
+
+  def check_for_duplicate(name)
+    message = "name '#{name}' is a duplicate of a previously created name"
+    raise ArgumentError, message if @@registry.include?(name)
+  end
+
+  def validate_pattern(name)
+    message = "name '#{name}' doesn't fit the pattern"
+    raise ArgumentError, message unless name =~ /[[:alpha:]]{2}[[:digit:]]{3}/
+  end
+
+  def create_name(args)
+    if args[:name_generator]
+      args[:name_generator].call
+    else
+      "#{generate_chars(2)}#{generate_numbers(3)}"
+    end
+  end
+
+  def generate_numbers(length)
+    sprintf("%0#{length}d", rand(10**length))
+  end
+
+  def generate_chars(length)
+    ('A'..'Z').to_a.sample(length).join
+  end
+end
 
 robot = Robot.new
 puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
-generator = lambda { "AB123" }
+
+generator = -> { "AB123" }
 robot = Robot.new(:name_generator => generator)
 puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
-generator = lambda { "your mom" }
+
 begin
   robot = Robot.new(:name_generator => generator)
   puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
@@ -37,8 +57,10 @@ rescue ArgumentError => error
   puts error.message
 end
 
-
-# Errors!
-# generator = -> { 'AA111' }
-# Robot.new(name_generator: generator)
-# Robot.new(name_generator: generator)
+generator = -> { "your mom" }
+begin
+  robot = Robot.new(:name_generator => generator)
+  puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
+rescue ArgumentError => error
+  puts error.message
+end

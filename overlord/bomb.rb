645fd52236cdc63605e55744efeb4848dc492b66
Colleen Sain <csain@enova.com>
2014-09-17 17:08:28 -0500

    Added more unit tests and functionality to bomb model

--- overlord/bomb.rb
+++ overlord/bomb.rb
@@ -3,14 +3,33 @@ class Bomb
 
   def initialize(options = {})
     @active = false
+    @exploded = false
     @activation_code = options[:activation_code] || 1234
     @deactivation_code = options[:deactivation_code] || 0
+    @deactivation_attempts = 0
 
     raise ArgumentError unless valid_input?
   end
 
   def activated?
     @active
+  end
+
+  def exploded?
+    @exploded
+  end
+
+  def activate(code)
+    @active = true if code == activation_code
+  end
+
+  def deactivate(code)
+    if code == deactivation_code
+      @active = false
+    else
+      @deactivation_attempts += 1
+      explode if @deactivation_attempts >= 3
+    end
   end
 
   private
@@ -21,5 +40,9 @@ class Bomb
 
   def int_in_range?(input)
     input.is_a?(Integer) && input.between?(0,9999)
+  end
+  
+  def explode
+    @exploded = true
   end
 end
--- overlord/spec/bomb_spec.rb
+++ overlord/spec/bomb_spec.rb
@@ -3,10 +3,15 @@ require_relative "../bomb"
 
 describe Bomb do
   let(:bomb) { Bomb.new }
+  let(:activated_bomb) { bomb.activate(1234) }
 
   describe "#initialize" do
     it "is initially decatived" do
       expect(bomb).not_to be_activated
+    end
+    
+    it "is initially has not exploded..." do
+      expect(bomb).not_to be_exploded
     end
 
     it "has a default activation code of 1234" do
@@ -39,6 +44,47 @@ describe Bomb do
       expect { Bomb.new(deactivation_code: 0.5) }.to raise_error ArgumentError
       expect { Bomb.new(deactivation_code: 10000) }.to raise_error ArgumentError
       expect { Bomb.new(deactivation_code: "words") }.to raise_error ArgumentError
+    end
+  end
+
+  describe "#activate" do
+
+    it "should be activated on correct activation code entry" do
+      bomb.activate(1234)
+      expect(bomb).to be_activated
+    end
+
+    it "should remain deactivated if incorrect activation code entry" do
+      bomb.activate(1111)
+      expect(bomb).not_to be_activated
+    end
+
+    it "should stay activated upon additional activation entries" do
+      bomb.activate(1234)
+      bomb.activate(1234)
+      expect(bomb).to be_activated
+    end
+  end
+
+  describe "#deactivate" do
+    it "should become inactive upon correct entry of deactivation code" do
+      bomb.activate(1234)
+      bomb.deactivate(0)
+      expect(bomb).not_to be_activated
+    end
+
+    it "should stay active upon entry of incorrect deactivation code" do
+      bomb.activate(1234)
+      bomb.deactivate(1234)
+      expect(bomb).to be_activated
+    end
+
+    it "should explode after three incorrect deactivation attempts" do
+      bomb.activate(1234)
+      bomb.deactivate(1)
+      bomb.deactivate(1)
+      bomb.deactivate(1)
+      expect(bomb).to be_exploded
     end
   end
 end

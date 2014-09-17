6bdb9af2d5ec20a24442cdcac1fa7b188dcbb8a3
Colleen Sain <csain@enova.com>
2014-09-17 16:31:29 -0500

    Initial code for bomb class model

--- overlord/bomb.rb
+++ overlord/bomb.rb
@@ -1,3 +1,25 @@
 class Bomb
+  attr_reader :activation_code, :deactivation_code
 
+  def initialize(options = {})
+    @active = false
+    @activation_code = options[:activation_code] || 1234
+    @deactivation_code = options[:deactivation_code] || 0
+
+    raise ArgumentError unless valid_input?
+  end
+
+  def activated?
+    @active
+  end
+
+  private
+
+  def valid_input?
+    int_in_range?(activation_code) && int_in_range?(deactivation_code)
+  end
+
+  def int_in_range?(input)
+    input.is_a?(Integer) && input.between?(0,9999)
+  end
 end
--- overlord/spec/bomb_spec.rb
+++ overlord/spec/bomb_spec.rb
@@ -14,7 +14,7 @@ describe Bomb do
     end
 
     it "has a default deactivation code of 0000" do
-      expect(bomb.activation_code).to eq(0)
+      expect(bomb.deactivation_code).to eq(0)
     end
 
     it "can have an activation code set on boot" do
@@ -28,17 +28,17 @@ describe Bomb do
     end
 
     it "must have an integer activation code between 0-9999" do
-      expect { bomb.new(activation_code: -5) }.to raise_error ArgumentError
-      expect { bomb.new(activation_code: 0.5) }.to raise_error ArgumentError
-      expect { bomb.new(activation_code: 10000) }.to raise_error ArgumentError
-      expect { bomb.new(activation_code: "A String") }.to raise_error ArgumentError
+      expect { Bomb.new(activation_code: -5) }.to raise_error ArgumentError
+      expect { Bomb.new(activation_code: 0.5) }.to raise_error ArgumentError
+      expect { Bomb.new(activation_code: 10000) }.to raise_error ArgumentError
+      expect { Bomb.new(activation_code: "words") }.to raise_error ArgumentError
     end
 
     it "must have an integer deactivation code between 0-9999" do
       expect { Bomb.new(deactivation_code: -5) }.to raise_error ArgumentError
       expect { Bomb.new(deactivation_code: 0.5) }.to raise_error ArgumentError
       expect { Bomb.new(deactivation_code: 10000) }.to raise_error ArgumentError
-      expect { Bomb.new(deactivation_code: "A String") }.to raise_error ArgumentError
+      expect { Bomb.new(deactivation_code: "words") }.to raise_error ArgumentError
     end
   end
 end

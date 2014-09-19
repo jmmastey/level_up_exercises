f1893f8d03ffc5ead1c0d860a68a98b8e3862d7e
Colleen Sain <csain@enova.com>
2014-09-19 11:16:58 -0500

    Activation/deactovation code form submission

--- overlord/overlord.rb
+++ overlord/overlord.rb
@@ -1,11 +1,18 @@
 # run `ruby overlord.rb` to run a webserver for this app
 
 require 'sinatra'
+require_relative "bomb"
 
 enable :sessions
 
 get '/' do
   erb :index
+end
+
+post '/set-codes' do
+  @bomb = Bomb.new(activation_code: params[:activation_code],
+                   deactivation_code: params[:deactivation_code])
+  erb :toggle_bomb
 end
 
 # we can shove stuff into the session cookie YAY!

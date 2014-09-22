cbb666a927d0fae74046095532c1506b10f34073
johnmcconnell <johnnyillinois@gmail.com>
2014-09-22 17:15:35 -0500

    added codebox javascript on click

--- overlord/public/js/app.js
+++ overlord/public/js/app.js
@@ -3,3 +3,37 @@
  */
 
 
+codebox = {
+  "stack" : [],
+  "formatted_code": function() {
+    return this.stack.join('');
+  },
+  "update_dom" : function() {
+    $('#codebox-code').val(this.formatted_code());
+  },
+  "press" : function(code) {
+     if (this.stack.length < 4) {
+       this.stack.push(code);
+       this.update_dom();
+     }
+   },
+   "delete" : function() {
+     this.stack.pop();
+     this.update_dom();
+   },
+   "submit" : function() {
+     // add code to send ajax
+   }
+}
+
+
+window.onload = function() {
+  $('#codebox button').click(function(e) {
+    var code = e.target.textContent;
+    codebox.press(code);
+  });
+
+  $('#codebox-delete').click(function(e) {
+    codebox.delete();
+  });
+}

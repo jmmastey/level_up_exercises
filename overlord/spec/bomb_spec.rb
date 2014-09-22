6282d24937948cf0d8ce99cb21f4947c476d2f6a
Colleen Sain <csain@enova.com>
2014-09-22 10:00:34 -0500

    Change bomb specs for code to be in strings

--- overlord/spec/bomb_spec.rb
+++ overlord/spec/bomb_spec.rb
@@ -15,68 +15,60 @@ describe Bomb do
     end
 
     it "has a default activation code of 1234" do
-      expect(bomb.activation_code).to eq(1234)
+      expect(bomb.activation_code).to eq("1234")
     end
 
     it "has a default deactivation code of 0000" do
-      expect(bomb.deactivation_code).to eq(0)
+      expect(bomb.deactivation_code).to eq("0000")
     end
 
-    it "can have an activation code set on boot" do
-      bomb = Bomb.new(activation_code: 1111)
-      expect(bomb.activation_code).to eq(1111)
+    it "can have custom codes set on boot" do
+      bomb = Bomb.new("1111", "4444")
+      expect(bomb.activation_code).to eq("1111")
+      expect(bomb.deactivation_code).to eq("4444")
     end
 
     it "sets codes to defaults if initialized with empty string" do
-      bomb = Bomb.new(activation_code: "", deactivation_code: "")
-      expect(bomb.activation_code).to eq(1234)
-      expect(bomb.deactivation_code).to eq(0)
+      bomb = Bomb.new("", "")
+      expect(bomb.activation_code).to eq("1234")
+      expect(bomb.deactivation_code).to eq("0000")
     end
 
-    it "string integer codes work, too" do
-      bomb = Bomb.new(activation_code: "2222", deactivation_code: "3333")
-      expect(bomb.activation_code).to eq(2222)
-      expect(bomb.deactivation_code).to eq(3333)
+    it "must have a 4-digit string activation code" do
+      expect { Bomb.new("-5") }.to raise_error ArgumentError
+      expect { Bomb.new("0.5") }.to raise_error ArgumentError
+      expect { Bomb.new("10000") }.to raise_error ArgumentError
+      expect { Bomb.new("words") }.to raise_error ArgumentError
+      expect { Bomb.new(1234) }.to raise_error ArgumentError
     end
 
-    it "can have an deactivation code set on boot" do
-      bomb = Bomb.new(deactivation_code: 1111)
-      expect(bomb.deactivation_code).to eq(1111)
-    end
-
-    it "must have an integer activation code between 0-9999" do
-      expect { Bomb.new(activation_code: -5) }.to raise_error ArgumentError
-      expect { Bomb.new(activation_code: 0.5) }.to raise_error ArgumentError
-      expect { Bomb.new(activation_code: 10000) }.to raise_error ArgumentError
-      expect { Bomb.new(activation_code: "words") }.to raise_error ArgumentError
-    end
-
-    it "must have an integer deactivation code between 0-9999" do
-      expect { Bomb.new(deactivation_code: -5) }.to raise_error ArgumentError
-      expect { Bomb.new(deactivation_code: 0.5) }.to raise_error ArgumentError
-      expect { Bomb.new(deactivation_code: 10000) }.to raise_error ArgumentError
-      expect { Bomb.new(deactivation_code: "words") }.to raise_error ArgumentError
+    it "must have a 4-digit string deactivation code" do
+      expect { Bomb.new("1111", "-5") }.to raise_error ArgumentError
+      expect { Bomb.new("1111", "0.5") }.to raise_error ArgumentError
+      expect { Bomb.new("1111", "10000") }.to raise_error ArgumentError
+      expect { Bomb.new("1111", "words") }.to raise_error ArgumentError
+      expect { Bomb.new("1111", 1234) }.to raise_error ArgumentError
     end
   end
 
   describe "#activate" do
     it "should be activated on correct activation code entry" do
-      bomb.activate(1234)
+      bomb.activate("1234")
       expect(bomb).to be_activated
     end
 
-    it "should have 0 deactivation attempts after re-activation" do
-      bomb.activate(1234)
-      expect(bomb).to be_activated
+    it "should have 0 deactivation attempts after activation" do
+      bomb.activate("1234")
+      expect(bomb.deactivation_attempt).to eq(0)
     end
 
     it "should remain deactivated if incorrect activation code entry" do
-      bomb.activate(1111)
+      bomb.activate("1111")
       expect(bomb).not_to be_activated
     end
 
     it "should stay activated upon additional activation entries" do
-      2.times { bomb.activate(1234) }
+      2.times { bomb.activate("1234") }
       expect(bomb).to be_activated
       expect(bomb.status).to eq("activated")
     end
@@ -84,30 +76,30 @@ describe Bomb do
 
   describe "#deactivate" do
     it "should become inactive upon correct entry of deactivation code" do
-      bomb.activate(1234)
+      bomb.activate("1234")
       bomb.deactivate(0)
       expect(bomb).not_to be_activated
       expect(bomb.status).to eq("deactivated")
     end
 
     it "should stay active upon entry of incorrect deactivation code" do
-      bomb.activate(1234)
-      bomb.deactivate(1234)
+      bomb.activate("1234")
+      bomb.deactivate("1234")
       expect(bomb).to be_activated
     end
 
     it "should explode after three incorrect deactivation attempts" do
-      bomb.activate(1234)
+      bomb.activate("1234")
       3.times { bomb.deactivate(1) }
       expect(bomb).to be_exploded
       expect(bomb.status).to eq("exploded")
     end
 
     it "should reset to 0 deactivation attempts after re-activation" do
-      bomb.activate(1234)
-      bomb.deactivate(2222)
-      bomb.deactivate(0000)
-      bomb.activate(1234)
+      bomb.activate("1234")
+      bomb.deactivate("2222")
+      bomb.deactivate("0000")
+      bomb.activate("1234")
       expect(bomb.deactivation_attempts).to eq(0)
     end
   end

c7ad3741eac192dabb680b81f02ca3f6900b67c5
Franco Reyes <freyes@enova.com>
2014-09-22 11:08:38 -0500

    Add Delete cascades and unique constraints
    Added unique constraints to avoid having duplicate users, beers, beer
    styles, breweries and ratings. Also added deletes on cascades to remove
    child entries when parent entries are removed to keep data integrity.

--- yadda/yadda_db.sql
+++ yadda/yadda_db.sql
@@ -29,8 +29,8 @@ CREATE TABLE beer_styles (
 
 CREATE TABLE beers (
     id serial PRIMARY KEY,
-    brewery_id integer REFERENCES breweries,
-    beer_style_id integer REFERENCES beer_styles,
+    brewery_id integer REFERENCES breweries ON DELETE CASCADE,
+    beer_style_id integer REFERENCES beer_styles ON DELETE CASCADE,
     name varchar(64) NOT NULL,
     description text NOT NULL,
     created_at timestamp NOT NULL,
@@ -44,6 +44,7 @@ CREATE TABLE users (
     id serial PRIMARY KEY,
     first_name varchar(64) NOT NULL,
     last_name varchar(64) NOT NULL,
+    email     varchar (255) NOT NULL,
     birth_date date NOT NULL,
     blood_type varchar(2) NOT NULL,
     created_at timestamp NOT NULL,
@@ -54,8 +55,8 @@ CREATE TABLE users (
 
 CREATE TABLE ratings (
     id serial PRIMARY KEY,
-    user_id integer REFERENCES users,
-    beer_id integer REFERENCES beers,
+    user_id integer REFERENCES users ON DELETE CASCADE,
+    beer_id integer REFERENCES beers ON DELETE CASCADE,
     rating  integer NOT NULL,
     comment text default '',
     created_at timestamp NOT NULL,
@@ -63,3 +64,10 @@ CREATE TABLE ratings (
     created_by varchar(64) NOT NULL,
     updated_by varchar(64) NOT NULL
 );
+
+CREATE UNIQUE INDEX breweries_name_and_address ON breweries(name, address);
+CREATE UNIQUE INDEX beer_styles_style ON beer_styles(style);
+CREATE UNIQUE INDEX beers_breweries_styles_name ON beers(brewery_id, beer_style_id, name);
+CREATE UNIQUE INDEX ratings_user_and_beer_id ON ratings(user_id, beer_id);
+CREATE UNIQUE INDEX users_email ON users(email);
+

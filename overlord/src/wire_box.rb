1b58ef8a7b75c9d947ea41ac85b5a76c35126855
johnmcconnell <johnnyillinois@gmail.com>
2014-09-24 16:51:00 -0500

    removed default port from app

--- overlord/src/wire_box.rb
+++ overlord/src/wire_box.rb
@@ -1,6 +1,8 @@
 require_relative "wire"
 
 class WireBox
+  attr_reader :wires, :triggered
+  alias_method :triggered?, :triggered
   def initialize(params)
     @wires = params[:wire_colors].map { |color| Wire.new(color) }
     @safe_wire = @wires.find { |wire| wire.color ==  params[:safe_color] }
@@ -15,11 +17,4 @@ class WireBox
     @triggered = true unless wire == @safe_wire
   end
 
-  def triggered?
-    @triggered
-  end
-
-  def wires
-    @wires
-  end
 end

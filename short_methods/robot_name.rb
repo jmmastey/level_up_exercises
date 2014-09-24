b71bcc0baa13acb36fa9898dbb6bc3017c9f1061
Stacy Mullins <smullins@clm-smullins.enova.com>
2014-09-24 13:55:25 -0500

    Add short methods exercise

--- short_methods/robot_name.rb
+++ short_methods/robot_name.rb
@@ -6,29 +6,50 @@ class Robot
   @@registry
 
   def initialize(args = {})
-    @@registry ||= []
-    @name_generator = args[:name_generator]
-
-
-    if @name_generator
-      @name = @name_generator.call
-    else
-      generate_char = -> { ('A'..'Z').to_a.sample }
-      generate_num = -> { rand(10) }
-
-      @name = "#{generate_char.call}#{generate_char.call}#{generate_num.call}#{generate_num.call}#{generate_num.call}"
-    end
-
-    raise NameCollisionError, 'There was a problem generating the robot name!' if !(name =~ /[[:alpha:]]{2}[[:digit:]]{3}/) || @@registry.include?(name)
-    @@registry << @name
+   @@registry ||= []
+   @name_generator = args[:name_generator]
+   @name = pending_name
+   check_errors
+   @@registry << @name
   end
 
+
+
+  def pending_name   
+   @name = @name_generator ?  @name_generator.call : name_output  
+  end
+
+  def name_output
+    "#{gen_char}#{gen_num}"
+  end
+
+  def gen_char    
+      ('A'..'Z').to_a.sample(2).join    
+  end
+
+  def gen_num    
+    ('0'..'9').to_a.sample(3).join     
+  end
+
+  def check_errors  
+   raise_error if name_match || @@registry.include?(name)
+  end
+
+  def raise_error
+    raise NameCollisionError, 'There was a problem generating the robot name!' 
+  end
+
+  def name_match
+    !(name =~ /^\w{2}\d{3}$/)
+  end
 end
 
 robot = Robot.new
 puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
 
-# Errors!
-# generator = -> { 'AA111' }
-# Robot.new(name_generator: generator)
-# Robot.new(name_generator: generator)
+generator = -> { 'AA111' }
+robot = Robot.new(name_generator: generator)
+puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
+generator = -> { 'AB007' }
+robot = Robot.new(name_generator: generator)
+puts "My pet robot's name is #{robot.name}, but we usually call him sparky."

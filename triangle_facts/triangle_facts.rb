40c16888fd37db3ff105b6efd8dd84df7c23bdff
pwojt <pwojtowicz@enova.com>
2014-09-25 16:59:02 -0500

    Refactor triangles once more

--- triangle_facts/triangle_facts.rb
+++ triangle_facts/triangle_facts.rb
@@ -17,7 +17,7 @@ class Triangle
   end
 
   def scalene?
-    !equilateral? && !isosceles?
+    !(equilateral? || !isosceles?)
   end
 
   def recite_facts
@@ -32,23 +32,22 @@ class Triangle
     angles = calculate_angles(side1, side2, side3)
     puts "The angles of this triangle are #{angles.join(",")}"
 
-    puts "This triangle is also a right triangle!" if angles.include? 90
-    puts
+    puts "This triangle is also a right triangle!" if angles.include?(90)
   end
 
   def calculate_angles(a, b, c)
-    angleA = radians_to_degrees(Math.acos((b**2 + c**2 - a**2) / (2.0 * b * c)))
-    angleB = radians_to_degrees(Math.acos((a**2 + c**2 - b**2) / (2.0 * a * c)))
-    angleC = radians_to_degrees(Math.acos((a**2 + b**2 - c**2) / (2.0 * a * b)))
-
-     [angleA, angleB, angleC]
+    [to_angle(a, b, c), to_angle(b, a, c), to_angle(c, a, b)]
   end
 
   def radians_to_degrees(rads)
-     (rads * 180 / Math::PI).round
+    (rads * 180 / Math::PI).round
+  end
+
+  def to_angle(angle_side, side_b, side_c)
+    radians_to_degrees(Math.acos((side_b**2 + side_c**2 - angle_side**2) /
+                       (2.0 * side_b * side_c)))
   end
 end
-
 
 triangles = [
   [5, 5, 5],
@@ -59,4 +58,3 @@ triangles.each do |sides|
   tri = Triangle.new(*sides)
   tri.recite_facts
 end
-

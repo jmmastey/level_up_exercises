f0fe3a4acf24110975601b54e2aaa064b8c2c135
Dan Kotowski <dkotowski@enova.com>
2014-09-25 11:13:02 -0500

    Modified calculator and formatter to allow any number of cohorts.

--- data_science/spec/split_test_calculator_spec.rb
+++ data_science/spec/split_test_calculator_spec.rb
@@ -1,42 +1,27 @@
 require_relative '../split_test_calculator.rb'
 
 describe SplitTestCalculator do
-
-  it "can be created using a hash of data" do
-    control_group = SplitTestGroup.new(name: "A",
-                                       views: 1349,
-                                       conversions: 47)
-    variation_group = SplitTestGroup.new(name: "B",
-                                         views: 1543,
-                                         conversions: 79)
-
-    SplitTestCalculator.new(control_group, variation_group)
+  let(:control_group) do
+    SplitTestGroup.new(name: "A",
+                       views: 1349,
+                       conversions: 47)
   end
 
-  it "requires that the control and variation groups are assigned" do
-    expect { SplitTestCalculator.new(nil, nil)}.to raise_error(ArgumentError)
-    expect { SplitTestCalculator.new(nil, SplitTestGroup.new).to raise_error(ArgumentError) }
+  let(:variation_group) do
+    SplitTestGroup.new(name: "B",
+                       views: 1543,
+                       conversions: 79)
   end
 
-  let(:calc) do
-    control_group = SplitTestGroup.new(name: "A",
-                                       views: 1349,
-                                       conversions: 47)
-    variation_group = SplitTestGroup.new(name: "B",
-                                         views: 1543,
-                                         conversions: 79)
-    SplitTestCalculator.new(control_group, variation_group)
+  let(:calc) { SplitTestCalculator.new(control_group, variation_group) }
+
+  it "can be created using an array of groups" do
+    expect { calc }.not_to raise_error
   end
 
   describe "#better_group" do
     it "can determine the group with the bigger conversion rate" do
-      expect(calc.better_group).to eq(:variation_group)
-    end
-  end
-
-  describe "#confident?" do
-    it "returns true if 95% confident in a difference between the variations" do
-      expect(calc).to be_confident
+      expect(calc.better_group).to eq(variation_group)
     end
   end
 

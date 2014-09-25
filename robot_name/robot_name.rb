31e2b31c6dd52beacf6992bdf952ee8024cd81e4
pwojt <pwojtowicz@enova.com>
2014-09-25 16:30:14 -0500

    Refactor robots to clean them up a bit

--- robot_name/robot_name.rb
+++ robot_name/robot_name.rb
@@ -6,38 +6,30 @@ class Robot
   @@registry = []
 
   def initialize(name_generator = nil)
-    @name_generator = name_generator
-    @name = generate_name
-    if is_valid?(@name) && is_uniq?(@name)
-      @@registry << @name
-    else
-      raise NameCollisionError, 'There was a problem generating the robot name!' 
-    end
+    @name_generator = name_generator || method(:default_name_generator)
+    generate_name
   end
 
   private
+
   def generate_name
-    if @name_generator
-      @name = @name_generator.call
-    else
-      @name = "#{2.times.collect { generate_char }.join}" <<
-        "#{3.times.collect { generate_num }.join}"
+    @name = @name_generator.call
+    unless valid?(@name) && uniq?(@name)
+      raise NameCollisionError, 'There was a problem generating the robot name!' 
     end
+    @@registry << @name
   end
 
-  def generate_num
-    rand(10)
+  def default_name_generator
+    @name = (2.times.map { ('A'..'Z').to_a.sample } + 
+             3.times.map { rand(10) }).join
   end
 
-  def generate_char
-    ('A'..'Z').to_a.sample
-  end
-
-  def is_valid?(name)
+  def valid?(name)
     name =~ /[[:alpha:]]{2}[[:digit:]]{3}/
   end
 
-  def is_uniq?(name)
+  def uniq?(name)
     !@@registry.include?(name)
   end
 end
@@ -46,6 +38,11 @@ robot = Robot.new
 puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
 
 # Errors!
-# generator = -> { 'AA111' }
-# Robot.new(name_generator: generator)
-# Robot.new(name_generator: generator)
+generator = -> { 'AA111' }
+Robot.new(generator)
+begin 
+  Robot.new(generator)
+  puts "Faild, exception not raised"
+rescue
+  puts "Works, error called!!!"
+end

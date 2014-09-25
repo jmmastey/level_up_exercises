e3d470c15ce70fcb03bb02e2c2db4a73c2623136
Zamir Syed <zsyed@enova.com>
2014-09-25 09:38:25 -0500

    Moving to javascript driver for Capybara

--- overlord/features/overlord.feature
+++ overlord/features/overlord.feature
@@ -6,26 +6,31 @@ Feature: Overlord Bomb-Status Page
   Background:
     Given I create a bomb from the main page
 
+
+  @javascript
   Scenario: I visit the main page and click create-bomb
     Then I should see the bomb status page
     And the bomb should be intact
     And the bomb should not be activated
 
+  @javascript
   Scenario: I visit the main page, create a bomb, activate it
-    When I select code 1234
-    And I press activate
+    When I activate the bomb with code 1234
     Then I should see the bomb status page
     And the bomb should be intact
     And the bomb should be activated
 
+  @javascript
   Scenario: I visit the main page, create a bomb, activate it, deactivate it
-    When I activate it and deactivate it
+    When I activate it
+    And I deactivate it
     Then I should see the bomb status page
     And the bomb should be intact
     And the bomb should not be activated
     And the deactivation attempts should be 0
     And the bomb message should be "Bomb deactivated"
 
+  @javascript
   Scenario: I visit the main page, create a bomb, activate it, deactivate with wrong code
     When I activate it and unsuccessfully deactivate it 1 time
     Then I should see the bomb status page
@@ -34,6 +39,7 @@ Feature: Overlord Bomb-Status Page
     And the deactivation attempts should be 1
     And the bomb message should be "Wrong code"
 
+  @javascript
   Scenario: I visit the main page, create a bomb, activate it, deactivate with wrong code twice
     When I activate it and unsuccessfully deactivate it 2 times
     Then I should see the bomb status page
@@ -42,6 +48,7 @@ Feature: Overlord Bomb-Status Page
     And the deactivation attempts should be 2
     And the bomb message should be "Wrong code"
 
+  @javascript
   Scenario: I visit the main page, create a bomb, activate it, deactivate with wrong code thrice
     When I activate it and unsuccessfully deactivate it 3 times
     Then I should see the bomb status page
@@ -50,6 +57,7 @@ Feature: Overlord Bomb-Status Page
     And the deactivation attempts should be 3
     And the bomb message should be "Bomb exploded - too many attempts!"
 
+  @javascript
   Scenario: I visit the main page, create a bomb, snip the wires
     When I click on snip 1 time
     Then I should see the bomb status page
@@ -58,6 +66,7 @@ Feature: Overlord Bomb-Status Page
     And the deactivation attempts should be 0
     And the bomb message should be "Bomb wires snipped and is now defunct"
 
+  @javascript
   Scenario: I visit the main page, create a bomb, snip the wires twice
     When I click on snip 2 times
     Then I should see the bomb status page
@@ -66,14 +75,17 @@ Feature: Overlord Bomb-Status Page
     And the deactivation attempts should be 0
     And the bomb message should be "Sorry, bomb wires have been snipped."
 
+  @javascript
   Scenario: I visit the main page, create a bomb, activate it, deactivate it, activate it
-    When I activate it and deactivate it
-	And I activate it
+    When I activate it
+    And I deactivate it
+    And I activate it
     Then I should see the bomb status page
     And the bomb should be intact
     And the bomb should be activated
     And the deactivation attempts should be 0
 
+  @javascript
   Scenario: I visit the main page, create a bomb, activate it and detonate it
     When I activate it
     And I detonate the bomb
--- overlord/features/step_definitions/overlord_steps.rb
+++ overlord/features/step_definitions/overlord_steps.rb
@@ -11,16 +11,6 @@ When /I select code (\d+)/ do |code_value|
   set_keypad_code(code_value)
 end
 
-When /I press activate/ do
-  click_activate_button
-end
-
-When /I press deactivate/ do
-  click_deactivate_button
-end
-
-
-
 When /I click on snip (\d+) time/ do |snips|
   snips.to_i.times { click_snip_wires }
 end
@@ -30,29 +20,20 @@ When /I detonate the bomb/ do
 end
 
 When /I activate it$/ do
-  set_keypad_code('1234')
-  click_activate_button
+  activate_bomb
+end
+
+When /I activate the bomb with code (\d+)/ do |code|
+  activate_bomb(code)
 end
 
 When /I deactivate it$/ do
-  set_keypad_code('1234')
-  click_deactivate_button
-end
-
-When /I activate it and deactivate it/ do
-  set_keypad_code("1234")
-  click_activate_button
-  set_keypad_code("0000")
-  click_deactivate_button
+  deactivate_bomb
 end
 
 When /I activate it and unsuccessfully deactivate it (\d+) time/ do |attempts|
-  set_keypad_code("1234")
-  click_activate_button
-  attempts.to_i.times do
-    set_keypad_code("1111")
-    click_deactivate_button
-  end
+  activate_bomb
+  attempts.to_i.times { deactivate_bomb('1111') }
 end
 
 
@@ -67,41 +48,61 @@ end
 
 # Integrity
 Then /the bomb should be intact/ do
-  expect(value_of_field("integrity")).to eq("Intact")
+  expect(integrity).to eq("Intact")
 end
 
 Then /the bomb should be exploded/ do
-  expect(value_of_field("integrity")).to eq("Blown to shreds")
+  expect(integrity).to eq("Blown to shreds")
 end
 
 Then /the bomb should be snipped/ do
-  expect(value_of_field("integrity")).to eq("Wires are cut")
+  expect(integrity).to eq("Wires are cut")
 end
 
 
 # Activation
 Then /the bomb should be activated/ do
-  expect(value_of_field("activation")).to eq("Active")
+  expect(activation).to eq("Active")
 end
 
 Then /the bomb should not be activated/ do
-  expect(value_of_field("activation")).to eq("Inactive")
+  expect(activation).to eq("Inactive")
 end
 
 Then /the bomb should have defunct activation/ do
-  expect(value_of_field("activation")).to eq("NA")
+  expect(activation).to eq("NA")
 end
 
 
 Then /the deactivation attempts should be (\d+)/ do |attempts|
-  expect(value_of_field("deact_attempts")).to eq(attempts)
+  expect(deact_attempts).to eq(attempts)
 end
 
 Then /the bomb message should be \"(.*?)\"/ do |message|
-  expect(value_of_field("bomb_msg")).to eq(message)
+  expect(bomb_message).to eq(message)
 end
 
 
+
+def activation
+  value_of_field("activation")
+end
+
+def deactivation
+  value_of_field("deactivation")
+end
+
+def integrity
+  value_of_field("integrity")
+end
+
+def deact_attempts
+  value_of_field("deact_attempts")
+end
+
+def bomb_message
+  value_of_field("bomb_msg")
+end
 
 
 def value_of_field(label)
@@ -109,37 +110,32 @@ def value_of_field(label)
   field[:content]
 end
 
+def activate_bomb(code = '1234')
+  set_keypad_code(code)
+  click_activate_button
+end
+
+def deactivate_bomb(code = '0000')
+  set_keypad_code(code)
+  click_deactivate_button
+end
+
 def click_activate_button
-  set_keypad_action("activate")
-  click_submit_action
+  page.execute_script("click_activate()")
 end
 
 def click_deactivate_button
-  set_keypad_action("deactivate")
-  click_submit_action
-end
-
-def click_submit_action
-  button = page.find("button[@id='keypad_submit']", :visible => false)
-  button.click
+  page.execute_script("click_deactivate()")
 end
 
 def click_snip_wires
-  set_keypad_action("snip")
-  click_submit_action
+  page.execute_script("click_snip()")
 end
 
 def set_keypad_code(code)
-  field = page.find("input[@id='keypad_code']")
-  field.set("#{code}")
-end
-
-def set_keypad_action(value)
-  field = page.find("input[@id='keypad_action']")
-  field.set(value)
+  page.execute_script("set_keypad_code(\"#{code}\")")
 end
 
 def detonate_bomb
-  set_keypad_action("detonate")
-  click_submit_action
+  page.execute_script("detonate_bomb()")
 end

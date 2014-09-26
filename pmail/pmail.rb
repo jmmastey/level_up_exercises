3bbc97760e3012dd0da2c8e664448151dc438e95
Franco Reyes <freyes@enova.com>
2014-09-25 22:33:53 -0500

    Creation of view and css file

--- pmail/pmail.rb
+++ pmail/pmail.rb
@@ -3,10 +3,5 @@
 require 'sinatra'
 
 get '/' do
-  "Time to build an app around here. Start time: " + start_time
-end
-
-# we can shove stuff into the session cookie YAY!
-def start_time
-  session[:start_time] ||= (Time.now).to_s
+  haml :index
 end

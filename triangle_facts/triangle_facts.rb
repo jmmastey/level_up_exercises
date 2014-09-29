7753062e786efdb1c20201dce07e1488ec4e8ba3
rcowan <ryancowan@gmail.com>
2014-09-29 17:02:29 -0500

    first pass, mostly spacing after commas

--- triangle_facts/triangle_facts.rb
+++ triangle_facts/triangle_facts.rb
@@ -1,9 +1,9 @@
 # Killer facts about triangles AWW YEAH
 class Triangle
-	attr_accessor :side1,:side2,:side3
+	attr_accessor :side1, :side2, :side3
 
-	def initialize(side1,side2,side3)
-		@side1,@side2,@side3 = side1,side2,side3
+	def initialize(side1, side2, side3)
+		@side1, @side2, @side3 = side1, side2, side3
 	end
 
 	def equilateral()
@@ -11,11 +11,11 @@ class Triangle
 	end
 
 	def isosceles()
-		return [side1,side2,side3].uniq.length == 2
+		return [side1, side2, side3].uniq.length == 2
 	end
 
 	def scalene()
-		unless ! (equilateral || isosceles)
+		unless !(equilateral || isosceles)
 	    false
 	  else
 	    true
@@ -27,14 +27,14 @@ class Triangle
 		puts 'This triangle is isosceles! Also, that word is hard to type.' if isosceles 
 		puts 'This triangle is scalene and mathematically boring.' if scalene 
 
-		angles = self.calculate_angles(side1,side2,side3)
+		angles = self.calculate_angles(side1, side2, side3)
 		puts 'The angles of this triangle are ' + angles.join(',')
 
 		puts 'This triangle is also a right triangle!' if angles.include? 90
 		puts ''
 	end
 
-	def calculate_angles(a,b,c)
+	def calculate_angles(a, b, c)
 		angleA = radians_to_degrees(Math.acos((b**2 + c**2 - a**2) / (2.0 * b * c)))
 		angleB = radians_to_degrees(Math.acos((a**2 + c**2 - b**2) / (2.0 * a * c)))
 		angleC = radians_to_degrees(Math.acos((a**2 + b**2 - c**2) / (2.0 * a * b)))
@@ -49,9 +49,10 @@ end
 
 
 triangles = [
-	[5,5,5],
-	[5,12,13],
+	[5, 5, 5],
+	[5, 12, 13],
 ]
+
 triangles.each { |sides|
 	tri = Triangle.new(*sides)
 	tri.recite_facts

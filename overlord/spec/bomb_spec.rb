c90acfd4e7b12fd5009ff8027a5bb96e5bf075a1
Lawrence Walters <lawrencewalters@users.noreply.github.com>
2014-09-30 12:53:37 -0500

    reorganizing tests to show happy/sad/error path. also used context to centralize some setup functionality.

--- overlord/spec/bomb_spec.rb
+++ overlord/spec/bomb_spec.rb
@@ -1,10 +1,10 @@
 require 'rspec'
 require 'bomb'
 
-describe 'Activate bomb' do
+describe Bomb do
   subject(:bomb) { Bomb.new }
 
-  let(:code_error) { "code must be 4 numbers" }
+  let(:err_msg) { "code must be 4 numbers" }
   let(:valid_code) { "1234" }
   let(:short_code) { "123" }
   let(:empty_code) { "" }
@@ -15,99 +15,106 @@ describe 'Activate bomb' do
     bomb.activate(activation_code, deactivation_code)
   end
 
-  it 'should activate with good code' do
-    activate(valid_code, valid_code)
-    expect(bomb).to be_activated
+  context '.activate' do
+    context 'happy path' do
+      it 'when using valid code' do
+        activate(valid_code, valid_code)
+        is_expected.to be_activated
+      end
+    end
+
+    context 'sad path' do
+      it 'with no activation code' do
+        expect { activate(empty_code, valid_code) }.to raise_error(/#{err_msg}/)
+      end
+
+      it 'with no activation code' do
+        expect { activate(empty_code, valid_code) }.to raise_error(/#{err_msg}/)
+      end
+
+      it 'with no deactivation code' do
+        expect { activate(valid_code, empty_code) }.to raise_error(/#{err_msg}/)
+      end
+
+      it 'with short activation code' do
+        expect { activate(short_code, valid_code) }.to raise_error(/#{err_msg}/)
+      end
+
+      it 'with short deactivation code' do
+        expect { activate(valid_code, short_code) }.to raise_error(/#{err_msg}/)
+      end
+
+      it 'with non numeric activation code' do
+        expect { activate(alpha_code, valid_code) }.to raise_error(/#{err_msg}/)
+      end
+
+      it 'with non numeric deactivation code' do
+        expect { activate(valid_code, alpha_code) }.to raise_error(/#{err_msg}/)
+      end
+
+    end
+
+    context 'error path' do
+      it 'when already activated' do
+        activate(valid_code, valid_code)
+        expect { activate(valid_code, valid_code) }.to raise_error(Bomb::BombError)
+      end
+
+      it 'when already exploded' do
+        activate(valid_code, valid_code)
+        Bomb::MAX_ATTEMPTS.times { bomb.deactivate(invalid_code) }
+        expect(bomb).to be_exploded
+        expect { activate(valid_code, valid_code) }.to raise_error(Bomb::BombError)
+      end
+
+      it 'when I enter the wrong code too many times' do
+        bomb.activate(valid_code, valid_code)
+        Bomb::MAX_ATTEMPTS.times { bomb.deactivate(invalid_code) }
+        expect(bomb).to be_exploded
+      end
+
+    end
   end
 
-  it 'should fail to activate with no activation code' do
-    expect { activate(empty_code, valid_code) }.to raise_error(/#{code_error}/)
-  end
+  context '.deactivate' do
+    context 'happy path' do
+      it 'when I enter the correct code' do
+        bomb.activate(valid_code, valid_code)
+        bomb.deactivate(valid_code)
+        expect(bomb).not_to be_activated
+        expect(bomb).not_to be_exploded
+      end
 
-  it 'should fail to activate with no deactivation code' do
-    expect { activate(valid_code, empty_code) }.to raise_error(/#{code_error}/)
-  end
+      it 'when I enter the right code on the last attempt' do
+        bomb.activate(valid_code, valid_code)
+        (Bomb::MAX_ATTEMPTS - 1).times { bomb.deactivate(invalid_code) }
+        bomb.deactivate(valid_code)
+        expect(bomb).not_to be_activated
+        expect(bomb).not_to be_exploded
+      end
 
-  it 'should fail to activate with short activation code' do
-    expect { activate(short_code, valid_code) }.to raise_error(/#{code_error}/)
-  end
+      it 'should report the number of attempts remaining' do
+        bomb.activate(valid_code, valid_code)
+        bomb.deactivate(invalid_code)
+        expect(bomb.attempts_remaining).to eq(Bomb::MAX_ATTEMPTS - 1)
+      end
+    end
 
-  it 'should fail to activate with short deactivation code' do
-    expect { activate(valid_code, short_code) }.to raise_error(/#{code_error}/)
-  end
+    context 'sad path' do
+      it 'when I enter the wrong code' do
+        bomb.activate(valid_code, valid_code)
+        bomb.deactivate(invalid_code)
+        expect(bomb).to be_activated
+        expect(bomb).not_to be_exploded
+      end
+    end
 
-  it 'should fail to activate with non numeric activation code' do
-    expect { activate(alpha_code, valid_code) }.to raise_error(/#{code_error}/)
-  end
-
-  it 'should fail to activate with non numeric deactivation code' do
-    expect { activate(valid_code, alpha_code) }.to raise_error(/#{code_error}/)
-  end
-
-  it 'should fail to activate when already activated' do
-    activate(valid_code, valid_code)
-    expect { activate(valid_code, valid_code) }.to raise_error(Bomb::BombError)
-  end
-
-  it 'should fail to activate when already exploded' do
-    activate(valid_code, valid_code)
-    Bomb::MAX_ATTEMPTS.times { bomb.deactivate(invalid_code) }
-    expect(bomb).to be_exploded
-    expect { activate(valid_code, valid_code) }.to raise_error(Bomb::BombError)
-  end
-end
-
-describe 'Deactivate bomb' do
-  subject(:bomb) { Bomb.new }
-
-  let(:valid_code) { "1234" }
-  let(:invalid_code) { "5678" }
-
-  it 'should deactivate when I enter the correct code' do
-    bomb.activate(valid_code, valid_code)
-    bomb.deactivate(valid_code)
-    expect(bomb).not_to be_activated
-    expect(bomb).not_to be_exploded
-  end
-
-  it 'should not deactivate when I enter the wrong code' do
-    bomb.activate(valid_code, valid_code)
-    bomb.deactivate(invalid_code)
-    expect(bomb).to be_activated
-    expect(bomb).not_to be_exploded
-  end
-
-  it 'should deactivate when I enter the right code on the last attempt' do
-    bomb.activate(valid_code, valid_code)
-    (Bomb::MAX_ATTEMPTS - 1).times { bomb.deactivate(invalid_code) }
-    bomb.deactivate(valid_code)
-    expect(bomb).not_to be_activated
-    expect(bomb).not_to be_exploded
-  end
-
-  it 'should report the number of attempts remaining' do
-    bomb.activate(valid_code, valid_code)
-    bomb.deactivate(invalid_code)
-    expect(bomb.attempts_remaining).to eq(Bomb::MAX_ATTEMPTS - 1)
-  end
-end
-
-describe 'Explosive scenarios' do
-  subject(:bomb) { Bomb.new }
-
-  let(:valid_code) { "1234" }
-  let(:invalid_code) { "5678" }
-
-  it 'should explode when I enter the wrong code too many times' do
-    bomb.activate(valid_code, valid_code)
-    Bomb::MAX_ATTEMPTS.times { bomb.deactivate(invalid_code) }
-    expect(bomb).to be_exploded
-  end
-
-  it 'should not work when exploded' do
-    bomb.activate(valid_code, valid_code)
-    Bomb::MAX_ATTEMPTS.times { bomb.deactivate(invalid_code) }
-    expect(bomb).to be_exploded
-    expect { bomb.deactivate(valid_code) }.to raise_error(Bomb::BombError)
+    context 'error path' do
+      it 'when exploded' do
+        bomb.activate(valid_code, valid_code)
+        Bomb::MAX_ATTEMPTS.times { bomb.deactivate(invalid_code) }
+        expect { bomb.deactivate(valid_code) }.to raise_error(Bomb::BombError)
+      end
+    end
   end
 end

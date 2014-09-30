036b5a8b605b30ea9e43c4922a4948363334a8e9
Zamir Syed <zsyed@enova.com>
2014-09-30 13:11:00 -0500

    Added Users

---
+++ local_events_feed/app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def create
+    @user = User.create(user_params)
+
+    if @user.save
+      redirect_to @user
+    else
+      render 'index'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
+
+end
--- local_events_feed/config/routes.rb
+++ local_events_feed/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -12,7 +12,9 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  resources :users do
+    resources :events
+  end
 
   # Example resource route with options:
   #   resources :products do

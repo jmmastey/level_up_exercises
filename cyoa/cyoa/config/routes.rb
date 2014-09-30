88a4bd50e90d584c1a26a5de444b287178cb363a
Colleen Sain <csain@enova.com>
2014-09-30 16:35:09 -0500

    Layout to haml

--- cyoa/cyoa/config/routes.rb
+++ cyoa/cyoa/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get "static_pages/home"
+  root "static_pages#home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

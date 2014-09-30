572721f967d7a05f6d32a95dcdf134703710c823
rcowan <ryancowan@gmail.com>
2014-09-30 10:40:28 -0500

    minor comment cleanup, addition of angles var, double quotes, use of triangle_type instead of three functions for each type

--- triangle_facts/triangle_facts.rb
+++ triangle_facts/triangle_facts.rb
@@ -1,40 +1,38 @@
-# Killer facts about triangles AWW YEAH
+# Classify and display information about triangles, based on knowing sides.
 class Triangle
-  attr_accessor :side1, :side2, :side3
+  attr_accessor :side1, :side2, :side3, :angles
 
   def initialize(side1, side2, side3)
     @side1, @side2, @side3 = side1, side2, side3
+
+    @angles = calculate_angles
   end
 
-  def equilateral
-    side1 == side2 && side2 == side3
-  end
-
-  def isosceles
-    [side1, side2, side3].uniq.length == 2
-  end
-
-  def scalene
-    if equilateral || isosceles
-      false
+  def triangle_type
+    case [side1, side2, side3].uniq.length
+    when 1
+      "This triangle is equilateral!"
+    when 2
+      "This triangle is isosceles! Also, that word is hard to type."
     else
-      true
+      "This triangle is scalene and mathematically boring."
     end
   end
 
   def recite_facts
-    puts 'This triangle is equilateral!' if equilateral 
-    puts 'This triangle is isosceles! Also, that word is hard to type.' if isosceles 
-    puts 'This triangle is scalene and mathematically boring.' if scalene 
+    puts triangle_type
 
-    angles = self.calculate_angles(side1, side2, side3)
-    puts 'The angles of this triangle are ' + angles.join(',')
+    puts "The angles of this triangle are #{angles.join(',')}"
 
-    puts 'This triangle is also a right triangle!' if angles.include? 90
-    puts ''
+    puts "This triangle is also a right triangle!" if angles.include? 90
+    puts "\n"
   end
 
-  def calculate_angles(a, b, c)
+  def calculate_angles
+    a = side1
+    b = side2
+    c = side3
+
     angleA = radians_to_degrees(Math.acos((b**2 + c**2 - a**2) / (2.0 * b * c)))
     angleB = radians_to_degrees(Math.acos((a**2 + c**2 - b**2) / (2.0 * a * c)))
     angleC = radians_to_degrees(Math.acos((a**2 + b**2 - c**2) / (2.0 * a * b)))
@@ -50,6 +48,7 @@ end
 triangles = [
   [5, 5, 5],
   [5, 12, 13],
+  [5, 5, 7],
 ]
 
 triangles.each do |sides|

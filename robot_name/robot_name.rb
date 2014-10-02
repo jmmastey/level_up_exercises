2038b3d011f045920549a828a76877b6ea9056d9
rcowan <ryancowan@gmail.com>
2014-10-02 10:29:17 -0500

    remove output statement after forcing an error

--- robot_name/robot_name.rb
+++ robot_name/robot_name.rb
@@ -55,5 +55,3 @@ puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
 generator = -> { 'AA111' }
 Robot.new(name_generator: generator)
 Robot.new(name_generator: generator)
-
-puts "My pet robot's name is #{robot.name}, but we usually call him sparky."

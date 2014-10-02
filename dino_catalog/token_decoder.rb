f8e664a559b39baf0e9a4396d598c1122693652b
Jon Marks <jmarks1@enova.com>
2014-10-02 09:59:33 -0500

    Broke out to token_decoder.rb

---
+++ dino_catalog/token_decoder.rb
@@ -0,0 +1,47 @@
+# A mixin whereby a class can select one instance from a predefined list that corresponds with
+# some input token; and an instance can be queried for its correspondence to input tokens.
+# Used to implement keywords from the data files that translate to dinosaur attributes.
+module TokenDecoder
+
+  # isValidToken?
+  #
+  # Parameters
+  #   prefix (string) - word to test as potential prefix of standard
+  #   standard (string) - word to test whether prefix is leading match of
+  # Returns: True iff "prefix" is a proper prefix of "standard" of more than 3 chars
+  # Implements very simple method to equate input tokens to those recognized as identifying
+  # objects descriptive of dinosaurs during input source interpretation
+  def isValidToken?(prefix)
+
+    (prefix.length >= 3) && (getStandard().slice(0, prefix.length).casecmp(prefix) == 0)
+  end
+
+  # getStandard (abstract)
+  #
+  # Returns: (string) A token that represents this object against which other tokens are matched
+  # Must be overridden in comprising classes to provide a token to match
+  def getStandard
+    raise NotImplementedError
+  end
+
+  # createByToken (primary class interface method)
+  #
+  # Parameters
+  #   token (string) - A token word to be used to select an instance of comprising class to create
+  # Returns: (TokenFactory) An instance of the comprising class 
+  # Called on comprising class to select an instance for the input token
+  def self.createByToken(token)
+
+    getStandardObjects.each do |instance|
+
+      return instance if instance.isValidToken?(token)
+    end
+
+  # getStandardObjects (class, abstract)
+  #
+  # Returns: Predefined list of comprising class instances that may be selected by tokens
+  def self.getStandardObjects
+    raise NotImplementedError
+  end
+end  # module TokenDecoder
+

14483cb9805c6dd23149f962fb7b46af20577518
Zamir Syed <zsyed@enova.com>
2014-10-02 12:53:07 -0500

    Added optional arguments

--- local_events_feed/test/assets/test_theatre_in_chicago_scraper.rb
+++ local_events_feed/test/assets/test_theatre_in_chicago_scraper.rb
@@ -1,6 +1,12 @@
 require_relative '../../lib/assets/theatre_in_chicago_scraper'
 
-events = TheatreInChicagoScraper.get_events(2014, 11)
-events.each do |event|
-  puts event
+if ARGV.count == 2
+  year  = ARGV[0].to_i
+  month = ARGV[1].to_i
+else
+  year = 2014
+  month = 11
 end
+
+events = TheatreInChicagoScraper.get_events(year, month)
+events.each { |event| puts event }

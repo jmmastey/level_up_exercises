3bb61baf0d9264135bf811b823c4547cb5acdc33
Colleen Sain <csain@enova.com>
2014-10-02 16:15:42 -0500

    Service migration/model

--- cyoa/cyoa/db/schema.rb
+++ cyoa/cyoa/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141002211205) do
+ActiveRecord::Schema.define(version: 20141002211438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,13 @@ ActiveRecord::Schema.define(version: 20141002211205) do
     t.integer  "service_id"
     t.string   "type"
     t.datetime "recorded_on"
+  end
+
+  create_table "services", force: true do |t|
+    t.string   "name"
+    t.string   "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "songs", force: true do |t|

74b0e1da5af12188f3d410d71e35bf5b349abf16
Colleen Sain <csain@enova.com>
2014-10-03 10:14:23 -0500

    Song tests and model validations

--- cyoa/cyoa/app/models/song.rb
+++ cyoa/cyoa/app/models/song.rb
@@ -1,3 +1,7 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  validates :artist, presence: true
+  validates :name, presence: true,
+                   uniqueness: { case_sensitive: false, scope: :artist }
 end
--- cyoa/cyoa/spec/models/song_spec.rb
+++ cyoa/cyoa/spec/models/song_spec.rb
@@ -2,34 +2,39 @@ require 'spec_helper'
 
 describe Song do
   let(:artist) { FactoryGirl.create(:artist) }
-  let(:song) { artist.songs.build(name: "Single Ladies") }
+  let(:song_name) { "Single Ladies" }
+  let(:song) { artist.songs.build(name: song_name) }
 
   it { should respond_to(:name) }
   it { should respond_to(:artist_id) }
-  its(:artist) { should eq artist }
 
-  describe "when song doesn't have a name" do
-    it "should not be valid" do
+  describe "#name" do
+    it "when song doesn't have a name it should not be valid" do
       song.name = " "
       expect(song).not_to be_valid
     end
-  end
 
-  describe "when artist isn't present" do
-    it "should not be valid" do
-      song.artist = nil
-      expect(song).not_to be_valid
+    context "when artist already has a song by that name" do
+      it "should not be valid" do
+        song.save
+        same_song = song.dup
+        same_song.name = song.name.downcase
+        same_song.save
+
+        expect(same_song).not_to be_valid
+      end
     end
   end
 
-  describe "when artist already has a song by that name" do
-    it "should not be valid" do
-      song.save
-      same_song = song.dup
-      same_song.name = song.name
-      same_song.save
+  describe "#artist" do
+    it "when artist isn't present should not be valid" do
+      song.artist = nil
+      expect(song).not_to be_valid
+    end
 
-      expect(same_song).not_to be_valid
+    it "knows its artist" do
+      song.save
+      expect(song.artist).to eq artist
     end
   end
 end

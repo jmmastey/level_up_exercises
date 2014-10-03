9727f747c9052b65d59d4b0a6b61bac3ab381bf5
Jon Marks <jmarks1@enova.com>
2014-10-03 12:21:19 -0500

    Finished dino_catalog program

--- dino_catalog/dino_catalog.rb
+++ dino_catalog/dino_catalog.rb
@@ -1,139 +1,249 @@
 #!/usr/bin/ruby
-
-require "token_decoder"
-
-# DinoAmbulation
 #
-# Class handling and representing dinosaur locomotion (bi/quradrupedalism). Because this is a fixed
-# list, it can be represented as such in software.
-class DinoAmbulation
-  include TokenDecoder
-
-  attr_reader :ambulation     # (string) descriptive noun
-
-  def initialize(ambulation_mode)
-    @ambulation = ambulation_mode
-  end
-
-  # Enumerates all represented modes of ambulation and defines constants for each
-  @@amb_list = [
-      BIPEDAL       = new("bipedal"),
-      QUADRUPEDAL   = new("quadrupedal"),
-      UNKNOWN       = new("unknown")
-  ]
-
-  def self.getStandardObjects
-    @@amb_list
-  end
-
-  def getStandard
-    @ambulation
-  end
-end   # class DinoAmbulation
-
-
-# DinoDiet
+# dinosaur_catalog.rb
 #
-# Objects that represent the various specific types of diets that dinosaurs can have. Because is is
-# in fact a fixed list, it can be represented as such in software.
-class DinoDiet
-  include TokenDecoder
-  
-  attr_reader :diet           # What such a diet is called (string)
-  attr_reader :carnivorous    # Whether the diet is considered carnivorous (boolean)
+# Fri Oct  3 11:56:12 CDT 2014
+# Jonathan Marks (jmarks1@enova.com)
+#
+# Implements CLI application as solution to "dinodex" leveluprails exercise. Represents dinosaurs internally
+# as CSV::Row objects with # no higher-level data structure. Not designed to export API for use in other 
+# software.
+#
 
-  def initialize(diet_name, is_carnivore)
-    @diet, @carnivorous = diet_name, is_carnivore
-  end
+require "csv"
+require "optparse"
 
-  @@diet_list = [
-    INSECTIVORE               = new("insectivore", true),
-    PISCIVORE                 = new("piscivore", true),
-    CARNIVORE                 = new("carnivorous", true),
-    UNSPECIFIED_CARNIVORE     = new("unspecified carnivore", true),
-    UNSPECIFIED_NONCARNIVORE  = new("unspecified non-carnivore", false),
-    UNKNOWN_DIET              = new("unknown diet", false)
-  ]
+class DinoTable
 
+  attr_reader :csv_table
+
+  # Field names encounted that are keys in this hash are translated to corresponding values
+  @@header_map = {
+    'genus'           => 'name',
+    'weight_in_lbs'   => 'weight',
+  }
+
+  # Diet type tokens mapping true in this hash are recognized as carnivorous diets
+  @@carnivorous_diets = {
+    'insectivore' => true,
+    'piscivore' => true,
+    'carnivore' => true
+  }
+
+
+  # fix_dino_table_row
   #
-  # Implementing TokenDecoder interface
+  # Parameters
+  #   row (CSV::Row) A newly read row from CSV file
+  # Returns: (CSV::Row) A CSV row, possibly modified from the row passed as input
+  # Normalize a CSV row to ensure all known data are accounted and in proper form for query.
+  # NOTE: A CSV field converter won't suffice because these actions use data from multiple
+  # fields at once.
+  def fix_dino_table_row(row)
+
+    # Complete the "carnivore" field based on diet if incomplete and we know the diet
+    row[:carnivore] =
+      !!@@carnivorous_diets[row.field(:diet)] unless row.header?(:carnivore)
+
+    # Complete "diet" field w/generic info if we only know whether dino is a carnivore
+    #
+    # NOTE: ASK CUSTOMER WHETHER THESE GENERIC ASSUMPTIONS ARE DESIRABLE AND CORRECT
+    #
+    row[:diet] =
+      row.field(:carnivore) ? "carnivore" : "herbivore" unless row.header?(:diet)
+
+    # Validate/normalize other fields? (walking, name, weight, period?)
+
+    return row
+  end
+
+
+  # read_dino_csv
   #
+  # Parameters(csv_stream)
+  #   filename (string) - A readable CSV-formatted data file
+  # Populates the internal CSV table with data found in the CSV input
+  def read_dino_csv(filename)
 
-  def self.getStandardObjects
-    @@diet_list
+      CSV.foreach(filename, {
+        :headers          => true,
+        :return_headers   => false,
+        :skip_blanks      => true,
+        :header_converters => [
+          # Order matters; first translate and then make into symbol
+          ->(header_name) { h = header_name.downcase; @@header_map[h] || h },
+          :symbol
+        ],
+        :converters => [
+
+          ->(fval, finfo) {
+            # There are few enough fields to warrant a simple if ladder here.
+            if fval.nil?
+              ;
+            elsif [:name, :period, :continent].include?(finfo.header)
+              fval[0].upcase!
+            elsif finfo.header == :carnivore
+              fval = (fval.casecmp("yes") == 0)
+            elsif finfo.header == :weight
+              fval = fval.to_i
+            else
+              fval.downcase!
+            end
+
+            fval
+          }
+        ]
+      }) { |new_row|
+
+        # Accumulate the new fixed up rows into a CSV table
+        @csv_table << fix_dino_table_row(new_row)
+      }
   end
 
-  def getStandard
-    @diet
-  end
-end   # class DinoDiet
-
-
-# TimePeriod
-#
-# Objects that represent a major interval in evolutionary/geological history. Because this is a fixed list
-# it can be represented as such in software.
-class TimePeriod
-  include TokenDecoder
-
-  attr_reader :formal_period
-  attr_reader :sub_period
-
-  def initialize(formal_period, sub_period = nil)
-    @sub_period, @formal_period = sub_period, formal_period
-  end
-
-  # These terms are given meaning in the field of paleontology. Order counts as that determines how token 
-  # matches are tested.
-  @@period_list = [
-    EARLY_OXFORDIAN   = new("Oxfordian", :early),
-    LATE_OXFORDIAN    = new("Oxfordian", :late),
-    OXFORDIAN         = new("Oxfordian"), 
-    EARLY_PERMIAN     = new("Permian", :early),
-    LATE_PERMIAN      = new("Permian", :late),
-    PERMIAN           = new("Permian"),
-    EARLY_ALBIAN      = new("Albian", :early),
-    LATE_ALBIAN       = new("Albian", :late),
-    ALBIAN            = new("Albian"),
-    EARLY_TRIASSIC    = new("Triassic", :early),
-    LATE_TRIASSIC     = new("Triassic", :late),
-    TRIASSIC          = new("Triassic"),
-    EARLY_JURASSIC    = new("Jurassic", :early),
-    LATE_JURASSIC     = new("Jurassic", :late),
-    JURASSIC          = new("Jurassic"),
-    CRETACEOUS        = new("Cretaceous"), 
-    EARLY_CRETACEOUS  = new("Cretaceous", :early), 
-    LATE_CRETACEOUS   = new("Cretaceous", :late), 
-    UNKOWN_PERIOD     = new("Unknown")
-  ]
-
-  def self.getStandardObjects
-    @@period_list
-  end
-
-  def getStandard
-    @standard_token ||= (@sub_period.nil? ? "" : (@sub_period.to_s + " ")) + @formal_period
-  end
-end 
-
-class Dinosaur
 
   # initialize
   #
-  # Parameters:
-  #   taxon (string) - Defining clade
-  #   period (TimePeriod)
-  #   diet (DinoDiet)
-  #   weight (integer)
-  #   ambulation (DinoAmbulation)
-  def initialize(taxon, period, diet, weight, ambulation, description = nil)
+  # Parameters
+  #   filenames (variadic list of strings) - List of filenames of CSV input files to read
+  # Constructs a DinoTable, optionally initializing with data from CSV input files
+  def initialize(*filenames)
 
-    @taxon, @period, @diet, @weight, @ambulation, @description =
-      taxon, period, diet, weight, ambulation, description
+    # Accumulator for full dinosaur data set
+    @csv_table = CSV::Table.new([])
+
+    filenames.each { |datafile| read_dino_csv(datafile) }
+
+    @csv_table.each { |r| puts r.inspect; puts "\n" }
+  end 
+end     # class DinoTable
+
+
+# Implements the main program; command-line interface that identfies input data files and
+# selects filters to apply to dinosaur data constructed from input files.
+class DinoDex
+
+  # Initialized with dinosaur information after construction
+  attr_reader :dino_table
+
+  def initialize
+
+    @filters = [->(csvrow) { true }]
+    @dino_table = DinoTable.new
+  end
+
+  # parse_options
+  #
+  # Parameters
+  #   options (Array of Strings) - List of command-line options (defaults to ARGV)
+  # Configures the DinoDex by interpreting command-line arguments in an arg vector
+  def parse_options(options = ARGV)
+
+    optparser = OptionParser.new { |opts|
+
+      opts.banner = "Usage #{opts.program_name} [-f <input_file>]+ [-l|-s] [-c|-C] [-b|-q] [-p <period>]"
+      opts.separator ""
+      opts.separator "Description of options:"
+      
+      opts.on("-f", "--input-file filename", "Use data from a properly-formatted CSV input file") { |fname| 
+        @dino_table.read_dino_csv(fname) 
+      }
+
+      opts.on("-l", "--large", "Select only large dinosaurs") {
+        @filters << ->(csvrow) { (! (w = csvrow[:weight]).nil?) && (w > 4000) }
+      }
+
+      opts.on("-s", "--small", "Select only small dinosaurs") {
+        @filters << ->(csvrow) { (! (w = csvrow[:weight]).nil?) && (w <= 4000) }
+      }
+
+      opts.on("-c", "--carnivores", "Select only carnivorous beasts") {
+        @filters << ->(csvrow) { (! (c = csvrow[:carnivore]).nil?) && c }
+      }
+
+      opts.on("-C", "--non-carnivorous", "Select only non-carnivorous beasts") {
+        @filters << ->(csvrow) { (! (c = csvrow[:carnivore]).nil?) && !c }
+      }
+
+      opts.on("-b", "--bipedal", "Select only bipeds") {
+        @filters << ->(csvrow) { (! (w = csvrow[:walking]).nil?) && (w == "biped") }
+      }
+
+      opts.on("-q", "--quadrupedal", "Select only quadrupeds") {
+        @filters << ->(csvrow) { (! (w = csvrow[:walking]).nil?) && (w == "biped") }
+      }
+
+      opts.on("-p", "--period [period]", "Select dinosaurs from periods matching expression") { |expr|
+        @filters << ->(csvrow) { (! (p = csvrow[:period]).nil?) && (p =~ /#{expr}/i) }
+      }
+
+      opts.on_tail("-h", "--help", "Show option summary") {
+        puts opts
+        exit 1
+      }
+    }
+  
+    begin
+
+      optparser.parse!(options)
+    rescue Exception => ex
+    
+      puts ex.message + "\n"
+      puts optparser
+      exit 1
+    end
+  end
+
+  # write_field
+  #
+  # Parameters
+  #   csvrow (CSV::Row) - A CSV::Row describing a dinosaur
+  #   fieldname (symbol) - CSV row field identifier
+  #   label (string) - Thingie to print in front of field value to identify the data to user
+  # Writes to stdout a formatted description of a specified dinosaur attribute denoted in the CSV::Row
+  def write_field(csvrow, fieldname, label)
+
+    puts "#{label}: #{csvrow[fieldname]}\n" if csvrow.include?(fieldname) && ! csvrow[fieldname].nil?
+  end
+
+
+  # print_dinosaur
+  #
+  # Parameters
+  #   csvrow (CSV::Row) - A CSV::Row describing a dinosaur
+  # Writes to stdout a formatted description of all the dinosaurs attributes (we care about) in the CSV::Row
+  def print_dinosaur(csvrow)
+
+    puts "Name: #{csvrow[:name]}\n"
+    write_field(csvrow, :description, "Description")
+    write_field(csvrow, :period, "Period")
+    write_field(csvrow, :weight, "Weight (lbs)")
+    write_field(csvrow, :continent, "Continent")
+    write_field(csvrow, :diet, "Diet")
+    write_field(csvrow, :carnivore, "Carnivorous")
+    write_field(csvrow, :walking, "Mode of Ambulation")
+  end
+
+  
+  # print_dinosaurs
+  #
+  # Applies filters to the list of all known dinosaurs and prints to stdout a formatted list of all that 
+  # survive the filter criteria
+  def print_dinosaurs
+
+    # Ought to do this another way that shortcuts filter evaluation when one fails
+    @dino_table.csv_table.delete_if { |csvrow|
+      ! @filters.inject(true) { |keep, filter_method| keep && filter_method.call(csvrow) }
+    }.each { |surviving_csvrow|
+      print_dinosaur(surviving_csvrow)
+      puts
+    }
   end
 end
 
-ARGV.each { |a|
-  puts "a: #{TimePeriod.createByToken(a).inspect}\n"
-}
+#
+# Main program begins
+#
 
+d = DinoDex.new
+d.parse_options
+d.print_dinosaurs

21a6320977c456a72c85826c80f840f61ec8515a
Dan Kotowski <dkotowski@enova.com>
2014-10-05 22:09:23 -0500

    Refactored initialization to look for specific keys when assigning variables.

--- data_science/data_entry.rb
+++ data_science/data_entry.rb
@@ -2,16 +2,12 @@ class DataEntry
   attr_reader :cohort, :date, :result
 
   def initialize(data = {})
-    map_fields(data)
+    @cohort = data[:cohort]
+    @date = data[:date]
+    @result = data[:result]
   end
 
   def conversion?
     @result == 1
-  end
-
-  private
-
-  def map_fields(data)
-    data.each { |key, value| instance_variable_set("@#{key}", value) }
   end
 end

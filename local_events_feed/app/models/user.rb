d193a5e94829547bd03318bc3459b880660986ee
Zamir Syed <zsyed@enova.com>
2014-10-06 12:00:45 -0500

    Regex now disallows double dots

--- local_events_feed/app/models/user.rb
+++ local_events_feed/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 35 }
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }

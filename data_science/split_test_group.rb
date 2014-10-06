fa562d853003d321a1647e930360eaa96e87e298
Dan Kotowski <dkotowski@enova.com>
2014-10-05 22:10:51 -0500

    Refactored conversion rate methods.

--- data_science/split_test_group.rb
+++ data_science/split_test_group.rb
@@ -10,28 +10,26 @@ class SplitTestGroup
   end
 
   def conversion_rate
-    # check for @views == 0
+    return 0 if @views == 0
+
     @conversion_rate ||= @conversions.to_f / @views
   end
 
   def conversion_rate_error
-    unless @conversion_rate_error
-      @conversion_rate_error = CONFIDENCE_STD_ERROR_FACTOR * standard_error
-    end
-
-    @conversion_rate_error
+    @conversion_rate_error ||= CONFIDENCE_STD_ERROR_FACTOR * standard_error
   end
 
   def conversion_rate_range
-    unless @conversion_rate_range
-      min = conversion_rate - conversion_rate_error
-      max = conversion_rate + conversion_rate_error
-    end
-
-    @conversion_rate_range = [min, max]
+    @conversion_rate_range ||= calculate_conversion_rate_range
   end
 
   private
+
+  def calculate_conversion_rate_range
+    min = conversion_rate - conversion_rate_error
+    max = conversion_rate + conversion_rate_error
+    [min, max]
+  end
 
   def standard_error
     Math.sqrt(conversion_rate * (1 - conversion_rate) / @views)

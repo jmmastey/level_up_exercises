3cdde84ffe967db62128a01f308ef00598fa2522
Dan Kotowski <dkotowski@enova.com>
2014-10-05 22:10:13 -0500

    Moved all let statements to top of spec. Added let statements describing constant values.

--- data_science/spec/chi_square_spec.rb
+++ data_science/spec/chi_square_spec.rb
@@ -1,21 +1,26 @@
 require_relative "../chi_square"
 
 describe ChiSquare do
-  it "can be initialized with no parameters" do
-    ChiSquare.new
-  end
-
   let(:chi_square) { ChiSquare.new }
+  let(:group_a_num_conversions) { 47 }
+  let(:group_b_num_conversions) { 79 }
+  let(:group_a_num_views) { 1349 }
+  let(:group_b_num_views) { 1543 }
+
   let(:group_a) do
     SplitTestGroup.new(name: "A",
-                       num_views: 1349,
-                       num_conversions: 47)
+                       num_views: group_a_num_views,
+                       num_conversions: group_a_num_conversions)
   end
 
   let(:group_b) do
     SplitTestGroup.new(name: "B",
-                       num_views: 1543,
-                       num_conversions: 79)
+                       num_views: group_b_num_views,
+                       num_conversions: group_b_num_conversions)
+  end
+
+  it "can be initialized with no parameters" do
+    ChiSquare.new
   end
 
   describe "#test_statistic" do

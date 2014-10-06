743c6f94c7933083cd33ac7ff672876c0ae822a6
Zamir Syed <zsyed@enova.com>
2014-10-06 11:58:58 -0500

    Added user model with password authentication

--- local_events_feed/app/models/user.rb
+++ local_events_feed/app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 35 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
+  validates :password, length: { minimum: 6 }
   has_and_belongs_to_many :events
+  has_secure_password
 end
--- local_events_feed/db/schema.rb
+++ local_events_feed/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141006144249) do
+ActiveRecord::Schema.define(version: 20141006155346) do
 
   create_table "events", force: true do |t|
     t.string   "name"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20141006144249) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "email"
+    t.string   "password_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true

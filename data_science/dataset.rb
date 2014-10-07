efcd7bcf593e0b8b767823737ecfb806acab7424
rcowan <ryancowan@gmail.com>
2014-10-07 14:30:10 -0500

    work on Dataset methods to calculate conversion, percent of conversion, standard error

--- data_science/dataset.rb
+++ data_science/dataset.rb
@@ -1,6 +1,7 @@
 require_relative 'datum'
 
 class Dataset
+  CONFIDENCE = 1.96
   attr_accessor :results
 
   def initialize(data)
@@ -12,7 +13,41 @@ class Dataset
     @results.length
   end
 
+  def total_in_group(cohort)
+    count = 0
+
+    @results.each do |result|
+      count += 1 if result.cohort == cohort
+    end
+
+    count
+  end
+
   def number_of_conversions(cohort)
-    1
+    count = 0
+
+    @results.each do |result|
+      count += 1 if result.cohort == cohort && result.result == 1
+    end
+
+    count
+  end
+
+  def percentage_of_conversion(cohort)
+    (number_of_conversions(cohort).to_f / total_in_group(cohort).to_f).round(4)
+  end
+
+  def calculate_standard_error(cohort)
+    p = percentage_of_conversion(cohort)
+    n = total_in_group(cohort)
+
+    ((Math.sqrt(p * (1 - p) / n)) * CONFIDENCE).round(4)
   end
 end
+
+require_relative 'json_loader'
+
+json = JSONLoader.new('source_data.json').fetch_data
+dataset = Dataset.new(json)
+puts dataset.calculate_standard_error('A')# * 100
+puts dataset.calculate_standard_error('B')# * 100
--- data_science/dataset_spec.rb
+++ data_science/dataset_spec.rb
@@ -2,7 +2,7 @@ require_relative 'json_loader'
 require_relative 'dataset'
 
 describe Dataset do
-  json = JSONLoader.new('test_data.json').fetch_data
+  json = JSONLoader.new('source_data.json').fetch_data
   dataset = Dataset.new(json)
 
   it "should be a Dataset object" do
@@ -14,16 +14,35 @@ describe Dataset do
   end
 
   it "should initialize with data" do
-    expect(dataset.results.length).to eq(4)
+    expect(dataset.results.length).to eq(2892)
   end
 
   it "should tell us the total sample size" do
     expect(dataset).to respond_to(:total_sample_size)
-    expect(dataset.total_sample_size).to eq(4)
+    expect(dataset.total_sample_size).to eq(2892)
   end
 
-  it "should tell us the number of conversions" do
+  it "should tell us the total users in group" do
+    expect(dataset).to respond_to(:total_in_group).with(1).argument
+    expect(dataset.total_in_group('A')).to eq(1349)
+    expect(dataset.total_in_group('B')).to eq(1543)
+  end
+
+  it "should tell us the number of conversion" do
     expect(dataset).to respond_to(:number_of_conversions).with(1).argument
-    expect(dataset.number_of_conversions('A')).to eq(1)
+    expect(dataset.number_of_conversions('A')).to eq(47)
+    expect(dataset.number_of_conversions('B')).to eq(79)
+  end
+
+  it "should tell us the percentage of conversion" do
+    expect(dataset).to respond_to(:percentage_of_conversion).with(1).argument
+    expect(dataset.percentage_of_conversion('A')).to eq(0.0348)
+    expect(dataset.percentage_of_conversion('B')).to eq(0.0512)
+  end
+
+  it "should tell us the standard error" do
+    expect(dataset).to respond_to(:calculate_standard_error).with(1).argument
+    expect(dataset.calculate_standard_error('A')).to eq(0.0098)
+    expect(dataset.calculate_standard_error('B')).to eq(0.0110)
   end
 end

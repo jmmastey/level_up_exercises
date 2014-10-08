97bdd12d697b9c1454cb69e8dd40a06959cea43a
Saahithi Gunda <sgunda@enova.com>
2014-10-08 18:02:58 -0500

    refactored

--- arrowhead/arrowhead.rb
+++ arrowhead/arrowhead.rb
@@ -15,21 +15,35 @@ class Arrowhead
     },
   }
 
-  def self.verify_shape(region, shape)
-    CLASSIFICATIONS[region][shape] ||
-      raise("Unknown shape value. Are you sure you know what you're talking about?")
+  attr_accessor :region, :shape
+
+  def initialize(region, shape)
+    @region = region
+    @shape = shape
+    raise error_message unless valid?
   end
 
-  def self.verify_region(region, shape)
-    verify_shape(region, shape) ||
-      raise("Unknown region, please provide a valid region.")
+  def error_message
+      if !CLASSIFICATIONS[@region]
+        @error_message ||= "Unknown region, please provide a valid region."
+      elsif !CLASSIFICATIONS[@region][@shape]
+        @error_message ||= "Unknown shape value. Are you sure you know what you're talking about?"
+      end
   end
 
-  def self.classify(region, shape)
-    arrowhead = verify_region(region, shape)
-    puts "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
+  def valid?
+    !error_message
+  end
+
+  def classification
+    @classification ||= CLASSIFICATIONS[region] && CLASSIFICATIONS[region][shape]
+  end
+
+  def classify
+    puts "You have a(n) #{classification} arrowhead. Probably priceless."
   end
 end
 
-puts Arrowhead.classify(:northern_plains, :bifurcated)
-puts Arrowhead.classify(:far_west, :stemmed)
+puts Arrowhead.new(:northern_plains, :bifurcated).classify
+#puts Arrowhead.new(nil, :bifurcated).classify
+#puts Arrowhead.new(:northern_plains, nil).classify

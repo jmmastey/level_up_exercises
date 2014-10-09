fb59cc32b47f6e1b3a1a2fa4575251dc4622a805
Ramya Kannan <ramyakannan@dhcp-10-224-109-165.enova.com>
2014-10-09 16:30:03 -0500

    fixed some spaces

--- happenings/app/controllers/calendar_controller.rb
+++ happenings/app/controllers/calendar_controller.rb
@@ -2,10 +2,9 @@ class CalendarController < ApplicationController
   before_filter :prepare_session
   def prepare_session
     ScraperWorker.perform_async
-	end
-
+  end
   def show
-	  @date = params[:date] ? Date.parse(params[:date]) : Date.today
-	  @events_by_date = Event.select("id, title, location, date, time")
-	end
+    @date = params[:date] ? Date.parse(params[:date]) : Date.today
+    @events_by_date = Event.select("id, title, location, date, time")
+  end
 end

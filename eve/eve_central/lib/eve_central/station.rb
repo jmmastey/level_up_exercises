3412f4de4889d27ed7849571b957f5fa1a6eb4f8
Dan Kotowski <dkotowski@enova.com>
2014-10-10 15:03:43 -0500

    Added ActiveModel validation to model.

--- eve/eve_central/lib/eve_central/station.rb
+++ eve/eve_central/lib/eve_central/station.rb
@@ -1,10 +1,23 @@
+require "active_model"
+
 module EveCentral
   class Station
-    attr_reader :id, :name
+    include ActiveModel::Validations
+    include ActiveModel::Conversion
+    extend ActiveModel::Naming
+
+    attr_accessor :id, :name
+
+    validates_presence_of :id
+    validates_numericality_of :id, only_integer: true, greater_than_or_equal_to: 0
 
     def initialize(id, name = nil)
       @id = id
       @name = name
+    end
+
+    def persisted?
+      false
     end
   end
 end

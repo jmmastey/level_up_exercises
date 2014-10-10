c621d5976773fdda53fca3e21a95b6a8e6c8ca44
rcowan <ryancowan@gmail.com>
2014-10-10 12:19:49 -0500

    Try separating bomb states into separate pages with separate actions

--- overlord/lib/overlord.rb
+++ overlord/lib/overlord.rb
@@ -4,20 +4,53 @@ class Overlord < Sinatra::Base
   enable :sessions
 
   get '/' do
-    # @state = 'bomb is not activated'
-    session[:bomb_state] ||= 'bomb is not activated'
+    session[:bomb_state] ||= 'not booted'
+    session[:attempts] = 0
     erb :home
   end
 
+  get '/inactive' do
+    # session[:bomb_state] = 'inactive'
+    erb :bomb
+  end
+
+  get '/active' do
+    # session[:bomb_state] = 'active'
+    erb :bomb
+  end
+
+  get '/explode' do
+    # session[:bomb_state] = 'blown up'
+    erb :explode
+  end
+
+  post '/booted' do
+    session[:bomb_state] = 'inactive'
+    session[:activate_code] ||= 1234
+    session[:deactivate_code] ||= 0000
+    redirect '/inactive'
+  end
+
   post '/activated' do
-    # @state = 'bomb is activated'
-    session[:bomb_state] = 'POST bomb is activated'
-    redirect '/'
+    redirect '/inactive' unless params[:code] == session[:activate_code]
+
+    session[:bomb_state] = 'active'
+    redirect '/active'
+  end
+
+  post '/inactivated' do
+    if params[:code] == session[:deactivate_code] && session[:attempts] < 2
+      session[:bomb_state] = 'inactive'
+      redirect '/inactive'
+    else
+      redirect '/detonated', 303
+    end
   end
 
   post '/detonated' do
     # @state = 'bomb has blown up'
-    session[:bomb_state] = 'bomb has blown up'
+    session[:bomb_state] = 'blown up'
+    redirect '/explode'
   end
 
   # start the server if ruby file executed directly

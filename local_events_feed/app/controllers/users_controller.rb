4e572369fd4c5112545a1d231fbcfea82066a475
Zamir Syed <zsyed@enova.com>
2014-10-10 09:15:58 -0500

    Removed unused method

--- local_events_feed/app/controllers/users_controller.rb
+++ local_events_feed/app/controllers/users_controller.rb
@@ -13,16 +13,12 @@ class UsersController < ApplicationController
       sign_in(@user)
       redirect_to @user
     else
-      render 'sign_up'
+      render 'new'
     end
   end
 
   def show
     @user = current_user
-  end
-
-  def please_sign_in
-    render plain: "Please sign in first"
   end
 
   def remove_event

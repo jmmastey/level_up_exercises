fc913d6e531630f4ec58c8a5c10c31f1c4b7011a
Colleen Sain <csain@enova.com>
2014-10-13 15:39:36 -0500

    Added recorded_on method to metric

--- cyoa/cyoa/app/models/metric.rb
+++ cyoa/cyoa/app/models/metric.rb
@@ -7,4 +7,15 @@ class Metric < ActiveRecord::Base
   validates :service, presence: true
   validates :value, presence: true
   #validates :category, presence: true
+
+  before_validation :set_recorded_on
+
+  private
+
+  def set_recorded_on
+    sec_in_day = 86400
+    date_unix_days = nbs_date.to_i
+    date_unix_sec = date_unix_days * sec_in_day
+    recorded_on = Time.at(date_unix_sec).utc
+  end
 end

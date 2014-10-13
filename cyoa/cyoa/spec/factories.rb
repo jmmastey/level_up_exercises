1eac6af39fab9eb9539db8d2b884758a5839c611
Colleen Sain <csain@enova.com>
2014-10-13 15:41:05 -0500

    Fixed factory duplication issues

--- cyoa/cyoa/spec/factories.rb
+++ cyoa/cyoa/spec/factories.rb
@@ -1,17 +1,33 @@
 FactoryGirl.define do
   factory :artist, aliases: [:beyonce] do
     name "Beyonce"
+    nbs_id 1111
+    grooveshark_id 2222
+  end
+  
+  factory :another_artist, class: Artist, aliases: [:jayz] do
+    name "Jay-Z"
+    nbs_id 1234
+    grooveshark_id 4321
   end
 
- # factory :service do
- #   name "Facebook"
- #   url "http://facebook.com"
- # end
+  factory :song do |f|
+    f.name "Single Ladies"
+    f.association :artist
+  end
 
-  factory :song do
-    name "Single Ladies"
+  factory :category do
+    name "fans"
+  end
+
+  factory :metric do
+    value 10
+    category
     artist
   end
 
-  #factory :char
+  factory :service do
+    name "Facebook"
+    url "http://facebook.com"
+  end
 end

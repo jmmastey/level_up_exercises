d5b3a362e5689a5e1ca92ad2093591e6508f5d3a
Colleen Sain <csain@enova.com>
2014-10-15 11:29:05 -0500

    Separated out charts view

--- cyoa/cyoa/app/controllers/static_pages_controller.rb
+++ cyoa/cyoa/app/controllers/static_pages_controller.rb
@@ -1,5 +1,4 @@
 class StaticPagesController < ApplicationController
   def home
-    @charts = Chart.all
   end
 end
--- cyoa/cyoa/config/routes.rb
+++ cyoa/cyoa/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'charts/index'
 
   root "static_pages#home"
+
+  resources :charts, only: :index
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

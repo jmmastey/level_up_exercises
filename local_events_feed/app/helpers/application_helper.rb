24516eeefa28add380f1ce7ada9be51af7b6f022
Zamir Syed <zsyed@enova.com>
2014-10-15 13:21:39 -0500

    Moved rendering of calendar-publish into helper

--- local_events_feed/app/helpers/application_helper.rb
+++ local_events_feed/app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def render_calendar_publish(event)
+    calendar = Icalendar::Calendar.new
+    calendar.add_event(event.ics)
+    calendar.publish
+    calendar.to_ical
+  end
 end

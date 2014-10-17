af1b2f70e80faa92d206995877971762b21d66c5
Dan Kotowski <dkotowski@enova.com>
2014-10-17 17:32:22 -0500

    Added default route for Home. Changed SASS format from SCSS to SASS.

--- eve/eve/config/application.rb
+++ eve/eve/config/application.rb
@@ -22,5 +22,8 @@ module Eve
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Use SASS syntax
+    config.sass.preferred_syntax = :sass
   end
 end

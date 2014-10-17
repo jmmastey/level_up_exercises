97eeaf8aa8010c257ffd7d43906738c6f566a7a1
Colleen Sain <csain@enova.com>
2014-10-17 16:09:59 -0500

    Refactored metric tests

--- cyoa/cyoa/spec/models/metric_spec.rb
+++ cyoa/cyoa/spec/models/metric_spec.rb
@@ -1,38 +1,43 @@
 require "spec_helper"
 
 describe Metric do
-  subject(:metric) { FactoryGirl.create(:metric) }
+  subject(:metric) { FactoryGirl.build(:metric) }
 
   it { is_expected.to be_valid }
-  it { is_expected.to respond_to(:artist) }
-  it { is_expected.to respond_to(:service) }
-  it { is_expected.to respond_to(:category) }
 
-  describe "#validations" do
+  describe "#artist" do
+    it { is_expected.to respond_to(:artist) }
+
     it "must have an artist" do
-      metric.artist = nil
-      expect(metric).not_to be_valid
-    end
-
-    it "must have a service" do
-      metric.service = nil
-      expect(metric).not_to be_valid
-    end
-
-    it "must have a category" do
-      metric.category = nil
-      expect(metric).not_to be_valid
-    end
-
-    it "must have a date" do
-      metric.nbs_date = nil
-      expect(metric).not_to be_valid
+      expect(FactoryGirl.build(:metric, artist: nil)).not_to be_valid
     end
   end
 
-  it "has recorded_on calculated on creation" do
-    oct_12 = Time.new(2014, 10, 12, 12, 0, 0).to_i / (60 * 60 * 24)
-    metric_oct_12 = FactoryGirl.create(:metric, nbs_date: oct_12.to_s) 
-    expect(metric_oct_12.recorded_on).to eq(Date.new(2014, 10, 12))
+  describe "#service" do
+    it { is_expected.to respond_to(:service) }
+
+    it "must have a service" do
+      expect(FactoryGirl.build(:metric, service: nil)).not_to be_valid
+    end
+  end
+
+  describe "#category" do
+    it { is_expected.to respond_to(:category) }
+
+    it "must have a category" do
+      expect(FactoryGirl.build(:metric, category: nil)).not_to be_valid
+    end
+  end
+
+  describe "#recorded_on" do
+    it "must have a date" do
+      expect(FactoryGirl.build(:metric, nbs_date: nil)).not_to be_valid
+    end
+
+    it "has recorded_on calculated on creation" do
+      oct_12 = Time.new(2014, 10, 12, 12, 0, 0).to_i / (60 * 60 * 24)
+      metric_oct_12 = FactoryGirl.create(:metric, nbs_date: oct_12.to_s)
+      expect(metric_oct_12.recorded_on).to eq(Date.new(2014, 10, 12))
+    end
   end
 end

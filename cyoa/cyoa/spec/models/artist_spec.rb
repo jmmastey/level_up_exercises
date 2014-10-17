5db10a2aa42f84381b17240149fae6ca49fad89f
Colleen Sain <csain@enova.com>
2014-10-17 15:29:03 -0500

    Refactored artist spec

--- cyoa/cyoa/spec/models/artist_spec.rb
+++ cyoa/cyoa/spec/models/artist_spec.rb
@@ -5,24 +5,30 @@ describe Artist do
 
   it { is_expected.to be_valid }
 
-  it { is_expected.to respond_to(:name) }
   it { is_expected.to respond_to(:metrics) }
   it { is_expected.to respond_to(:songs) }
 
-  describe "required attributes" do
+  describe "#name" do
+    it { is_expected.to respond_to(:name) }
+
     it "must have a name" do
       expect(FactoryGirl.build(:artist, name: "")).not_to be_valid
     end
+
+    it "name of artist is unique" do
+      expect(FactoryGirl.build(:another_artist, name: artist.name.downcase)).not_to be_valid
+    end
   end
 
-  describe "adding a new artist with duplicate attributes" do
-    it "name of artist is unique" do
-      expect(FactoryGirl.build(:artist, name: artist.name.downcase, grooveshark_id: 1, nbs_id: 1)).not_to be_valid
-    end
-
-    it "api ids are unique" do
-      expect(FactoryGirl.build(:another_artist, grooveshark_id: artist.grooveshark_id)).not_to be_valid
+  describe "#nbs_id" do
+    it "has a unique nbs id" do
       expect(FactoryGirl.build(:another_artist, nbs_id: artist.nbs_id)).not_to be_valid
+    end
+  end
+
+  describe "#grooveshark_id" do
+    it "has a unique grooveshark id" do
+      expect(FactoryGirl.build(:another_artist, grooveshark_id: artist.grooveshark_id)).not_to be_valid
     end
   end
 end
--- cyoa/cyoa/spec/models/user_spec.rb
+++ cyoa/cyoa/spec/models/user_spec.rb
@@ -1,5 +1,4 @@
-require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
 end

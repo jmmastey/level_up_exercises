14e0beaf6ea6a2bdc608278499ae713852363cd1
paulh16 <paulh16@gmail.com>
2014-10-20 11:41:31 -0500

    create build_catalog method and print lines from csv file

--- dino_catalog/lib/catalog.rb
+++ dino_catalog/lib/catalog.rb
@@ -1,19 +1,34 @@
 class Catalog
 
-  # load in the csv file
-
-  @@filepath = nil
+  # Initialize the new catalog
 
   def initialize(path=nil)
     # locate the file
-    @@filepath = File.join(APP_ROOT)
-    puts @@filepath
+    @filepath = File.join(APP_ROOT, path)
+    puts @filepath
+
     # if file exists, parse it
-    if @@filepath && File.exists?(@@filepath)
+    if @filepath && File.exists?(@filepath)
       puts "I found a file!"
+      build_catalog(@filepath)
     else
       raise IOError, "No file was found"
     end
+  end
+
+  # load in the csv file
+
+  def build_catalog(path)
+    # read the file
+    #File.open(path) do |f|
+      #while record - f.gets
+        #csv_line = record.chomp.split(',')
+        #puts csv_line
+      #end
+    #end
+    puts File.readlines(path)
+    # add dinosaurs to the catalog
+
   end
 
   # launch the program

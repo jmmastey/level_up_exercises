112087a19e4c641ec18b1b7312f465a3194fa796
Ozer Chagatai <ochagatai@clm-ochagatai.enova.com>
2014-10-20 13:40:56 -0500

    took out style errors in triangle facts file

--- triangle_facts/triangle_facts.rb
+++ triangle_facts/triangle_facts.rb
@@ -1,61 +1,56 @@
 # Killer facts about triangles AWW YEAH
 class Triangle
-	attr_accessor :side1, :side2, :side3
+  attr_accessor :side1, :side2, :side3
 
-	def initialize(side1, side2, side3)
-		@side1, @side2, @side3 = side1, side2, side3
-	end
+  def initialize(side1, side2, side3)
+    @side1, @side2, @side3 = side1, side2, side3
+  end
 
-	def equilateral()
-		side1 == side2 && side2 == side3
-	end
+  def equilateral
+    side1 == side2 && side2 == side3
+  end
 
-	def isosceles()
-		[side1, side2, side3].uniq.length == 2
-	end
+  def isosceles
+    [side1, side2, side3].uniq.length == 2
+  end
 
-	def scalene(equilateral, isosceles)
-		!(equilateral || isosceles)
-	end
+  def scalene
+    !(equilateral || isosceles)
+  end
 
-	def recite_facts
-		puts 'This triangle is equilateral!' if equalateral?
-		puts 'This triangle is isosceles! Also, that word is hard to type.' if isosceles?
-		puts 'This triangle is scalene and mathematically boring.' if scalene?
+  def recite_facts
+    puts 'This triangle is equilateral!' if equilateral
+    puts 'This triangle is isosceles! Also, \
+    that word is hard to type.' if isosceles
+    puts 'This triangle is scalene and mathematically boring.' if scalene
 
-		angles = self.calculate_angles(side1,side2,side3)
-		puts 'The angles of this triangle are ' + angles.join(',')
+    angles = calculate_angles(side1, side2, side3)
+    puts 'The angles of this triangle are ' + angles.join(',')
 
-		puts 'This triangle is also a right triangle!' if angles.include? 90
-		puts ''
-	end
+    puts 'This triangle is also a right triangle!' if angles.include? 90
+  end
 
-	def foo(a,b,c)
-		a**2 + b**2 - c**2
-	end
+  def law_of_cosines(x, y, z)
+    radians_to_degrees(Math.acos((x**2 + y**2 - z**2) /
+     (2.0 * x * y)))
+  end
 
-	def calculate_angles(a,b,c)
-		angleA = radians_to_degrees(Math.acos((foo(b,c,a)) /
-		 (2.0 * b * c)))
-		angleB = radians_to_degrees(Math.acos((a**2 + c**2 - b**2) /
-		 (2.0 * a * c)))
-		angleC = radians_to_degrees(Math.acos((a**2 + b**2 - c**2) /
-		 (2.0 * a * b)))
+  def calculate_angles(a, b, c)
+    angle_a = law_of_cosines(b, c, a)
+    angle_b = law_of_cosines(a, c, b)
+    angle_c = law_of_cosines(a, b, c)
 
-		[angleA, angleB, angleC]
-	end
+    [angle_a, angle_b, angle_c]
+  end
 
-	def radians_to_degrees(rads)
-		(rads * 180 / Math::PI).round
-	end
+  def radians_to_degrees(rads)
+    (rads * 180 / Math::PI).round
+  end
 end
 
-
 triangles = [
-	[5, 5, 5],
-	[5, 12, 13],
+  [5, 5, 5],
+  [5, 12, 13],
 ]
-triangles.each { |sides|
-	tri = Triangle.new(*sides)
-	tri.recite_facts
-}
+
+triangles.each { |sides| Triangle.new(*sides).recite_facts }

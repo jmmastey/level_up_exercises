f9693ce163376af04eee6e7fce8705e35824f69b
Ozer Chagatai <ochagatai@clm-ochagatai.enova.com>
2014-10-20 15:54:15 -0500

    made second implementation

--- arrowhead/arrowhead.rb
+++ arrowhead/arrowhead.rb
@@ -15,23 +15,16 @@ class Arrowhead
     },
   }
 
-  def self.shape_in_shapes(shape, shapes)
-    if shapes.include? shape
-      arrowhead = shapes[shape]
-      puts "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
-    else
-      raise "Unknown shape value. \
-      Are you sure you know what you're talking about?"
-    end
-  end
-  # FIXME: I don't have time to deal with this.
   def self.classify(region, shape)
-    if CLASSIFICATIONS.include? region
-      shapes = CLASSIFICATIONS[region]
-      shape_in_shapes(shapes, shape)
-    else
-      raise "Unknown region, please provide a valid region."
-    end
+    raise "Unknown region, please " \
+          "provide a valid region." unless CLASSIFICATIONS.include? region
+
+    shapes = CLASSIFICATIONS[region]
+    raise "Unknown shape value. Are you sure " \
+          "you know what you're talking about?" unless shapes.include? shape
+
+    arrowhead = shapes[shape]
+    puts "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
   end
 end
 

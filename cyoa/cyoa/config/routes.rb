4178a1fe0314fafb2782dd76e13a4e6cbfd012e7
Colleen Sain <csain@enova.com>
2014-10-20 10:55:07 -0500

    Devise customization

--- cyoa/cyoa/config/routes.rb
+++ cyoa/cyoa/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, path_name: {sign_in: "login", sign_out: "logout"}
   root "static_pages#home"
 
   resources :charts, only: :index

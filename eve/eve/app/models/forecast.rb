3bf1445fa102c39f5a8ecefaa00f2ad473dd973b
Dan Kotowski <dkotowski@enova.com>
2014-10-21 15:44:36 -0500

    Removed forecasting models.  Will not cache searches/calculations.

--- eve/eve/app/models/forecast.rb
+++
@@ -1,4 +0,0 @@
-class Forecast < ActiveRecord::Base
-  validates_presence_of :forecast_request, :target_date
-  belongs_to :forecast_request
-end
--- eve/eve/app/models/forecast_request.rb
+++
@@ -1,8 +0,0 @@
-class ForecastRequest < ActiveRecord::Base
-  belongs_to :item
-  has_many :forecasts
-
-  validates_presence_of :history_start_time, :history_end_time, :item, :num_target_days, :target_date
-  validates_numericality_of :min_price, :average_price, :max_price,
-    greater_than_or_equal_to: 0
-end

8961a08a5c105254996a19d52e4c0ff681e9c5e6
Colleen Sain <csain@enova.com>
2014-10-21 16:47:30 -0500

    Can remove artist via front-end

--- cyoa/cyoa/app/controllers/artists_controller.rb
+++ cyoa/cyoa/app/controllers/artists_controller.rb
@@ -4,4 +4,10 @@ class ArtistsController < ApplicationController
   def index
     @artists = current_user.artists
   end
+
+  def destroy
+    artist = Artist.find(params[:id])
+    current_user.remove_artist(artist)
+    redirect_to :back
+  end
 end

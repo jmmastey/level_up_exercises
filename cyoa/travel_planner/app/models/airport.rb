763ba2954a1c3feefbf26d66cd86344522d0008a
Lawrence Walters <lawrencewalters@users.noreply.github.com>
2014-10-21 09:25:48 -0500

    setting up airport relationships

--- cyoa/travel_planner/app/models/airport.rb
+++ cyoa/travel_planner/app/models/airport.rb
@@ -1,3 +1,7 @@
 class Airport < ActiveRecord::Base
+  has_one :destination_flight, class_name: "Flight", foreign_key: "destination_airport_id"
+  has_one :origin_flight, class_name: "Flight", foreign_key: "origin_airport_id"
+  belongs_to :origin_airport, class_name: "Airport"
+
   belongs_to :location
 end

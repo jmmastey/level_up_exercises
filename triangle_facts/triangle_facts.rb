6f4eba934d66564f45d9c5f07f5a676575640161
jillalynch <jill.lynch5@gmail.com>
2014-10-21 12:58:12 -0500

    return calculate angle to original method and remove angles as properties of Triangle object

--- triangle_facts/triangle_facts.rb
+++ triangle_facts/triangle_facts.rb
@@ -1,4 +1,6 @@
 # jlynch - Determines which type and angles of triangle given sides
+require 'pry'
+
 class Triangle
   attr_accessor :side1, :side2, :side3
 
@@ -6,42 +8,37 @@ class Triangle
     @side1 = side1
     @side2 = side2
     @side3 = side3
-    calculate_angles
   end
 
-  def equilateral?
+  def is_equilateral?
     # side1 is a method.  Created by attr_accessor that gets @side1
     (side1 == side2) && (side2 == side3)
   end
 
-  def isosceles?
+  def is_isosceles?
     [side1, side2, side3].uniq.length == 2
   end
 
-  def scalene?
-    !(equilateral? || isosceles?)
+  def is_scalene?
+    true unless (is_equilateral? || is_isosceles?)
   end
 
   def recite_facts
-    puts "This triangle is equilateral!" if equilateral?
-    puts "This triangle is isosceles! Also, that word is hard to type." if isosceles?
-    puts "This triangle is scalene and mathematically boring." if scalene?
-    puts "The angles of this triangle are #{@angles.join(', ')}"
-    puts "This triangle is also a right triangle!" if @angles.include? 90
+    puts "This triangle is equilateral!" if is_equilateral?
+    puts "This triangle is isosceles! Also, that word is hard to type." if is_isosceles?
+    puts "This triangle is scalene and mathematically boring." if is_scalene?
+    puts "The angles of this triangle are #{calculate_angles.join(', ')}"
+    puts "This triangle is also a right triangle!" if calculate_angles.include? 90
     puts ""
   end
 
-  def calculate_angles
-    positions = [[@side3, @side2, @side1],
-                 [@side1, @side3, @side2],
-                 [@side1, @side2, @side3]]
-    angle_a, angle_b, angle_c = [0, 0, 0] # ensure angle_a points to memory
-    @angles = [angle_a, angle_b, angle_c]
+	def calculate_angles
+		angleA = radians_to_degrees(Math.acos((side2**2 + side3**2 - side1**2) / (2.0 * side2 * side3)))
+		angleB = radians_to_degrees(Math.acos((side1**2 + side3**2 - side2**2) / (2.0 * side1 * side3)))
+		angleC = radians_to_degrees(Math.acos((side1**2 + side2**2 - side3**2) / (2.0 * side1 * side2)))
 
-    positions.each_with_index do |position, index|
-      @angles[index] = law_of_cosines(position)
-    end
-  end
+		return [angleA, angleB, angleC]
+	end
 
   def law_of_cosines(sides)
     radians_to_degrees(Math.acos((sides[0]**2 + sides[1]**2 - sides[2]**2) / (2.0 * sides[0] * sides[1])))

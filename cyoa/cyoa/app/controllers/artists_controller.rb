d4e8b3cd2db2cffc5f62ebf88d0c3711358352bc
Colleen Sain <csain@enova.com>
2014-10-22 11:05:52 -0500

    Adding an artist from the artist page

--- cyoa/cyoa/app/controllers/artists_controller.rb
+++ cyoa/cyoa/app/controllers/artists_controller.rb
@@ -3,11 +3,18 @@ class ArtistsController < ApplicationController
 
   def index
     @artists = current_user.artists
+    @artist = Artist.new
   end
 
   def destroy
     artist = Artist.find(params[:id])
     current_user.remove_artist(artist)
+    redirect_to :back
+  end
+
+  def create
+    artist = Artist.find_or_create_by_unique_name(params[:artist][:name])
+    current_user.artists << artist
     redirect_to :back
   end
 end
--- cyoa/cyoa/spec/models/user_spec.rb
+++ cyoa/cyoa/spec/models/user_spec.rb
@@ -2,6 +2,9 @@ require 'spec_helper'
 
 describe User do
   subject(:user) { FactoryGirl.create(:user) }
+  let(:beyonce) { Artist.yonce }
+  let(:xxyyxx) { xxyyxx = Artist.find_or_create_by_unique_name("xxyyxx") }
+  let(:not_an_artist) { asdf = Artist.find_or_create_by_unique_name("asdfsjkfhsdkjsfsr") }
 
   it { is_expected.to be_valid }
 
@@ -9,14 +12,30 @@ describe User do
     it { is_expected.to respond_to(:artists) }
 
     it "has default artists on creation" do
-      expect(user.artists).to include(Artist.yonce)
+      expect(user.artists).to include(beyonce)
     end
   end
 
   describe "#remove_artist" do
     it "can remove artists" do
-      user.remove_artist(Artist.yonce)
-      expect(user.artists).not_to include(Artist.yonce)
+      user.remove_artist(beyonce)
+      expect(user.artists).not_to include(beyonce)
+    end
+  end
+
+  describe "#add_artist" do
+    it "can add a new artist" do
+      user.add_artist(xxyyxx)
+      expect(user.artists).to include(xxyxx)
+    end
+  end
+
+  describe "#nbs_artists" do
+    it "returns only artists w/ a nbs_id" do
+      user.add_artist(not_an_artist)
+      user.add_artist(xxyyxx)
+      expect(user.nbs_artists).to include(xxyxx)
+      expect(user.nbs_artists).not_to include(not_an_artist)
     end
   end
 end

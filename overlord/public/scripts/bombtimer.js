595a4c1194a2133cf707f5cd745878e8c5c52692
Stacy Mullins <smullins10@gmail.com>
2014-10-22 10:48:36 -0500

    Add js timer 2nd draft

--- overlord/public/scripts/bombtimer.js
+++ overlord/public/scripts/bombtimer.js
@@ -2,4 +2,11 @@ function explode(){
   document.getElementById("bomb_inactive").src ="images/inactive_bomb.jpg";
   setTimeout("document.getElementById('bomb_inactive').src ='images/exploded_bomb.jpg';",
  30000);
-}
+} 
+
+
+while Time.now
+  puts Time.now +=1
+break if Time.now =5
+end
+

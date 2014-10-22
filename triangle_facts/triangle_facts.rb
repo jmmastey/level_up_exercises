c78ed7be367647b3329be54dbd88ac3758c14048
jillalynch <jill.lynch5@gmail.com>
2014-10-22 11:17:19 -0500

    Remove cachingof angles, and returns to orginal which  calculates them dynamicly in recite_facts method

--- triangle_facts/triangle_facts.rb
+++ triangle_facts/triangle_facts.rb
@@ -1,25 +1,11 @@
 require 'pry'
 # jlynch - Determines which type and angles of triangle given sides
-# wanted to the program to know to recalculate angles if one side was changed
-# without having to create a completely new triangle object.
+# Talking to Ozzie he said no @angles - no caching those angles!
 class Triangle
-  attr_reader :sides
+  attr_accessor :sides
 
   def initialize(side1, side2, side3)
     @sides = { side1: side1, side2: side2, side3: side3 }
-  end
-
-  # creating custom setter method so that if a one side is changed
-  # angles are set to nil, and angles function knows to recalculate
-  # I would repeat for side2, side3 --> for now it just does side1
-  def side_setter=(new_side_length)
-    @sides.merge!({ side1: new_side_length })
-    @angles = nil
-    angles
-  end
-
-  def angles
-    @angles ||= calculate_angles_from_sides
   end
 
   def is_equilateral?
@@ -50,7 +36,7 @@ class Triangle
 
   private
 
-  def calculate_angles_from_sides
+  def angles
     angle_a = radians_to_degrees(Math.acos((sides[:side2]**2 + sides[:side3]**2 - sides[:side1]**2) / (2.0 * sides[:side2] * sides[:side3])))
     angle_b = radians_to_degrees(Math.acos((sides[:side1]**2 + sides[:side3]**2 - sides[:side2]**2) / (2.0 * sides[:side1] * sides[:side3])))
     angle_c = radians_to_degrees(Math.acos((sides[:side1]**2 + sides[:side2]**2 - sides[:side3]**2) / (2.0 * sides[:side1] * sides[:side2])))
@@ -64,6 +50,6 @@ triangles.each do |sides|
   tri = Triangle.new(*sides)
   tri.recite_facts
   print "Let's change side 1 to 4in ... "
-  tri.side_setter = 4
+  tri.sides[:side1] = 4
   tri.recite_facts
 end

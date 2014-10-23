e40eabc106ef000687576aee762470612902e72f
Aushadhee Mahakumarage <mahakumarage@gmail.com>
2014-10-23 10:38:16 -0500

    Fixed some logic error and cleaned up the code

--- triangle_facts/triangle_facts.rb
+++ triangle_facts/triangle_facts.rb
@@ -6,27 +6,23 @@ class Triangle
     @side1, @side2, @side3 = side1, side2, side3
   end
 
-  def equilateral
-    side1 == side2 && side2 == side3
+  def equilateral?
+    (side1 == side2) && (side2 == side3)
   end
 
-  def isosceles
+  def isosceles?
     [side1, side2, side3].uniq.length == 2
   end
 
-  def scalene
-    if !equilateral && !isosceles
-      false
-    else
-      true
-    end
+  def scalene?
+    !(equilateral? || isosceles?)
   end
 
   def recite_facts
-    puts 'This triangle is equilateral!' if equalateral
-    puts 'This triangle is scalene and mathematically boring.' if scalene
+    puts 'This triangle is equilateral!' if equilateral?
+    puts 'This triangle is scalene and mathematically boring.' if scalene?
 
-    if isosceles
+    if isosceles?
       puts 'This triangle is isosceles! Also, that word is hard to type.'
     end
 
@@ -44,7 +40,7 @@ class Triangle
   def calculate_angles(a, b, c)
     angle_a = calculate_angle_from_vertices(a, b, c)
     angle_b = calculate_angle_from_vertices(b, a, c)
-    angle_c = calculate_angle_from_vertices(a, a, b)
+    angle_c = calculate_angle_from_vertices(c, b, a)
 
     [angle_a, angle_b, angle_c]
   end

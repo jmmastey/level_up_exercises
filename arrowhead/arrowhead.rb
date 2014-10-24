171601548251640ad6d6ccb8dc02b3a4267de031
Aushadhee Mahakumarage <mahakumarage@gmail.com>
2014-10-24 15:04:11 -0500

    Refactored code to remove arrowhead anti-pattern

--- arrowhead/arrowhead.rb
+++ arrowhead/arrowhead.rb
@@ -1,4 +1,7 @@
 class Arrowhead
+  REGION_ERROR = 'Invalid Region Error'
+  SHAPE_ERROR  = 'Invalid Shape Error'
+
   # This seriously belongs in a database.
   CLASSIFICATIONS = {
     far_west: {
@@ -15,20 +18,44 @@ class Arrowhead
     },
   }
 
-  # FIXME: I don't have time to deal with this.
-  def self.classify(region, shape)
-    if CLASSIFICATIONS.include? region
-      shapes = CLASSIFICATIONS[region]
-      if shapes.include? shape
-        arrowhead = shapes[shape]
-        puts "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
-      else
-        raise "Unknown shape value. Are you sure you know what you're talking about?"
-      end
-    else
-      raise "Unknown region, please provide a valid region."
-    end
+  def self.raise_error(error)
+    raise error
   end
+
+  def self.raise_shape_error
+    raise_error SHAPE_ERROR
+  end
+
+  def self.raise_region_error
+    raise_error REGION_ERROR
+  end
+
+  def self.validate_region(region)
+    raise_region_error unless CLASSIFICATIONS.include? region
+  end
+
+  def self.validate_shape(region, shape)
+    validate_region region
+    raise_shape_error unless CLASSIFICATIONS[region].include? shape
+  end
+
+  def self.find_arrowhead(region, shape)
+    CLASSIFICATIONS[region][shape]
+  end
+
+  def self.print_arrowhead(arrowhead)
+    puts "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
+  end
+
+  def self.classify(region, shape)
+    validate_shape(region, shape)
+    arrowhead = find_arrowhead(region, shape)
+    print_arrowhead(arrowhead)
+  end
+
+  private_class_method :raise_error, :raise_shape_error, :raise_region_error,
+    :validate_shape, :validate_region, :find_arrowhead,
+    :print_arrowhead
 end
 
-puts Arrowhead.classify(:northern_plains, :bifurcated)
+puts Arrowhead.classify(:northern_plains_dfsf, :bifurcated)

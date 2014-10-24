57e952f4113af0182628c80d7f2a9b2bd17eeb86
Colleen Sain <csain@enova.com>
2014-10-24 11:39:07 -0500

    Included a seeder file

--- cyoa/cyoa/db/seeds.rb
+++ cyoa/cyoa/db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+artist_list = [
+  "Alison Krauss",
+  "Bassnectar",
+  "Beyonce",
+  "Big L",
+  "Disclosure",
+  "Jay Z",
+  "Justice",
+  "Kanye West",
+  "Katy Perry",
+  "Kesha",
+  "Kiss",
+  "Lady Gaga",
+  "Lorde",
+  "Madonna",
+  "Metallica",
+  "Paul Simon",
+  "Phish",
+  "Prince",
+  "Sound Tribe Sector 9",
+  "STS9",
+  "SBTRKT",
+  "Taylor Swift",
+  "The Grateful Dead",
+  "The Rolling Stones",
+  "The Shins",
+  "Umphrey's McGee"
+]
+
+artist_list.each do |name|
+  Artist.find_or_create_by_unique_name(name)
+end
+

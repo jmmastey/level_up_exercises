7d7275041d912803e6804c98b2ed2ad43c9104ab
Steve Durko <sdurko@enova.com>
2014-10-26 05:39:23 -0500

    unfinished refactoring

--- robot_name/robot_name.rb
+++ robot_name/robot_name.rb
@@ -7,21 +7,31 @@ class Robot
 
   def initialize(args = {})
     @@registry ||= []
+    
+   # if args[:name_generator]
+
+    
     @name_generator = args[:name_generator]
 
     if @name_generator
       @name = @name_generator.call
     else
-      generate_char = -> { ('A'..'Z').to_a.sample }
-      generate_num = -> { rand(10) }
-
-      @name = "#{generate_char.call}#{generate_char.call}#{generate_num.call}#{generate_num.call}#{generate_num.call}"
+     @name = generate_name
     end
 
     raise NameCollisionError, 'There was a problem generating the robot name!' if !(name =~ /[[:alpha:]]{2}[[:digit:]]{3}/) || @@registry.include?(name)
     @@registry << @name
   end
 end
+
+  def set_name(name)
+  
+  end
+
+  def generate_name
+    generate_char = -> { ('A'..'Z').to_a.sample }
+    "#{generate_char.call}#{generate_char.call}#{rand(10)}#{rand(10)}#{rand(10)}"
+  end
 
 robot = Robot.new
 puts "My pet robot's name is #{robot.name}, but we usually call him sparky."

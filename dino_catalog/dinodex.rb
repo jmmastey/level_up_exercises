63c60385437e39fa052a43f1eb1e8c4bdd57dd6d
rickytux <rick.dsida@icloud.com>
2014-10-27 13:24:23 -0500

    added ability to parse dinodex.csv into Dinosaur object.  Added method to convert biped/quadruped to integer for object

--- dino_catalog/dinodex.rb
+++ dino_catalog/dinodex.rb
@@ -1,14 +1,38 @@
 require 'csv'
 
-dinosaurs = CSV.read('dinodex.csv', headers:true)
-
-dinosaurs.each do |dino|
-  p dino.to_hash
-
-end
 class Dinosaur
-  def init(options)
-    @name = option[:Name]
+    attr_reader :name
+  def initialize(options)
+    @name = options["NAME"].to_s
+    @period = options["PERIOD"].to_s
+    @continent = options["CONTINENT"].to_s
+    @diet = options["DIET"].to_s
+    @weight_in_lbs = options["WEIGHT_IN_LBS"].to_i
+    @legs = convert(options["WALKING"])
+    @description = options["DESCRIPTION"].to_s
+  end
 
+
+  def convert(dino)
+    if dino.downcase == "biped"
+      2
+    elsif dino.downcase == "quadruped"
+      4
+    else
+      "other"
+    end
   end
 end
+
+dinosaurs = CSV.read('dinodex.csv', headers:true)
+
+p dinosaurs
+library = []
+dinosaurs.each do |dino|
+    row =dino.to_hash
+    library << Dinosaur.new(row)
+
+end
+
+
+# p library[4]

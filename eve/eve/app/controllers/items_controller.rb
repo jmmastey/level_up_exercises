2bf498571f1b7415da8edfaa772dafc6d5e433a3
Dan Kotowski <dkotowski@enova.com>
2014-10-27 08:38:34 -0500

    Added Kaminari pagination.

--- eve/eve/app/controllers/items_controller.rb
+++ eve/eve/app/controllers/items_controller.rb
@@ -1,4 +1,28 @@
 class ItemsController < ApplicationController
   def index
+    @search_query = params[:q]
+    @items = get_items
+    @items = search_items(@items, @search_query) if @search_query
+
+    respond_to do |format|
+      format.html do
+        @items = @items.page(params[:page])
+        render haml: @items
+      end
+      format.json do
+        render json: @items
+      end
+    end
+  end
+
+  private
+
+  def get_items
+    Item.order(:in_game_id)
+  end
+
+  def search_items(items, query)
+    query = query.strip.upcase
+    items.where("UPPER(name) like ?", "%#{query}%")
   end
 end

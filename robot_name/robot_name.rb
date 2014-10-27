db8a703fa44486f72d87c02cccec1ceabfac4c45
Aushadhee Mahakumarage <mahakumarage@gmail.com>
2014-10-27 13:56:13 -0500

    Exceptions moved to their own classes and [files]

--- robot_name/robot_name.rb
+++ robot_name/robot_name.rb
@@ -1,4 +1,5 @@
-class NameCollisionError < RuntimeError; end
+require_relative 'name_collision_error'
+require_relative 'pattern_error'
 
 class Robot
   attr_accessor :name
@@ -18,7 +19,7 @@ class Robot
       @name = "#{generate_char.call}#{generate_char.call}#{generate_num.call}#{generate_num.call}#{generate_num.call}"
     end
 
-    raise NameCollisionError, 'There was a problem generating the robot name!' if !(name =~ /[[:alpha:]]{2}[[:digit:]]{3}/) || @@registry.include?(name)
+    raise NameCollisionError if !(name =~ /[[:alpha:]]{2}[[:digit:]]{3}/) || @@registry.include?(name)
     @@registry << @name
   end
 end

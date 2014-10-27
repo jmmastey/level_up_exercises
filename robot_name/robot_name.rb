3849a29c658b50e7a87f05a6ed8a827467689ac9
Aushadhee Mahakumarage <mahakumarage@gmail.com>
2014-10-27 13:36:06 -0500

    test portion of the code moved to a separate file

--- robot_name/robot_name.rb
+++ robot_name/robot_name.rb
@@ -22,11 +22,3 @@ class Robot
     @@registry << @name
   end
 end
-
-robot = Robot.new
-puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
-
-# Errors!
-# generator = -> { 'AA111' }
-# Robot.new(name_generator: generator)
-# Robot.new(name_generator: generator)
---
+++ robot_name/robot_name_test.rb
@@ -0,0 +1,9 @@
+require_relative 'robot_name'
+
+robot = Robot.new
+puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
+
+# Errors!
+# generator = -> { 'AA111' }
+# Robot.new(name_generator: generator)
+# Robot.new(name_generator: generator)

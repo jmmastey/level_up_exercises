447aaf890b243f86e2bfd9195376cd0a61de26de
Jon Marks <jmarks1@enova.com>
2014-10-27 14:07:45 -0500

    Reformatted

--- arrowhead/arrowhead.rb
+++ arrowhead/arrowhead.rb
@@ -15,28 +15,37 @@ class Arrowhead
     },
   }
 
+  @user_messages =
+  {
+    unknown_region: "Unknown region, please provide a valid region.",
+    unknown_shape: "Unknown shape value. Are you sure you know what you're talking about?",
+    describe_arrowhead: "You have a(n) '%s' arrowhead. Probably priceless.",
+  }
+
   def self.classify(region, shape)
     arrowhead = select_arrowhead_by_shape(shape, get_region_shapes(region))
     print_arrowhead(arrowhead)
   end
 
   def self.print_arrowhead(arrowhead)
-    puts "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
+    puts user_message(:describe_arrowhead, arrowhead)
   end
   private_class_method :print_arrowhead
 
   def self.get_region_shapes(region)
-    CLASSIFICATIONS[region] ||
-      raise("Unknown region, please provide a valid region.")
+    CLASSIFICATIONS[region] || raise(user_message(:unknown_region))
   end
   private_class_method :get_region_shapes
 
   def self.select_arrowhead_by_shape(shape, region_shapes)
-    message = "Unknown shape value. "
-    message += "Are you sure you know what you're talking about?"
-    region_shapes[shape] || raise(message)
+    region_shapes[shape] || raise(user_message(:unknown_shape))
   end
   private_class_method :select_arrowhead_by_shape
+
+  def self.user_message(message_sym, *a)
+    format(@user_messages[message_sym], *a)
+  end
+  private_class_method :user_message
 end
 
 puts Arrowhead.classify(:northern_plains, :bifurcated)

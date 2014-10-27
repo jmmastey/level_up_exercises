b0369914175224bc000f92cec5c23db14b85d0fa
Zamir Syed <zsyed@enova.com>
2014-10-27 11:15:24 -0500

    Moving to pixel sizing for fixed components

--- pmail/pete_mail/app/assets/javascripts/application.js
+++ pmail/pete_mail/app/assets/javascripts/application.js
@@ -15,29 +15,60 @@
 //= require turbolinks
 //= require_tree .
 
+function hide_toggle_menues_other_than(element) {
+}
+
 function setup_event_handlers() {
+
+	// Search Options
 	$( "#search-options-div" ).mouseenter( function() {
-		console.log("Mouse entered");
 		$( "#search-options-div .tooltip" ).show();
 	});
 
 	$( "#search-options-div" ).mouseleave( function() {
-		console.log("Mouse left");
 		$( "#search-options-div .tooltip" ).hide();
 	});
 
+	$( "#search-options-div" ).click( function() {
+		$( "#search-options-menu-div" ).toggle();
+	});
+
+
+	// Apps Button
 	$( "#apps-button-div" ).mouseenter( function() {
-		console.log("Mouse entered");
 		$( "#apps-button-div .tooltip" ).show();
 	});
 
 	$( "#apps-button-div" ).mouseleave( function() {
-		console.log("Mouse left");
 		$( "#apps-button-div .tooltip" ).hide();
 	});
 
+
+	// Mail Options
 	$( "#mail-options-button-div" ).click( function() {
 		$( "#mail-options-list-div" ).toggle();
+	});
+
+
+	// Manage Account
+	$( "#manage-account" ).mouseenter( function() {
+		$( "#manage-account-menu" ).show("fast");
+	});
+
+	$( "#manage-account-menu" ).click( function() {
+		$( "#manage-account-menu" ).hide();
+	});
+
+	
+	// Mail Item Selection
+	$( "#mail-item-selection-div .down-arrow" ).click( function() {
+		$( "#mail-item-selection-options-div" ).toggle();
+	});
+
+	
+	// Mark All As Read
+	$( "#mark-all-as-read-div" ).click( function() {
+		$( "#mark-all-as-read-menu-div" ).toggle();
 	});
 }
 

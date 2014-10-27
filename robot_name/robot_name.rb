9fd354b31141881d72ec2c7334c8669485ac58e2
Aushadhee Mahakumarage <mahakumarage@gmail.com>
2014-10-27 14:46:45 -0500

    Refactored code, still not robust

--- robot_name/robot_name.rb
+++ robot_name/robot_name.rb
@@ -2,24 +2,49 @@ require_relative 'name_collision_error'
 require_relative 'pattern_error'
 
 class Robot
-  attr_accessor :name
+  attr_reader :name
 
-  @@registry
+  NAME_LENGTH = 5
+  NAME_CHAR_LENGTH = 2
+  NAME_NUM_LENGTH = 3
+
+  class << self
+    attr_accessor :registry
+  end
 
   def initialize(args = {})
-    @@registry ||= []
-    @name_generator = args[:name_generator]
-
-    if @name_generator
-      @name = @name_generator.call
-    else
-      generate_char = -> { ('A'..'Z').to_a.sample }
-      generate_num = -> { rand(10) }
-
-      @name = "#{generate_char.call}#{generate_char.call}#{generate_num.call}#{generate_num.call}#{generate_num.call}"
-    end
-
-    raise NameCollisionError if !(name =~ /[[:alpha:]]{2}[[:digit:]]{3}/) || @@registry.include?(name)
-    @@registry << @name
+    self.class.registry ||= []
+    @name_generator = args[:name_generator] || default_name_generator
+    generate_name
   end
+
+  def generate_name
+    name = @name_generator.call
+
+    check_name_collision(name)
+    check_pattern(name)
+
+    self.class.registry << (@name = name)
+  end
+
+  def check_name_collision(name)
+    raise NameCollisionError if self.class.registry.include? name
+  end
+
+  def check_pattern(name)
+    pattern = "^[A-Z]{#{NAME_CHAR_LENGTH}}[0-9]{#{NAME_NUM_LENGTH}}$"
+    raise PatternError unless name =~ Regexp.new(pattern)
+  end
+
+  def default_name_generator
+    lambda do
+      NAME_LENGTH.times.to_a.each_with_object('') do |i, name|
+        range = i < NAME_CHAR_LENGTH ? ('A'..'Z') : ('0'..'9')
+        name << range.to_a.sample
+      end
+    end
+  end
+
+  private :generate_name, :check_name_collision, :check_pattern,
+    :default_name_generator
 end

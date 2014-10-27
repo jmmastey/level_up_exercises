4faea54698d089821115a2c91e3e1bc9f47ef10a
jillalynch <jill.lynch5@gmail.com>
2014-10-27 11:15:57 -0500

    pulled out triangle_facts_description in order to reduce multiple puts.  Also implemented comment  classifies the type & returns a symbol that could be used as a lookup in a hash of messages

--- triangle_facts/triangle_facts.rb
+++ triangle_facts/triangle_facts.rb
@@ -1,6 +1,12 @@
 class Triangle
   attr_accessor :side1, :side2, :side3
 
+  SHAPE_DESCRIPTION = {
+    equilateral: "equilateral!",
+    isosceles: "isosceles! Also, that word is hard to type.",
+    scalene: "scalene and mathematically boring."
+  }
+
   def initialize(side1, side2, side3)
     @side1 = side1
     @side2 = side2
@@ -19,11 +25,19 @@ class Triangle
     true unless is_equilateral? || is_isosceles?
   end
 
-  def recite_facts
+  def triangle_shape_description
+    if is_equilateral?
+      :equilateral
+    elsif is_isosceles?
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def recite_facts(shape_type)
     print "This triangle: #{ self.object_id }, is "
-    puts "equilateral!" if is_equilateral?
-    puts "isosceles! Also, that word is hard to type." if is_isosceles?
-    puts "scalene and mathematically boring." if is_scalene?
+    puts SHAPE_DESCRIPTION[triangle_shape_description]
     puts "The angles of this triangle are #{ angles_from_sides.join(', ') }"
     puts "This triangle is also a right triangle!" if angles_from_sides.include? 90
     puts ""
@@ -39,8 +53,7 @@ class Triangle
     positions = [[side3, side2, side1],
                  [side1, side3, side2],
                  [side1, side2, side3]]
-    angle_a, angle_b, angle_c = [0, 0, 0]
-    angles = [angle_a, angle_b, angle_c]
+    angles = [0, 0, 0]
 
     positions.each_with_index do |position, index|
       angles[index] = law_of_cosines(position)
@@ -57,8 +70,8 @@ triangles = [[5, 5, 5], [5, 12, 13]]
 
 triangles.each do |sides|
   tri = Triangle.new(*sides)
-  tri.recite_facts
-  print "Let's change side 1 to 4in ... "
+  tri.recite_facts(tri.triangle_shape_description)
+  print "Let's change side 1 to 4in ..."
   tri.side1 = 4
-  tri.recite_facts
+  tri.recite_facts(tri.triangle_shape_description)
 end

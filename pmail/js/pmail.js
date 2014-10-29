3e62e363718e5a92b84c10516adc0cba1d233b0f
Stacy Mullins <smullins10@gmail.com>
2014-10-28 22:51:00 -0500

    Add stopPropagation for sidebar li

--- pmail/js/pmail.js
+++ pmail/js/pmail.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
-   $('li').click(function(){
+
+  $('li').click(function() {
+     $("li").removeClass("bold");
+});
+   $('li').click(function(event){
+     event.stopPropagation();
      $(this).addClass("bold");
-   });
+     });
 });

cad0a2d8184b714b00b550256cc613c7f4053f45
paulh16 <paulh16@gmail.com>
2014-10-29 14:24:13 -0500

    Adjust file for the Syntax section of the Ruby Style Guide

--- triangle_facts/triangle_facts.rb
+++ triangle_facts/triangle_facts.rb
@@ -3,19 +3,19 @@ class Triangle
   attr_accessor :side1, :side2, :side3
 
   def initialize(side1, side2, side3)
-    @side1,  @side2, @side3 = side1, side2, side3
+    @side1, @side2, @side3 = side1, side2, side3
   end
 
-  def equilateral()
-    return side1 == side2 && side2 == side3
+  def equilateral
+    side1 == side2 && side2 == side3
   end
 
-  def isosceles()
-    return [side1, side2, side3].uniq.length == 2
+  def isosceles
+    [side1, side2, side3].uniq.length == 2
   end
 
-  def scalene()
-    unless !(equilateral || isosceles)
+  def scalene
+    if equilateral || isosceles
       false
     else
       true
@@ -23,7 +23,7 @@ class Triangle
   end
 
   def recite_facts
-    puts 'This triangle is equilateral!' if equalateral
+    puts 'This triangle is equilateral!' if equilateral
 
     puts 'This triangle is isosceles! Also, that word is hard to type.' if isosceles
 
@@ -45,11 +45,11 @@ class Triangle
 
     angleC = radians_to_degrees(Math.acos((a**2 + b**2 - c**2) / (2.0 * a * b)))
 
-    return [angleA, angleB, angleC]
+    [angleA, angleB, angleC]
   end
 
   def radians_to_degrees(rads)
-    return (rads * 180 / Math::PI).round
+    (rads * 180 / Math::PI).round
   end
 end
 

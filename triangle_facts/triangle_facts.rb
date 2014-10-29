f4e80f1d660d94203abd8f21037343d60923ce6c
paulh16 <paulh16@gmail.com>
2014-10-29 14:46:32 -0500

    Adjust file for the Collections section of the Ruby Style Guide

--- triangle_facts/triangle_facts.rb
+++ triangle_facts/triangle_facts.rb
@@ -3,7 +3,9 @@ class Triangle
   attr_accessor :side_1, :side_2, :side_3
 
   def initialize(side_1, side_2, side_3)
-    @side_1, @side_2, @side_3 = side_1, side_2, side_3
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
   end
 
   def equilateral?

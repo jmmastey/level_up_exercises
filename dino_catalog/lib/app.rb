6c9fc1627fda9f2373a654e0c4a132a05476421a
paulh16 <paulh16@gmail.com>
2014-10-29 12:03:35 -0500

    Create User Processing module

--- dino_catalog/lib/app.rb
+++ dino_catalog/lib/app.rb
@@ -8,6 +8,7 @@ require 'option_parser'
 require 'build_csv'
 require 'csv_modifier'
 require 'user_prompts'
+require 'user_processing'
 
 class App
   include BuildCsv
@@ -15,24 +16,10 @@ class App
   include Filters
   include Display
   include JsonExport
-
-  SEARCH_REGEX = {
-    bipeds: /biped/,
-    carnivores: /carnivore|insectivore|piscivore/,
-    periods: /cretaceous|permian|jurassic|oxfordian|albian|triassic/,
-    sizes: /big|small/
-  }
+  include UserProcessing
 
   def initialize(name)
     @app_name = name
-  end
-
-  def create_dinosaur_entry(name, attributes)
-    Dinosaur.new(name, attributes)
-  end
-
-  def create_catalog(filepath)
-    @catalog = Catalog.new(filepath)
   end
 
   def launch!(csv_filename=nil, user_hash_input=nil)
@@ -42,59 +29,8 @@ class App
     obtain_user_filters
   end
 
-  def obtain_user_filters
-    @filtered_dinosaurs = nil
-    print UserPrompts::USER_SEARCH_PROMPT
-    print '> '
-    user_input = gets.chomp.downcase
-    exit! if user_input == 'exit' || user_input == 'quit'
-    search_terms = get_user_search_terms(user_input)
-    @filtered_dinosaurs = filter_results(@catalog, search_terms)
-    puts "\nYour search resulted in #{@filtered_dinosaurs.size} dinosaurs:"
-    print_search_summary(@filtered_dinosaurs)
-    user_processing(@filtered_dinosaurs)
-  end
-
-  # def direct_user_input_search(search_terms)
-  #   @filtered_dinosaurs = nil
-  #   @filtered_dinosaurs = filter_results(@catalog, search_terms)
-  #   puts "\nYour search resulted in #{@filtered_dinosaurs.size} dinosaurs:"
-  #   print_search_summary(@filtered_dinosaurs)
-  #   user_processing(@filtered_dinosaurs)
-  # end
-  # User hash input to be implemented in a later iteration
-
-  def user_processing(dinosaurs)
-    print UserPrompts::USER_PROCESSING_PROMPT
-    print '> '
-    user_input = gets.chomp.downcase
-    exit! if user_input == 'exit' || user_input == 'quit'
-    user_actions(user_input)
-  end
-
-  def user_actions(input)
-    exit! if input == 'exit' || input == 'quit'
-    if input == 'print'
-      print_dinosaur_set(@filtered_dinosaurs)
-      user_processing(@filtered_dinosaurs)
-    elsif input == 'search'
-      launch!(@csv_filename)
-    elsif input == 'json'
-      convert_to_json(@filtered_dinosaurs)
-      puts "\nYour JSON file has been saved as json_export.json in the main directory."
-      user_processing(@filtered_dinosaurs)
-    else
-      print_dinosaur_instance(@filtered_dinosaurs, input)
-      user_processing(@filtered_dinosaurs)
-    end
-  end
-
-  def get_user_search_terms(phrase)
-    search_terms = {}
-    SEARCH_REGEX.each do |term, regex|
-      search_terms[term] = phrase.scan(regex) unless phrase.scan(regex).empty? #  use inject?
-    end
-    search_terms
+  def create_catalog(filepath)
+    @catalog = Catalog.new(filepath)
   end
 
   def filter_results(catalog, filters)
@@ -111,5 +47,14 @@ class App
     search_results.each { |dinosaur| puts "#{dinosaur.name}"}
     puts "\n"
   end
+
+  # def direct_user_input_search(search_terms)
+  #   @filtered_dinosaurs = nil
+  #   @filtered_dinosaurs = filter_results(@catalog, search_terms)
+  #   puts "\nYour search resulted in #{@filtered_dinosaurs.size} dinosaurs:"
+  #   print_search_summary(@filtered_dinosaurs)
+  #   user_processing(@filtered_dinosaurs)
+  # end
+  # User hash input to be implemented in a later iteration
 
 end

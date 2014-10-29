a970d0083c9ddb4713a8c96c08a8af91c0c2d11e
Dan Kotowski <dkotowski@enova.com>
2014-10-29 11:48:04 -0500

    Modified Item controller structure.

--- eve/eve/spec/controllers/items_controller_spec.rb
+++ eve/eve/spec/controllers/items_controller_spec.rb
@@ -1,11 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe ItemsController, :type => :controller do
+  let(:valid_attributes) do
+    skip("Add a hash of attributes valid for your model")
+  end
+
+  let(:invalid_attributes) do
+    skip("Add a hash of attributes invalid for your model")
+  end
+
+  let(:valid_session) { {} }
 
   describe "GET index" do
-    it "returns http success" do
-      get :index
-      expect(response).to be_success
+    it "assigns all items as @items" do
+      item = Item.create! valid_attributes
+      get :index, {}, valid_session
+      expect(assigns(:items)).to eq([item])
     end
   end
 

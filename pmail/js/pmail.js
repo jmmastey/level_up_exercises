281bfaecc1a5970dbbda27a3967bbb18eb4f3de5
Stacy Mullins <smullins10@gmail.com>
2014-10-30 09:44:22 -0500

    Add logo

--- pmail/js/pmail.js
+++ pmail/js/pmail.js
@@ -7,15 +7,22 @@ $(document).ready(function(){
      event.stopPropagation();
      $(this).addClass("bold");
      });
-  
+
+    $('li').click(function() {
+     $(this).css({"border-left": "0px"});
+});
+   $('li').click(function(event){
+     event.stopPropagation();
+     $(this).css({"border-left":"4px solid #dd4b39"});
+
+     });
 
  $('li').hover(
    function (){
      $(this).css({"background-color":"#F2F2F0"});
   },
   function () {
-           $(this).css({"background-color":"white"});
+      $(this).css({"background-color":"white"});
   }
-
   );
  });

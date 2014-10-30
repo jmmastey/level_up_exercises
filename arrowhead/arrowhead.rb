87f0c20e1c26cfb14c02f9544fd66feacd775d80
Zachary Huang <chuang@enova.com>
2014-10-30 12:24:06 -0500

    Update arrowhead solution

--- arrowhead/arrowhead.rb
+++ arrowhead/arrowhead.rb
@@ -14,20 +14,13 @@ class Arrowhead
       bifurcated: "Oxbow",
     },
   }
-
-  # FIXME: I don't have time to deal with this.
+  # FIXME : I don't have time to deal with this.
   def self.classify(region, shape)
-    if CLASSIFICATIONS.include? region
-      shapes = CLASSIFICATIONS[region]
-      if shapes.include? shape
-        arrowhead = shapes[shape]
-        puts "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
-      else
-        raise "Unknown shape value. Are you sure you know what you're talking about?"
-      end
-    else
-      raise "Unknown region, please provide a valid region."
-    end
+    shapes =  CLASSIFICATIONS[region]
+    raise "Unknown region, please provide a valid region." if shapes.nil?
+    arrowhead = shapes[shape]
+    raise "Unknown shape value. please provide a valid shape." if arrowhead.nil?
+    puts "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
   end
 end
 

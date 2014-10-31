d54368396dd3f95d5622b3c77406b9844ee08c82
Colleen Sain <csain@enova.com>
2014-10-31 11:43:13 -0500

    Improved UX

--- cyoa/cyoa/app/controllers/artists_controller.rb
+++ cyoa/cyoa/app/controllers/artists_controller.rb
@@ -18,14 +18,14 @@ class ArtistsController < ApplicationController
     unless current_user.add_artist_name(name)
       message = "Whoops! We can't seem to find any data on the artist "
       message += "'#{name}' :("
-      flash[:info] = message
+      flash[:alert] = message
     end
     redirect_to :back
   end
 
   def show
     unless @artist && @artist.nbs_id
-      flash[:info] = "Whoops! We were unable to locate that artist you requested :("
+      flash[:alert] = "Whoops! We were unable to locate that artist you requested :("
       redirect_to artists_path
     end
   end
--- cyoa/cyoa/app/models/user.rb
+++ cyoa/cyoa/app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
        # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_and_belongs_to_many :artists
+  has_and_belongs_to_many :artists, uniq: true
 
   after_create :set_defaults
 
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
 
   def add_artist_name(artist_name)
     artist = Artist.find_or_create_by_unique_name(artist_name)
-    artists << artist
+    artists << artist unless artists.include? artist
     artist.nbs_id
   end
 

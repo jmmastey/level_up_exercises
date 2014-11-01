f7a2c5966ba3243c4439a3ebc9cf7b6cb17c2d75
Steve Durko <sdurko@enova.com>
2014-11-01 07:26:45 -0500

    refactored robot name class

--- robot_name/robot_name.rb
+++ robot_name/robot_name.rb
@@ -1,38 +1,62 @@
 class NameCollisionError < RuntimeError; end
+class DuplicateNameError < RuntimeError; end
 
+# Class that generates a robot name or takes the given name
 class Robot
   attr_accessor :name
+  NAME_COLLISION_ERROR = 'There was a problem generating the robot name!'
+  DUPLICATE_NAME_ERROR = 'The robot name has already been used!'
 
-  @@registry
+  @@registry = []
 
-  def initialize(args = {})
-    @@registry ||= []
-    
-    @name_generator = args[:name_generator]
-
-    if @name_generator
-      @name = @name_generator.call
-    else
-     @name = generate_name
-    end
-
-    raise NameCollisionError, 'There was a problem generating the robot name!' if !(name =~ /[[:alpha:]]{2}[[:digit:]]{3}/) || @@registry.include?(name)
+  def initialize(name = '')
+    @name = create_name(name)
+    validate_name(@name)
     @@registry << @name
   end
 
+  private
+
+  def create_name(name)
+    name.empty? ? generate_name : name
+  end
+
+  def validate_name(name)
+    fail NameCollisionError, NAME_COLLISION_ERROR unless
+    name =~ /[[:alpha:]]{2}[[:digit:]]{3}/
+    fail DuplicateNameError, DUPLICATE_NAME_ERROR if @@registry.include?(name)
+  end
+
   def generate_name
-    generate_char = -> { ('A'..'Z').to_a.sample }
-    "#{generate_char.call}#{generate_char.call}#{rand(10)}#{rand(10)}#{rand(10)}"
+    name = ''
+    2.times { name << generate_char }
+    3.times { name << generate_number }
+    name
+  end
+
+  def generate_char
+    ('A'..'Z').to_a.sample
+  end
+
+  def generate_number
+    rand(10).to_s
   end
 
   def to_s
-    "My pet robot's name is #{@name}, but we usually call him sparky." 
+    "My pet robot's name is #{@name}, but we usually call him sparky."
   end
 end
 
 robot = Robot.new
 puts robot
 
-generator = -> { 'AA111' }
-robot =  Robot.new(name_generator: generator)
-puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
+# Name collision trigger
+# robot = Robot.new('A111')
+# puts robot
+
+robot =  Robot.new('AA111')
+puts robot
+
+# Duplicate name error trigger
+# robot =  Robot.new('AA111')
+# puts robot

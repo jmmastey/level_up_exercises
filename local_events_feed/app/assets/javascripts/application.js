3c7bd7a30290af4098c357392d68c4d9802090d9
Zamir Syed <zsyed@enova.com>
2014-11-02 16:52:25 -0600

    Added tripod to home-menu buttons

--- local_events_feed/app/assets/javascripts/application.js
+++ local_events_feed/app/assets/javascripts/application.js
@@ -18,3 +18,34 @@
 function setUserShowingsScrollable() {
 	$( "#user-event-showings" ).addClass("scrollable");
 }
+
+function drawTripod(ctx, centerX, centerY, bgnR, endR) {
+	var cos30 = Math.cos(33 * Math.PI / 180.0);
+	var sin30 = Math.sin(33 * Math.PI / 180.0);
+
+	ctx.beginPath();
+	ctx.moveTo(centerX, centerY - bgnR);
+	ctx.lineTo(centerX, centerY - endR);
+
+	ctx.moveTo(centerX - bgnR * cos30, centerY + bgnR * sin30);
+	ctx.lineTo(centerX - endR * cos30, centerY + endR * sin30);
+
+	ctx.moveTo(centerX + bgnR * cos30, centerY + bgnR * sin30);
+	ctx.lineTo(centerX + endR * cos30, centerY + endR * sin30);
+	ctx.stroke();
+}
+
+function drawHomeBackground() {
+	var canvas = document.getElementById("home-menu-canvas");
+	var ctx = canvas.getContext("2d");
+
+	canvas.width = 500;
+	canvas.height = 500;
+
+	ctx.globalAlpha = 0.4;
+	ctx.strokeStyle = "#000077";
+	ctx.lineCap = "round";
+
+	ctx.lineWidth = 20;
+	drawTripod(ctx, 250, 308, 75, 160);
+}

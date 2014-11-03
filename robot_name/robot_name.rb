748aab523088da651c4b6cd446459de1565b4a10
cnorrisjr <cnorris@enocva.com>
2014-11-03 12:55:29 -0600

    Robot name
    Refactored robot name

--- robot_name/robot_name.rb
+++ robot_name/robot_name.rb
@@ -1,32 +1,57 @@
 class NameCollisionError < RuntimeError; end
+class IllegalCharactersError < RuntimeError; end
 
 class Robot
   attr_accessor :name
 
-  @@registry
-
   def initialize(args = {})
     @@registry ||= []
     @name_generator = args[:name_generator]
+    creates_name
+  end
 
+  def creates_name
     if @name_generator
-      @name = @name_generator.call
+      create_name_using_name_generator
     else
-      generate_char = -> { ('A'..'Z').to_a.sample }
-      generate_num = -> { rand(10) }
-
-      @name = "#{generate_char.call}#{generate_char.call}#{generate_num.call}#{generate_num.call}#{generate_num.call}"
+      create_name_using_gen_caller
     end
+  end
 
-    raise NameCollisionError, 'There was a problem generating the robot name!' if !(name =~ /[[:alpha:]]{2}[[:digit:]]{3}/) || @@registry.include?(name)
+  def create_name_using_name_generator
+    @name = @name_generator.call
+    validates_name
+  end
+
+  def create_name_using_gen_caller
+    gen_char = -> { ('A'..'Z').to_a.sample }
+    gen_num = -> { rand(10) }
+    char = "#{gen_char.call}#{gen_char.call}"
+    num = "#{gen_num.call}#{gen_num.call}#{gen_num.call}"
+    @name = char + num
+    validates_name
+  end
+
+  def validates_name
+    raise IllegalCharactersError unless @name =~ /[[:alpha:]]{2}[[:digit:]]{3}/
+    raise NameCollisionError if @@registry.include?(@name)
     @@registry << @name
+  end
+
+  def to_s
+    "My pet robot's name is #{name}, but we usually call him sparky"
   end
 end
 
-robot = Robot.new
-puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
+robot1 = Robot.new
+robot2 = Robot.new
 
+puts robot1
+puts robot2
+
+#
 # Errors!
-# generator = -> { 'AA111' }
-# Robot.new(name_generator: generator)
-# Robot.new(name_generator: generator)
+generator = -> { 'AA111' }
+puts Robot.new(name_generator: generator)
+
+#puts Robot.new(name_generator: generator)

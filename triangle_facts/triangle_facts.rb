9d1f5ced180fa6d4eac4161ea527c842c771e5a1
paulh16 <paulh16@gmail.com>
2014-11-03 10:36:30 -0600

    Refactor determining triangle angles

--- triangle_facts/triangle_facts.rb
+++ triangle_facts/triangle_facts.rb
@@ -4,58 +4,49 @@ class Triangle
     @side_1 = side_1
     @side_2 = side_2
     @side_3 = side_3
+    @angles = triangle_angles([@side_1, @side_2, @side_3])
+  end
+
+  def triangle_angles(sides)
+    numerator = sides.inject(0.0) { |sum, side| sum + side**2 }
+    denominator = sides.inject(1.0) { |product, side| product * side } * 2
+    sides.map do |side|
+      radians_to_degrees(Math.acos((numerator - 2 * side**2) / (denominator / side)))
+    end
   end
 
   def radians_to_degrees(radians)
     (radians * 180 / Math::PI).round
   end
 
-  def angle_1
-    numerator = @side_2**2 + @side_3**2 - @side_1**2
-    denominator = 2.0 * @side_2 * @side_3
-    radians_to_degrees(Math.acos(numerator / denominator))
-  end
-
-  def angle_2
-    numerator = @side_1**2 + @side_3**2 - @side_2**2
-    denominator = 2.0 * @side_1 * @side_3
-    radians_to_degrees(Math.acos(numerator / denominator))
-  end
-
-  def angle_3
-    numerator = @side_1**2 + @side_2**2 - @side_3**2
-    denominator = 2.0 * @side_1 * @side_2
-    radians_to_degrees(Math.acos(numerator / denominator))
-  end
-
   def equilateral?
-    [side_1, side_2, side_3].uniq.length == 1
+    @angles.uniq.length == 1
   end
 
   def isosceles?
-    [side_1, side_2, side_3].uniq.length == 2
+    @angles.uniq.length == 2
   end
 
   def scalene?
-    [side_1, side_2, side_3].uniq.length == 3
+    @angles.uniq.length == 3
   end
 
   def right?
-    [angle_1, angle_2, angle_3].include?(90)
+    @angles.include?(90)
   end
 
   def recite_facts
     puts "This triangle is equilateral!" if equilateral?
     puts "This triangle is isosceles! Also, that word is hard to type." if isosceles?
     puts "This triangle is scalene and mathematically boring." if scalene?
-    puts "The angles of this triangle are #{angle_1}, #{angle_2}, #{angle_3}"
+    puts "The angles of this triangle are: #{@angles}"
     puts "This triangle is also a right triangle!" if right?
     puts
   end
 
   protected
 
-  attr_reader :side_1, :side_2, :side_3
+  attr_reader :side_1, :side_2, :side_3, :angles
 end
 
 triangles = [

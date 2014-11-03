d5655ddd6db954f49992dd616a4425c90593b926
Aushadhee Mahakumarage <mahakumarage@gmail.com>
2014-11-03 15:00:17 -0600

    new line fix

--- robot_name/robot_name.rb
+++ robot_name/robot_name.rb
@@ -2,8 +2,6 @@ require_relative 'robot_name_registry'
 require_relative 'robot_name_generator'
 require_relative 'registry_error'
 
-# require_relative 'robot_name_factory_error'
-
 class Robot
   attr_reader :name
 
--- robot_name/robot_name_test.rb
+++ robot_name/robot_name_test.rb
@@ -7,11 +7,12 @@ sparky = Robot.new(registry)
 puts "Robot name is #{sparky.name}, but we usually call him Sparky"
 
 # Check name collisions
-static_generator = lambda {'AA123'}
-billy = Robot.new(registry, name_generator: static_generator)
-# bobby = Robot.new(registry, name_generator: static_generator)
+static_generator = -> { 'AA123' }
+Robot.new(registry, name_generator: static_generator)
+# Robot.new(registry, name_generator: static_generator)
 
 # Check different schema
-static_generator = lambda {'AA1234'}
-arthur = Robot.new(registry, name_generator: static_generator, schema: "[[:upper:]]{2}[[:digit:]]{4}")
+static_generator = -> { 'AA1234' }
+arthur = Robot.new(registry, name_generator: static_generator,
+ schema: "[[:upper:]]{2}[[:digit:]]{4}")
 p arthur.name

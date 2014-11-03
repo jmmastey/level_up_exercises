b0788ee84f811c8bc5bcf0c8561b6ad1a3bf6817
Srikanth Astaputhra <sastaputhra@enova.com>
2014-11-03 08:49:58 -0600

    able to save results in database

--- event_system/app/controllers/weather_controller.rb
+++ event_system/app/controllers/weather_controller.rb
@@ -3,7 +3,25 @@ class WeatherController < ApplicationController
   def index
     if params.has_key? "region"
       Rails.logger.info " PARAMS #{params["region"]["name"].inspect}"
-      @scraping =  WebScraper.scrape
+      if WeatherForecast.where("created_at::date = current_date").count <= 0
+        @scraping =  WebScraper.scrape
+
+        @scraping.each do |key,hash_temp|
+          wf = WeatherForecast.new
+          wf.weather_day = key
+          wf.high        = hash_temp["high"] if hash_temp.has_key? "high"
+          wf.low         = hash_temp["low"]  if hash_temp.has_key? "low"
+          wf.save!
+        end
+      else
+        weather_forecast = WeatherForecast.select("weather_day,high,low").where("created_at::date = current_date")
+        @scraping = {}
+        weather_forecast.each do |rec|
+          puts rec
+          @scraping[rec.weather_day] = {}
+          @scraping[rec.weather_day] = {"high" => rec.high, "low" => rec.low}
+        end
+      end
     end
   end
 

23c87de0685e0abac5f2eceb164dc02f92da2a5c
Stacy Mullins <smullins10@gmail.com>
2014-11-03 13:51:56 -0600

    Separate views,refactor logic

--- overlord/overlord.rb
+++ overlord/overlord.rb
@@ -11,35 +11,51 @@ set :bind, "0.0.0.0"
 
 get "/" do
   session[:bomb] = Bomb.new
-  @bomb =  session[:bomb]
+  @bomb = session[:bomb]
   erb :inactive
 end
 
 post "/activate" do
   code = params[:activation_code]
   if valid_code?(code)
-    bomb =  session[:bomb]
+    bomb = session[:bomb]
     bomb.enter_code(code.to_i)
     bomb.activate
+     @bomb = session[:bomb]
+    if bomb.active?
+      erb :activated
+    else
+      erb :inactive
+    end
+  else
+    @bomb = session[:bomb]
+    erb :inactive
   end
-  @bomb = session[:bomb]
-  erb :bomb_view #change to :activated
 end
 
 post "/deactivate" do
   code = params[:deactivation_code]
-  puts
   if valid_code?(code)
   bomb = session[:bomb]
   bomb.enter_code(code.to_i)
   bomb.deactivate
- end
-  @bomb = session[:bomb]
-  erb :bomb_view
+     @bomb = session[:bomb]
+     if bomb.exploded?
+        erb :exploded
+     else
+        erb :activated
+     end
+  else
+    @bomb = session[:bomb]
+    erb :inactive
+  end
+  end
+
+
+  # @bomb = session[:bomb] 
   # :activated if code 'attempt' is incorrect but not past limit
   # :inactive if code 'attempt' is correct
   # :exploded if code 'attempt' is incorrect and past limit
-end
 
 def valid_code?(code)
   /\A[0-9]{4}\Z/.match(code)

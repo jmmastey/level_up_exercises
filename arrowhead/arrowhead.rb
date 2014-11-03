7c10a21458e7583fcf9bc09cf3d74fa7205e0795
paulh16 <paulh16@gmail.com>
2014-11-03 12:38:07 -0600

    Move error handling to .classify and .determine_arrowhead methods. Modify .region_exists? and .shape_in_region? to return a Boolean

--- arrowhead/arrowhead.rb
+++ arrowhead/arrowhead.rb
@@ -18,22 +18,22 @@ class Arrowhead
   # FIXME: I don't have time to deal with this.
   def self.classify(region, arrowhead_shape)
     regional_shapes = CLASSIFICATIONS[region]
-    confirm_region_exists(region)
+    raise "Unknown region, please provide a valid region." unless region_exists?(region)
     determine_arrowhead(regional_shapes, arrowhead_shape)
   end
 
-  def self.confirm_region_exists(region)
-    raise "Unknown region, please provide a valid region." unless CLASSIFICATIONS.include? region
+  def self.region_exists?(region)
+    CLASSIFICATIONS.include? region
   end
 
   def self.determine_arrowhead(shapes_in_region, arrowhead_shape)
-    confirm_shape_in_region(shapes_in_region, arrowhead_shape)
+    raise "Unknown shape value. Are you sure you know what you're talking about?" unless shape_in_region?(shapes_in_region, arrowhead_shape)
     arrowhead = shapes_in_region[arrowhead_shape]
     puts "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
   end
 
-  def self.confirm_shape_in_region(regional_shapes, arrowhead_shape)
-    raise "Unknown shape value. Are you sure you know what you're talking about?" unless regional_shapes.include? arrowhead_shape
+  def self.shape_in_region?(regional_shapes, arrowhead_shape)
+    regional_shapes.include? arrowhead_shape
   end
 end
 

e6fa6196810873982f2feede2fcbf72b8054997e
paulh16 <paulh16@gmail.com>
2014-11-03 16:08:12 -0600

    Refactor generate_new_name and assert_valid_name regex

--- robot_name/robot_name.rb
+++ robot_name/robot_name.rb
@@ -3,13 +3,14 @@ class NameCollisionError < RuntimeError; end
 class Robot
   attr_accessor :name
 
+  VALID_NAME = /[[:alpha:]]{2}[[:digit:]]{3}/
+
   @@registry
 
   def initialize(args = {})
     @@registry ||= []
     @name_generator = args[:name_generator]
     assign_name
-    assert_valid_name
     add_to_registry
   end
 
@@ -19,22 +20,33 @@ class Robot
     else
       generate_new_name
     end
+    assert_valid_name
   end
 
   def generate_new_name
-    generate_char = -> { ('A'..'Z').to_a.sample }
-    generate_num = -> { rand(10) }
-    @name = "#{generate_char.call}#{generate_char.call}#{generate_num.call}#{generate_num.call}#{generate_num.call}"
+    @name = "#{character}#{character}#{number}#{number}#{number}"
   end
 
   def assert_valid_name
-    if !(name =~ /[[:alpha:]]{2}[[:digit:]]{3}/) || @@registry.include?(name)
+    if !(name =~ VALID_NAME) || @@registry.include?(name)
       raise NameCollisionError, 'There was a problem generating the robot name!'
     end
   end
 
   def add_to_registry
     @@registry << @name
+  end
+
+  private
+
+  def character
+    generate_char = -> { ('A'..'Z').to_a.sample }
+    generate_char.call
+  end
+
+  def number
+    generate_num = -> { rand(10) }
+    generate_num.call
   end
 end
 

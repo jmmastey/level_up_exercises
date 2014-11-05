f69069c634d9fe9f3b00cec4390f57e8de29a20e
Sushanth Iringentavida <siringentavida@enova.com>
2014-11-05 13:40:19 -0600

    Made changes based on Joe's comments

--- arrowhead/arrowhead.rb
+++ arrowhead/arrowhead.rb
@@ -1,5 +1,5 @@
-# Classify arrowheads based on region and shape
 class Arrowhead
+  # This seriously belongs in a database
   CLASSIFICATIONS = {
     far_west: {
       notched: "Archaic Side Notch",
@@ -17,21 +17,17 @@ class Arrowhead
 
   # FIXME: I don't have time to deal with this.
   def self.classify(region, shape)
-    if CLASSIFICATIONS.include? region
-      output_arrw region, shape
-    else
-      raise "Unknown region, please provide a valid region."
-    end
+    error1 =  "Unknown region, please provide a valid region."
+    raise error1 unless CLASSIFICATIONS.include? region
+    output_arrow(region, shape)
   end
 
-  def self.output_arrw(arrw_region, arr_shape)
-    shapes = CLASSIFICATIONS[arrw_region]
-    if shapes.include? arr_shape
-      puts "You have a(n) '#{shapes[arr_shape]}' arrowhead, probably priceless."
-    else
-      raise "Unknown shape. Are you sure you know what you're talking about?"
-    end
+  def self.output_arrow(arrow_region, arrow_shape)
+    shapes = CLASSIFICATIONS[arrow_region]
+    error2 = "Unknown shape. Are you sure you know what you're talking about?"
+    raise error2 unless shapes.include?(arrow_shape)
+    puts "You have a(n) '#{shapes[arrow_shape]}' arrowhead, probably priceless."
   end
 end
 
-puts Arrowhead.classify(:far_west, :bifurcated)
+puts Arrowhead.classify(:northern_plains, :bifurcated)

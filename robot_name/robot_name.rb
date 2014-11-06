32f865f4085c157aaa400c733f6684587d2f0883
cnorrisjr <cnorris@enocva.com>
2014-11-06 15:46:59 -0600

    Robot Name

--- robot_name/robot_name.rb
+++ robot_name/robot_name.rb
@@ -1,57 +1,53 @@
-class NameCollisionError < RuntimeError; end
-class IllegalCharactersError < RuntimeError; end
+require 'set'
+
+class NameCollisionError < RuntimeError
+  def message
+    "That Name already exists"
+  end
+end
+
+class IllegalCharactersError < RuntimeError
+  def message
+    "Illegal Characters were found"
+  end
+end
 
 class Robot
   attr_accessor :name
 
   def initialize(args = {})
-    @@registry ||= []
-    @name_generator = args[:name_generator]
-    creates_name
+    @@register ||= Set.new
+    @name_gen = args[:name_generator]
+
+    r_name = ('A'..'Z').to_a.sample(2).join("") + (0..9).to_a.sample(3).join("")
+
+    @name_gen.nil? ? create_name { r_name } : create_name { @name_gen.call }
   end
 
-  def creates_name
-    if @name_generator
-      create_name_using_name_generator
-    else
-      create_name_using_gen_caller
-    end
-  end
-
-  def create_name_using_name_generator
-    @name = @name_generator.call
-    validates_name
-  end
-
-  def create_name_using_gen_caller
-    gen_char = -> { ('A'..'Z').to_a.sample }
-    gen_num = -> { rand(10) }
-    char = "#{gen_char.call}#{gen_char.call}"
-    num = "#{gen_num.call}#{gen_num.call}#{gen_num.call}"
-    @name = char + num
+  def create_name
+    @name = yield
     validates_name
   end
 
   def validates_name
     raise IllegalCharactersError unless @name =~ /[[:alpha:]]{2}[[:digit:]]{3}/
-    raise NameCollisionError if @@registry.include?(@name)
-    @@registry << @name
+    raise NameCollisionError if @@register.include?(@name)
+    @@register << @name
   end
 
   def to_s
     "My pet robot's name is #{name}, but we usually call him sparky"
   end
+
+  def self.display_name
+    @@register.to_a.each do | name |
+       puts "My pet robot's name is #{name}, but we usually call him sparky"
+    end
+  end
 end
 
-robot1 = Robot.new
-robot2 = Robot.new
-
-puts robot1
-puts robot2
-
-#
-# Errors!
+puts Robot.new
+puts Robot.new
 generator = -> { 'AA111' }
 puts Robot.new(name_generator: generator)
-
-#puts Robot.new(name_generator: generator)
+puts Robot.new(name_generator: generator)

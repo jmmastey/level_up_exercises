949b52ab915921d31d62b6bfe88b05fa371a18ae
Ramya Kannan <ramyakannan@dhcp-10-224-159-57.enova.com>
2014-11-06 14:51:41 -0600

    some refactor based on comments

--- happenings/app/workers/scraper_worker.rb
+++ happenings/app/workers/scraper_worker.rb
@@ -16,29 +16,38 @@ class ScraperWorker
       scraper_helper.display_all_events
       sleep(1)
       scraper_helper.write_to_file('tmp.html')
-      scrape_and_save_events
+      events = scrape_events
+      save_events(doc)
     end
   end
 
   private
-  def scrape_and_save_events
-    event_time_array = []
+  def scrape_events
     puts "hashifying events"
     f = File.open("tmp.xml")
     doc = Nokogiri::XML(f)
     f.close
+    doc
+  end
+
+  private
+  def all_events_array(doc)
     event_date_nodes = scraper_helper.all_date_nodes(doc)
     event_title_nodes = scraper_helper.all_title_nodes(doc)
     event_location_nodes = scraper_helper.all_location_nodes(doc)
-    event_time_array = scraper_helper.listify_time_nodes(doc)
+    event_time_array = scraper_helper.listify_time_nodes(doc).reject(&:empty?)
+    event_time_array.zip(event_title_nodes, event_location_nodes, event_date_nodes)
+    event_time_array
+  end
 
-    #found zip and thought it was cool but now i dont know 
-    ##how to refactor this
-    event_time_array.reject(&:empty?).zip(event_title_nodes, event_location_nodes, event_date_nodes).each do|event_time, event_title_node, event_location_node, event_date_node|
-      event_location = scraper_helper.make_pretty_string(event_location_node)
-      event_title = scraper_helper.make_pretty_string(event_title_node.text)
-      event_date = scraper_helper.make_pretty_string(event_date_node)
-      Event.create(title: event_title, location: event_location, time: event_time, date: event_date
+  private
+  def save_events(doc)
+    events_aray = all_events_array(doc)
+    event_time_array.each do|event_time, event_title_node, event_location_node, event_date_node|
+      event_location = scraper_helper.strip(event_location_node)
+      event_title = scraper_helper.strip(event_title_node.text)
+      event_date = scraper_helper.strip(event_date_node)
+      Event.create(title: event_title, location: event_location, time: event_time, date: event_date)
     end
   end
 end

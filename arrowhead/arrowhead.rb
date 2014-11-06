c4a3922b9d7fd463972beedac99c9f68a2284ba3
Tyler Breland <workwith@tylerbreland.me>
2014-11-06 15:54:17 -0600

    initial commit

--- arrowhead/arrowhead.rb
+++ arrowhead/arrowhead.rb
@@ -1,33 +1,25 @@
-class Arrowhead
-  # This seriously belongs in a database.
-  CLASSIFICATIONS = {
-    far_west: {
-      notched: "Archaic Side Notch",
-      stemmed: "Archaic Stemmed",
-      lanceolate: "Agate Basin",
-      bifurcated: "Cody",
-    },
-    northern_plains: {
-      notched: "Besant",
-      stemmed: "Archaic Stemmed",
-      lanceolate: "Humboldt Constricted Base",
-      bifurcated: "Oxbow",
-    },
-  }
+require 'json'
 
-  # FIXME: I don't have time to deal with this.
+class Arrowhead
+  File.open('classifications.json', 'r') do |f|
+    CLASSIFICATIONS = JSON.load(f)
+  end
+
   def self.classify(region, shape)
-    if CLASSIFICATIONS.include? region
-      shapes = CLASSIFICATIONS[region]
-      if shapes.include? shape
-        arrowhead = shapes[shape]
-        puts "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
-      else
-        raise "Unknown shape value. Are you sure you know what you're talking about?"
-      end
-    else
-      raise "Unknown region, please provide a valid region."
-    end
+    shapes = region_shapes(region.to_s)
+    puts arrowhead(shapes, shape.to_s)
+  end
+
+  def self.region_shapes(region)
+    e = "Unknown region, please provide a valid region."
+    raise e unless CLASSIFICATIONS.include? region
+    CLASSIFICATIONS[region]
+  end
+
+  def self.arrowhead(shapes, shape)
+    e = "Unknown shape value. Are you sure you know what you're talking about?"
+    raise e unless shapes.include? shape
+    "You have a(n) '#{shapes[shape]}' arrowhead. Probably priceless."
   end
 end
 

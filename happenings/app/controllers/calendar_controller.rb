5f371e29c4c15cf6b0f5c311cec1654c5177310f
Ramya Kannan <ramyakannan@dhcp-10-224-159-57.enova.com>
2014-11-06 16:36:48 -0600

    to keep hound happy

--- happenings/app/controllers/calendar_controller.rb
+++ happenings/app/controllers/calendar_controller.rb
@@ -3,6 +3,7 @@ class CalendarController < ApplicationController
   def prepare_session
     ScraperWorker.perform_async
   end
+
   def show
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
     @events_by_date = Event.select("id, title, location, date, time")

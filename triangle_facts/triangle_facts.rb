e97951bfca112a2fcaeae7bb02406e100c7e4903
Tyler Breland <workwith@tylerbreland.me>
2014-11-07 12:33:01 -0600

    added law_of_cosines function, and removed angle methods

--- triangle_facts/triangle_facts.rb
+++ triangle_facts/triangle_facts.rb
@@ -44,37 +44,28 @@ class Triangle
     angles.include?(90)
   end
 
+  def angle_from_sides(side)
+    angle = law_of_cosines(side)
+    radians_to_degrees(Math.acos(angle))
+  end
+
   def calculate_angles
-    [angle_a, angle_b, angle_c]
-  end
-
-  def angle_a
-    x = side2**2 + side3**2 - side1**2
-    y = 2.0 * side2 * side3
-
-    radians_to_degrees(angle_radians(x, y))
-  end
-
-  def angle_b
-    x = side1**2 + side3**2 - side2**2
-    y = 2.0 * side1 * side3
-
-    radians_to_degrees(angle_radians(x, y))
-  end
-
-  def angle_c
-    x = side1**2 + side2**2 - side3**2
-    y = 2.0 * side1 * side2
-
-    radians_to_degrees(angle_radians(x, y))
+    ('a'..'c').to_a.map { |side| angle_from_sides(side) }
   end
 
   def radians_to_degrees(rads)
     (rads * 180 / Math::PI).round
   end
 
-  def angle_radians(x, y)
-    Math.acos(x / y)
+  def law_of_cosines(side)
+    case side.to_sym
+      when :a
+        (side2**2 + side3**2 - side1**2) / (2.0 * side2 * side3)
+      when :b
+        (side1**2 + side3**2 - side2**2) / (2.0 * side1 * side3)
+      when :c
+        (side1**2 + side2**2 - side3**2) / (2.0 * side1 * side2)
+    end
   end
 
   FACTS = {

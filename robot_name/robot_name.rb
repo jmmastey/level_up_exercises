f6a07a84fc1385ebec1c6bd643fcf651de74fe29
Sushanth Iringentavida <siringentavida@enova.com>
2014-11-07 11:13:06 -0600

    Refactored robot_name.rb

--- robot_name/robot_name.rb
+++ robot_name/robot_name.rb
@@ -3,30 +3,49 @@ class NameCollisionError < RuntimeError; end
 class Robot
   attr_accessor :name
 
-  @@registry
-
   def initialize(args = {})
     @@registry ||= []
     @name_generator = args[:name_generator]
+  end
 
+  def robo_name
     if @name_generator
       @name = @name_generator.call
+      repeat_check
     else
-      generate_char = -> { ('A'..'Z').to_a.sample }
-      generate_num = -> { rand(10) }
-
-      @name = "#{generate_char.call}#{generate_char.call}#{generate_num.call}#{generate_num.call}#{generate_num.call}"
+      generate_name
     end
+  end
 
-    raise NameCollisionError, 'There was a problem generating the robot name!' if !(name =~ /[[:alpha:]]{2}[[:digit:]]{3}/) || @@registry.include?(name)
+  def generate_name
+    char_name = "#{ 2.times.map { generate_char }.join }"
+    num_name = "#{ 3.times.map { generate_num }.join }"
+    @name = "#{ char_name }#{ num_name }"
+    repeat_check
+  end
+
+  def generate_char
+    ('A'..'Z').to_a.sample
+  end
+
+  def generate_num
+    rand(10)
+  end
+
+  def condition?
+    !(@name =~ /[[:alpha:]]{2}[[:digit:]]{3}/) || @@registry.include?(@name)
+  end
+
+  def repeat_check
+    fail NameCollisionError, 'There was a problem generating the robot name!' if condition?
     @@registry << @name
+    @name
   end
 end
 
 robot = Robot.new
-puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
-
+puts "My pet robot's name is #{robot.robo_name}, but we usually call him sparky."
 # Errors!
-# generator = -> { 'AA111' }
-# Robot.new(name_generator: generator)
-# Robot.new(name_generator: generator)
+generator = -> { 'AA111' }
+puts "My second robot name is #{Robot.new(name_generator: generator).robo_name}"
+puts "My third robot name is #{Robot.new(name_generator: generator).robo_name}"

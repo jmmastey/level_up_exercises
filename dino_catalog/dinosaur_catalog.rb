91bcb50d0647d336854dba15f58da597f56527c8
Srikanth Astaputhra <sastaputhra@enova.com>
2014-11-07 16:09:28 -0600

    refactored dino

--- dino_catalog/dinosaur_catalog.rb
+++ dino_catalog/dinosaur_catalog.rb
@@ -1,61 +1,69 @@
 require 'csv'
 require 'json'
 
-class Dinosaur
+class DinosaurParserAndSearch
   attr_accessor :result_dinosaurs
   def initialize
-    @dinosaurs = []
+    @csv_list_dinosaurs = []
     @result_dinosaurs = []
     CSV.foreach('dinodex.csv', :headers => true) do |row|
-      @dinosaurs << row.to_hash
+      @csv_list_dinosaurs << row.to_hash
     end
+    @result_dinosaurs =  @csv_list_dinosaurs
   end
 
   def get_name
     dinosaur_names = []
-    @result_dinosaurs.each do |dinosaur|
-      dinosaur_names << dinosaur["NAME"]
-    end
+    @result_dinosaurs.each{ |dinosaur| dinosaur_names << dinosaur["NAME"] }
     dinosaur_names
   end
 
-  def search_dinosaur(options)
-    @dinosaurs = @result_dinosaurs unless @result_dinosaurs.empty?
-    @result_dinosaurs = []
-    options.each do |key, val|
-      next if key == "compare"
-      @dinosaurs.each do |dinosaur|
-        if dinosaur["PERIOD"].split(' ').length == 2
-          dinosaur["PERIOD"] = dinosaur["PERIOD"].split(' ')[1]
-        end
-        if options["compare"] == "greater"
-          if dinosaur[key].to_i > val.to_i
-            @result_dinosaurs << dinosaur unless
-                                            @result_dinosaurs.include? dinosaur['NAME']
+  def smart_search_dinosaur(options)
+    case options["compare"]
+    when "greater"
+      idx = -1
+      search_dinosaur(options, "greater")
+    when "lesser"
+      search_dinosaur(options, "lesser")
+    when "equal"
+      search_dinosaur(options, "equal")
+    end
+
+    self
+  end
+
+  def search_dinosaur(options, symbol)
+    idx = -1
+    resultant = []
+    @result_dinosaurs.each do |dinosaur|
+      idx += 1
+
+      dinosaur.each do |key, value|
+        if options.has_key?(key) && options[key]
+          if symbol == "greater" && options[key] <= value.to_i
+            @result_dinosaurs.delete_at idx
+            idx -= 1
           end
-        elsif options["compare"] == "lesser"
-          if dinosaur[key].to_i < val.to_i
-            @result_dinosaurs << dinosaur unless
-                                            @result_dinosaurs.include? dinosaur['NAME']
+          if symbol == "lesser" && options[key] >= value.to_i
+            @result_dinosaurs.delete_at idx
+            idx -= 1
           end
-        else
-          if val.is_a? (Array)
-            if val.include? dinosaur[key].downcase
-              @result_dinosaurs << dinosaur unless
-                                              @result_dinosaurs.include? dinosaur['NAME']
+          if symbol == "equal"
+            if options[key].is_a?(Array) && !options[key].include?(dinosaur[key].downcase)
+              @result_dinosaurs.delete_at idx
+              idx -= 1
+            elsif options[key] != dinosaur[key].downcase
+              @result_dinosaurs.delete_at idx
+              idx -= 1
             end
-          elsif dinosaur[key].downcase == val
-            @result_dinosaurs << dinosaur unless
-                                            @result_dinosaurs.include? dinosaur['NAME']
           end
         end
       end
     end
-    self
   end
 
   def get_description(name)
-    @dinosaurs.each do |dinosaur|
+    @search_list_dinosaurs.each do |dinosaur|
       if dinosaur["NAME"].downcase == name.downcase
         dinosaur.each do |key, value|
           puts key + " => " + value unless value == nil
@@ -67,7 +75,7 @@ class Dinosaur
   def as_json
     hash_json ||= {}
     i = 0
-    @dinosaurs.each do |dinosaur|
+    @search_list_dinosaurs.each do |dinosaur|
       dinosaur.each do |key, value|
         hash_json[i] ||= {}
         hash_json[i][key] = value
@@ -82,21 +90,22 @@ options = Hash.new
 options1 = Hash.new
 options2 = Hash.new
 
-options['WALKING'] = "biped"
-options['compare'] = "equal"
-dinosaur = Dinosaur.new
+ options['WEIGHT_IN_LBS'] = 2000
+ options['compare'] = "greater"
+ dinosaur = DinosaurParserAndSearch.new
 
-dinosaur1 = Dinosaur.new
+ result = dinosaur.smart_search_dinosaur(options)
+ p result.get_name.inspect
 
-options1['DIET'] = ['carnivore', 'insectivore']
-options1['compare'] = "equal"
-result = dinosaur.search_dinosaur(options1)
-p result.get_name.inspect
+ dinosaur1 = DinosaurParserAndSearch.new
 
-options2['WEIGHT_IN_LBS'] = 2000
-options2['compare'] = "greater"
-result = dinosaur.search_dinosaur(options2).search_dinosaur(options1)
-p result.get_name.inspect
 
-result = dinosaur1.search_dinosaur(options2)
-p result.get_name.inspect
+# result = dinosaur1.smart_search_dinosaur(options1)
+# p result.get_name.inspect
+
+options2['DIET'] = ['carnivore', 'insectivore']
+options2['compare'] = "equal"
+#p result.get_name.inspect
+
+result1 = dinosaur1.smart_search_dinosaur(options2)
+p result1.get_name.inspect

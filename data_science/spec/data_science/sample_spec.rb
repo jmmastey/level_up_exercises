180c2e0de87be44711c6e805a150c4a78e41f1b0
paulh16 <paulh16@gmail.com>
2014-11-10 14:26:52 -0600

    Add Factory Girl

--- data_science/spec/data_science/sample_spec.rb
+++ data_science/spec/data_science/sample_spec.rb
@@ -14,4 +14,12 @@ describe Sample do
     @sample.add_data_to_sample(@sample_data)
     expect(@sample.data_points.size).to eq(2)
   end
+
+  context 'calculate sample statistics'
+    before do
+
+    end
+    it 'calculates the total sample size' do
+
+    end
 end

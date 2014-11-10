be076a9373e9c2ac564f6c3bc9c3158a008a8dcf
rcowan <ryancowan@gmail.com>
2014-11-10 12:58:53 -0600

    split out custom styles and scripts, further cleanup and refactoring to split out context from styling and functionality

---
+++ pmail/scripts/pmail.js
@@ -0,0 +1,9 @@
+//show the user profile popup box
+function showUserProfile() {
+  $("#popup-user-profile-container").show();
+}
+
+//hide the user profile popup box
+function hideUserProfile() {
+  $("#popup-user-profile-container").hide();
+}

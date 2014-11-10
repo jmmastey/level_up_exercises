6d9247a17b74396528010c18f6399126de3c09e1
Ozer Chagatai <ochagata@gmail.com>
2014-11-10 07:58:32 -0600

    Initial implementation of Data Science

--- data_science/data_box.rb
+++ data_science/data_box.rb
@@ -1,6 +1,6 @@
 require 'abanalyzer'
-
-class Dataset
+class DataBox
+  # 1.96 is a confidence level of approx. 95%
   CONFIDENCE_LEVEL = 1.96
   PROBABILITY_THRESHOLD = 0.05
 
@@ -8,7 +8,7 @@ class Dataset
     @results = data
   end
 
-  def total_sample_size
+  def sample_size
     @results.length
   end
 
@@ -16,18 +16,19 @@ class Dataset
     @results.count { |result| result["cohort"] == cohort }
   end
 
-  def number_of_conversions(cohort)
-    @results.count { |result| result["cohort"] == cohort && result["result"] == 1 }
+  def amount_of_conversions(cohort)
+    @results.count do |result|
+      result["cohort"] == cohort && result["result"] == 1
+    end
   end
 
   def percentage_of_conversion(cohort)
-    number_of_conversions(cohort).to_f / total_in_group(cohort).to_f
+    amount_of_conversions(cohort).to_f / total_in_group(cohort).to_f
   end
 
   def calculate_standard_error(cohort)
     p = percentage_of_conversion(cohort)
     n = total_in_group(cohort)
-
     ((Math.sqrt(p * (1 - p) / n)) * CONFIDENCE_LEVEL)
   end
 
@@ -37,24 +38,20 @@ class Dataset
 
   def calculate_group_probabilities
     groups = {}
-
     all_group_types.each do |group|
-      clicks = number_of_conversions(group)
+      clicks = amount_of_conversions(group)
       total = total_in_group(group)
 
       groups[group] = { success: clicks, failure: (total - clicks) }
     end
-
     ABAnalyzer::ABTest.new(groups).chisquare_p
   end
 
   def cohort_percentages
     percentages = {}
-
     all_group_types.each do |group|
       percentages[group] = percentage_of_conversion(group)
     end
-
     Hash[percentages.sort_by { |_, percent| percent }]
   end
 
@@ -62,7 +59,7 @@ class Dataset
     if calculate_group_probabilities >= PROBABILITY_THRESHOLD
       "No clear winner"
     else
-      "Cohort #{cohort_percentages.to_a.last.first} is the winner"
+      "Winner: Cohort #{cohort_percentages.to_a.last.first}"
     end
   end
 end

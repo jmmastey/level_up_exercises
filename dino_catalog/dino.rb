b156e54648dd581b8ebf6d2d204349fe3854d184
Tyler Breland <workwith@tylerbreland.me>
2014-11-10 15:23:07 -0600

    updated init logic, removed carnivore accessor

--- dino_catalog/dino.rb
+++ dino_catalog/dino.rb
@@ -1,55 +1,24 @@
 class Dinosaur
-  attr_accessor :name, :period, :continent, :diet, :weight_in_lbs, :walking
-  attr_accessor :description, :carnivore
+  attr_accessor :name, :period, :continent, :diet, :weight_in_lbs, :walking,
+    :description
 
   def initialize(dino_hash)
-    data = {}
-    dino_keys = {
-      name:          nil,
-      period:        nil,
-      continent:     nil,
-      diet:          nil,
-      weight_in_lbs: nil,
-      walking:       nil,
-      description:   nil,
-    }
-    keys = dino_keys.keys
-
-    dino_hash.each do |key, val|
-      data[key] = val if keys.include? key
-    end
-
-    @name          = data[:name]
-    @period        = data[:period]
-    @continent     = data[:continent]
-    @diet          = data[:diet]
-    @weight_in_lbs = data[:weight_in_lbs]
-    @walking       = data[:walking]
-    @description   = data[:description]
-    @carnivore     = carnivore?
+    @name          = dino_hash[:name]
+    @period        = dino_hash[:period]
+    @continent     = dino_hash[:continent]
+    @diet          = dino_hash[:diet]
+    @weight_in_lbs = dino_hash[:weight_in_lbs]
+    @walking       = dino_hash[:walking]
+    @description   = dino_hash[:description]
   end
 
-  def to_hash
-    hash = {}
-    instance_variables.each do |var|
+  def to_h
+    instance_variables.inject({}) do |hash, var|
       hash[var.to_s.delete("@")] = instance_variable_get(var)
     end
-    hash
   end
 
   def to_json
-    to_hash.to_json
-  end
-
-  private
-
-  def carnivore?
-    carnivores = [
-      'Carnivore', # obvs
-      'Insectivore',
-      'Piscivore',
-    ]
-
-    !carnivores.index(@diet).nil?
+    to_h.to_json
   end
 end

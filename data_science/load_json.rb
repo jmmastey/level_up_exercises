eb17e0e36406ecb8766d7c7f754519deea985a0f
Tyler Breland <workwith@tylerbreland.me>
2014-11-11 14:34:04 -0600

    better file loader

--- data_science/load_json.rb
+++ data_science/load_json.rb
@@ -1,21 +1,13 @@
 require "json"
 
 class LoadJSON
-  attr_accessor :file_name
+  attr_accessor :file_path
 
   def initialize(file_name)
-    @file_name = file_name
+    @file_path = File.expand_path(file_name)
   end
 
   def parse
-    JSON.parse(File.read(path))
-  end
-
-  def path
-    File.expand_path(file_name)
-  end
-
-  def exist?
-    File.exist?(path)
+    JSON.parse(File.read(file_path))
   end
 end
--- data_science/load_json_spec.rb
+++ data_science/load_json_spec.rb
@@ -5,11 +5,11 @@ describe LoadJSON do
   let(:source_data) { LoadJSON.new("source_data.json") }
 
   it "can locate source file" do
-    expect(source_data.exist?).to be(true)
+    expect(File.exist?(source_data.file_path)).to be(true)
   end
 
   it "loads from valid JSON filename" do
-    expect(source_data.file_name).to end_with(".json")
+    expect(source_data.file_path).to end_with(".json")
   end
 
   it "loads data from a data source" do

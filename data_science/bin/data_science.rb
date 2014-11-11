b7e0bb3518a57093fcc2b9453e4481580371bcb3
paulh16 <paulh16@gmail.com>
2014-11-11 13:00:19 -0600

    Print statistical results

--- data_science/bin/data_science.rb
+++ data_science/bin/data_science.rb
@@ -1,4 +1,5 @@
 require_relative '../lib/data_science/conversion_test'
+require_relative '../lib/data_science/json_parser'
 
 conversion_test = ConversionTest.new("A_B_Test")
 
@@ -6,4 +7,6 @@ data_file = ARGV.shift || File.join(File.dirname(__FILE__), "source_data.json")
 
 json_data = JsonParser.parse_file(data_file)
 
-conversion_test.import_data(json_data)
+conversion_test.import_json_data(json_data)
+
+conversion_test.print_statistical_results("A", "B")
--- data_science/lib/data_science/conversion_test.rb
+++ data_science/lib/data_science/conversion_test.rb
@@ -1,4 +1,5 @@
 require 'json'
+require_relative 'sample'
 
 class ConversionTest
   attr_reader :name, :sample
@@ -10,5 +11,16 @@ class ConversionTest
 
   def import_json_data(json_data)
     @sample.add_data_to_sample(json_data)
+  end
+
+  def print_statistical_results(group_1, group_2)
+    puts "\nBelow are the results of the web conversion data:\n\n"
+    puts "Total Size of the Control Group: #{@sample.cohort_size(group_1)}"
+    puts "Number of Conversions for the Control Group: #{@sample.conversions(group_1)}\n\n"
+    puts "Total Size of the Test Group: #{@sample.cohort_size(group_2)}"
+    puts "Number of Conversions for the Test Group: #{@sample.conversions(group_2)}\n\n"
+    puts "Conversion rate for the Control Group: #{@sample.conversion_rate(group_1)} +- #{@sample.error_bars(group_1)*100}%"
+    puts "Conversion rate for the Test Group: #{@sample.conversion_rate(group_2)} +- #{@sample.error_bars(group_2)*100}%\n\n"
+    puts "Confidence Level: #{@sample.confidence_level(group_1, group_2)}"
   end
 end

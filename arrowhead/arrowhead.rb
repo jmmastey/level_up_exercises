d40c3be24ae9d389bc2fc9d400816dbda72abf37
msabdeljalil <msabdeljalil@gmail.com>
2014-11-11 11:27:42 -0600

    V2 After code review

--- arrowhead/arrowhead.rb
+++ arrowhead/arrowhead.rb
@@ -1,4 +1,17 @@
 class Arrowhead
+
+  class InvalidRegionError < RuntimeError
+    def to_s
+      "Unknown region, please provide a valid region."
+    end
+  end
+
+  class InvalidShapeError < RuntimeError
+    def to_s
+      "Unknown shape value. Are you sure you know what you're talking about?"
+    end
+  end
+
   # This seriously belongs in a database.
   CLASSIFICATIONS = {
     far_west: {
@@ -15,25 +28,19 @@ class Arrowhead
     },
   }
 
-  def self.classify(region, shape)
-    regional_shapes = shapes_in_region(region)
-    classify_by_shape(regional_shapes, shape)
-  end
-
-  def self.shapes_in_region(region)
-    if CLASSIFICATIONS.include? region
-      return CLASSIFICATIONS[region]
-    else
-      raise "Unknown region, please provide a valid region."
+  def self.classify(target_region, target_shape)
+    shapes = classify_region(target_region)
+    classify_shape(shapes, target_shape).tap do |this|
+      puts "You have a(n) '#{this}' arrowhead. Probably priceless."
     end
   end
 
-  def self.classify_by_shape(shapes, shape)
-    if shapes.include? shape
-      return "You have a(n) '#{shapes[shape]}' arrowhead. Probably priceless."
-    else
-      raise "Unknown shape value. Are you sure you know what you're talking about?"
-    end
+  def self.classify_region(region)
+    CLASSIFICATIONS.fetch(region) { raise InvalidRegionError }
+  end
+
+  def self.classify_shape(shapes, target_shape)
+    shapes.fetch(target_shape) { raise InvalidShapeError }
   end
 end
 

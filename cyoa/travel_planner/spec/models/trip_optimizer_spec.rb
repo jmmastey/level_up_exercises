b0ddad80f80e1a72c4706ddf7785a80263a5e5ca
Lawrence Walters <lawrencewalters@users.noreply.github.com>
2014-11-13 08:18:41 -0600

    rubocop format: hash inputs don't need curly braces

--- cyoa/travel_planner/spec/models/trip_optimizer_spec.rb
+++ cyoa/travel_planner/spec/models/trip_optimizer_spec.rb
@@ -4,12 +4,12 @@ require_relative '../../app/models/trip_optimizer'
 describe 'TripOptimizer', :vcr do
   context 'Shortest Trip' do
     subject(:optimizer) do
-      TripOptimizer.new({
-        from: "ORD",
-        to: "LGA",
-        meeting_start: "2014-11-30T12:15:00.000".to_datetime,
-        meeting_length: 1.5
-      })
+      TripOptimizer.new(
+        from:           "ORD",
+        to:             "LGA",
+        meeting_start:  "2014-11-30T12:15:00.000".to_datetime,
+        meeting_length: 1.5,
+      )
     end
 
     it 'given travel details, pick two flights' do

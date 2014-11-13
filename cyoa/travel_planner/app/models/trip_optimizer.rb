972054c31154fa28ad3d242e1bfbb9e4fe1f39b5
Lawrence Walters <lawrencewalters@users.noreply.github.com>
2014-11-13 09:20:04 -0600

    refactoring to make prety

--- cyoa/travel_planner/app/models/trip_optimizer.rb
+++ cyoa/travel_planner/app/models/trip_optimizer.rb
@@ -2,21 +2,60 @@ require 'flight_stats/schedule'
 require 'active_support/all'
 
 class TripOptimizer
-  attr_accessor :from, :to, :meeting_start, :meeting_length
+  attr_accessor :from,
+                :to,
+                :meeting_start,
+                :meeting_length,
+                :all_departures,
+                :all_returns,
+                :departure,
+                :return
 
   def initialize(options={})
-    @from = options[:from]
-    @to = options[:to]
-    @meeting_start = options[:meeting_start]
+    @from           = options[:from]
+    @to             = options[:to]
+    @meeting_start  = options[:meeting_start]
     @meeting_length = options[:meeting_length]
+    @schedule        = FlightStats::Schedule.new
   end
 
   def pick_shortest_flights
-    schedule = FlightStats::Schedule.new
-    arriving = schedule.get_flights_arriving_before(@meeting_start, @from, @to)
-    departing = schedule.get_flights_departing_after(meeting_end, @to, @from)
+    @all_departures = get_departures(@meeting_start, @from, @to)
+    @all_returns = get_returns(meeting_end, @to, @from)
 
-    [arriving[0], departing[0]]
+    @departure = get_latest_departure(@all_departures)
+    @return = get_earliest_arrival(@all_returns)
+
+    get_departure_and_return
+  end
+
+  private
+
+  def get_departures(meeting_start, from, to)
+    @schedule.get_flights_arriving_before(meeting_start, from, to)
+  end
+
+  def get_returns(meeting_end, from, to)
+    @schedule.get_flights_departing_after(meeting_end, from, to)
+  end
+
+  def get_departure_and_return
+    {
+      departure: @departure,
+      return:    @return
+    }
+  end
+
+  def get_earliest_arrival(flights)
+    flights.min_by do |f|
+      f["arrivalTimeUtc"].to_datetime
+    end
+  end
+
+  def get_latest_departure(flights)
+    flights.max_by do |f|
+      f["departureTimeUtc"].to_datetime
+    end
   end
 
   def meeting_end
--- cyoa/travel_planner/spec/models/trip_optimizer_spec.rb
+++ cyoa/travel_planner/spec/models/trip_optimizer_spec.rb
@@ -15,5 +15,31 @@ describe 'TripOptimizer', :vcr do
     it 'given travel details, pick two flights' do
       expect(optimizer.pick_shortest_flights.length).to eq(2)
     end
+
+    it 'makes all departing flights available' do
+      optimizer.pick_shortest_flights
+      expect(optimizer.all_departures.length).to be > 0
+    end
+
+    it 'makes all returning flights available' do
+      optimizer.pick_shortest_flights
+      expect(optimizer.all_returns.length).to be > 0
+    end
+
+    it 'picks the best departing flight' do
+      optimizer.pick_shortest_flights
+      best = optimizer.all_departures.max_by do |f|
+        f["departureTimeUtc"].to_datetime
+      end
+      expect(optimizer.departure).to eq(best)
+    end
+
+    it 'picks the best returning flight' do
+      optimizer.pick_shortest_flights
+      best = optimizer.all_returns.min_by do |f|
+        f["arrivalTimeUtc"].to_datetime
+      end
+      expect(optimizer.return).to eq(best)
+    end
   end
 end

9e694d5b97b6e7037ffaa01199787c9ef93a6b4f
Lawrence Walters <lawrencewalters@users.noreply.github.com>
2014-11-13 22:20:12 -0600

    factory_girl fixes for associated objects - valid Trip!

--- cyoa/travel_planner/spec/factories/meetings.rb
+++ cyoa/travel_planner/spec/factories/meetings.rb
@@ -1,9 +1,15 @@
 require "faker"
 
 FactoryGirl.define do
-  factory :meeting do |f|
-    f.start { Faker::Time.forward(3, :morning) }
-    f.length 1.5
+  factory :meeting do
+    start { Faker::Time.forward(3, :morning) }
+    length 1.5
     location
+  end
+
+  factory :meeting_lga, class: Meeting do
+    start { Faker::Time.forward(3, :morning) }
+    length 1.5
+    association :location, factory: :laguardia_location
   end
 end
--- cyoa/travel_planner/spec/factories/trips.rb
+++ cyoa/travel_planner/spec/factories/trips.rb
@@ -4,4 +4,9 @@ FactoryGirl.define do
   factory :trip do
     association :home_location, factory: :location
   end
+
+  factory :lga_trip, class: Trip do
+    association :home_location, factory: :laguardia_location
+    meetings {[FactoryGirl.create(:meeting_lga)]}
+  end
 end

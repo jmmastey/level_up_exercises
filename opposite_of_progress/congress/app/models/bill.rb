8fed2e8d75fd4fcfac557da74f000bd6bdc7570f
rcowan <ryancowan@gmail.com>
2014-11-17 15:18:18 -0600

    clean up bill and legislator fetch methods, minor update to simplify bill partial

--- opposite_of_progress/congress/app/models/bill.rb
+++ opposite_of_progress/congress/app/models/bill.rb
@@ -12,19 +12,7 @@ class Bill < ActiveRecord::Base
       bill = Bill.where(bill_id: result['bill_id'])
 
       if bill.count == 0
-        Bill.create(bill_id: result['bill_id'],
-                    bill_type: result['bill_type'],
-                    number: result['number'],
-                    congress: result['congress'],
-                    chamber: result['chamber'],
-                    introduced_on: result['introduced_on'],
-                    last_action_at: result['last_action_at'],
-                    last_vote_at: result['last_vote_at'],
-                    last_version_on: result['last_version_on'],
-                    official_title: result['official_title'],
-                    short_title: result['short_title'],
-                    sponsor_id: result['sponsor_id'],
-                    enacted_at: result['history']['enacted_at'])
+        Bill.create(build_object_hash(result))
       else
         bill = bill.first
 
@@ -32,20 +20,27 @@ class Bill < ActiveRecord::Base
         new_hash = ApplicationHelper::to_md5_hash result, Bill.new
 
         unless old_hash == new_hash
-          Bill.update(bill.id, bill_type: result['bill_type'],
-                      number: result['number'],
-                      congress: result['congress'],
-                      chamber: result['chamber'],
-                      introduced_on: result['introduced_on'],
-                      last_action_at: result['last_action_at'],
-                      last_vote_at: result['last_vote_at'],
-                      last_version_on: result['last_version_on'],
-                      official_title: result['official_title'],
-                      short_title: result['short_title'],
-                      sponsor_id: result['sponsor_id'],
-                      enacted_at: result['history']['enacted_at'])
+          Bill.update(bill.id, build_object_hash(result))
         end
       end
     end
+  end
+
+  def build_object_hash result
+    {
+      bill_id: result['bill_id'],
+      bill_type: result['bill_type'],
+      number: result['number'],
+      congress: result['congress'],
+      chamber: result['chamber'],
+      introduced_on: result['introduced_on'],
+      last_action_at: result['last_action_at'],
+      last_vote_at: result['last_vote_at'],
+      last_version_on: result['last_version_on'],
+      official_title: result['official_title'],
+      short_title: result['short_title'],
+      sponsor_id: result['sponsor_id'],
+      enacted_at: result['history']['enacted_at']
+    }
   end
 end
--- opposite_of_progress/congress/app/models/legislator.rb
+++ opposite_of_progress/congress/app/models/legislator.rb
@@ -19,25 +19,7 @@ class Legislator < ActiveRecord::Base
       legislator = Legislator.where(bioguide_id: result['bioguide_id'])
 
       if legislator.count == 0
-        Legislator.create(bioguide_id: result['bioguide_id'],
-                          birthday: result['birthday'],
-                          chamber: result['chamber'],
-                          party: result['party'],
-                          title: result['title'],
-                          term_start: result['term_start'],
-                          term_end: result['term_end'],
-                          gender: result['gender'],
-                          first_name: result['first_name'],
-                          nickname: result['nickname'],
-                          middle_name: result['middle_name'],
-                          last_name: result['last_name'],
-                          state: result['state'],
-                          twitter_id: result['twitter_id'],
-                          facebook_id: result['facebook_id'],
-                          phone: result['phone'],
-                          website: result['website'],
-                          office: result['office'],
-                          contact_form: result['contact_form'])
+        Legislator.create(build_object_hash(result))
       else
         legislator = legislator.first
 
@@ -45,27 +27,33 @@ class Legislator < ActiveRecord::Base
         new_hash = ApplicationHelper::to_md5_hash result, Legislator.new
 
         unless old_hash == new_hash
-          Legislator.update(legislator.id, 
-                            birthday: result['birthday'],
-                            chamber: result['chamber'],
-                            party: result['party'],
-                            title: result['title'],
-                            term_start: result['term_start'],
-                            term_end: result['term_end'],
-                            gender: result['gender'],
-                            first_name: result['first_name'],
-                            nickname: result['nickname'],
-                            middle_name: result['middle_name'],
-                            last_name: result['last_name'],
-                            state: result['state'],
-                            twitter_id: result['twitter_id'],
-                            facebook_id: result['facebook_id'],
-                            phone: result['phone'],
-                            website: result['website'],
-                            office: result['office'],
-                            contact_form: result['contact_form'])
+          Legislator.update(legislator.id, build_object_hash(result))
         end
       end
     end
+  end
+
+  def build_object_hash result
+    {
+      bioguide_id: result['bioguide_id'],
+      birthday: result['birthday'],
+      chamber: result['chamber'],
+      party: result['party'],
+      title: result['title'],
+      term_start: result['term_start'],
+      term_end: result['term_end'],
+      gender: result['gender'],
+      first_name: result['first_name'],
+      nickname: result['nickname'],
+      middle_name: result['middle_name'],
+      last_name: result['last_name'],
+      state: result['state'],
+      twitter_id: result['twitter_id'],
+      facebook_id: result['facebook_id'],
+      phone: result['phone'],
+      website: result['website'],
+      office: result['office'],
+      contact_form: result['contact_form']
+    }
   end
 end

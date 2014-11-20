f357e25dc47c4ab88977efe51164d1d3e6c62efc
rcowan <ryancowan@gmail.com>
2014-11-20 14:14:09 -0600

    flesh out bills and legislators api tests

--- opposite_of_progress/congress/spec/requests/api/bills_spec.rb
+++ opposite_of_progress/congress/spec/requests/api/bills_spec.rb
@@ -1,9 +1,35 @@
 require 'spec_helper'
 
 describe "Bills API", :type => :request do
-  it "retrieves the data of a specific bill" do
-    bill = FactoryGirl.create(:bill)
-    get "/bills/#{bill.id}.json"
-    expect(response).to be_success
+  before :all do
+    @bill = FactoryGirl.create(:bill)
+  end
+
+  describe "has a functional bill detail page" do
+    before :all do
+      get "/bills/#{@bill.id}.json"
+    end
+
+    it "retrieves the bill detail page" do
+      expect(response).to be_success
+    end
+
+    it "retrieves data of a specific bill" do
+      expect(response.body).to include(@bill.bill_id)
+    end
+  end
+
+  describe "has a functional bill list page" do
+    before :all do
+      get "/bills.json"
+    end
+
+    it "retrieves the bill list page" do
+      expect(response).to be_success
+    end
+
+    it "retrieves data of a bill from a list page" do
+      expect(response.body).to include(@bill.bill_id)
+    end
   end
 end

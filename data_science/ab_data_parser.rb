58a0432d6ac9b0814568376473bab35458344194
zachary-huang <chuang@enova.com>
2014-11-20 16:23:36 -0600

    update ab_data_parser.rb

--- data_science/ab_data_parser.rb
+++ data_science/ab_data_parser.rb
@@ -12,16 +12,17 @@ module ABDataParser
 
   def self.parse_json_obj(json_obj)
     json_obj = JSON.parse(json_obj)
-    groups = json_obj.group_by { |record| record[@variant_key.to_s] }.values
-    groups.each { |variant| compute_data(variant) }
+    #groups = json_obj.group_by { |record| record[@variant_key.to_s] }.values
+    groups = json_obj.partition{ |item| item[@variant_key] == 'A' }
+    groups.each { |variant| populate_data(variant) }
     rescue JSON::ParserError
       raise "unable to parse the given json data"
   end
 
-  def self.compute_data(variant)
-    variant_name = variant[0][@variant_key.to_s].to_sym
-    total_samples = variant.length
-    conversions = variant.select { |row| row[@result_key.to_s] == 1 }.length
+  def self.populate_data(data)
+    variant_name = data.first[@variant_key.to_s].to_sym
+    total_samples = data.length
+    conversions = data.count { |row| row[@result_key.to_s] == 1 }
     @ab_test_data[variant_name] = { total_samples: total_samples,
                                     conversions: conversions }
   end

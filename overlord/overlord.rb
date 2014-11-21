c2d82243b672d05ef253e2e9258a2dd86fc8e882
Tyler Breland <workwith@tylerbreland.me>
2014-11-20 20:35:21 -0600

    working app

--- overlord/overlord.rb
+++ overlord/overlord.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'haml'
 require 'json'
+# require 'run_later'
 
 class Overlord < Sinatra::Base
   configure do
@@ -10,12 +11,37 @@ class Overlord < Sinatra::Base
     set :session_secret, "http://open.spotify.com/track/1hR0fIFK2qRG3f3RF70pb7"
   end
 
+  before do
+    session[:bomb_state] ||= 'inactive'
+    session[:countdown_active] ||= false
+    session[:codes] ||= {
+      :activation => ['1234'],
+      :deactivation => ['0000'],
+    }
+
+    # if the zero hour has past, set bomb_state to 'detonated'
+    begin
+      if session[:detonation][:zero_hour] < Time.now
+        session[:bomb_state] = 'detonated'
+        session[:countdown_active] = false
+        session[:detonation] = nil
+      else
+        session[:countdown_active] = true
+      end
+    rescue => e
+      #
+    end
+  end
+
   # www methods
 
   get '/' do
-    session[:bomb_state] ||= 'inactive'
-    @bomb_state = 'Bomb is ' + session[:bomb_state]
     haml :index
+  end
+
+  get '/new' do
+    session.clear
+    redirect '/'
   end
 
   # api methods
@@ -25,7 +51,28 @@ class Overlord < Sinatra::Base
   end
 
   get '/api/state' do
-    do_json(:session => session.to_hash)
+    do_json({
+      :bomb_state => session[:bomb_state],
+      :detonation => session[:detonation],
+    })
+  end
+
+  get '/api/zero_hour' do
+    do_json(:zero_hour => session[:detonation][:zero_hour])
+  end
+
+  post '/api/custom_code' do
+    register_for = params[:register_for]
+    code = params[:code]
+
+    if register_for != 'activation' && register_for != 'deactivation'
+      do_json(:error => 'Cannot set code for method "' + register_for + '"')
+    elsif session[:codes][register_for.to_sym].include?(code)
+      do_json(:error => 'That registration code already exists')
+    else
+      session[:codes][register_for.to_sym] << code
+      do_json(:success => true)
+    end
   end
 
   post '/api/state' do
@@ -33,20 +80,28 @@ class Overlord < Sinatra::Base
     code = params[:pass]
     state = params[:state]
 
-    session[:bomb_state] = 'active' if CODES[:activation].include?(code)
-    session[:bomb_state] = 'inactive' if CODES[:deactivation].include?(code)
+    session[:bomb_state] = 'active' if session[:codes][:activation].include?(code)
+    if session[:codes][:deactivation].include?(code)
+      session[:bomb_state] = 'inactive'
+      session.delete(:detonation)
+    end
+    do_json(:bomb_state => session[:bomb_state])
+  end
 
-    do_json(:session => session.to_hash)
+  post '/api/detonate' do
+    now = Time.now
+    wait_seconds = params[:wait].to_i
+    session[:detonation] = {
+      :zero_hour => now + wait_seconds,
+      :countdown_start => now
+    }
+
+    do_json(session[:detonation])
   end
 
   private
 
   def do_json(data)
-    { :data => data }.to_json
+    data.to_json
   end
-
-  CODES = {
-    :activation => ['1234'],
-    :deactivation => ['0000'],
-  }
 end
--- overlord/public/js/src/main.js
+++ overlord/public/js/src/main.js
@@ -1,5 +1,5 @@
 var $ = require('jquery');
-
+var moment = require('moment');
 var JSConsole = require('./components/console');
 
 
@@ -8,15 +8,23 @@ $(function() {
     commands: commands()
   });
 
-  getState.call(jsConsole).done(function (res) {
-    jsConsole.log("\nBomb state: " + res.data.session.bomb_state + "\n");
+  getState.call(jsConsole).then(function (data, status, res) {
+    jsConsole.log("\nBomb state: " + data.bomb_state);
+
+    if (!data.detonation) {
+      return $.Deferred().resolve(data, status, res);
+    }
+
+    return detonationCountdown.call(jsConsole, data.detonation, status, res).then(function () {
+      return getState.call(jsConsole);
+    }).then(function (data) {
+      jsConsole.log("\nBomb detonated");
+    });
   });
 
-  $('.js-console-submit').on('click', function () {
-    jsConsole.submit_query();
-  });
 
-  $('[data-js="fullscreen-console"]').on('click', function () {
+  $('[data-js="fullscreen-console"]').on('click', function (e) {
+    e.preventDefault();
     $('.desk').toggleClass('active');
   });
 
@@ -34,65 +42,152 @@ function commands () {
   return [{
     name: 'whoami',
     desc: 'outputs current user',
+    help: 'outputs current user',
     callback: function () {
       this.log("Overlord\n");
     }
   }, {
     name: 'state',
     desc: 'get the bomb\'s current state',
+    help: 'get the bomb\'s current state',
     callback: function () {
       var self = this;
       getState.call(this).done(function (res) {
-         self.log("\n" + res.data.session.bomb_state + "\n");
+         self.log("\n" + res.bomb_state + "\n");
       });
     }
   }, {
     name: 'activate',
     desc: 'activate the bomb',
-    help: 'activate the bomb with a password\nUsage: activate 1234',
+    help: 'activate the bomb with a password\nUsage: activate [password-string]',
     callback: updateState('active')
   }, {
     name: 'deactivate',
     desc: 'deactivate the bomb',
-    help: 'deactivate the bomb with a password\nUsage: deactivate 0000',
+    help: 'deactivate the bomb with a password\nUsage: deactivate [password-string]',
     callback: updateState('inactive')
   }, {
     name: 'detonate',
     desc: 'detonate the bomb',
-    help: 'deactivate the bomb in <n> seconds\nUsage: detonate 10',
-    callback: detonate
+    help: 'deactivate the bomb in <n> seconds\nUsage: detonate [countdown-seconds]',
+    callback: detonationSequence
+  }, {
+    name: 'register',
+    desc: 'register custom activation/deactivation codes',
+    help: 'register custom activation/deactivation codes\nUsage: register [activation|deactivation] [password-string]',
+    callback: registerNewCodes
   }];
 }
 
+function registerNewCodes (args) {
+  var jsConsole = this;
+  var deferred = $.Deferred();
+  var loader = dotLoader(function (dots) {
+    jsConsole.output_elm.append(dots);
+  });
+  var params = {
+    register_for: args[0],
+    code: args[1]
+  };
+
+  $.post('/api/custom_code', params).then(function (data, status, res) {
+    if (!data || data.error) {
+      if (data.error) jsConsole.log("\n" + data.error);
+      return deferred.reject(res);
+    }
+    jsConsole.log("\nNew " + params.register_for + " code registered");
+  }).fail(function () {
+    jsConsole.log("\nERROR: could not register code");
+  }).always(function (argument) {
+    clearInterval(loader);
+  });
+}
+
 /**
- * detonate bomb callback
+ * callback for 'detonate' command
+ * @param  {Array} args
+ */
+function detonationSequence (args) {
+  var jsConsole = this;
+  var deferred = $.Deferred();
+  var loader = dotLoader(function (dots) {
+    jsConsole.output_elm.append(dots);
+  });
+
+  getState.call(jsConsole).then(function (data, status, res) {
+    clearTimeout(loader);
+    if (!data.bomb_state || data.bomb_state == 'inactive') {
+      return deferred.reject(res, "inactive");
+    }
+    return $.post('/api/detonate', {wait: args[0] || 3});
+  }).then(function (data, status, res) {
+    return detonationCountdown.call(jsConsole, data, status, res);
+  }).then(function () {
+    return getState.call(jsConsole);
+  }).then(function (data) {
+    jsConsole.log("\nBomb detonated");
+  }).fail(function (e) {
+    jsConsole.log("\nERROR: can't launch bomb");
+  });
+}
+
+/**
+ * detonationCountdown bomb callback
  * @param  {[type]} args [description]
  * @return {[type]}      [description]
  */
-function detonate (args) {
-  debugger;
+function detonationCountdown (data, status, res) {
+
+  var jsConsole       = this;
+  var now             = moment();
+  var zero_hour       = moment(data.zero_hour, 'YYYY-MM-DD HH:mm:ss Z');
+  var ms_remaining    = zero_hour - now;
+  var lines           = jsConsole.output_elm.text().split('\n');
+  var deferred        = $.Deferred();
+  var lessThanZero;
+
+  lines.push(''); // add an empty line to put our timer in
+
+  window.countdown = setInterval(function () {
+
+    lessThanZero = ms_remaining < 0;
+
+    lines[lines.length - 1] = 'Bomb detonating in: ' + (lessThanZero ? '0.0' : (ms_remaining / 1000)) + 's';
+    jsConsole.output_elm.text(lines.join('\n'));
+    ms_remaining -= 100;
+
+    if (lessThanZero) {
+      clearInterval(window.countdown);
+      return deferred.resolve(data, status, res);
+    }
+  }, 100);
+
+  return deferred;
 }
 
 /**
  * update bomb state
  * @param  {String} state
- * @return {Promise}
+ * @return {Function}
  */
 function updateState (state) {
-
   return function (args) {
-    var self = this;
+    var jsConsole = this;
     var loader = dotLoader(function (dots) {
-      self.output_elm.append(dots);
+      jsConsole.output_elm.append(dots);
     });
-
-    var data = {
+    var params = {
       state: state,
       pass: args[0]
     };
 
-    $.post('/api/state', data).done(function (res) {
-      self.log("\nBomb is " + res.data.session.bomb_state + "\n");
+    $.post('/api/state', params).done(function (res) {
+      jsConsole.log("\nBomb is " + res.bomb_state + "\n");
+
+      if (res.bomb_state == 'inactive' && window.countdown) {
+        clearInterval(window.countdown);
+        jsConsole.log("\nDetonation sequence cancelled");
+      }
     }).always(function () {
       clearTimeout(loader);
     });
@@ -124,7 +219,7 @@ function dotLoader (fn, speed) {
   speed = speed || 20;
 
   return setInterval(function () {
-    i++;
-    fn(Array(i).join('. '));
+    i += 1;
+    fn(Array(i).join('. ')); // return a string of i number of dots...
   }, speed);
 }

0b3578e871ba99bd0f58c9314015b776c2006f47
rcowan <ryancowan@gmail.com>
2014-11-24 11:07:42 -0600

    setup user authentication

---
+++ opposite_of_progress/congress/app/controllers/users_controller.rb
@@ -0,0 +1,7 @@
+class UsersController < ApplicationController
+  def preferences
+    # sort_by = params["sort_by"] if params["sort_by"]
+    # @results = Bill.order(sort_by).paginate(:page => params[:page], :per_page => ApplicationHelper::PAGINATION_COUNT)
+
+  end
+end
--- opposite_of_progress/congress/db/schema.rb
+++ opposite_of_progress/congress/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141120210400) do
+ActiveRecord::Schema.define(version: 20141121151055) do
 
   create_table "bills", force: true do |t|
     t.string   "bill_id"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 20141120210400) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.boolean  "email_preferences"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true

eac32230d02b9e9d0aa388475f4cdd9acf3635a9
paulh16 <paulh16@gmail.com>
2014-12-02 16:49:30 -0600

    Correct RSpec setup and create Show spec

--- cyoa/artjourney/features/support/env.rb
+++ cyoa/artjourney/features/support/env.rb
@@ -56,3 +56,6 @@ end
 # See https://github.com/cucumber/cucumber-rails/blob/master/features/choose_javascript_database_strategy.feature
 Cucumber::Rails::Database.javascript_strategy = :truncation
 
+# Factory Girl Config
+World(FactoryGirl::Syntax::Methods)
+
--- cyoa/artjourney/spec/models/show_spec.rb
+++ cyoa/artjourney/spec/models/show_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Show, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe '#name' do
+    it 'has a name' do
+      show = FactoryGirl::build(:show)
+      expect(show.name).to eq("Show-1")
+    end
+  end
 end

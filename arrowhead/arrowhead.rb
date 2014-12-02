15068f1908b38fcaa0b709b10a70dc57e4e3d135
jillalynch <jill.lynch5@gmail.com>
2014-12-02 17:36:57 -0600

    fixed rubocop typos

--- arrowhead/arrowhead.rb
+++ arrowhead/arrowhead.rb
@@ -14,28 +14,28 @@ class Arrowhead
       bifurcated: "Oxbow",
     },
   }
- 
+
   attr_reader :region, :shape
- 
+
   def initialize(region, shape)
     @region = region if valid_region?(region)
     @shape  = shape  if valid_shape?(region, shape)
   end
- 
+
   def valid_region?(region)
-   return region if CLASSIFICATIONS.has_key?(region)
-   raise "Unknown region, please provide a valid region."
+    return region if CLASSIFICATIONS.key?(region)
+    raise "Unknown region, please provide a valid region."
   end
- 
+
   def valid_shape?(region, shape)
-    return shape if CLASSIFICATIONS[region].has_key?(shape)
+    return shape if CLASSIFICATIONS[region].key?(shape)
     raise "Unknown shape value. Are you sure you know what you're talking about?"
   end
- 
+
   def classify
     arrowhead = CLASSIFICATIONS[region][shape]
     puts "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
   end
 end
- 
+
 puts Arrowhead.new(:northern_plains, :bifurcated).classify

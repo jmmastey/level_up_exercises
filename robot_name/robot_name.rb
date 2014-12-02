e3deaa3f18d56f44eb2b211c28c955934da3a362
jillalynch <jill.lynch5@gmail.com>
2014-12-02 10:37:36 -0600

    uses begin rescue block and private methods

--- robot_name/robot_name.rb
+++ robot_name/robot_name.rb
@@ -3,28 +3,53 @@ class NameCollisionError < RuntimeError; end
 class Robot
   attr_accessor :name
 
-  @@registry
+  @@registry = ["DT447"]
 
   def initialize(args = {})
+
     @@registry ||= []
-    @name_generator = args[:name_generator]
 
-    if @name_generator
-      @name = @name_generator.call
-    else
-      generate_char = -> { ('A'..'Z').to_a.sample }
-      generate_num = -> { rand(10) }
+    @name = args[:name_generator]
 
-      @name = "#{generate_char.call}#{generate_char.call}#{generate_num.call}#{generate_num.call}#{generate_num.call}"
+    begin
+      raise NameCollisionError if invalid_name?(@name)
+    rescue
+      @name = create_name
     end
 
-    raise NameCollisionError, 'There was a problem generating the robot name!' if !(name =~ /[[:alpha:]]{2}[[:digit:]]{3}/) || @@registry.include?(name)
     @@registry << @name
+  end
+
+  private
+
+  def invalid_name?(name)
+    invalid_format?(name) || registry_duplicate?(name)
+  end
+
+  def invalid_format?(name)
+    !(name =~ /[[:alpha:]]{2}[[:digit:]]{3}/)
+  end
+
+  def registry_duplicate?(name)
+    @@registry.include?(name)
+  end
+
+  def create_name
+    generate_char = -> { ('A'..'Z').to_a.sample }
+    generate_num = -> { rand(10) }
+
+    "#{generate_char.call}#{generate_char.call}#{generate_num.call}#{generate_num.call}#{generate_num.call}"
   end
 end
 
 robot = Robot.new
 puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
+
+robot3 = Robot.new
+puts "My pet robot's name is #{robot3.name}, but we usually call him sparky."
+
+robot2 = Robot.new(name_generator: "DT447")
+puts "My pet robot's name is #{robot2.name}, but we usually call him sparky."
 
 # Errors!
 # generator = -> { 'AA111' }

d35b1a5154704d5db0c568d4ecf1250d3f6703ee
jillalynch <jill.lynch5@gmail.com>
2014-12-02 17:32:48 -0600

    basic solution

--- arrowhead/arrowhead.rb
+++ arrowhead/arrowhead.rb
@@ -14,21 +14,28 @@ class Arrowhead
       bifurcated: "Oxbow",
     },
   }
-
-  # FIXME: I don't have time to deal with this.
-  def self.classify(region, shape)
-    if CLASSIFICATIONS.include? region
-      shapes = CLASSIFICATIONS[region]
-      if shapes.include? shape
-        arrowhead = shapes[shape]
-        puts "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
-      else
-        raise "Unknown shape value. Are you sure you know what you're talking about?"
-      end
-    else
-      raise "Unknown region, please provide a valid region."
-    end
+ 
+  attr_reader :region, :shape
+ 
+  def initialize(region, shape)
+    @region = region if valid_region?(region)
+    @shape  = shape  if valid_shape?(region, shape)
+  end
+ 
+  def valid_region?(region)
+   return region if CLASSIFICATIONS.has_key?(region)
+   raise "Unknown region, please provide a valid region."
+  end
+ 
+  def valid_shape?(region, shape)
+    return shape if CLASSIFICATIONS[region].has_key?(shape)
+    raise "Unknown shape value. Are you sure you know what you're talking about?"
+  end
+ 
+  def classify
+    arrowhead = CLASSIFICATIONS[region][shape]
+    puts "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
   end
 end
-
-puts Arrowhead.classify(:northern_plains, :bifurcated)
+ 
+puts Arrowhead.new(:northern_plains, :bifurcated).classify

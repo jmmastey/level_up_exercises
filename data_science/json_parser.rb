f3498a07fe5321c0c6d9dbe99c51fe4e4e3900f4
Srikanth Astaputhra <sastaputhra@enova.com>
2014-12-02 09:28:26 -0600

    parsing json data to hash

--- data_science/json_parser.rb
+++ data_science/json_parser.rb
@@ -1,5 +1,14 @@
+require "json"
 class JsonParser
   def self.parse(file_name)
     raise ArgumentError unless File.exists?(file_name)
+    data_json = JSON.parse(file_name.read)
+    parsed_json = {}
+    data_json.each do |data|
+      parsed_json[data["date"]] ||= {}
+      parsed_json[data["date"]][data["cohort"]] ||= 0
+      parsed_json[data["date"]][data["cohort"]] += data["result"]
+    end
+    parsed_json
   end
 end
--- data_science/spec/json_parser_spec.rb
+++ data_science/spec/json_parser_spec.rb
@@ -1,13 +1,27 @@
-require 'spec_helper'
+require "spec_helper"
+require "json"
+require "tempfile"
 
 describe JsonParser do
   describe "#parse" do
+    let(:file)              { Tempfile.open("json_data.json") }
+
+    before do
+      raw_data = [{date:"2014-03-20",cohort:"B",result:0},
+                  {date:"2014-03-21",cohort:"A",result:0},
+                  {date:"2014-03-20",cohort:"B",result:0},
+                  {date:"2014-03-21",cohort:"B",result:1}]
+      file.write(raw_data.to_json)
+      file.rewind
+    end
+
     it "errors when invalid file is passed" do
       expect { described_class.parse("bogus file") }.to raise_error ArgumentError
     end
 
-    it "parses json and stores results in to hash" do
-
+    it "parses json and stores results in to hash with date as keys" do
+      data_json = described_class.parse(file)
+      expect(data_json["2014-03-21"]["B"]).to eq(1)
     end
   end
 end

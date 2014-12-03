128028c795488adc9d002a223ea4d5ebe97f13eb
Dan Kotowski <dkotowski@enova.com>
2014-12-03 16:53:56 -0600

    Deleted auto-generated view specs for watches

--- eve/eve/spec/views/watches/show.html.haml_spec.rb
+++ eve/eve/spec/views/watches/show.html.haml_spec.rb
@@ -1,18 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe "watches/show", :type => :view do
-  before(:each) do
-    @watch = assign(:watch, Watch.create!(
-      :nickname => "Nickname",
-      :item => nil,
-      :user => nil
-    ))
-  end
-
-  it "renders attributes in <p>" do
-    render
-    expect(rendered).to match(/Nickname/)
-    expect(rendered).to match(/Item/)
-    expect(rendered).to match(/User/)
-  end
 end

ff91107299552a8f9bd50ae879853e8a17fa3996
johnmcconnell <johnnyillinois@gmail.com>
2014-12-04 17:30:45 -0600

    fixed some hound yelling

--- cyoa/openmooc/db/migrate/20141019024459_add_confirmable_to_devise.rb
+++ cyoa/openmooc/db/migrate/20141019024459_add_confirmable_to_devise.rb
@@ -5,11 +5,11 @@ class AddConfirmableToDevise < ActiveRecord::Migration
     add_column :users, :confirmed_at, :datetime
     add_column :users, :confirmation_sent_at, :datetime
     # add_column :users, :unconfirmed_email, :string # Only if using reconfirmable
-    add_index :users, :confirmation_token, :unique => true
+    add_index :users, :confirmation_token, unique: true
     # User.reset_column_information # Need for some types of updates, but not for update_all.
     # To avoid a short time window between running the migration and updating all existing
     # users as confirmed, do the following
-    User.update_all(:confirmed_at => Time.now)
+    User.update_all(confirmed_at: Time.now)
     # All existing user accounts should be able to log in after this.
   end
 
--- cyoa/openmooc/db/schema.rb
+++ cyoa/openmooc/db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141202180723) do
+ActiveRecord::Schema.define(version: 20_141_202_180_723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "aliases", force: true do |t|
-    t.string   "text"
-    t.integer  "fill_in_the_blank_answer_id"
+    t.string "text"
+    t.integer "fill_in_the_blank_answer_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20141202180723) do
   add_index "aliases", ["fill_in_the_blank_answer_id"], name: "index_aliases_on_fill_in_the_blank_answer_id", using: :btree
 
   create_table "contents", force: true do |t|
-    t.integer  "page_content_id"
+    t.integer "page_content_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -34,21 +34,21 @@ ActiveRecord::Schema.define(version: 20141202180723) do
   add_index "contents", ["page_content_id"], name: "index_contents_on_page_content_id", using: :btree
 
   create_table "courses", force: true do |t|
-    t.string   "title"
-    t.string   "subject"
-    t.string   "topic"
+    t.string "title"
+    t.string "subject"
+    t.string "topic"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.text     "description"
-    t.integer  "page_content_id"
+    t.text "description"
+    t.integer "page_content_id"
   end
 
   add_index "courses", ["page_content_id"], name: "index_courses_on_page_content_id", using: :btree
 
   create_table "feedbacks", force: true do |t|
-    t.integer  "user_id"
-    t.string   "subject"
-    t.text     "message"
+    t.integer "user_id"
+    t.string "subject"
+    t.text "message"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -56,8 +56,8 @@ ActiveRecord::Schema.define(version: 20141202180723) do
   add_index "feedbacks", ["user_id"], name: "index_feedbacks_on_user_id", using: :btree
 
   create_table "fill_in_the_blank_answers", force: true do |t|
-    t.string   "text"
-    t.integer  "fill_in_the_blank_question_id"
+    t.string "text"
+    t.integer "fill_in_the_blank_question_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -65,7 +65,7 @@ ActiveRecord::Schema.define(version: 20141202180723) do
   add_index "fill_in_the_blank_answers", ["fill_in_the_blank_question_id"], name: "fitb_answer_belongs_to_fill_in_the_blank_question_index", using: :btree
 
   create_table "fill_in_the_blank_questions", force: true do |t|
-    t.integer  "page_content_id"
+    t.integer "page_content_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -73,9 +73,9 @@ ActiveRecord::Schema.define(version: 20141202180723) do
   add_index "fill_in_the_blank_questions", ["page_content_id"], name: "index_fill_in_the_blank_questions_on_page_content_id", using: :btree
 
   create_table "lessons", force: true do |t|
-    t.string   "name"
-    t.integer  "course_id"
-    t.integer  "position"
+    t.string "name"
+    t.integer "course_id"
+    t.integer "position"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -83,9 +83,9 @@ ActiveRecord::Schema.define(version: 20141202180723) do
   add_index "lessons", ["course_id"], name: "index_lessons_on_course_id", using: :btree
 
   create_table "multiple_choice_answers", force: true do |t|
-    t.integer  "multiple_choice_question_id"
-    t.string   "text"
-    t.boolean  "correct"
+    t.integer "multiple_choice_question_id"
+    t.string "text"
+    t.boolean "correct"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -93,7 +93,7 @@ ActiveRecord::Schema.define(version: 20141202180723) do
   add_index "multiple_choice_answers", ["multiple_choice_question_id"], name: "index_multiple_choice_answers_on_multiple_choice_question_id", using: :btree
 
   create_table "multiple_choice_questions", force: true do |t|
-    t.integer  "page_content_id"
+    t.integer "page_content_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -101,49 +101,49 @@ ActiveRecord::Schema.define(version: 20141202180723) do
   add_index "multiple_choice_questions", ["page_content_id"], name: "index_multiple_choice_questions_on_page_content_id", using: :btree
 
   create_table "page_contents", force: true do |t|
-    t.text     "content"
-    t.text     "html"
+    t.text "content"
+    t.text "html"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "pages", force: true do |t|
-    t.string   "type"
-    t.integer  "lesson_id"
-    t.integer  "position"
-    t.integer  "content_id"
-    t.string   "content_type"
+    t.string "type"
+    t.integer "lesson_id"
+    t.integer "position"
+    t.integer "content_id"
+    t.string "content_type"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "pages", ["content_id", "content_type"], name: "index_pages_on_content_id_and_content_type", using: :btree
+  add_index "pages", %w(content_id content_type), name: "index_pages_on_content_id_and_content_type", using: :btree
   add_index "pages", ["lesson_id"], name: "index_pages_on_lesson_id", using: :btree
 
   create_table "quiz_activities", force: true do |t|
-    t.integer  "question_id"
-    t.string   "question_type"
+    t.integer "question_id"
+    t.string "question_type"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "quiz_activities", ["question_id", "question_type"], name: "index_quiz_activities_on_question_id_and_question_type", using: :btree
+  add_index "quiz_activities", %w(question_id question_type), name: "index_quiz_activities_on_question_id_and_question_type", using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string "email",                  default: "", null: false
+    t.string "encrypted_password",     default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.string   "confirmation_token"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
+    t.string "unconfirmed_email"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree

0a222ccb85f812b594cfd4afb760ff42c97412f4
paulh16 <paulh16@gmail.com>
2014-12-04 17:42:30 -0600

    Get rid of old database migration

--- db/migrate/20141202210022_create_shows.rb
+++
@@ -1,9 +0,0 @@
-class CreateShows < ActiveRecord::Migration
-  def change
-    create_table :shows do |t|
-      t.string :name
-
-      t.timestamps null: false
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141202210022) do
-
-  create_table "shows", force: true do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end

348e631bfb9e7e93e3769be1642440d96456d1e8
rcowan <ryancowan@gmail.com>
2014-12-05 16:30:04 -0600

    scope out basic all_sorted methods

--- opposite_of_progress/congress/app/models/deed.rb
+++ opposite_of_progress/congress/app/models/deed.rb
@@ -1,10 +1,8 @@
 class Deed < ActiveRecord::Base
+  scope :all_sorted, ->(page, sort_by) { order(sort_by).paginate(page: page) }
+
   def self.all_related(deed)
     Deed.where('id != ? and (bioguide_id = ? or bill_id = ?)',
                deed.id, deed.bioguide_id, deed.bill_id)
-  end
-
-  def self.all_sorted(page, sort_by = "date DESC")
-    Deed.order(sort_by).paginate(page: page)
   end
 end
--- opposite_of_progress/congress/app/models/legislator.rb
+++ opposite_of_progress/congress/app/models/legislator.rb
@@ -1,6 +1,7 @@
 # Base Legislator class
 class Legislator < ActiveRecord::Base
   scope :by_bioguide_id, ->(id) { where(bioguide_id: id).first }
+  scope :all_sorted, ->(page, sort_by) { order(sort_by).paginate(page: page) }
 
   def full_name
     [title, first_name, nickname, last_name, display_party].join(" ")
@@ -42,10 +43,5 @@ class Legislator < ActiveRecord::Base
       office: result['office'],
       contact_form: result['contact_form'],
     }
-  end
-
-  def self.all_sorted(page, sort_by = "created_at DESC")
-    Legislator.order(sort_by)
-              .paginate(page: page)
   end
 end

fb25a11e96be410663266bdafca628ab67d85377
Lawrence Walters <lawrencewalters@users.noreply.github.com>
2014-12-08 08:33:11 -0600

    use tap, clean airport lookup. (refactor based on PR notes)

--- cyoa/travel_planner/app/models/flight_mapper.rb
+++ cyoa/travel_planner/app/models/flight_mapper.rb
@@ -1,18 +1,24 @@
 class FlightMapper
   def flight_stats_to_flight_h(flightstats)
-    flight = {}
-    lookup_airports(flight, flightstats)
-    set_times(flight, flightstats)
-    flight
+    Hash.new.tap do |flight|
+      lookup_airports(flight, flightstats)
+      set_times(flight, flightstats)
+    end
   end
 
   private
 
   def lookup_airports(flight, flightstats)
-    airport = Airport.find_by_code!(flightstats["departureAirportFsCode"])
-    flight["origin_airport"] = airport
-    airport = Airport.find_by_code!(flightstats["arrivalAirportFsCode"])
-    flight["destination_airport"] = airport
+    flight["origin_airport"] = origin_airport(flightstats)
+    flight["destination_airport"] = destination_airport(flightstats)
+  end
+
+  def destination_airport(flightstats)
+    Airport.find_by_code!(flightstats["arrivalAirportFsCode"])
+  end
+
+  def origin_airport(flightstats)
+    Airport.find_by_code!(flightstats["departureAirportFsCode"])
   end
 
   def set_times(flight, flightstats)

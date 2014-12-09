e6680c287142b3209a95da941a28ed25ad8f9e61
rickytux <rick.dsida@icloud.com>
2014-12-08 22:10:10 -0600

    refactored the robot class, don't care for the class variable but was best way I know (besides database) to store the running number of names

--- robot_name/robot_name.rb
+++ robot_name/robot_name.rb
@@ -1,39 +1,53 @@
+# require 'pry'
+# require 'pry-nav'
+
 class NameCollisionError < RuntimeError
   def to_s
-    'There was a problem generating the robat name'
+    'There was a problem generating the robot name'
   end
 end
 
 class Robot
-  attr_accessor :name, :registry
+  attr_accessor :name
+  @@registry
 
   def initialize(args = {})
-    @registry ||= []
-    @name = []
-    if (@name_generator = args[:name_generator])
+    @@registry ||= []
+    # binding.pry
+    @name_generator = args[:name_generator]
+    need_name?
+    @@registry.push(check_name(name))
+  end
+
+  def need_name?
+    if @name_generator
       @name = @name_generator.call
     else
-      generate_char = -> { ('A'..'Z').to_a.sample }
-      generate_num = -> { rand(10) }
-      2.times { @name << generate_char.call}
-      3.times { @name << generate_num.call}
-      @name = @name.join('')
+      create_name
     end
-    check_name(name)
-    @registry << @name
+  end
+
+  def create_name
+    @name = []
+    generate_char = -> { ('A'..'Z').to_a.sample }
+    generate_num = -> { rand(10) }
+    2.times { @name << generate_char.call }
+    3.times { @name << generate_num.call }
+    @name = @name.join('')
   end
 
   def check_name(name)
-    raise NameCollisionError if @registry.include?(name)
+    raise NameCollisionError if @@registry.include?(name)
     raise NameCollisionError unless name =~ /[[:alpha:]]{2}[[:digit:]]{3}/
+    name
   end
-
 end
 
 robot = Robot.new
 puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
 
 # Errors!
-# generator = -> { 'AA111' }
-# Robot.new(name_generator: generator)
+generator = -> { 'AB312' }
+robot = Robot.new(name_generator: generator)
+puts "Robot's name is #{robot.name}, but we usually call him Sparky"
 # Robot.new(name_generator: generator)

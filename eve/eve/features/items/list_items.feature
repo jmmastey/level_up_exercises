21766fbd31d483b7afd10cbf562abd8f1f6ab2f9
Dan Kotowski <dkotowski@enova.com>
2014-12-10 14:36:33 -0600

    Updated item cukes.

--- eve/eve/features/items/list_items.feature
+++ eve/eve/features/items/list_items.feature
@@ -14,17 +14,20 @@ Feature: List items
 
 	Scenario: Counting items
 		When I visit the items page
-		Then I should see 5 items
+		Then I see 5 items
 	
 	Scenario Outline: Listing items
 		When I visit the items page
-		Then I should see <in_game_id> in the EVE ID column
-		And I should see "<name>" in the name column
+		Then I see <in_game_id> in the EVE ID column
+		And I see "<name>" in the name column
 
 		Examples:
 			| in_game_id | name             |
 			| 34         | Tritanium        |
-			| 35         | Pyerite          |
 			| 36         | Mexallon         |
-			| 37         | Isogen           |
 			| 44         | Enriched Uranium |
+
+	Scenario: Navigating to item orders
+		When I visit the items page
+		And I click the "View orders" link for item #34
+		Then I see the orders page
--- eve/eve/features/step_definitions/item_steps.rb
+++ eve/eve/features/step_definitions/item_steps.rb
@@ -1,23 +1,38 @@
-Given(/^I have the following items:$/) do |items|
-  items.hashes.each do |item|
-    FactoryGirl.create(:item, item)
+def create_items(items_params)
+  @items = items_params.map do |item_param|
+    FactoryGirl.create(:item, item_param)
   end
+end
+
+def find_item(in_game_id)
+  @items.select { |item| item.in_game_id == in_game_id }.first
+end
+
+Given(/^I have the following items:$/) do |items|
+  create_items(items.hashes)
 end
 
 When(/^I visit the items page$/) do
   visit "/items"
 end
 
-Then(/^I should see (\d+) items$/) do |count|
+When(/^I click the "([^"]+)" link for item #(\d+)$/) do |link_text, in_game_id|
+  item = find_item(in_game_id.to_i)
+  within("#item-#{item.id}") do
+    click_link(link_text)
+  end
+end
+
+Then(/^I see (\d+) items$/) do |count|
   expect(page).to have_css("tr.item", count: count)
 end
 
-Then(/^I should see (\d+) in the EVE ID column$/) do |id|
+Then(/^I see (\d+) in the EVE ID column$/) do |id|
   expect(page).to have_css("tr.item td.field-in_game_id",
                            text: id.to_s)
 end
 
-Then(/^I should see "([^"]+)" in the name column$/) do |name|
+Then(/^I see "([^"]+)" in the name column$/) do |name|
   expect(page).to have_css("tr.item td.field-name",
                            text: name)
 end

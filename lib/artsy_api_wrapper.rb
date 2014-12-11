8f182f25384375dfe5efc841018c9506779bf0ec
paulh16 <paulh16@gmail.com>
2014-12-11 16:59:20 -0600

    Refactor artsy_api_wrapper

--- lib/artsy_api_wrapper.rb
+++ lib/artsy_api_wrapper.rb
@@ -1,36 +1,36 @@
 require 'hyperclient'
 
 class ArtsyApiWrapper
-  attr_reader :artsy_api
+  attr_reader :api
 
   def initialize(client_id:, client_secret:)
-    @artsy_api = set_up_api(client_id, client_secret)
-    @client_id = client_id
-    @client_secret = client_secret
-    #@xapp_token = @api.api.tokens.xapp_token._post(client_id: client_id, client_secret: client_secret).token
+    @token = get_token(client_id, client_secret)
+    @api = request_api
   end
 
-  def set_up_api(client_id, client_secret)
+  def request_api
     Hyperclient.new('https://api.artsy.net/api') do |api|
       api.headers['Accept'] = 'application/vnd.artsy-v2+json'
+      api.headers['X-Xapp-Token'] = @token
     end
   end
 
   def get_artist(name:)
-    artsy_api.artist(id: name)
+    api.artist(id: name)
   end
 
   private
 
-  def token
-    artsy_api.tokens.xapp_token._post(client_id: @client_id, client_secret: @client_secret).token
+  def get_token(client_id, client_secret)
+    api = Hyperclient.new('https://api.artsy.net/api') do |api|
+      api.headers['Accept'] = 'application/vnd.artsy-v2+json'
+    end
+    api.tokens.xapp_token._post(client_id: client_id, client_secret: client_secret).token
   end
 end
 
 client_id = '094ac11b91081fbcd043'
 client_secret = 'a1213c1b069c9479db3728a3686a6907'
 new_api = ArtsyApiWrapper.new(client_id: client_id, client_secret: client_secret)
-puts new_api.artsy_api.tokens.xapp_token._post(client_id: client_id, client_secret: client_secret).token
 
-puts new_api.get_artist(name: 'andy-warhol')
-
+puts new_api.get_artist(name: 'andy-warhol').name

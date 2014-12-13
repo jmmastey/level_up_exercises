cc4a96bdbadd608df14dfa919353682f8587c128
Anthony Rubin <arubin@enova.com>
2014-12-12 18:19:38 -0600

    Move examples into separate file

--- dino_catalog/dinodex.rb
+++ dino_catalog/dinodex.rb
@@ -77,38 +77,3 @@ class DinoDex
       :walking, :continent, :description
   end
 end
-
-if __FILE__ == $PROGRAM_NAME
-  dinos = DinoLoader.load("dinodex.csv") +
-          DinoLoader.load("african_dinosaur_export.csv")
-
-  dinodex = DinoDex.new(dinos)
-  dinodex.filter_by_walking! "Biped"
-  puts "Bipeds"
-  dinodex.display_all
-
-  dinodex = DinoDex.new(dinos)
-  dinodex.filter_carnivores!
-  puts "Carnivores"
-  dinodex.display_all
-
-  dinodex = DinoDex.new(dinos)
-  dinodex.filter_by_period! "Cretaceous"
-  puts "Cretaceous Period"
-  dinodex.display_all
-
-  dinodex = DinoDex.new(dinos)
-  dinodex.filter_big!
-  puts "Big"
-  dinodex.display_all
-
-  dinodex = DinoDex.new(dinos)
-  dinodex.filter_small!
-  puts "Small"
-  dinodex.display_all
-
-  dinodex = DinoDex.new(dinos)
-  dinodex.filter_by_walking!("Biped").filter_carnivores!.filter_small!
-  puts "Small Bipedal Carnivores"
-  dinodex.display_all
-end

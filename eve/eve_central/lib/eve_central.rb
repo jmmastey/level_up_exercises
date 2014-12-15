95eaa3216b8f2dd4c609f91e66eea112eb81cdbb
Dan Kotowski <dkotowski@enova.com>
2014-12-15 15:32:34 -0600

    Fixed gem - required necessary files in eve_central.rb

--- eve/eve_central/lib/eve_central.rb
+++ eve/eve_central/lib/eve_central.rb
@@ -1,4 +1,12 @@
 require "eve_central/version"
+require "eve_central/errors"
+require "eve_central/item"
+require "eve_central/marketstat"
+require "eve_central/marketstat_item"
+require "eve_central/marketstat_order_group"
+require "eve_central/quicklook"
+require "eve_central/quicklook_item"
+require "eve_central/station"
 
 module EveCentral
 end

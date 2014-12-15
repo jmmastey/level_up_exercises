783abc4b3f3da32dcd1c7091dadac580aab719eb
Anthony Rubin <arubin@enova.com>
2014-12-15 14:03:35 -0600

    Cleanup Robot class
    Fix style issues and refactor code into shorter, more logical methods. Remove
    use of class variable. Move custom exceptions into Robot class.

--- robot_name/robot_name.rb
+++ robot_name/robot_name.rb
@@ -1,25 +1,50 @@
-class NameCollisionError < RuntimeError; end
-
 class Robot
   attr_accessor :name
 
-  @@registry
+  class NameCollisionError < RuntimeError
+    def initialize(message = "Name already exists in robot registry!")
+      super
+    end
+  end
+
+  class InvalidNameError < RuntimeError
+    def initialize(message = "Invalid name for a robot! How rude!")
+      super
+    end
+  end
+
+  def self.registry
+    @registry ||= []
+  end
 
   def initialize(args = {})
-    @@registry ||= []
-    @name_generator = args[:name_generator]
+    @name_generator = args[:name_generator] || name_generator
 
-    if @name_generator
-      @name = @name_generator.call
-    else
-      generate_char = -> { ('A'..'Z').to_a.sample }
-      generate_num = -> { rand(10) }
+    @name = @name_generator.call
+    validate_robot
 
-      @name = "#{generate_char.call}#{generate_char.call}#{generate_num.call}#{generate_num.call}#{generate_num.call}"
-    end
+    Robot.registry << @name
+  end
 
-    raise NameCollisionError, 'There was a problem generating the robot name!' if !(name =~ /[[:alpha:]]{2}[[:digit:]]{3}/) || @@registry.include?(name)
-    @@registry << @name
+  def name_generator
+    -> { "#{generate_chars(2)}#{generate_nums(3)}" }
+  end
+
+  def generate_chars(n = 1)
+    n.times.inject("") { |chars| chars + ('A'..'Z').to_a.sample }
+  end
+
+  def generate_nums(n = 1)
+    n.times.inject("") { |nums| nums + rand(10).to_s }
+  end
+
+  def validate_robot
+    raise InvalidNameError unless valid_name?
+    raise NameCollisionError if Robot.registry.include?(name)
+  end
+
+  def valid_name?
+    name =~ /[[:alpha:]]{2}[[:digit:]]{3}/
   end
 end
 

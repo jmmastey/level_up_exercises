458448fd8e2a63bc20e922a0823a29b2c7e04e8a
rickytux <rick.dsida@icloud.com>
2014-12-16 10:53:38 -0600

    changed reset form to be hidden by the css instead of by javascript.  Results in no original generation and subsequent hiding of the reset button.  Javascript still handles the appearance of the botton.

--- overlord/public/javascripts/application.js
+++ overlord/public/javascripts/application.js
@@ -1,6 +1,6 @@
 var intruder =0
 $(document).ready(function(){
-  $('#reset').hide();
+  // $('#reset').hide();
   $('#activate').on('click', function(event){
     var entered_code = $('#code').val();
     var activate_code = '<%= session[:activate]%>'

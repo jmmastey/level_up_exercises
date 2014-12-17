8645220d42485c47432c658e831fa39cd2e9f57a
Jeremy Finzel <jfinzel@enova.com>
2014-12-17 09:47:30 -0600

    Class methods all moved to instance methods in DinosaurImport for simplicity

--- dino_catalog/dinosaur_import.rb
+++ dino_catalog/dinosaur_import.rb
@@ -40,41 +40,37 @@ class DinosaurImport
     dinosaurs = []
     data.each do |row|
       hash = row.to_hash
-      hash = self.class.convert_import_hash(hash)
+      hash = convert_import_hash(hash)
       dinosaurs << Dinosaur.new(hash)
     end
     dinosaurs
   end
 
-  class << self
-
-    def convert_import_hash(hash)
-      hash_converted = {}
-      hash.each_pair do |key, value|
-        key_converted = convert_import_keys(key)
-        value_converted = convert_import_values(key_converted, value)
-        hash_converted.merge!(key_converted => value_converted)
-      end
-      hash_converted
+  def convert_import_hash(hash)
+    hash_converted = {}
+    hash.each_pair do |key, value|
+      key_converted = convert_import_keys(key)
+      value_converted = convert_import_values(key_converted, value)
+      hash_converted.merge!(key_converted => value_converted)
     end
+    hash_converted
+  end
 
-    def convert_import_keys(key)
-      key_converted = key.downcase
-      if AUTOCONVERT_KEYS.key?(key_converted)
-        AUTOCONVERT_KEYS[key_converted]
-      else
-        key_converted
-      end
+  def convert_import_keys(key)
+    key_converted = key.downcase
+    if AUTOCONVERT_KEYS.key?(key_converted)
+      AUTOCONVERT_KEYS[key_converted]
+    else
+      key_converted
     end
+  end
 
-    def convert_import_values(key_converted, value)
-      if AUTOCONVERT_KEYS_VALUES.key?(key_converted)
-        value_convert_hash = AUTOCONVERT_KEYS_VALUES[key_converted]
-        value_convert_hash.key?(value) ? value_convert_hash[value] : value
-      else
-        value
-      end
+  def convert_import_values(key_converted, value)
+    if AUTOCONVERT_KEYS_VALUES.key?(key_converted)
+      value_convert_hash = AUTOCONVERT_KEYS_VALUES[key_converted]
+      value_convert_hash.key?(value) ? value_convert_hash[value] : value
+    else
+      value
     end
-
   end
 end

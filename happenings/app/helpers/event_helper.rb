4e8b4c9ea74cfc6d2357eb2c5ecfb4d663fa7437
Sreeram Mittapalli <smittapalli@enovafinancial.com>
2014-12-17 03:01:14 -0600

    helper needs to be a module

--- happenings/app/helpers/event_helper.rb
+++ happenings/app/helpers/event_helper.rb
@@ -1,4 +1,4 @@
-class EventHelper
+module EventHelper
   def event_change_permitted_params(raw_params)
     raw_params.require(:event).permit(:title, :starts_at, :description, :url, :category)
   end

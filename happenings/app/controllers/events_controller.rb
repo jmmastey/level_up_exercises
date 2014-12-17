374f5a424f3595548aa5c3a1bdb7fe5e0e503056
Sreeram Mittapalli <smittapalli@enovafinancial.com>
2014-12-17 17:17:36 -0600

    filter params, create base event form

--- happenings/app/controllers/events_controller.rb
+++ happenings/app/controllers/events_controller.rb
@@ -1,12 +1,16 @@
 class EventsController < ApplicationController
   include EventHelper
   def index
+    permitted_params = filter_params(params)
+    session[:event_source] = permitted_params[:source] if permitted_params[:source].present?
+    @events = Event.with_source(session[:event_source] || :theatre_in_chicago)
   end
 
   def show
   end
 
   def new
+    @event = Event.new
   end
 
   def create
--- happenings/app/helpers/event_helper.rb
+++ happenings/app/helpers/event_helper.rb
@@ -1,5 +1,5 @@
 module EventHelper
-  def event_change_permitted_params(raw_params)
-    raw_params.require(:event).permit(:title, :starts_at, :description, :url, :category)
+  def filter_params(raw_params)
+    raw_params.permit(:title, :time, :date, :description, :url, :source)
   end
 end

b1a631d28d4fca7c15718dfb9911d068eaa1d9eb
Sreeram Mittapalli <smittapalli@enovafinancial.com>
2014-12-18 15:16:45 -0600

    use new create event interactor

--- happenings/app/interactors/create_event.rb
+++ happenings/app/interactors/create_event.rb
@@ -1,6 +1,32 @@
-module CreateEvent
+class CreateEvent
+  def initialize(event_info)
+    @event_info = event_info
+  end
+
   def self.create(event_info)
+    new(event_info).create
+  end
+
+  def create
     default_values = { event_source: :theatre_in_chicago }
-    Event.create(default_values.merge(event_info))
+    attribs = default_values.merge(@event_info)
+   
+    #return nil if event_exists?(convert_attribs(attribs))
+    Event.create(attribs)
+  end
+
+  private
+
+  def event_exists?(event_info)
+    time = 
+    date = 
+    Event.where(event_info).present?
+  end
+
+  def convert_attribs(event_info)
+    dup_event_info = event_info.dup
+    event_source_id = EventSource[dup_event_info.delete(:event_source)].id
+    dup_event_info[:event_source_id] = event_source_id
+    dup_event_info
   end
 end

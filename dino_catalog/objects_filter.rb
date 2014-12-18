8b6ea1c9e1ba2156a840a4ef73446d0902b23eb5
Jeremy Finzel <jfinzel@enova.com>
2014-12-18 12:22:13 -0600

    Added some exception handling for bad filters

--- dino_catalog/objects_filter.rb
+++ dino_catalog/objects_filter.rb
@@ -8,17 +8,29 @@ class ObjectsFilter
   end
 
   def self.operator_filters(objects, operator, attributes_values)
-    attributes_values.each do |attribute, value|
-      objects = add_filter(objects, operator, { attribute => value })
+    if attributes_values.class != Hash
+      filter_exception
+    else
+      attributes_values.each do |attribute, value|
+        objects = add_filter(objects, operator, { attribute => value })
+      end
+      objects
+    end
+  end
+
+  def self.add_filter(objects, operator, attribute_value)
+    begin
+      objects.select do |object|
+        object.send("#{attribute_value.keys[0]}")
+            .send(operator, attribute_value.values[0])
+      end
+    rescue
+      filter_exception
     end
     objects
   end
 
-  def self.add_filter(objects, operator, attribute_value)
-    objects.select do |object|
-      object.send("#{attribute_value.keys[0]}")
-          .send(operator, attribute_value.values[0])
-    end
-    objects
+  def self.filter_exception
+    raise "Invalid filters: must contain attributes of object class in format { operator <string> => { attribute: value } }"
   end
 end

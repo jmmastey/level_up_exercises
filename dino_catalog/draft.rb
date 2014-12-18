bfc5ac26833780c3505e3a0860ce3d2315798e9a
Dariusz Dzien <ddzien@enova.com>
2014-12-17 20:12:41 -0600

    Add CSV module

--- dino_catalog/draft.rb
+++ dino_catalog/draft.rb
@@ -1,23 +1,45 @@
-require 'csv'
+module CSVtoHash
+  
+  require 'csv'
+  
+  CSV::Converters[:blank_to_nil] = lambda do |field|
+    field && field.empty? ? nil : field
+  end
 
+  def CSV_to_hash(file)
+    @csv = CSV.read(file, :headers => true, :header_converters => :symbol, :converters => :all)
+    @csv = @csv.map {|row| row.to_hash}
+  end
+    
+#  dinos = CSV.read("dinodex.csv", :headers => true, :header_converters => :symbol, :converters => :all)
+#  dinos = dinos.map {|row| row.to_hash}
+#
+#  african_dinos = CSV.read("african_dinosaur_export.csv", :headers => true, :header_converters => :symbol, :converters => :all)
+#  african_dinos = african_dinos.map {|row| row.to_hash}
 
-CSV::Converters[:blank_to_nil] = lambda do |field|
-  field && field.empty? ? nil : field
+  def normalize_african_hash(dino_hash_array)
+    dino_hash_array.each do |dino|
+      dino[:name] = dino.delete :genus
+      dino[:weight_in_lbs] = dino.delete :weight
+      dino[:diet] = dino.delete :carnivore
+      dino[:continent] = "Africa"
+      if dino[:diet].eql? "Yes"
+        dino[:diet] = "Carnivore"
+      else
+        dino[:diet] = "Herbivore"
+      end
+    end
+  end
+
+#  african_dinos.each do |african_dino|
+#    african_dino[:name] = african_dino.delete :genus
+#    african_dino[:weight_in_lbs] = african_dino.delete :weight
+#    african_dino[:diet] = african_dino.delete :carnivore
+#    african_dino[:continent] = "Africa"
+#  end
 end
 
-dinos = CSV.read("dinodex.csv", :headers => true, :header_converters => :symbol, :converters => :all)
-dinos = dinos.map {|row| row.to_hash}
-
-african_dinos = CSV.read("african_dinosaur_export.csv", :headers => true, :header_converters => :symbol, :converters => :all)
-african_dinos = african_dinos.map {|row| row.to_hash}
-
-african_dinos.each do |african_dino|
-  african_dino[:name] = african_dino.delete :genus
-  african_dino[:weight_in_lbs] = african_dino.delete :weight
-  african_dino[:diet] = african_dino.delete :carnivore
-  african_dino[:continent] = "Africa"
-end
-
+include CSVtoHash
 
 class Dino
   attr_accessor :name, :period, :continent, :diet, :weight_in_lbs, :walking, :description
@@ -47,7 +69,7 @@ end
 
 class DinoDex
 
-#  attr_accessor :dinosaurs
+# attr_accessor :dinosaurs
   
   def initialize(dino_hash, african_dino_hash)
     @dinosaurs = []
@@ -56,7 +78,7 @@ class DinoDex
   end
   
   def print_all_facts dino_name
-    dino = dinosaurs.select {|dino| dino.name.downcase == dino_name.downcase}
+    dino = @dinosaurs.select {|dino| dino.name.to_s.downcase == dino_name.downcase}
     dino[0].instance_variables.each do |dino_fact|
       if dino[0].instance_variable_get(dino_fact)
         puts dino_fact.to_s[1..-1].capitalize + ":\n" + dino[0].instance_variable_get(dino_fact).to_s
@@ -113,13 +135,27 @@ class DinoDex
 end
 
 
-dino_dex = DinoDex.new(dinos, african_dinos)
-#dino_dex.print_all_facts("albertosaurus")
+
+joes_dinos = CSV_to_hash("dinodex.csv")
+#puts joes_dinos
+piratebay_dinos = normalize_african_hash(CSV_to_hash("african_dinosaur_export.csv"))
+
+puts piratebay_dinos
+
+dino_dex = DinoDex.new(joes_dinos, piratebay_dinos)
+dino_dex.print_all_facts("albertosaurus")
 #dino_dex.print_all_facts("afrovenator")
 #dino_dex.all_dinos
 #dino_dex.print_all_facts("suchomimus")
 #puts dino_dex.dinosaurs[3].is_fat?
 #puts dino_dex.dinosaurs[3].is_small?
 #p dino_dex.dinosaurs[3].weight_in_lbs
+puts "Enter options:"
+user_input = []
+user_input << gets.chomp
+p user_input
+if user_input.include? "afrovenator"
+  puts dino_dex.print_all_facts(user_input[0])
+end
 
-dino_dex.filter_dinos(["joe", "fat", "africa"])
+#dino_dex.filter_dinos(["joe", "fat", "africa"])

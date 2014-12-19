c11e4b838edb554b58cc6665995f9c0e0e5e4a2d
Jeremy Finzel <jfinzel@enova.com>
2014-12-19 14:27:54 -0600

    Triangle facts refactor.

--- triangle_facts/triangle_facts.rb
+++ triangle_facts/triangle_facts.rb
@@ -1,6 +1,7 @@
 # Killer facts about triangles AWW YEAH
 class Triangle
   attr_accessor :side1, :side2, :side3
+
   def initialize(side1, side2, side3)
     @side1 = side1
     @side2 = side2
@@ -8,11 +9,11 @@ class Triangle
   end
 
   def equilateral
-    @side1 == @side2 && @side2 == @side3
+    side1 == side2 && side2 == side3
   end
 
   def isosceles
-    [@side1, @side2, @side3].uniq.length == 2
+    [side1, side2, side3].uniq.length == 2
   end
 
   def scalene
@@ -29,18 +30,16 @@ class Triangle
         puts "This triangle is scalene and mathematically boring."
     end
 
-    angles = calculate_angles(side1, side2, side3)
+    angles = calculate_angles
     puts "The angles of this triangle are " + angles.join(",")
-
-    puts "This triangle is also a right triangle!" if angles.include? 90
+    puts "This triangle is also a right triangle!" if angles.include?(90)
     puts ""
   end
 
-  def calculate_angles(a, b, c)
-    angle_a = radians_to_degrees(Math.acos((b**2 + c**2 - a**2) / (2.0 * b * c)))
-    angle_b = radians_to_degrees(Math.acos((a**2 + c**2 - b**2) / (2.0 * a * c)))
-    angle_c = radians_to_degrees(Math.acos((a**2 + b**2 - c**2) / (2.0 * a * b)))
-
+  def calculate_angles
+    angle_a = radians_to_degrees(Math.acos((side2**2 + side3**2 - side1**2) / (2.0 * side2 * side3)))
+    angle_b = radians_to_degrees(Math.acos((side1**2 + side3**2 - side2**2) / (2.0 * side1 * side3)))
+    angle_c = radians_to_degrees(Math.acos((side1**2 + side2**2 - side3**2) / (2.0 * side1 * side2)))
     [angle_a, angle_b, angle_c]
   end
 
@@ -54,6 +53,6 @@ triangles = [
   [5, 12, 13],
 ]
 triangles.each do |sides|
-  tri = Triangle.new(*sides)
-  tri.recite_facts
+  triangle = Triangle.new(*sides)
+  triangle.recite_facts
 end

4fb4df642c0092ed2a7fbee34ce03ba247d88a68
paulh16 <paulh16@gmail.com>
2014-12-19 17:17:41 -0600

    Update Artsy API key

--- features/step_definitions/api_interaction_steps.rb
+++ features/step_definitions/api_interaction_steps.rb
@@ -1,5 +1,6 @@
 Given(/^I request artist data from the API for (.+)$/) do |name|
   full_name = name.downcase.gsub(/\s/, '-')
+  debugger
   RetrieveArtist.new(full_name)
 end
 
---
+++ spec/factories/user_factory.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :user do
+    email "user@artsy.com"
+    admin false
+  end
+end

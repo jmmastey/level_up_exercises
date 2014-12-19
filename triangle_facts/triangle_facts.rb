0eaefca6ffcdc267d5ac0a52ddd68d3a3f5768a5
Dariusz Dzien <ddzien@enova.com>
2014-12-19 11:34:10 -0600

    Minor tweaks

--- triangle_facts/triangle_facts.rb
+++ triangle_facts/triangle_facts.rb
@@ -1,19 +1,19 @@
 # Killer facts about triangles AWW YEAH
 class Triangle
-	attr_accessor :side1, :side2, :side3
+	attr_accessor :side_1, :side_2, :side_3
 
-	def initialize(side1, side2, side3)
-    @side1 = side1
-    @side2 = side2
-    @side3 = side3
+	def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
   end
 
   def equilateral?
-    side1 == side2 && side2 == side3
+    side_1 == side_2 && side_2 == side_3
   end
 
   def isosceles?
-    [side1, side2, side3].uniq.length == 2
+    [side_1, side_2, side_3].uniq.length == 2
   end
 
 	def scalene?
@@ -27,30 +27,33 @@ class Triangle
 		else puts "This triangle is scalene and mathematically boring."
     end
 
-		angles = calculate_angles(side1,side2,side3)
-		puts "The angles of this triangle are #{angles.join(",")}"
-
-		puts "This triangle is also a right triangle!" if angles.include? 90
+    recite_angle_facts
 	end
-
+  
+  def recite_angle_facts
+		angles = calculate_angles(side_1, side_2, side_3)
+		puts "The angles of this triangle are #{angles.join(",")}"
+		puts "This triangle is also a right triangle!" if angles.include? 90
+  end
+  
 	def calculate_angles(a, b, c)
-		angleA = radians_to_degrees(Math.acos((b**2 + c**2 - a**2) / (2.0 * b * c)))
-		angleB = radians_to_degrees(Math.acos((a**2 + c**2 - b**2) / (2.0 * a * c)))
-		angleC = radians_to_degrees(Math.acos((a**2 + b**2 - c**2) / (2.0 * a * b)))
-
-		[angleA, angleB, angleC]
+		angle_A = radians_to_degrees(Math.acos((b**2 + c**2 - a**2) / (2.0 * b * c)))
+		angle_B = radians_to_degrees(Math.acos((a**2 + c**2 - b**2) / (2.0 * a * c)))
+		angle_C = radians_to_degrees(Math.acos((a**2 + b**2 - c**2) / (2.0 * a * b)))
+		[angle_A, angle_B, angle_C]
 	end
 
 	def radians_to_degrees(rads)
 		(rads * 180 / Math::PI).round
 	end
+  
 end
 
-
 triangles = [
-	[5,5,5],
-	[5,12,13],
+	[5, 5, 5],
+	[5, 12, 13]
 ]
+
 triangles.each do |sides|
 	tri = Triangle.new(*sides)
 	tri.recite_facts

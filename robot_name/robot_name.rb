f9fb85885306d13f7202cbe51658c18730bbfec3
Dariusz Dzien <ddzien@enova.com>
2014-12-22 22:34:11 -0600

    More refactoring

--- robot_name/robot_name.rb
+++ robot_name/robot_name.rb
@@ -1,37 +1,29 @@
 class NameCollisionError < RuntimeError
+  def initialize(error_msg = 'There was a problem generating the robot name!')
+    super
+  end
 end
 
 class Robot
-  attr_accessor :name
+  attr_reader :name
 
-  @@registry
+  @@registry = []
 
   def initialize(args = {})
-    @@registry ||= []
-    @name_generator = args[:name_generator]
-
-    if @name_generator
-      p @name_generator.call
-      @name = @name_generator.call
-      generate_name
-    else
-      generate_char = -> { ('A'..'Z').to_a.sample }
-      generate_num = -> { rand(10) }
-
-      @name = "#{generate_char.call}#{generate_char.call}#{generate_num.call}#{generate_num.call}#{generate_num.call}"
-    end
-
-    raise NameCollisionError, 'There was a problem generating the robot name!' if !(name =~ /[[:alpha:]]{2}[[:digit:]]{3}/) || @@registry.include?(name)
+    @name = args[:name_generator] ? args[:name_generator].call : generate_name
+    check_for_errors
     @@registry << @name
   end
   
   private
+  def check_for_errors
+    raise NameCollisionError unless (name =~ /[[:alpha:]]{2}[[:digit:]]{3}/)
+    raise NameCollisionError if @@registry.include?(name)
+  end
   
   def generate_name
-    @testt = []
-    @testt << 2.times.collect { generate_char }
-    @testt << 3.times.collect { generate_num }
-    puts @testt.join
+    name = 2.times.collect { generate_char } + 3.times.collect { generate_num }
+    name.join
   end
   
   def generate_char
@@ -41,13 +33,12 @@ class Robot
   def generate_num
     rand(10)
   end
-  
 end
 
 robot = Robot.new
 puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
 
 # Errors!
-generator = -> { 'AA111' }
-Robot.new(name_generator: generator)
-Robot.new(name_generator: generator)
+#generator = -> { 'AA111' }
+#Robot.new(name_generator: generator)
+#Robot.new(name_generator: generator)

5ded06fb8d2970c18da230913be569c7839ad397
Jason Perkins <jperkins@me.com>
2014-12-23 12:40:58 -0600

    alphabetize methods

--- triangle_facts/triangle_facts.rb
+++ triangle_facts/triangle_facts.rb
@@ -5,36 +5,7 @@ class Triangle
   def initialize(side1, side2, side3)
     @side1, @side2, @side3 = side1, side2, side3
   end
-
-  def equilateral
-    side1 == side2 && side2 == side3
-  end
-
-  def isosceles
-    [side1, side2, side3].uniq.length == 2
-  end
-
-  def scalene
-    !equilateral && !isosceles
-  end
-
-  def recite_facts
-    if equilateral
-      puts 'This triangle is equilateral!'
-    elsif isosceles
-      puts 'This triangle is isosceles! Also, that word is hard to type.'
-    elsif scalene
-      puts 'This triangle is scalene and mathematically boring.'
-    end
-    
-    angles = self.calculate_angles(side1, side2, side3)
-
-    puts 'The angles of this triangle are ' + angles.join(',')
-
-    puts 'This triangle is also a right triangle!' if angles.include? 90
-    puts ''
-  end
-
+  
   def calculate_angles(a, b, c)  
     angle_a = radians_to_degrees(
       Math.acos((b**2 + c**2 - a**2) / (2.0 * b * c))
@@ -51,8 +22,37 @@ class Triangle
     [angle_a, angle_b, angle_c]
   end
 
+  def equilateral
+    side1 == side2 && side2 == side3
+  end
+
+  def isosceles
+    [side1, side2, side3].uniq.length == 2
+  end
+  
+  def recite_facts
+    if equilateral
+      puts 'This triangle is equilateral!'
+    elsif isosceles
+      puts 'This triangle is isosceles! Also, that word is hard to type.'
+    elsif scalene
+      puts 'This triangle is scalene and mathematically boring.'
+    end
+    
+    angles = self.calculate_angles(side1, side2, side3)
+
+    puts 'The angles of this triangle are ' + angles.join(',')
+
+    puts 'This triangle is also a right triangle!' if angles.include? 90
+    puts ''
+  end
+  
   def radians_to_degrees(rads)
     (rads * 180 / Math::PI).round
+  end
+
+  def scalene
+    !equilateral && !isosceles
   end
 end
 

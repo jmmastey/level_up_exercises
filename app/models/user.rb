df19af24bc049592614070683646d793e7abd5a9
paulh16 <paulh16@gmail.com>
2014-12-23 09:08:13 -0600

    Add User.rb validations for email and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable
+  devise :database_authenticatable, :registerable, :validatable
 
-  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i }
-  validates :password, length: { minimum: 5 }, allow_blank: true
+  #validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "Create new user" do
+
+    it "validates a new valid user" do
+      user = User.new
+      user.email = "paul@example.com"
+      user.password = "secret"
+      user.password_confirmation = "secret"
+
+      expect(user).to be_valid
+    end
+
+    it "does not validate a new invalid user" do
+      user = User.new
+      user.email = "haddad"
+      user.password = ""
+      user.password_confirmation = ""
+
+      user.valid?
+
+      expect(user).not_to be_valid
+    end
+
+    it "does not validate a new invalid user when passwords don't match" do
+      user = User.new
+      user.email = "paul@example.com"
+      user.password = "secret1"
+      user.password_confirmation = "secret2"
+
+      user.valid?
+
+      expect(user).not_to be_valid
+    end
+
+    it "does not validate a user with an invalid email address" do
+      user = User.new
+      user.email = "paul"
+      user.password = "secret"
+      user.password_confirmation = "secret"
+
+      expect(user).not_to be_valid
+    end
+
+    describe "validations" do
+
+      it { should validate_presence_of(:email) }
+      it { should validate_uniqueness_of(:email) }
+      it { should validate_presence_of(:password) }
+    end
+  end
 end

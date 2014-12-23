8a8a187c04caa5432f272ebb45db7c8e41974b69
Sreeram Mittapalli <smittapalli@enovafinancial.com>
2014-12-23 00:45:18 -0600

    wrote event spec for ical

--- happenings/spec/models/event_spec.rb
+++ happenings/spec/models/event_spec.rb
@@ -1,4 +1,19 @@
 require 'rails_helper'
 
 describe Event do
+  let(:event) { create :event }
+
+  describe 'ical' do
+    it 'should return an ical object' do
+      ical_event = event.to_ics
+      expect(ical_event.class).to eq(Icalendar::Event)
+    end
+
+    it 'should have properties of the event object' do
+      ical_event = event.to_ics
+      expect(ical_event.summary).to eq(event.title)
+      expect(ical_event.description).to match(/link/)
+      expect(ical_event.description).to match(/desc/)
+    end
+  end
 end

810902f19970c04bee575297c7d01d0e826761a2
jillalynch <jill.lynch5@gmail.com>
2014-12-24 14:23:38 -0600

    renamed a few classes

--- data_science/cohort.rb
+++ data_science/cohort.rb
@@ -1,27 +1,29 @@
 class Cohort
-  attr_accessor :name
+  attr_accessor :name, :pageviews
 
-  def initialize(name)
+  def initialize(name, pageviews)
     @name = name
+    @pageviews = pageviews
   end
 
   def conversion_rate
-
+    positive_conversions.to_f/cohort_views.to_f
   end
 
   def standard_error
-
+    Math.sqrt((conversion_rate*(1-conversion_rate)/cohort_views))
   end
 
+  #number data points that result 1 and cohort A
   def positive_conversions
-
+    @pageviews.select { |view| view.result == 1 }.size
   end
 
   def negative_conversions
-
+    @pageviews.select { |view| view.result == 0 }.size
   end
 
-  def total_views
-    3
+  def cohort_views
+    pageviews.size
   end
 end
--- data_science/spec/cohort_spec.rb
+++ data_science/spec/cohort_spec.rb
@@ -1,19 +1,120 @@
 require_relative '../cohort.rb'
 
-describe Cohort do
-  let(:cohort) { Cohort.new("A") }
+  describe Cohort do
+    context "when views are a mixed" do
+      let(:pageviews) { [double(:result => 1), double(:result => 0), double(:result => 1)] }
+      let(:cohort) { Cohort.new("A", pageviews) }
 
-  it "has a name" do
-    expect(cohort.name).to eq("A")
-  end
+      it "has a name" do
+        expect(cohort.name).to eq("A")
+      end
 
-  describe '#total_views' do
-    let(:cohort) { Cohort.new("A") }
+      describe '#total_views' do
+        it "counts the total views for a cohort" do
+          expect(cohort.cohort_views).to eq(3)
+        end
+      end
 
-    it "total number of views in the cohort" do
-      expect(cohort.total_views).to eq(3)
+      describe '#positive_conversions' do
+        it "counts the positives" do
+          expect(cohort.positive_conversions).to eq(2)
+        end
+      end
+
+      describe '#negative_conversions' do
+        it "counts the negatives" do
+          expect(cohort.negative_conversions).to eq(1)
+        end
+      end
+
+      describe '#conversion_rate' do
+        it "gives correct conversion_rate" do
+          expect(cohort.conversion_rate.round(2)).to eq(0.67)
+        end
+      end
+
+      describe '#standard_error' do
+        it "gives correct standard_error" do
+          expect(cohort.standard_error.round(2)).to eq(0.27)
+        end
+      end
     end
 
-  end
+    context "when view results are all negative" do
+      let(:pageviews) { [double(:result => 0), double(:result => 0), double(:result => 0)] }
+      let(:cohort) { Cohort.new("A", pageviews) }
 
-end
+      it "has a name" do
+        expect(cohort.name).to eq("A")
+      end
+
+      describe '#total_views' do
+        it "total number of views in the cohort" do
+          expect(cohort.cohort_views).to eq(3)
+        end
+      end
+
+      describe '#positive_conversions' do
+        it "total number of views in the cohort" do
+          expect(cohort.positive_conversions).to eq(0)
+        end
+      end
+
+      describe '#negative_conversions' do
+        it "total number of views in the cohort" do
+          expect(cohort.negative_conversions).to eq(3)
+        end
+      end
+
+      describe '#conversion_rate' do
+        it "gives correct conversion_rate" do
+          expect(cohort.conversion_rate.round(2)).to eq(0)
+        end
+      end
+
+      describe '#standard_error' do
+        it "gives correct standard_error" do
+          expect(cohort.standard_error.round(2)).to eq(0)
+        end
+      end
+    end
+
+    context "when view results are all positive" do
+      let(:pageviews) { [double(:result => 1), double(:result => 1), double(:result => 1)] }
+      let(:cohort) { Cohort.new("A", pageviews) }
+
+      it "has a name" do
+        expect(cohort.name).to eq("A")
+      end
+
+      describe '#total_views' do
+        it "total number of views in the cohort" do
+          expect(cohort.cohort_views).to eq(3)
+        end
+      end
+
+      describe '#positive_conversions' do
+        it "total number of views in the cohort" do
+          expect(cohort.positive_conversions).to eq(3)
+        end
+      end
+
+      describe '#negative_conversions' do
+        it "total number of views in the cohort" do
+          expect(cohort.negative_conversions).to eq(0)
+        end
+      end
+
+      describe '#conversion_rate' do
+        it "gives correct conversion_rate" do
+          expect(cohort.conversion_rate.round(2)).to eq(1)
+        end
+      end
+
+      describe '#standard_error' do
+        it "gives correct standard_error" do
+          expect(cohort.standard_error.round(2)).to eq(0)
+        end
+      end
+    end
+  end

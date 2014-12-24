c00bd2897d50db45e50cd50fdfdfb462178b970a
Jason Perkins <jperkins@me.com>
2014-12-24 11:23:43 -0600

    extracted methods

--- robot_name/robot_name.rb
+++ robot_name/robot_name.rb
@@ -3,25 +3,46 @@ class NameCollisionError < RuntimeError; end
 class Robot
   attr_accessor :name
 
-  @@registry
+  @@registry ||= []
 
   def initialize(args = {})
-    @@registry ||= []
-    @name_generator = args[:name_generator]
-
-    if @name_generator
-      @name = @name_generator.call
+    if args[:name_generator]
+      @name = args[:name_generator].call
     else
-      generate_char = -> { ('A'..'Z').to_a.sample }
-      generate_num = -> { rand(10) }
-
-      @name = "#{generate_char.call}#{generate_char.call}#{generate_num.call}#{generate_num.call}#{generate_num.call}"
+      @name = generate_name
     end
 
-    raise NameCollisionError, 'There was a problem generating the robot name!' if !(name =~ /[[:alpha:]]{2}[[:digit:]]{3}/) || @@registry.include?(name)
+    if name_is_unacceptable?(name)
+      # TODO: NameCollisionError and MalformedNameError
+      raise NameCollisionError, 'There was a problem generating the robot name!'
+    end
+    
     @@registry << @name
   end
+  
+  
+private
+  
+  def generate_name
+    generate_char = -> { ('A'..'Z').to_a.sample }
+    generate_num  = -> { rand(10) }
+
+    "#{generate_char.call}#{generate_char.call}#{generate_num.call}#{generate_num.call}#{generate_num.call}"
+  end
+  
+  def name_is_badly_formed?(name)
+    !(name =~ /[[:alpha:]]{2}[[:digit:]]{3}/)
+  end
+  
+  def name_exists_in_registry?(name)
+    @@registry.include?(name)
+  end
+  
+  def name_is_unacceptable?(name = '')
+    name_is_badly_formed?(name) || name_exists_in_registry?(name)
+  end
 end
+
 
 robot = Robot.new
 puts "My pet robot's name is #{robot.name}, but we usually call him sparky."

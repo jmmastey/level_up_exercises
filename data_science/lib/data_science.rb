b0eb38284bbb9ecf349f845dfc486b3c320c315e
Jason Perkins <jperkins@me.com>
2014-12-29 12:06:38 -0600

    rudimentary json loading

--- data_science/lib/data_science.rb
+++ data_science/lib/data_science.rb
@@ -1,5 +1,12 @@
+require 'json'
+
 class DataScience
+  # TODO: make sure the data loading is abstracted from the main 
+  # calculation code
   def initialize
+    raw_json = File.read(File.expand_path("../data/source_data.json"))
+    data     = JSON.parse(raw_json)
     
+    puts data
   end  
 end
--- data_science/spec/data_science_spec.rb
+++ data_science/spec/data_science_spec.rb
@@ -3,6 +3,25 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 describe "DataScience" do
   # let(:data_science) {  }
 
+
+=begin
+
+For a given experiment, we're looking to calculate the conversion rate of
+visitors as part of a split test. We care about the following factors:
+
+1. Total sample size 
+  
+2. Number of conversions for each part of the test.
+
+3. Percentage of conversion (including error bars) with a 95% confidence.
+
+4. Confidence level that the current leader is in fact better than random. You
+should use the Chi-square test for this, feel free to cheat with a simple
+calculator to get your initial calculations.
+  
+=end  
+  
+
   # -----------------------------------
   # Load data from CSV
   # -----------------------------------

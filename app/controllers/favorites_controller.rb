1d6580d17dfcef7505b66ac073658c4f8e8e2249
paulh16 <paulh16@gmail.com>
2015-01-02 11:27:26 -0600

    Complete feature: Unfavorite an artist while logged in

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,7 +4,15 @@ class FavoritesController < ApplicationController
   def create
     @artist = Artist.find(params[:artist_id])
     @artist.followers << current_user
-    flash[:notice] = "This artist is now a favorite of yours."
+    flash[:success] = "This artist is now a favorite of yours."
+    redirect_to @artist
+  end
+
+  def destroy
+    @artist = Artist.find(params[:artist_id])
+    favorite = current_user.favorites.find_by(artist_id: params[:artist_id])
+    favorite.destroy
+    flash[:success] = "This artist is no longer a favorite of yours."
     redirect_to @artist
   end
 end

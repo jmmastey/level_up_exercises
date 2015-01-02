a37299dc8669ec216e9008b2c7d59b056207d5f4
paulh16 <paulh16@gmail.com>
2015-01-02 10:03:55 -0600

    Complete Feature: Favorite artist while logged in

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,9 @@
 class FavoritesController < ApplicationController
 
   def create
-
+    @artist = Artist.find(params[:artist_id])
+    @artist.followers << current_user
+    flash[:notice] = "This artist is now a favorite of yours."
+    redirect_to @artist
   end
 end

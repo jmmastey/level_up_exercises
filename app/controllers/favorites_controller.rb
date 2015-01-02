0279b0dd1f99e02cdd55d94ddc4f47a684a2915a
paulh16 <paulh16@gmail.com>
2015-01-02 11:29:52 -0600

    Refactor favorites_controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,18 +1,23 @@
 class FavoritesController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_artist
 
   def create
-    @artist = Artist.find(params[:artist_id])
     @artist.followers << current_user
     flash[:success] = "This artist is now a favorite of yours."
     redirect_to @artist
   end
 
   def destroy
-    @artist = Artist.find(params[:artist_id])
     favorite = current_user.favorites.find_by(artist_id: params[:artist_id])
     favorite.destroy
     flash[:success] = "This artist is no longer a favorite of yours."
     redirect_to @artist
+  end
+
+  private
+
+  def find_artist
+    @artist = Artist.find(params[:artist_id])
   end
 end

6e699b02ddf98c592414053432b67cee85e26243
Dariusz Dzien <ddzien@enova.com>
2015-01-05 11:00:22 -0600

    Json import method created

--- data_science/lib/json_parser.rb
+++ data_science/lib/json_parser.rb
@@ -1,7 +1,13 @@
+require 'json'
+
 class JsonParser
 
-	def a
+	def initialize(json)
+		file = File.read(json)
+		data_hash = JSON.prase(file)
 
 	end
 
 end
+
+JsonParser.new('../source_data.json')
--- data_science/spec/json_tests.rb
+++ data_science/spec/json_tests.rb
@@ -1,13 +1,18 @@
 require 'spec_helper'
 
-	describe JsonParser do
+describe JsonParser do
 
-		it "should return exception for invalid file" do
-			a = JsonParser.new
-			puts a
-			puts "test"
-			a.start
-			# expect { JsonParser.new("blah.json") }.to raise_error
-		end
-		
+	it "should return exception for invalid file" do
+		expect { JsonParser.new("blahksd.json") }.to raise_error
 	end
+
+	it "should throw exception for nonexistent method" do 
+		expect { JsonParser.new.blah }.to raise_error
+	end
+
+	it "should create a hash with valid json input" do
+		test = JsonParser.new("source_data.json")
+		test.
+	end
+
+end

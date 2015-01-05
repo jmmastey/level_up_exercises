b92b0d18d64a2794f7c571c6362d3c62af9374ca
Dariusz Dzien <ddzien@enova.com>
2015-01-05 12:43:44 -0600

    Add hash existence test

--- data_science/lib/json_parser.rb
+++ data_science/lib/json_parser.rb
@@ -1,13 +1,21 @@
 require 'json'
 
 class JsonParser
+	attr_accessor :data_hash
 
 	def initialize(json)
 		file = File.read(json)
-		data_hash = JSON.prase(file)
+		@data_hash = JSON.parse(file)
+		puts data_hash.length
 
+	end
+
+	def talk
+		puts "test"
 	end
 
 end
 
-JsonParser.new('../source_data.json')
+#puts Dir.pwd
+
+#JsonParser.new("source_data.json")
--- data_science/spec/json_tests.rb
+++ data_science/spec/json_tests.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe JsonParser do
-
+	# let(:parser) { JsonParser.new }
 	it "should return exception for invalid file" do
-		expect { JsonParser.new("blahksd.json") }.to raise_error
+ 		expect { JsonParser.new("blahksd.json") }.to raise_error
 	end
 
 	it "should throw exception for nonexistent method" do 
@@ -12,7 +12,7 @@ describe JsonParser do
 
 	it "should create a hash with valid json input" do
 		test = JsonParser.new("source_data.json")
-		test.
+		expect(test.data_hash.length).to be > 0
 	end
 
 end

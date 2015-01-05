d628cbbc566914fa8b17f508724aff2cad5c4506
Dan Kotowski <dkotowski@enova.com>
2015-01-05 16:28:01 -0600

    Resized columns

--- yadda/schema.sql
+++ yadda/schema.sql
@@ -1,26 +1,26 @@
 CREATE TABLE addresses (
   id integer PRIMARY KEY,
-  address_line_1 varchar NOT NULL,
-  address_line_2 varchar,
-  city varchar NOT NULL,
-  region varchar NOT NULL,
-  postal_code varchar NOT NULL,
-  country varchar NOT NULL,
+  address_line_1 varchar(50) NOT NULL,
+  address_line_2 varchar(50),
+  city varchar(50) NOT NULL,
+  region varchar(10) NOT NULL,
+  postal_code varchar(10) NOT NULL,
+  country varchar(50) NOT NULL,
   created_at timestamp without time zone NOT NULL,
-  created_by varchar NOT NULL,
+  created_by varchar(40) NOT NULL,
   updated_at timestamp without time zone,
-  updated_by varchar
+  updated_by varchar(40)
 );
 
 CREATE TABLE breweries (
   id integer PRIMARY KEY,
-  name varchar NOT NULL,
+  name varchar(30) NOT NULL,
   description text NOT NULL,
   address_id integer NOT NULL,
   created_at timestamp without time zone NOT NULL,
-  created_by varchar NOT NULL,
+  created_by varchar(40) NOT NULL,
   updated_at timestamp without time zone,
-  updated_by varchar,
+  updated_by varchar(40),
   CONSTRAINT brewery_address_id_fkey FOREIGN KEY (address_id)
     REFERENCES addresses (id) MATCH SIMPLE
     ON UPDATE CASCADE ON DELETE CASCADE
@@ -28,30 +28,30 @@ CREATE TABLE breweries (
 
 CREATE TABLE users (
   id integer PRIMARY KEY,
-  username varchar NOT NULL UNIQUE,
-  email varchar NOT NULL UNIQUE,
-  encrypted_password varchar NOT NULL,
-  reset_password_token varchar,
+  username varchar(40) NOT NULL UNIQUE,
+  email varchar(40) NOT NULL UNIQUE,
+  encrypted_password varchar(40) NOT NULL,
+  reset_password_token varchar(40),
   reset_password_sent_at timestamp without time zone,
   active boolean NOT NULL,
   sign_in_count integer DEFAULT 0 NOT NULL,
-  location varchar,
+  location varchar(40),
   created_at timestamp without time zone NOT NULL,
-  created_by varchar NOT NULL,
+  created_by varchar(40) NOT NULL,
   updated_at timestamp without time zone,
-  updated_by varchar
+  updated_by varchar(40)
 );
 
 CREATE TABLE beers (
   id integer PRIMARY KEY,
-  style varchar NOT NULL,
+  style varchar(40) NOT NULL,
   description text NOT NULL,
   year char(4) NOT NULL,
   brewery_id integer NOT NULL,
   created_at timestamp without time zone NOT NULL,
-  created_by varchar NOT NULL,
+  created_by varchar(40) NOT NULL,
   updated_at timestamp without time zone,
-  updated_by varchar,
+  updated_by varchar(40),
   CONSTRAINT beer_brewery_id_fkey FOREIGN KEY (brewery_id)
     REFERENCES breweries (id) MATCH SIMPLE
     ON UPDATE CASCADE ON DELETE CASCADE
@@ -64,12 +64,12 @@ CREATE TABLE ratings (
   taste smallint NOT NULL,
   feel smallint NOT NULL,
   overall smallint NOT NULL,
-  beer_id integer NOT NULL,                                     -- Beer to be rated
-  user_id integer NOT NULL,                                     -- Reviewer
+  beer_id integer NOT NULL,                   -- Beer to be rated
+  user_id integer NOT NULL,                   -- Reviewer
   created_at timestamp without time zone NOT NULL,
-  created_by varchar NOT NULL,
+  created_by varchar(40) NOT NULL,
   updated_at timestamp without time zone,
-  updated_by varchar,
+  updated_by varchar(40),
   CONSTRAINT rating_beer_id_fkey FOREIGN KEY (beer_id)
     REFERENCES beers (id) MATCH SIMPLE
     ON UPDATE CASCADE ON DELETE CASCADE,
@@ -80,14 +80,14 @@ CREATE TABLE ratings (
 
 CREATE TABLE reviews (
   id integer PRIMARY KEY,
-  title varchar NOT NULL,
+  title varchar(40) NOT NULL,
   body text NOT NULL,
-  beer_id integer NOT NULL,                                     -- Beer to be reviewed
-  user_id integer NOT NULL,                                     -- Reviewer
+  beer_id integer NOT NULL,                   -- Beer to be reviewed
+  user_id integer NOT NULL,                   -- Reviewer
   created_at timestamp without time zone NOT NULL,
-  created_by varchar NOT NULL,
+  created_by varchar(40) NOT NULL,
   updated_at timestamp without time zone,
-  updated_by varchar,
+  updated_by varchar(40),
   CONSTRAINT review_beer_id_fkey FOREIGN KEY (beer_id)
     REFERENCES beers (id) MATCH SIMPLE
     ON UPDATE CASCADE ON DELETE CASCADE,

af023f73edb18132bd93b2e6089c4f8f06a6cf11
Jason Perkins <jperkins@me.com>
2015-01-05 09:25:07 -0600

    calculates conversions for each cohort

--- data_science/lib/data_science.rb
+++ data_science/lib/data_science.rb
@@ -1,10 +1,19 @@
 require 'json'
 
 class DataScience
+  attr_reader :conversions
+  
   # TODO: make sure the data loading is abstracted from the main 
   # calculation code
   def initialize(raw_json)
-    @sample = JSON.parse(raw_json)    
+    @sample = JSON.parse(raw_json)   
+    @conversions = Hash.new(0)
+    
+    @sample.each do |current_sample|
+      if current_sample['result'].to_i > 0
+        @conversions[current_sample['cohort']] += current_sample['result']
+      end
+    end 
   end
   
   def sample_size
--- data_science/spec/data_science_spec.rb
+++ data_science/spec/data_science_spec.rb
@@ -1,31 +1,40 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "DataScience" do
-  # let(:data_science) {  }
-
-
-=begin
-
-For a given experiment, we're looking to calculate the conversion rate of
-visitors as part of a split test. We care about the following factors:
-
-1. Total sample size 
+  # let(:sample) {  }
   
-2. Number of conversions for each part of the test.
+  # calculate the conversion rate of visitors as part of a split test.
 
-3. Percentage of conversion (including error bars) with a 95% confidence.
-
-4. Confidence level that the current leader is in fact better than random. You
-should use the Chi-square test for this, feel free to cheat with a simple
-calculator to get your initial calculations.
-  
-=end  
-  
-
-  # -----------------------------------
-  # Load data from CSV
-  # -----------------------------------
-  it 'does something' do
-    raise
+  # Total sample size 
+  it 'returns the total sample size' do
+    data_science = DataScience.new(
+      File.read(File.expand_path("data/source_data.json"))    
+    )
+    
+    expect(data_science.sample_size).to eq(2892)
   end
+  
+  it 'calculates the conversions for each cohort' do
+    data_science = DataScience.new(
+      File.read(File.expand_path("data/source_data.json"))    
+    )
+    
+    expect(data_science.conversions['A']).to eq(47)
+    expect(data_science.conversions['B']).to eq(79)
+  end
+  
+  # Number of conversions for each part of the test.
+  it 'returns the number/conversions for each part of the test'
+  
+  # Percentage of conversion (including error bars) with a 95% confidence.
+  it 'returns the percentage of conversions (w/error bars) with 95% confidence'
+  
+  # Confidence level that the current leader is in fact better than random. 
+  # You should use the Chi-square test for this. Feel free to cheat with 
+  # a simple calculator to get your initial calculations.
+  #
+  # http://www.usereffect.com/split-test-calculator
+  it 'calculates confidence interval that current leader is better than random'
+  
+  
 end

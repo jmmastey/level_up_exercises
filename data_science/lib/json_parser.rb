f135bbe3c18158da8a2c15edd7d0be74c45eb4b3
Dariusz Dzien <ddzien@enova.com>
2015-01-05 17:32:29 -0600

    Modify parsed data test

--- data_science/lib/json_parser.rb
+++ data_science/lib/json_parser.rb
@@ -1,4 +1,5 @@
 require 'json'
+require 'abanalyzer'
 
 class JsonParser
   attr_accessor :data_hash
@@ -6,20 +7,27 @@ class JsonParser
   def initialize(json)
     @data_hash = parse_and_split(json)
     # puts @data_hash.group_by { |k, v| k == "cohort" and v == "A"  }
-    puts @data_hash
+    #puts @data_hash
+
+    tester = ABAnalyzer::ABTest.new @data_hash
+    #puts tester.different?
+
   end
 
   private
 
   def parse_and_split(json)
     data = JSON.parse(File.read(json))
-    [
-    data.count { |hash| hash.value?("A") and hash["result"] == 1 },
-    data.count { |hash| hash.value?("A") and hash["result"] == 0 },
-    data.count { |hash| hash.value?("B") and hash["result"] == 1 },
-    data.count { |hash| hash.value?("B") and hash["result"] == 0 },
-  ]
+    { a_group: {
+      pass: data.count { |hash| hash.value?("A") and hash["result"] == 1 },
+      fail: data.count { |hash| hash.value?("A") and hash["result"] == 0 },
+    },
+    b_group: {
+      pass: data.count { |hash| hash.value?("B") and hash["result"] == 1 },
+      fail: data.count { |hash| hash.value?("B") and hash["result"] == 0 },
+  }
+  }
   end
 end
 
-JsonParser.new("../source_data.json")
+#JsonParser.new("../source_data.json")
--- data_science/spec/json_tests.rb
+++ data_science/spec/json_tests.rb
@@ -10,9 +10,17 @@ describe JsonParser do
 		expect { JsonParser.new.blah }.to raise_error
 	end
 
-	it "should create a hash with valid json input" do
+#	it "should create a hash with valid json input" do
+#		test = JsonParser.new("source_data.json")
+#		expect(test.data_hash.length).to be > 0
+#	end
+
+	it "should return results for each group" do
 		test = JsonParser.new("source_data.json")
-		expect(test.data_hash.length).to be > 0
+			puts test.data_hash
+			expect(test.data_hash).to include(:a_group, :b_groups)
+			expect(test.data_hash[:a_group]). to include(:pass)
+
 	end
 
 end

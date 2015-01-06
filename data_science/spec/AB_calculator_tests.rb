013f2a096f8d722cc97ac6ab486404f210c1ea47
Dariusz Dzien <ddzien@enova.com>
2015-01-06 10:39:16 -0600

    Split up program into two classes

---
+++ data_science/spec/AB_calculator_tests.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe AB_calculator do
+	it "should create a new test object" do
+		
+	end
+
+
+	
+end
--- data_science/spec/spec_helper.rb
+++ data_science/spec/spec_helper.rb
@@ -1,3 +1,4 @@
 require_relative '../lib/json_parser.rb'
+require_relative '../lib/AB_calculator.rb'
 
 require 'yaml'

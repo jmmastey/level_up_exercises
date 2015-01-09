025b53d42bf6526f6af320583221fed34ea67b10
Jason Perkins <jperkins@me.com>
2015-01-09 09:34:52 -0600

    removed dead code

--- data_science/spec/spec_helper.rb
+++ data_science/spec/spec_helper.rb
@@ -8,6 +8,3 @@ require_relative '../lib/data_science'
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
-
-# RSpec.configure do |config|
-# end

3556c89a62fd98a77dbf799a81f52fc2dc5b5c92
magicalbanana <kareemgan@gmail.com>
2015-01-09 13:49:38 -0600

    added dinosaur object.

---
+++ dino_catalog/dinosaur.rb
@@ -0,0 +1,22 @@
+require 'securerandom'
+
+class Dinosaur
+
+  attr_accessor :name, :period, :continent, :diet, :weight, :ambulation, :description
+
+  def initialize(options = {})
+    @name = options[:name] || nil
+    @period = options[:period] || nil
+    @continent = options[:continent] || nil
+    @diet = options[:diet] || nil
+    @weight = options[:weight] || nil
+    @ambulation = options[:ambulation] || nil
+    @description = options[:dscription] || nil
+  end
+
+  def to_hash
+    Hash[*instance_variables.map { |v|
+      [v.to_s.delete("@").to_sym, instance_variable_get(v)]
+    }.flatten]
+  end
+end

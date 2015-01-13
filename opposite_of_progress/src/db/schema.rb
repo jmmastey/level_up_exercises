7d1d2e83ecfb1e7071b5f4116df6d07f3199014a
Aushadhee Mahakumarage <mahakumarage@gmail.com>
2015-01-13 15:58:54 -0600

    fixed some problems with views

--- opposite_of_progress/src/db/schema.rb
+++ opposite_of_progress/src/db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20150113173732) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "bills", force: true do |t|
     t.string   "bill_type"
     t.string   "official_title"
@@ -33,7 +30,7 @@ ActiveRecord::Schema.define(version: 20150113173732) do
     t.string   "enacted_as"
     t.string   "latest_version_pdf"
     t.boolean  "active"
-    t.string   "offical_id"
+    t.string   "official_id"
   end
 
   create_table "good_deeds", force: true do |t|
@@ -69,7 +66,7 @@ ActiveRecord::Schema.define(version: 20150113173732) do
     t.string   "website"
   end
 
-  add_index "legislators", ["bioguide_id"], name: "index_legislators_on_bioguide_id", unique: true, using: :btree
+  add_index "legislators", ["bioguide_id"], name: "index_legislators_on_bioguide_id", unique: true
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -90,8 +87,8 @@ ActiveRecord::Schema.define(version: 20150113173732) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
--- opposite_of_progress/src/spec/factories/bills.rb
+++ opposite_of_progress/src/spec/factories/bills.rb
@@ -6,5 +6,6 @@ FactoryGirl.define do
     congress 113
     introduced_on '2014-01-05'
     last_action_at { rand(1..100).days.ago }
+    official_id { "#{bill_type}#{number}-#{congress}" }
   end
 end

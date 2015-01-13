8a8134be63b61ebbaeb9f628a11d5ba27695a1c2
magicalbanana <kareemgan@gmail.com>
2015-01-13 16:50:20 -0600

    added dinosaur class and modified.

--- dino_catalog/dinosaur.rb
+++ dino_catalog/dinosaur.rb
@@ -5,18 +5,26 @@ class Dinosaur
   attr_accessor :name, :period, :continent, :diet, :weight, :ambulation, :description
 
   def initialize(options = {})
-    @name = options[:name] || nil
-    @period = options[:period] || nil
-    @continent = options[:continent] || nil
-    @diet = options[:diet] || nil
+    @name = lower_case(options[:name]) || nil
+    @period = lower_case(options[:period]) || nil
+    @continent = lower_case(options[:continent]) || nil
+    @diet = lower_case(options[:diet]) || nil
     @weight = options[:weight] || nil
-    @ambulation = options[:ambulation] || nil
-    @description = options[:description] || nil
+    @ambulation = lower_case(options[:ambulation]) || nil
+    @description = lower_case(options[:description]) || nil
   end
 
   def to_hash
     Hash[*instance_variables.map { |v|
       [v.to_s.delete("@").to_sym, instance_variable_get(v)]
     }.flatten]
+  end
+
+  def lower_case(attribute)
+    if attribute.nil?
+      return false
+    else
+      attribute.downcase
+    end
   end
 end

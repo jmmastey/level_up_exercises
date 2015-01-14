6a99b63765405cf41b9b90eab6d88203a0ce460d
Dariusz Dzien <ddzien@enova.com>
2015-01-14 14:47:22 -0600

    Fix rubocop issues

--- data_science/lib/runner.rb
+++ data_science/lib/runner.rb
@@ -7,16 +7,16 @@ summary = ABDataSummary.new("../source_data.json")
 a_cohort = Cohort.new(summary.a_conv, summary.a_nonconv)
 b_cohort = Cohort.new(summary.b_conv, summary.b_nonconv)
 a_b_test = ABCalculator.new(
-	a_cohort.conversions, a_cohort.nonconvs, 
-	b_cohort.conversions, b_cohort.nonconvs
+  a_cohort.conversions, a_cohort.nonconvs,
+  b_cohort.conversions, b_cohort.nonconvs
 )
 if a_b_test.different?
-	puts "Samples are different at 0.05 significance"
-	if a_cohort.conversion_rate > b_cohort.conversion_rate
-		puts "A is winner"
-	else
-		puts "B is winner"
-	end
+  puts "Samples are different at 0.05 significance"
+  if a_cohort.conversion_rate > b_cohort.conversion_rate
+    puts "A is winner"
+  else
+    puts "B is winner"
+  end
 end
 
 puts "Confidence level: #{a_b_test.confidence_level}\n\n"
@@ -25,16 +25,16 @@ puts "Example with A as winner:"
 a_cohort = Cohort.new(100, 51)
 b_cohort = Cohort.new(51, 50)
 a_b_test = ABCalculator.new(
-	a_cohort.conversions, a_cohort.nonconvs, 
-	b_cohort.conversions, b_cohort.nonconvs
+  a_cohort.conversions, a_cohort.nonconvs,
+  b_cohort.conversions, b_cohort.nonconvs
 )
 if a_b_test.different?
-	puts "Samples are different at 0.05 significance"
-	if a_cohort.conversion_rate > b_cohort.conversion_rate
-		puts "A is winner"
-	else
-		puts "B is winner"
-	end
+  puts "Samples are different at 0.05 significance"
+  if a_cohort.conversion_rate > b_cohort.conversion_rate
+    puts "A is winner"
+  else
+    puts "B is winner"
+  end
 end
 
 puts "Confidence level: #{a_b_test.confidence_level}\n\n"
@@ -43,19 +43,18 @@ puts "Example with no winner:"
 a_cohort = Cohort.new(52, 51)
 b_cohort = Cohort.new(51, 52)
 a_b_test = ABCalculator.new(
-	a_cohort.conversions, a_cohort.nonconvs, 
-	b_cohort.conversions, b_cohort.nonconvs
+  a_cohort.conversions, a_cohort.nonconvs,
+  b_cohort.conversions, b_cohort.nonconvs
 )
 if a_b_test.different?
-	puts "Samples are different at 0.05 significance"
-	if a_cohort.conversion_rate > b_cohort.conversion_rate
-		puts "A is winner"
-	else
-		puts "B is winner"
-	end
+  puts "Samples are different at 0.05 significance"
+  if a_cohort.conversion_rate > b_cohort.conversion_rate
+    puts "A is winner"
+  else
+    puts "B is winner"
+  end
 else
-	puts "No winner at 0.05 significance"
+  puts "No winner at 0.05 significance"
 end
 
 puts "Confidence level: #{a_b_test.confidence_level}\n\n"
-

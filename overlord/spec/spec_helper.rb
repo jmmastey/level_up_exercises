bd8710478b6e5dfd7292b446383bf470a27b28e8
Jeremy Finzel <jfinzel@enova.com>
2015-01-14 09:32:11 -0600

    Rspec tags

--- overlord/spec/spec_helper.rb
+++ overlord/spec/spec_helper.rb
@@ -42,6 +42,8 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin

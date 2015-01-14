3c18348fd37a96a6861a90889c3425cfffae1fc8
magicalbanana <kareemgan@gmail.com>
2015-01-14 14:16:17 -0600

    refactored initialize to not use inline variable assignment.

--- triangle_facts/triangle_facts.rb
+++ triangle_facts/triangle_facts.rb
@@ -3,7 +3,9 @@ class Triangle
 	attr_accessor :side1,:side2,:side3
 
 	def initialize(side1,side2,side3)
-		@side1,@side2,@side3 = side1,side2,side3
+		@side1 = side1
+    @side2 = side2
+    @side3 = side3
 	end
 
 	def equilateral()
@@ -24,8 +26,8 @@ class Triangle
 
 	def recite_facts
 		puts 'This triangle is equilateral!' if equilateral
-		puts 'This triangle is isosceles! Also, that word is hard to type.' if isosceles 
-		puts 'This triangle is scalene and mathematically boring.' if scalene 
+		puts 'This triangle is isosceles! Also, that word is hard to type.' if isosceles
+		puts 'This triangle is scalene and mathematically boring.' if scalene
 
 		angles = self.calculate_angles(side1,side2,side3)
 		puts 'The angles of this triangle are ' + angles.join(',')

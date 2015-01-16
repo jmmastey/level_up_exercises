6a6bbe63cc514d84d4bbb526b1af558b059a3ccf
Jason Perkins <jperkins@me.com>
2015-01-16 11:17:15 -0600

    introducted subject in bomb spec

--- overlord/spec/bomb_spec.rb
+++ overlord/spec/bomb_spec.rb
@@ -1,13 +1,11 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe Overlord::Bomb do
-  # let(:overlord) do
-  #   Overlord.new
-  # end
+  subject { Overlord::Bomb.new }
 
   describe '#new' do
     it 'returns an inactive bomb' do
-      expect(Overlord::Bomb.new).not_to be_active
+      expect(subject).not_to be_active
     end
 
     it 'initializes itself to supplied parameters' do
@@ -19,10 +17,9 @@ describe Overlord::Bomb do
     context 'when the bomb is inactive' do
       context 'when it receives a valid activation code' do
         it 'activates the bomb' do
-          bomb = Overlord::Bomb.new
-          bomb.process_code('1234')
+          subject.process_code('1234')
 
-          expect(bomb).to be_active
+          expect(subject).to be_active
         end
       end
     end
@@ -30,12 +27,11 @@ describe Overlord::Bomb do
     context 'when the bomb is active' do
       context 'when it receives a valid deactivation code' do
         it 'activates the bomb' do
-          bomb = Overlord::Bomb.new
-          bomb.process_code('1234')
-          bomb.process_code('0000')
+          subject.process_code('1234')
+          subject.process_code('0000')
 
 
-          expect(bomb).not_to be_active
+          expect(subject).not_to be_active
         end
       end
     end
@@ -43,9 +39,8 @@ describe Overlord::Bomb do
 
   describe '#initialize_session' do
     it 'writes its state to the supplied hash' do
-      bomb = Overlord::Bomb.new
-      bomb.process_code('1234')
-      session = bomb.initialize_session
+      subject.process_code('1234')
+      session = subject.initialize_session
 
       expect(session[:state]).to eq('activated')
     end

ac3029cb63f6299bb897db8dd1febc4896e38608
rcowan <ryancowan@gmail.com>
2015-01-21 14:51:10 -0600

    rewrite insert_record and person_record methods

--- yadda/data-generator.rb
+++ yadda/data-generator.rb
@@ -8,14 +8,14 @@ require 'faker'
 
 def main
   generate_person_records 1
-  generate_brewery_records 5
-  generate_beer_records 5
-  generate_rating_records 3
+  # generate_brewery_records 5
+  # generate_beer_records 5
+  # generate_rating_records 3
 
   display_records "person"
-  display_records "brewery"
-  display_records "beer"
-  display_records "rating"
+  # display_records "brewery"
+  # display_records "beer"
+  # display_records "rating"
 end
 
 def generate_rating_records count=100
@@ -65,10 +65,10 @@ end
 def generate_person_records count=100
   count.times do
     insert_record "person", {
-      name: "'#{Faker::Name.first_name}'",
-      email: "'#{Faker::Internet.email}'",
-      birthday: "'#{Faker::Date.between("1900-01-01", Date.today)}'",
-      description: "'#{Faker::Lorem.sentence(3)}'",
+      name: Faker::Name.first_name,
+      email: Faker::Internet.email,
+      birthday: Faker::Date.between("1900-01-01", Date.today),
+      description: Faker::Lorem.sentence(3),
       updated_by: 1
     }
   end
@@ -76,10 +76,10 @@ end
 
 def insert_record table, info_hash
   @conn.exec( "INSERT INTO #{table} (#{info_hash.keys.join(",")})" \
-    "VALUES(#{info_hash.values.join(",")})" )
+    "VALUES('#{info_hash.values.join("','")}')" )
 end
 
-def display_records table="person"
+def display_records table
   @conn.exec( "SELECT * FROM #{table}" ) do |result|
     result.each do |row|
       puts row

818080a5d02f3afaabd1f48f8df44b55ce8f7789
rcowan <ryancowan@gmail.com>
2015-01-21 16:52:44 -0600

    data generator refactor

--- yadda/data-generator.rb
+++ yadda/data-generator.rb
@@ -8,14 +8,14 @@ require 'faker'
 
 def main
   generate_person_records 1
-  # generate_brewery_records 5
-  # generate_beer_records 5
-  # generate_rating_records 3
+  generate_brewery_records 5
+  generate_beer_records 5
+  generate_rating_records 3
 
   display_records "person"
-  # display_records "brewery"
-  # display_records "beer"
-  # display_records "rating"
+  display_records "brewery"
+  display_records "beer"
+  display_records "rating"
 end
 
 def generate_rating_records count=100
@@ -23,12 +23,13 @@ def generate_rating_records count=100
     insert_record "rating", {
       person_id: 1,
       beer_id: 1,
-      look: "'#{Faker::Number.digit}'",
-      smell: "'#{Faker::Number.digit}'",
-      taste: "'#{Faker::Number.digit}'",
-      feel: "'#{Faker::Number.digit}'",
-      overall: "'#{Faker::Number.digit}'",
-      description: "'#{Faker::Lorem.sentence(3)}'",
+      look: Faker::Number.digit,
+      smell: Faker::Number.digit,
+      taste: Faker::Number.digit,
+      feel: Faker::Number.digit,
+      overall: Faker::Number.digit,
+      description: Faker::Lorem.sentence(3),
+      updated_by: 1
     }
   end
 end
@@ -37,10 +38,9 @@ def generate_beer_records count=100
   count.times do
     insert_record "beer", {
       brewery_id: 1,
-      style: "'#{Faker::Hacker.noun}'",
-      description: "'#{Faker::Lorem.sentence(3)}'",
-      brewing_year: "'#{Faker::Date.between("2000-01-01", Date.today).year}'",
-      updated_at: "NOW()",
+      style: Faker::Hacker.noun,
+      description: Faker::Lorem.sentence(3),
+      brewing_year: Faker::Date.between("2000-01-01", Date.today).year,
       updated_by: 1
     }
   end
@@ -49,14 +49,13 @@ end
 def generate_brewery_records count=100
   count.times do
     insert_record "brewery", {
-      name: "'#{Faker::Company.name}'",
-      address: "'#{Faker::Address.street_address}'",
-      city: "'#{Faker::Address.city}'",
-      state: "'#{Faker::Address.state_abbr}'",
-      postal_code: "'#{Faker::Number.number(5)}'",
-      description: "'#{Faker::Lorem.sentence(3)}'",
-      founding_year: "'#{Faker::Date.between("1500-01-01", Date.today).year}'",
-      updated_at: "NOW()",
+      name: Faker::Company.name,
+      address: Faker::Address.street_address,
+      city: Faker::Address.city,
+      state: Faker::Address.state_abbr,
+      postal_code: Faker::Number.number(5),
+      description: Faker::Lorem.sentence(3),
+      founding_year: Faker::Date.between("1500-01-01", Date.today).year,
       updated_by: 1
     }
   end

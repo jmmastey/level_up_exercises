566f24f242c6fbb09f650fa52701d90e8fd72425
rcowan <ryancowan@gmail.com>
2015-01-21 09:36:13 -0600

    refactor of data generator

--- yadda/data-generator.rb
+++ yadda/data-generator.rb
@@ -20,54 +20,63 @@ end
 
 def generate_rating_records count=100
   count.times do
-    look = Faker::Number.digit
-    smell = Faker::Number.digit
-    taste = Faker::Number.digit
-    feel = Faker::Number.digit
-    overall = Faker::Number.digit
-    description = Faker::Lorem.sentence(3)
-
-    @conn.exec( "INSERT INTO rating (person_id, beer_id, look, smell, taste, feel, overall, description, modified_by)" \
-      "VALUES(1, 1, #{look}, #{smell}, #{taste}, #{feel}, #{overall}, '#{description}', 1)" )
+    insert_record "rating", {
+      person_id: 1,
+      beer_id: 1,
+      look: "'#{Faker::Number.digit}'",
+      smell: "'#{Faker::Number.digit}'",
+      taste: "'#{Faker::Number.digit}'",
+      feel: "'#{Faker::Number.digit}'",
+      overall: "'#{Faker::Number.digit}'",
+      description: "'#{Faker::Lorem.sentence(3)}'",
+    }
   end
 end
 
 def generate_beer_records count=100
   count.times do
-    style = Faker::Hacker.noun
-    description = Faker::Lorem.sentence(3)
-    brewing_year = Faker::Date.between("2000-01-01", Date.today).year
-
-    @conn.exec( "INSERT INTO beer (brewery_id, style, description, brewing_year, modified_on, modified_by)" \
-      "VALUES(1, '#{style}', '#{description}', '#{brewing_year}', NOW(), 1)" )
+    insert_record "beer", {
+      brewery_id: 1,
+      style: "'#{Faker::Hacker.noun}'",
+      description: "'#{Faker::Lorem.sentence(3)}'",
+      brewing_year: "'#{Faker::Date.between("2000-01-01", Date.today).year}'",
+      modified_on: "NOW()",
+      modified_by: 1
+    }
   end
 end
 
 def generate_brewery_records count=100
   count.times do
-    name = Faker::Company.name
-    address = Faker::Address.street_address
-    city = Faker::Address.city
-    state = Faker::Address.state_abbr
-    zip_code = Faker::Number.number(5)
-    description = Faker::Lorem.sentence(3)
-    founding_year = Faker::Date.between("1500-01-01", Date.today).year
-
-    @conn.exec( "INSERT INTO brewery(name, address, city, state, zip_code, description, founding_year, modified_on, modified_by) " \
-      "VALUES('#{name}', '#{address}', '#{city}', '#{state}', '#{zip_code}', '#{description}', '#{founding_year}', NOW(), 1)" )
+    insert_record "brewery", {
+      name: "'#{Faker::Company.name}'",
+      address: "'#{Faker::Address.street_address}'",
+      city: "'#{Faker::Address.city}'",
+      state: "'#{Faker::Address.state_abbr}'",
+      zip_code: "'#{Faker::Number.number(5)}'",
+      description: "'#{Faker::Lorem.sentence(3)}'",
+      founding_year: "'#{Faker::Date.between("1500-01-01", Date.today).year}'",
+      modified_on: "NOW()",
+      modified_by: 1
+    }
   end
 end
 
 def generate_person_records count=100
   count.times do
-    name = Faker::Name.first_name
-    email = Faker::Internet.email
-    birthday = Faker::Date.between("1900-01-01", Date.today)
-    description = Faker::Lorem.sentence(3)
-
-    @conn.exec( "INSERT INTO person(name, email, description, birthday, modified_by) " \
-      "VALUES('#{name}', '#{email}', '#{description}', '#{birthday}', 1)" )
+    insert_record "person", {
+      name: "'#{Faker::Name.first_name}'",
+      email: "'#{Faker::Internet.email}'",
+      birthday: "'#{Faker::Date.between("1900-01-01", Date.today)}'",
+      description: "'#{Faker::Lorem.sentence(3)}'",
+      modified_by: 1
+    }
   end
+end
+
+def insert_record table, info_hash
+  @conn.exec( "INSERT INTO #{table} (#{info_hash.keys.join(",")})" \
+    "VALUES(#{info_hash.values.join(",")})" )
 end
 
 def display_records table="person"

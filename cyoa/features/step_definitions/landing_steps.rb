63864bad9f73da3bee4b81c56320df86131681f0
Jeremy Finzel <jfinzel@enova.com>
2015-01-23 09:11:05 -0600

    Added more testing steps

---
+++ cyoa/features/step_definitions/landing_steps.rb
@@ -0,0 +1,19 @@
+Then(/^I see weather information$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I see a register now option$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I see a login option$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I see a logout option$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I see a settings option$/) do
+  pending # express the regexp above with the code you wish you had
+end
---
+++ cyoa/features/step_definitions/login_steps.rb
@@ -0,0 +1,7 @@
+Given(/^I am not logged in$/) do
+  # No Action - Not logged in by default after app start
+end
+
+Given(/^I am logged in$/) do
+  pending # express the regexp above with the code you wish you had
+end

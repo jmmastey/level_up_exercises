b3fd63fd7a546afbd5834c1d656cd8bcfc17afe4
Srikanth Astaputhra <sastaputhra@enova.com>
2015-01-26 20:23:33 -0600

    yadda part 1

---
+++ yadda/create_yadda_schema.sql
@@ -0,0 +1,146 @@
+BEGIN;
+
+CREATE SCHEMA IF NOT EXISTS yadda AUTHORIZATION sastaputhra;
+
+CREATE TABLE yadda.addresses(
+  id            SERIAL       PRIMARY KEY,
+  line_1        TEXT         NOT NULL,
+  line_2        TEXT,
+  state         TEXT         NOT NULL,
+  country       TEXT         NOT NULL,
+  zip_code      TEXT         NOT NULL,
+  inserted_by   TEXT         ,
+  inserted_on   TIMESTAMPTZ  ,
+  updated_by    TEXT         ,
+  updated_on    TIMESTAMPTZ
+);
+
+CREATE TABLE yadda.breweries(
+  id            SERIAL       PRIMARY KEY,
+  name          TEXT         NOT NULL,
+  address_id    INTEGER      NOT NULL REFERENCES yadda.addresses,
+  founding_year INTEGER      NOT NULL,
+  inserted_by   TEXT         ,
+  inserted_on   TIMESTAMPTZ  ,
+  updated_by    TEXT         ,
+  updated_on    TIMESTAMPTZ  ,
+  description   TEXT
+  );
+
+CREATE TABLE yadda.beers(
+  id            SERIAL       PRIMARY KEY,
+  style         TEXT         NOT NULL,
+  brewing_year  INTEGER      NOT NULL,
+  brewery_id    INTEGER      REFERENCES yadda.breweries,
+  inserted_by   TEXT         ,
+  inserted_on   TIMESTAMPTZ  ,
+  updated_by    TEXT         ,
+  updated_on    TIMESTAMPTZ  ,
+  description   TEXT
+);
+
+CREATE TABLE yadda.users(
+  id            SERIAL       PRIMARY KEY,
+  name          TEXT         NOT NULL,
+  email         TEXT,
+  password      TEXT,
+  phone         TEXT,
+  inserted_by   TEXT         ,
+  inserted_on   TIMESTAMPTZ  ,
+  updated_by    TEXT         ,
+  updated_on    TIMESTAMPTZ
+);
+
+CREATE TABLE yadda.ratings(
+  id            SERIAL       PRIMARY KEY,
+  beer_id       INTEGER      REFERENCES yadda.beers,
+  user_id       INTEGER      REFERENCES yadda.users,
+  look          INTEGER      NOT NULL CHECK(look between 0 AND 5),
+  smell         INTEGER      NOT NULL CHECK(look between 0 AND 5),
+  taste         INTEGER      NOT NULL CHECK(look between 0 AND 5),
+  feel          INTEGER      NOT NULL CHECK(look between 0 AND 5),
+  overall       INTEGER      NOT NULL CHECK(look between 0 AND 5),
+  inserted_by   TEXT         ,
+  inserted_on   TIMESTAMPTZ  ,
+  updated_by    TEXT         ,
+  updated_on    TIMESTAMPTZ
+);
+
+
+CREATE OR REPLACE FUNCTION  yadda.tg_inserts()
+RETURNS TRIGGER AS $$
+  BEGIN
+    NEW.inserted_on := current_timestamp;
+    NEW.inserted_by := current_user;
+    RETURN NEW;
+  END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION  yadda.tg_updates()
+RETURNS TRIGGER AS $$
+  BEGIN
+    NEW.updated_on := current_timestamp;
+    NEW.updated_by := current_user;
+    RETURN NEW;
+  END;
+$$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS check_address_inserts ON yadda.addresses;
+
+CREATE TRIGGER check_address_inserts
+  BEFORE INSERT
+  ON yadda.addresses
+  FOR EACH ROW EXECUTE PROCEDURE yadda.tg_inserts();
+
+DROP TRIGGER IF EXISTS check_breweries_inserts ON yadda.breweries;
+
+CREATE TRIGGER check_breweries_inserts
+  BEFORE INSERT
+  ON yadda.breweries
+  FOR EACH ROW EXECUTE PROCEDURE yadda.tg_inserts();
+
+DROP TRIGGER IF EXISTS check_beers_inserts ON yadda.breweries;
+
+CREATE TRIGGER check_beers_inserts
+  BEFORE INSERT
+  ON yadda.beers
+  FOR EACH ROW EXECUTE PROCEDURE yadda.tg_inserts();
+
+DROP TRIGGER IF EXISTS check_users_inserts ON yadda.breweries;
+
+CREATE TRIGGER check_users_inserts
+  BEFORE INSERT
+  ON yadda.users
+  FOR EACH ROW EXECUTE PROCEDURE yadda.tg_inserts();
+
+DROP TRIGGER IF EXISTS check_address_inserts ON yadda.addresses;
+
+CREATE TRIGGER check_address_updates
+  BEFORE UPDATE
+  ON yadda.addresses
+  FOR EACH ROW EXECUTE PROCEDURE yadda.tg_updates();
+
+DROP TRIGGER IF EXISTS check_breweries_updates ON yadda.breweries;
+
+CREATE TRIGGER check_breweries_updates
+  BEFORE UPDATE
+  ON yadda.breweries
+  FOR EACH ROW EXECUTE PROCEDURE yadda.tg_updates();
+
+DROP TRIGGER IF EXISTS check_beers_updates ON yadda.breweries;
+
+CREATE TRIGGER check_beers_updates
+  BEFORE UPDATE
+  ON yadda.beers
+  FOR EACH ROW EXECUTE PROCEDURE yadda.tg_updates();
+
+DROP TRIGGER IF EXISTS check_users_updates ON yadda.breweries;
+
+CREATE TRIGGER check_users_updates
+  BEFORE UPDATE
+  ON yadda.users
+  FOR EACH ROW EXECUTE PROCEDURE yadda.tg_updates();
+
+COMMIT;
+
+

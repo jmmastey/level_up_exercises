ae70952f64e7c0f536ae21cb34f8ff40bbc9fe4f
magicalbanana <kareemgan@gmail.com>
2015-01-28 13:03:34 -0600

    added datum class for handling and processing the json file.

--- data_science/model/datum.rb
+++ data_science/model/datum.rb
@@ -1,3 +1,5 @@
+require './service/json_parser.rb'
+
 module Model
   class Datum
     attr_accessor :cohort, :date, :result
@@ -6,6 +8,11 @@ module Model
       @cohort = cohort
       @date = date
       @result = result
+    end
+
+    def self.process_json(file)
+      json = Service::JsonParser.parse(file)
+      json.map { |j| new(j[:cohort], j[:date], j[:result]) }
     end
   end
 end

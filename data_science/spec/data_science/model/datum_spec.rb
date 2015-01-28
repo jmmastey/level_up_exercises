a1e7f135c54da2ba3022a27c616598e3b0899b62
magicalbanana <kareemgan@gmail.com>
2015-01-28 13:03:44 -0600

    added test for datum class.

--- data_science/spec/data_science/model/datum_spec.rb
+++ data_science/spec/data_science/model/datum_spec.rb
@@ -14,4 +14,26 @@ describe Model::Datum do
       it { expect(data).to have_attr_accessor(accessor) }
     end
   end
+
+  describe "#self.process_json" do
+    let(:datum) { Model::Datum.process_json(file) }
+    context "when the json data is not empty" do
+      let(:file) { './source_data.json' }
+      it "processes the json file" do
+        expect(datum).to be_truthy
+      end
+      it "returns an array" do
+        expect(datum).to be_a(Array)
+      end
+      it "returns a non empty array" do
+        expect(datum).to_not be_empty
+      end
+    end
+    context "when the json data is empty" do
+      let(:file) { './fixtures/no_data.json' }
+      it "raises an error" do
+        expect { datum }.to raise_error
+      end
+    end
+  end
 end

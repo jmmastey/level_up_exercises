8bc6707ae6daba0dfb2a25e9897d087fa0042d85
magicalbanana <kareemgan@gmail.com>
2015-01-28 17:04:07 -0600

    added custom matchers for matching states on the class bomb.

---
+++ overlord/spec/support/matchers/have_event.rb
@@ -0,0 +1,46 @@
+# This custom matcher can be used to test existence of events in a state machine
+#
+# Examples
+#
+#   it { is_expected.to have_event(:status, :event_name, [ :state1 => state2 ]) }
+#   it { is_expected.to have_event(:status, :event_name, [ :state1, state2 ] => [ state3, state4 ]) }
+#   it { is_expected.to have_event(:status, :event_name, { :state1 => state2, state1 => state3, :state4 => state5 etc })}
+
+
+RSpec::Matchers.define :have_event do |state_field, event_name, status_flows|
+  match do |model|
+    all_transitions = []
+
+    status_flows.each do |from, to|
+      from_arr = *from
+      to_arr = *to
+      from_arr.product(to_arr).each do |from, to|
+        all_transitions << OpenStruct({ :from => from, :to => to })
+      end
+    end
+
+    events = model.class.state_machines[state_field].events
+    event = events[event_name]
+
+    @unexpected_transitions = all_transitions.select do |transition|
+      !events.valid_for(model, :from => transition.from, :to => transition.to).include?(event)
+    end
+
+    @unexpected_transitions.empty?
+  end
+
+  failure_message do
+    unexpected_transitions_str = @unexpected_transitions.map do |t|
+      "#{event_name}: :#{t.from} => :#{t.to}"
+    end.join(', ')
+    "there are unexpected transitions in the expectation: [#{unexpected_transitions_str}]"
+  end
+
+  description do
+    "have #{event_name} event for #{state_field} with transitions #{status_flows}"
+  end
+
+  def OpenStruct(params)
+    params.is_a?(OpenStruct) ? params : OpenStruct.new(params)
+  end
+end

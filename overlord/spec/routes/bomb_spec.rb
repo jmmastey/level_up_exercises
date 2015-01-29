f6fa2a17ccaf12d6c94a9697901282e75a265763
Srikanth Astaputhra <sastaputhra@enova.com>
2015-01-28 22:20:39 -0600

    fixed rspec tests

--- overlord/spec/routes/bomb_spec.rb
+++ overlord/spec/routes/bomb_spec.rb
@@ -2,35 +2,32 @@ require_relative '../spec_helper'
 require 'timecop'
 
 describe 'bomb' do
-  let(:valid_bomb) do
-    { activation_code: '12342',
-      deactivation_code: '0220',
-      detonation_time: '55',
-      wires: [{ color: "red", diffuse: false },
-              { color: "green", diffuse: true }],
-    }.to_json
-  end
+  let(:valid_bomb)  { "activation_code=12342&deactivation_code=0000&detonation_time=50" }
 
+  before(:each) do
+    Bomb.destroy_all
+    Wire.destroy_all
+  end
   it 'should use default activation code if not given' do
     post '/bomb'
-    json = JSON.parse(last_response.body)
-    expect(json["activation_code"]).to eq("1234")
+    bomb = Bomb.last
+    expect(bomb.activation_code).to eq("1234")
   end
 
   it 'should not be activated when first booted' do
     post '/bomb'
-    json = JSON.parse(last_response.body)
-    expect(json["status"]).to eq("inactive")
+    bomb = Bomb.last
+    expect(bomb.status).to eq("inactive")
   end
 
   it 'should not use default activation code if given' do
     post '/bomb', valid_bomb
-    json = JSON.parse(last_response.body)
-    expect(json["activation_code"]).to eq("12342")
+    bomb = Bomb.last
+    expect(bomb.activation_code).to eq("12342")
   end
 
   context "activating the bomb" do
-    before (:each) do
+    before(:each) do
       Bomb.destroy_all
       Wire.destroy_all
       Bomb.create(

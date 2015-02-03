2726a5864f78face662b56afa24766423d8f3da3
Srikanth Astaputhra <sastaputhra@enova.com>
2015-02-03 07:29:30 -0600

    WIP

--- event_system/app/controllers/weather_controller.rb
+++ event_system/app/controllers/weather_controller.rb
@@ -5,6 +5,10 @@ class WeatherController < ApplicationController
     if params["region"] && params["region"]["name"] != ""
       @high_low_temperatures = WeatherHelper.parse_temperatures
       @detail_forecasts = WeatherHelper.parse_details
+      region_id = Region.where(:city => "Chicago").first.region_id
+
+      WeatherAlert.weather_alert(EmailContact.where(region_id: region_id),
+        @detail_forecasts).deliver
     end
   end
 
--- event_system/config/environments/development.rb
+++ event_system/config/environments/development.rb
@@ -34,4 +34,15 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+   :address              => "smtp.gmail.com",
+   :port                 => 587,
+   :user_name            => ENV['gmail_username'],
+   :password             => ENV['gmail_password'],
+   :authentication       => "plain",
+   :enable_starttls_auto => true
+  }
 end

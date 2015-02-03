3615f1985f812b8779bc0ccdc0225b9fd985e36a
Zach Bright <zbright@enova.com>
2015-02-03 17:44:31 -0600

    maybe its done now?

--- dino_catalog/dinosaur.rb
+++ dino_catalog/dinosaur.rb
@@ -2,7 +2,7 @@ class Dinosaur
   attr_accessor :name, :period, :continent, :diet, :weight, :walking, :description
 
   def to_json
-    {'name' => @name, 'period' => @period, 'continent' => @continent, 'diet' => @diet,
-      'weight' => @weight, 'walking' => @walking, 'description' => @description}
+    { 'name' => @name, 'period' => @period, 'continent' => @continent, 'diet' => @diet,
+      'weight' => @weight, 'walking' => @walking, 'description' => @descriptio n 
   end
 end

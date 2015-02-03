ea71aa16232f7573f560d10b512d63adfe4a90a5
Zach Bright <zbright@enova.com>
2015-02-03 17:40:51 -0600

    Changed to attr_accessor, removed self and more

--- dino_catalog/dino_list.rb
+++ dino_catalog/dino_list.rb
@@ -1,65 +1,61 @@
 require_relative 'dinosaur'
 class DinoList < Array
-
-  def is_pirate_list
-    @is_pirate_list
-  end
-
-  def is_pirate_list=(is_pirate_table)
-    @is_pirate_list = is_pirate_table
-  end
+  attr_accessor :is_pirate_list
 
   def get_big
-    self.select!{ |dino| dino.weight.to_i > 4000}
+    select! { |dino| dino.weight.to_i > 4000 }
   end
 
   def get_small
-    self.select!{ |dino| dino.weight != nil && dino.weight.to_i <= 4000}
+    select! { |dino| dino.weight && dino.weight.to_i <= 4000 }
   end
 
   def get_bipeds
-    self.select!{ |dino| dino.walking.downcase == 'biped'}
+    select!{ |dino| dino.walking.downcase == 'biped'}
   end
 
   def get_from_period(period_name)
-    self.select!{ |dino| dino.period.downcase.include? period_name}
+    select!{ |dino| dino.period.downcase.include? period_name.downcase}
   end
 
   def get_carnivores
-    self.select!{ |dino| dino.diet.downcase != 'herbivore'}
+    select!{ |dino| dino.diet.downcase != 'herbivore'}
   end
 
   def get_by_name(name)
-    self.select!{ |dino| dino.name.downcase == name}
+    select!{ |dino| dino.name.downcase == name}
   end
 
   def pretty_print
-    print_weight = self.select { |dino| dino.weight }.any?
-    print_desc = self.select { |dino| dino.description }.any?
+    if !any?
+      puts "#########     No Dinos Left to Display - Hit (R) to Reset Filters      #########"
+      return
+    end
 
-    table = Terminal::Table.new do |t|
+    print_weight = select { |dino| dino.weight }.any?
+    print_desc = select { |dino| dino.description }.any?
+
+    Terminal::Table.new do |t|
       t << set_headers(print_weight, print_desc)
       t << :separator
-      self.each do |dino|
+      each do |dino|
         t << prettify_dino(dino, print_weight, print_desc)
       end
     end
-
-    table
   end
 
   def set_headers (print_weight, print_desc)
     headers = ['Name', 'Period']
-    headers << 'Continent' unless @is_pirate_list
+    headers << 'Continent' unless is_pirate_list
     headers << 'Diet'
     headers << 'Weight' unless !print_weight
     headers << 'Walking'
-    headers << 'Description' unless @is_pirate_list || !print_desc
+    headers << 'Description' unless is_pirate_list || !print_desc
     headers
   end
 
   def prettify_dino (dino, print_weight, print_desc)
-    if @is_pirate_list
+    if is_pirate_list
       arr = [dino.name, dino.period, dino.diet]
       arr << dino.weight if print_weight
       arr << dino.walking
@@ -75,9 +71,8 @@ class DinoList < Array
 
   def to_json
     dino_arr = []
-    self.each{|d| dino_arr << d.to_json}
+    each{|d| dino_arr << d.to_json}
 
     puts dino_arr
   end
-
 end

a98e61697f00809ba41f5c2e1533f696d570da3b
Jeremy Finzel <jfinzel@enova.com>
2015-02-03 13:04:34 -0600

    Point Loader to module

--- cyoa/app/models/api_data_transfer/point_loader.rb
+++ cyoa/app/models/api_data_transfer/point_loader.rb
@@ -3,22 +3,32 @@ require './app/models/api/lat_lon_zipcode_client'
 require './app/helpers/private_attr_accessor'
 
 module PointLoader
-  extend PrivateAttrAccessor
-  private_attr_accessor :response
-  
-  def initialize(zip_code_list)
-    self.response = requester.new(zip_code_list)
-    load_points
+  def load(zip_code_list)
+    zip_code_list_for_load = zip_code_list_not_in_db
+    unless zip_code_list_for_load.empty?
+      call_api_and_load(zip_code_list_for_load)
+    end
   end
 
   private
 
-  def load_points
-    response.each do |zipcode, lat_lon|
+  def call_api_and_load(zip_code_list)
+    response = call_api(zip_code_list)
+    response.zip_code_lat_lon.each do |zipcode, lat_lon|
       point_model.new(lat:     lat_lon[0],
                       lon:     lat_lon[1],
                       zipcode: zipcode).save
     end
+  end
+
+  def zip_code_list_not_in_db
+    zip_code_list.split(" ").each_with_object([]) do |zip, not_in_db|
+      not_in_db << zip if point_model.find(zipcode: zipcode).empty?
+    end.join(" ")
+  end
+
+  def call_api(zip_code_list)
+    requester.new.request(zip_code_list)
   end
 
   def requester

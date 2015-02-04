881f5bb6c94fd345cc58e5e516f59805d9ace59a
Bud Siddhisena <bsiddhisens@enova.com>
2015-02-03 22:21:08 -0600

    Refactored dino datamapper class by introducing dino collection class

--- dino_catalog/dino_csv_data_mapper.rb
+++ dino_catalog/dino_csv_data_mapper.rb
@@ -1,22 +1,20 @@
-# Map between dinosaur and CSV data source
 require 'csv'
-require 'json'
 require './dinosaur'
 
 class DinoCSVDataMapper
-  attr_accessor :dinosaurs
+  attr_reader :dinosaurs
 
-  def initialize
-    @data_source = ['dinodex.csv', 'african_dinosaur_export.csv']
+  def initialize(*data_source)
     @dinosaurs = []
+    data_source = ['dinodex.csv'] if data_source.empty?
 
-    @data_source.each do |ds|
+    data_source.each do |ds|
       raise "Data source: #{ds} not found" unless File.file? ds
-      load_data ds
+      import ds
     end
   end
 
-  def load_data(csv_file)
+  def import(csv_file)
     csv_data = CSV.read(csv_file, headers: true)
     csv_data.each do |csv_row|
 
@@ -48,38 +46,4 @@ class DinoCSVDataMapper
     end
   end
 
-  def find(condition = nil)
-    dinosaurs_found = DinoCSVDataMapper.new
-
-    return dinosaurs_found unless condition
-    raise "find condition must be a hash" unless condition.is_a? Hash
-
-    dinosaurs_found.dinosaurs = @dinosaurs
-
-    condition.map do |attr, val|
-      dinosaurs_found.dinosaurs = 
-        dinosaurs_found.dinosaurs.select do |dino_obj|
-          (ret = dino_obj.send(attr)) && ret.downcase == val.downcase
-        end
-    end
-
-    dinosaurs_found
-  end
-
-  TONS_TO_POUNDS = 2000
-  def find_large
-    dinosaurs_found = DinoCSVDataMapper.new
-    dinosaurs_found.dinosaurs = @dinosaurs
-    dinosaurs_found.dinosaurs.select do |dino_obj|
-      dino_obj.weight > 2 * TONS_TO_POUNDS
-    end
-  end
-
-  def to_s
-    @dinosaurs.inject('') { |string, dino| string << dino.to_s }
-  end
-
-  def to_json
-    @dinosaurs.map { |dino| dino.to_json }
-  end
 end
--- dino_catalog/dino_data_tester.rb
+++ dino_catalog/dino_data_tester.rb
@@ -2,28 +2,29 @@
 # See https://github.com/jmmastey/level_up_exercises/tree/master/dino_catalog
 require 'pry'
 require './dino_csv_data_mapper'
+require './dinosaur_collection'
 
 # Run it
-dino_mapper = DinoCSVDataMapper.new
+dino_data_mapper = DinoCSVDataMapper.new('dinodex.csv', 'african_dinosaur_export.csv')
+dino_collection = DinosaurCollection.new(dino_data_mapper.dinosaurs)
 
 puts "\nDinos that walk biped"
-puts dino_mapper.find(walking: "biped")
-
+puts dino_collection.find(walking: "biped")
 puts "\nDinos that are carnivores"
-puts dino_mapper.find(diet: "carnivore")
+puts dino_collection.find(diet: "carnivore")
 
 puts "\nDinos from Jurassic period"
-puts dino_mapper.find(period: "jurassic")
+puts dino_collection.find(period: "jurassic")
 puts "\nDinos from Early/Late Cretaceous period"
-puts dino_mapper.find(period: "cretaceous")
+puts dino_collection.find(period: "cretaceous")
 
 puts "\nCarnivores from Europe"
-puts dino_mapper.find(diet: "carnivore", continent: "europe")
+puts dino_collection.find(diet: "carnivore", continent: "europe")
 puts "\nCarnivores from Europe (take 2)"
-puts dino_mapper.find(diet: "carnivore").find(continent: "europe")
+puts dino_collection.find(diet: "carnivore").find(continent: "europe")
 
 puts "\nBig Dinos > 4000 lbs (2 tons)"
-puts dino_mapper.find_large
+puts dino_collection.find_large
 
 puts "\nExport JSON"
-p dino_mapper.find.to_json
+p dino_collection.find.to_json

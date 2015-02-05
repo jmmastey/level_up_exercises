fb3675834dfa09e64bad9ebf494e90782f3fe85c
Jeremy Finzel <jfinzel@enova.com>
2015-02-05 07:19:22 -0600

    Weather partial changes

--- cyoa/app/controllers/home_controller.rb
+++ cyoa/app/controllers/home_controller.rb
@@ -1,9 +1,5 @@
 class HomeController < ApplicationController
   def index
-    # if session[:user_id].nil?
-    #   @user
-    # else
-    #   @user = User.find(session[:user_id])
-    # end
+    
   end
 end

8da367e183fa33ee69dd092b44c108829eb04803
Paul Haddad <paulh16@gmail.com>
2015-02-07 11:15:33 -0600

    JS: add checking of emails

--- pmail/js/build/production.js
+++ pmail/js/build/production.js
@@ -1,4 +1,14 @@
+$(function() {
 
+  // Check and uncheck emails
+
+  $('.emails').on('click', 'tr span:first-of-type', function() {
+    var $checkbox = $(this);
+    $checkbox.closest('tr').toggleClass('checked');
+    $checkbox.toggleClass('fa-square-o fa-check-square-o');
+  });
+
+});
 
 // Avoid `console` errors in browsers that lack a console.
 (function() {
--- pmail/js/main.js
+++ pmail/js/main.js
@@ -1,1 +1,11 @@
+$(function() {
 
+  // Check and uncheck emails
+
+  $('.emails').on('click', 'tr span:first-of-type', function() {
+    var $checkbox = $(this);
+    $checkbox.closest('tr').toggleClass('checked');
+    $checkbox.toggleClass('fa-square-o fa-check-square-o');
+  });
+
+});

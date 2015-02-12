f0b873034978d3809fb5d1ffaa80d97bace1355d
magicalbanana <kareemgan@gmail.com>
2015-02-12 14:50:13 -0600

    setup env and paths.

---
+++ overlord/spec/features/support/env.rb
@@ -0,0 +1,27 @@
+ENV['RACK_ENV'] = 'test'
+
+require File.join(File.dirname(__FILE__), *%w[.. .. .. overlord.rb])
+
+require 'capybara'
+require 'capybara-webkit'
+require 'capybara/cucumber'
+require 'rspec'
+
+Capybara.configure do |config|
+  config.app = Overlord
+  config.default_driver = :webkit
+end
+
+DataMapper.setup(:default, "sqlite::memory")
+
+class OverlordWorld
+  include Capybara::DSL
+  include RSpec::Expectations
+  include RSpec::Matchers
+
+  DataMapper.auto_migrate!
+end
+
+World do
+  OverlordWorld.new
+end

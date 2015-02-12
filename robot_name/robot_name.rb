f2b5dc5de4eec4ff99f9b3fda7f4c538c1fa8782
Aditya Mattos <amattos@CLM-amattos.local>
2015-02-12 16:10:28 -0600

    Refactored code for readability

--- robot_name/robot_name.rb
+++ robot_name/robot_name.rb
@@ -3,22 +3,33 @@ class NameCollisionError < RuntimeError; end
 class Robot
   attr_accessor :name
 
-  @@registry
+  def generate_char 
+    ('A'..'Z').to_a.sample
+  end
+  
+  def generate_num
+    rand(10) 
+  end
+
+  def name_generator
+    if @name_generator
+      @name = @name_generator.call
+    else
+      @name = "#{generate_char}#{generate_char}#{generate_num}#{generate_num}#{generate_num}"
+    end
+  end
+
+  def is_error?
+    if !(@name =~ /[[:alpha:]]{2}[[:digit:]]{3}/) || @@registry.include?(name)
+      raise NameCollisionError, 'There was a problem generating the robot name!'
+    end
+  end
 
   def initialize(args = {})
     @@registry ||= []
     @name_generator = args[:name_generator]
-
-    if @name_generator
-      @name = @name_generator.call
-    else
-      generate_char = -> { ('A'..'Z').to_a.sample }
-      generate_num = -> { rand(10) }
-
-      @name = "#{generate_char.call}#{generate_char.call}#{generate_num.call}#{generate_num.call}#{generate_num.call}"
-    end
-
-    raise NameCollisionError, 'There was a problem generating the robot name!' if !(name =~ /[[:alpha:]]{2}[[:digit:]]{3}/) || @@registry.include?(name)
+    @name = name_generator 
+    is_error?
     @@registry << @name
   end
 end
@@ -27,6 +38,7 @@ robot = Robot.new
 puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
 
 # Errors!
-# generator = -> { 'AA111' }
-# Robot.new(name_generator: generator)
-# Robot.new(name_generator: generator)
+generator = -> { 'AA111' }
+Robot.new(name_generator: generator)
+Robot.new(name_generator: generator)
+

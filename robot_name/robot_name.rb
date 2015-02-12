d0f4483921c6ef659c6ca1b8e933efa6e0365315
shishirtewari <stewari@enova.com>
2015-02-12 16:28:07 -0600

    Robot Name Changes

--- robot_name/robot_name.rb
+++ robot_name/robot_name.rb
@@ -1,25 +1,51 @@
 class NameCollisionError < RuntimeError; end
+class InvalidFormatError < RuntimeError; end
 
+# Generates robot's name
 class Robot
   attr_accessor :name
-
-  @@registry
 
   def initialize(args = {})
     @@registry ||= []
     @name_generator = args[:name_generator]
+  end
 
+  def name
     if @name_generator
       @name = @name_generator.call
+      fail InvalidFormatError, 'Not supported format' unless pattern_check?
     else
-      generate_char = -> { ('A'..'Z').to_a.sample }
-      generate_num = -> { rand(10) }
-
-      @name = "#{generate_char.call}#{generate_char.call}#{generate_num.call}#{generate_num.call}#{generate_num.call}"
+      generate_name
     end
+    repeat_check
+  end
 
-    raise NameCollisionError, 'There was a problem generating the robot name!' if !(name =~ /[[:alpha:]]{2}[[:digit:]]{3}/) || @@registry.include?(name)
+  def random_num
+    rand(10)
+  end
+
+  def random_char
+    ('A'..'Z').to_a.sample
+  end
+
+  def generate_name
+    str_arr = []
+    (1..5).each do |i|
+      str_arr << (i == 1 || i == 2 ? random_char : random_num.to_s)
+    end
+    @name = str_arr.join
+  end
+
+  def pattern_check?
+    @name =~ /[[:alpha:]]{2}[[:digit:]]{3}/
+  end
+
+  def repeat_check
+    if @@registry.include?(@name)
+      fail NameCollisionError, 'There was a problem generating the robot name!'
+    end
     @@registry << @name
+    @name
   end
 end
 
@@ -27,6 +53,6 @@ robot = Robot.new
 puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
 
 # Errors!
-# generator = -> { 'AA111' }
-# Robot.new(name_generator: generator)
-# Robot.new(name_generator: generator)
+generator = -> { 'AB111' }
+puts "My second robot's name is #{Robot.new(name_generator: generator).name}"
+puts "My third robot's name is #{Robot.new(name_generator: generator).name}"

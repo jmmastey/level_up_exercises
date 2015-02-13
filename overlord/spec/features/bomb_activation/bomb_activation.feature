ab5f7239c580e29cdfd37a3593233cac06676290
magicalbanana <kareemgan@gmail.com>
2015-02-13 13:10:34 -0600

    bomb_activation features added.

--- overlord/spec/features/bomb_activation/bomb_activation.feature
+++ overlord/spec/features/bomb_activation/bomb_activation.feature
@@ -2,9 +2,29 @@ Feature: Bomb Activation
 
   Scenario: Activating a bomb with a valid code
     Given I am in the home page
-    When I hover on the Overlord image
-      And I click on the image
+    Then I should see an overlord image
+      And should have a name Valentine's Bomb
+    When hovering on the Overlord image
+      And clicking the image
     Then I should be redirected to bomb activation page
+      And should see a field for entering my code
+      And should see a submit button
+    When I will enter my boot code:
+      | code | 6969 |
+      And click the submit button
+    Then be redirected to the bomb creation page
 
-    
-
+  Scenario: Activating a bomb with an invalid code
+    Given I am in the home page
+    Then I should see an overlord image
+      And should have a name Valentine's Bomb
+    When hovering on the Overlord image
+      And clicking the image
+    Then I should be redirected to bomb activation page
+      And should see a field for entering my code
+      And should see a submit button
+    When I will enter my boot code:
+      | code | 1000 |
+      And click the submit button
+    Then not be redirected to the bomb creation page
+      And remain on the boot device page
--- overlord/spec/features/step_definitions/bomb_activation_steps.rb
+++ overlord/spec/features/step_definitions/bomb_activation_steps.rb
@@ -4,15 +4,56 @@ Given(/^I am in the home page$/) do
   expect(url).to eq('/')
 end
 
-When(/^I hover on the Overlord image$/) do
+Then(/^I should see an overlord image$/) do
+  expect(page.find("#overlord")["src"]).to have_content("overlord.png")
+end
+
+Then(/^should have a name Valentine's Bomb$/) do
+  expect(page.find(".name")).to have_content("VALENTINE'S BOMB")
+end
+
+When(/^hovering on the Overlord image$/) do
   page.find('#overlord').trigger(:mouseover)
 end
 
-When(/^I click on the image$/) do
+When(/^clicking the image$/) do
   page.find('#overlord').trigger(:click)
 end
 
 Then(/^I should be redirected to bomb activation page$/) do
+  url = URI.parse(current_url).request_uri
+  expect(url).to eq('/boot_device')
+end
+
+Then(/^should see a field for entering my code$/) do
+  expect(page.find('#boot_code')).to be_truthy
+end
+
+Then(/^should see a submit button$/) do
+  expect(page).to have_selector("button[type=submit]")
+end
+
+When(/^I will enter my boot code:$/) do |table|
+  # table is a Cucumber::Ast::Table
+  code = table.rows_hash
+  fill_in "boot_code", :with => code[:code]
+end
+
+When(/^click the submit button$/) do
+  page.find("button[type=submit]").trigger(:click)
+end
+
+Then(/^be redirected to the bomb creation page$/) do
+  url = URI.parse(current_url).request_uri
+  expect(url).to eq("/create_bomb")
+end
+
+Then(/^not be redirected to the bomb creation page$/) do
+  url = URI.parse(current_url).request_uri
+  expect(url).to_not eq("/create_bomb")
+end
+
+Then(/^remain on the boot device page$/) do
   url = URI.parse(current_url).request_uri
   expect(url).to eq('/boot_device')
 end

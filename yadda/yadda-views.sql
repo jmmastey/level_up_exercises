e499e00950b3b4fdb5d9f3b0715d1ae8c1374c43
rcowan <ryancowan@gmail.com>
2015-02-13 09:06:05 -0600

    minor updates to views, move to more inner joins

--- yadda/yadda-views.sql
+++ yadda/yadda-views.sql
@@ -1,4 +1,7 @@
-/* Top beers from a given brewery, according to their total ratings. */
+#try to keep inner joins
+#preferable over subqueries
+
+-- Top beers from a given brewery, according to their total ratings. 
 create or replace view top_beers AS
   select brewery.brewery_id as breweryid,
     brewery.name as breweryname,
@@ -12,14 +15,14 @@ create or replace view top_beers AS
              group by beer_id) ratings on ratings.beer_id = beer.beer_id
   order by ratings_count desc;
 
-/* "Recent score" for a beer, where only ratings within the last six months are counted and the ratings within that period are averaged. */
+-- "Recent score" for a beer, where only ratings within the last six months are counted and the ratings within that period are averaged. 
 create or replace view recent_score AS
   select brewery.brewery_id as breweryid,
     brewery.name as breweryname,
     beer.name as beername,
     ratings.computed
   from brewery 
-  left join beer on beer.brewery_id = brewery.brewery_id 
+  inner join beer on beer.brewery_id = brewery.brewery_id 
   left join (select beer_id,
                AVG(rating.overall) as computed 
              from rating 
@@ -27,13 +30,13 @@ create or replace view recent_score AS
              group by beer_id) ratings on ratings.beer_id = beer.beer_id
   order by computed desc;
 
-/* "You might also enjoy", which picks beers of the same style with high average scores, and then sorts them randomly. */
+-- "You might also enjoy", which picks beers of the same style with high average scores, and then sorts them randomly. 
 create or replace view also_enjoy AS
   select beer.name as beername,
     beer.style as beerstyle,
     ratings.computed
   from beer
-  left join (select beer_id,
+  inner join (select beer_id,
                AVG(rating.overall) as computed 
              from rating 
              group by beer_id) ratings on ratings.beer_id = beer.beer_id

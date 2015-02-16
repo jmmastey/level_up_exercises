f0a1ea9dd3ee8771e599012d8028f155dee073d9
Aditya Mattos <amattos@clm-amattos.enova.com>
2015-02-16 10:22:40 -0600

    temporary Commit

--- arrowhead/arrowhead.rb
+++ arrowhead/arrowhead.rb
@@ -1,33 +1,34 @@
 class Arrowhead
-  # This seriously belongs in a database.
-  CLASSIFICATIONS = {
-    far_west: {
-      notched: "Archaic Side Notch",
-      stemmed: "Archaic Stemmed",
-      lanceolate: "Agate Basin",
-      bifurcated: "Cody",
-    },
-    northern_plains: {
-      notched: "Besant",
-      stemmed: "Archaic Stemmed",
-      lanceolate: "Humboldt Constricted Base",
-      bifurcated: "Oxbow",
-    },
-  }
+# This seriously belongs in a database.
+CLASSIFICATIONS = {
+	far_west: {
+	notched: "Archaic Side Notch",
+	         stemmed: "Archaic Stemmed",
+	         lanceolate: "Agate Basin",
+	         bifurcated: "Cody",
+	          },
+	northern_plains: {
+	notched: "Besant",
+	         stemmed: "Archaic Stemmed",
+	         lanceolate: "Humboldt Constricted Base",
+	         bifurcated: "Oxbow",
+	                 },
+	}
 
-  # FIXME: I don't have time to deal with this.
+# FIXME: I don't have time to deal with this.
   def self.classify(region, shape)
     if CLASSIFICATIONS.include? region
-      shapes = CLASSIFICATIONS[region]
-      if shapes.include? shape
-        arrowhead = shapes[shape]
-        puts "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
-      else
-        raise "Unknown shape value. Are you sure you know what you're talking about?"
-      end
-    else
-      raise "Unknown region, please provide a valid region."
-    end
+  	  shapes = CLASSIFICATIONS[region]
+  		if shapes.include? shape
+  		  arrowhead = shapes[shape]
+  		  puts "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
+  		else
+  		  raise "Unknown shape value. Are you sure you know what you're "+
+          "talking about?"
+  		end
+  	else
+  	  raise "Unknown region, please provide a valid region."
+  	end
   end
 end
 

31085fde03b742494d5e02763d505bdf74c64cae
Aditya Mattos <amattos@clm-amattos.enova.com>
2015-02-16 10:38:48 -0600

    Refactored code as per review comments

--- arrowhead/arrowhead.rb
+++ arrowhead/arrowhead.rb
@@ -1,36 +1,43 @@
 class Arrowhead
-  # This seriously belongs in a database.
+# This seriously belongs in a database.
   CLASSIFICATIONS = {
     far_west: {
       notched: "Archaic Side Notch",
-      stemmed: "Archaic Stemmed",
-      lanceolate: "Agate Basin",
-      bifurcated: "Cody",
-    },
+               stemmed: "Archaic Stemmed",
+               lanceolate: "Agate Basin",
+               bifurcated: "Cody",
+              },
     northern_plains: {
       notched: "Besant",
-      stemmed: "Archaic Stemmed",
-      lanceolate: "Humboldt Constricted Base",
-      bifurcated: "Oxbow",
-    },
-  }
+               stemmed: "Archaic Stemmed",
+               lanceolate: "Humboldt Constricted Base",
+               bifurcated: "Oxbow",
+                     },
+                    }
 
-  # FIXME: I don't have time to deal with this.
+# FIXME: I don't have time to deal with this.
 
-  def self.exists_region? region
+  def self.exists_region?(region)
     CLASSIFICATIONS.include? region
   end
-  
+
   def self.exists_shape?(region, shape)
     CLASSIFICATIONS[region].include? shape
   end
-  
+
   def self.classify(region, shape)
-     raise "Unknown region, please provide a valid region." unless exists_region? region
-     raise "Unknown shape value. Are you sure you know what you're talking about?" unless exists_shape?(region, shape)
-     puts "You have a(n) '#{CLASSIFICATIONS[region][shape]}' arrowhead. Probably priceless."
+    unless exists_region? region
+      raise "Unknown region, please provide a valid region."
+    end
+    unless exists_shape?(region, shape)
+      raise "Unknown shape value. Are you sure you know what you're "
+        +"talking about?"
+    end
+    puts "You have a(n) '#{CLASSIFICATIONS[region][shape]}' arrowhead. "
+      +"Probably priceless."
   end
 
 end
 
 puts Arrowhead.classify(:northern_plains, :bifurcated)
+

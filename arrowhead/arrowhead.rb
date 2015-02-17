97559050aa4d98847e290f0606118e979512f352
shishirtewari <stewari@enova.com>
2015-02-17 12:03:35 -0600

    Arrowhead review comments incorporated

--- arrowhead/arrowhead.rb
+++ arrowhead/arrowhead.rb
@@ -1,37 +1,37 @@
+# class arrowhead
 class Arrowhead
   # This seriously belongs in a database.
   CLASSIFICATIONS = {
-      far_west: {
-      notched: "Archaic Side Notch",
-      stemmed: "Archaic Stemmed",
-      lanceolate: "Agate Basin",
-      bifurcated: "Cody",
-  },
-      northern_plains: {
-      notched: "Besant",
-      stemmed: "Archaic Stemmed",
-      lanceolate: "Humboldt Constricted Base",
-      bifurcated: "Oxbow",
-  },
+    far_west:
+      {
+        notched: 'Archaic Side Notch',
+        stemmed: 'Archaic Stemmed',
+        lanceolate: 'Agate Basin',
+        bifurcated: 'Cody'
+      },
+    northern_plains:
+      {
+        notched: 'Besant',
+        stemmed: 'Archaic Stemmed',
+        lanceolate: 'Humboldt Constricted Base',
+        bifurcated: 'Oxbow'
+      }
   }
 
   def self.classify(region, shape)
-
-    raise 'Unknown region, please provide a valid region.' unless self.isRegion(region)
-    raise  "Unknown shape value. Are you sure you know what you're talking about?" unless self.isShape(region, shape)
+    fail 'Unknown region, please enter valid region.' unless region?(region)
+    fail 'Unknown shape, please enter valid shape.' unless shape?(region, shape)
 
     arrowhead = CLASSIFICATIONS[region][shape]
     puts "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
-
   end
 
-  def self.isRegion(region)
+  def self.region?(region)
     CLASSIFICATIONS.include? region
   end
 
-  def self.isShape(region, shape)
+  def self.shape?(region, shape)
     CLASSIFICATIONS[region].include? shape
   end
-
 end
 puts Arrowhead.classify(:northern_plains, :bifurcated)

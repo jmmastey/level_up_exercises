490736f5faa31d40400c8f6e608722f43cea3878
Zach Bright <zbright@enova.com>
2015-03-03 15:33:28 -0600

    working version with navbar refactor

---
+++ pmail/pmail.js
@@ -0,0 +1,21 @@
+function toggleSelection(id) {
+    $(id).toggleClass('fa-square-o fa-check-square')
+    $(id).closest('.email').toggleClass('selected');
+  }
+
+  function toggleFavorite(id) {
+    $(id).toggleClass('fa-star fa-star-o');
+  }
+
+  $('.top-dropdown').click(function() {
+    $(this).siblings('.dropdown-panel').toggle();
+  })
+
+  $('.sidebar-menu li a').click(function() {
+    if ($(this).hasClass("top-dropdown")) {
+      $('.more-list').toggle();
+    } else {
+      $('.sidebar-menu li').removeClass('active');
+      return $(this.parentNode).addClass('active');
+    }
+  })

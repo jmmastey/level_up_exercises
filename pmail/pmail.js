0c322d4e5129d367a7c993737beb07b6c85c0c64
Zach Bright <zbright@enova.com>
2015-03-03 17:41:34 -0600

    I broke the javascripts.....

--- pmail/pmail.js
+++ pmail/pmail.js
@@ -1,21 +1,27 @@
 function toggleSelection(id) {
     $(id).toggleClass('fa-square-o fa-check-square');
     $(id).closest('.email').toggleClass('selected');
-  };
+  }
 
   function toggleFavorite(id) {
     $(id).toggleClass('fa-star fa-star-o');
+  }
+
+
+  var ready = function() {
+    $('.top-dropdown').click(function() {
+      $(this).siblings('.dropdown-panel').toggle();
+    });
+
+    $('.sidebar-menu li a').click(function() {
+      if ($(this).hasClass('top-dropdown')) {
+        $('.more-list').toggle();
+      } else {
+        $('.sidebar-menu li').removeClass('active');
+        return $(this.parentNode).addClass('active');
+      }
+    });
   };
 
-  $('.top-dropdown').click(function() {
-    $(this).siblings('.dropdown-panel').toggle();
-  })
-
-  $('.sidebar-menu li a').click(function() {
-    if ($(this).hasClass('top-dropdown')) {
-      $('.more-list').toggle();
-    } else {
-      $('.sidebar-menu li').removeClass('active');
-      return $(this.parentNode).addClass('active');
-    }
-  })
+  $(document).ready(ready);
+  $(document).on('page:load', ready);

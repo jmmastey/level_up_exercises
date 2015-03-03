a1e68fc1c79b709d30b50a87dee021b73f457941
Zach Bright <zbright@enova.com>
2015-03-03 16:59:17 -0600

    hound

--- pmail/pmail.js
+++ pmail/pmail.js
@@ -1,18 +1,18 @@
 function toggleSelection(id) {
-    $(id).toggleClass('fa-square-o fa-check-square')
+    $(id).toggleClass('fa-square-o fa-check-square');
     $(id).closest('.email').toggleClass('selected');
-  }
+  };
 
   function toggleFavorite(id) {
     $(id).toggleClass('fa-star fa-star-o');
-  }
+  };
 
   $('.top-dropdown').click(function() {
     $(this).siblings('.dropdown-panel').toggle();
   })
 
   $('.sidebar-menu li a').click(function() {
-    if ($(this).hasClass("top-dropdown")) {
+    if ($(this).hasClass('top-dropdown')) {
       $('.more-list').toggle();
     } else {
       $('.sidebar-menu li').removeClass('active');

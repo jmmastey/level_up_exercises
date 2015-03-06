34ddf71343c135aeefaa851f57c9adf370ca630e
Kevin Shu <kshu@enova.com>
2015-03-06 13:24:34 -0600

    fixed tabs

--- triangle_facts/triangle_facts.rb
+++ triangle_facts/triangle_facts.rb
@@ -1,28 +1,28 @@
 # Killer facts about triangles AWW YEAH
 class Triangle
-	attr_accessor :side1, :side2, :side3
+  attr_accessor :side1, :side2, :side3
 
-	def initialize(side1, side2, side3)
-		@side1, @side2, @side3 = side1, side2, side3
-	end
+  def initialize(side1, side2, side3)
+    @side1, @side2, @side3 = side1, side2, side3
+  end
 
-	def equilateral
-		side1 == side2 && side2 == side3
-	end
+  def equilateral
+    side1 == side2 && side2 == side3
+  end
 
-	def isosceles
-		[side1, side2, side3].uniq.length == 2
-	end
+  def isosceles
+    [side1, side2, side3].uniq.length == 2
+  end
 
-	def scalene
+  def scalene
     if equilateral || isosceles
       true
     else
       false
     end
-	end
+  end
 
-	def recite_facts
+  def recite_facts
     case
     when equilateral
       puts 'This triangle is equilateral!'
@@ -32,32 +32,32 @@ class Triangle
       puts 'This triangle is scalene and mathematically boring.'
     end
 
-		angles = calculate_angles(side1, side2, side3)
-		puts 'The angles of this triangle are ' + angles.join(', ')
+    angles = calculate_angles(side1, side2, side3)
+    puts 'The angles of this triangle are ' + angles.join(', ')
 
-		puts 'This triangle is also a right triangle!' if angles.include? 90
-		puts ''
-	end
+    puts 'This triangle is also a right triangle!' if angles.include? 90
+    puts ''
+  end
 
-	def calculate_angles(a, b, c)
-		angle_a = radians_to_degrees(Math.acos((b**2 + c**2 - a**2) / (2.0 * b * c)))
-		angle_b = radians_to_degrees(Math.acos((a**2 + c**2 - b**2) / (2.0 * a * c)))
-		angle_c = radians_to_degrees(Math.acos((a**2 + b**2 - c**2) / (2.0 * a * b)))
+  def calculate_angles(a, b, c)
+    angle_a = radians_to_degrees(Math.acos((b**2 + c**2 - a**2) / (2.0 * b * c)))
+    angle_b = radians_to_degrees(Math.acos((a**2 + c**2 - b**2) / (2.0 * a * c)))
+    angle_c = radians_to_degrees(Math.acos((a**2 + b**2 - c**2) / (2.0 * a * b)))
 
-		[angle_a, angle_b, angle_c]
-	end
+    [angle_a, angle_b, angle_c]
+  end
 
-	def radians_to_degrees(rads)
-		(rads * 180 / Math::PI).round
-	end
+  def radians_to_degrees(rads)
+    (rads * 180 / Math::PI).round
+  end
 end
 
 
 triangles = [
-	[5,5,5],
-	[5,12,13],
+  [5,5,5],
+  [5,12,13],
 ]
 triangles.each do |sides|
-	tri = Triangle.new(*sides)
-	tri.recite_facts
+  tri = Triangle.new(*sides)
+  tri.recite_facts
 end

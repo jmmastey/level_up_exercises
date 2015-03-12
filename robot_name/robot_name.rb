ccee05f04a1bee9e34e2d467a59a32313c960dfd
Joshua Horvath <jhorvath@clm-jhorvath.enova.com>
2015-03-12 10:19:32 -0500

    Revert "Initial commit of robot name exercise"
    This reverts commit 1afad3db4eb1602dd809916e9c89d379d146d17a.

--- robot_name/robot_name.rb
+++ robot_name/robot_name.rb
@@ -1,53 +1,25 @@
 class NameCollisionError < RuntimeError; end
-class NameInvalidError < RuntimeError; end
 
 class Robot
-  attr_reader :name
+  attr_accessor :name
 
-  ERR_INVALID   = 'An invalid robot name was generated!'
-  ERR_COLLISION = 'A duplicate robot name was generated!'
+  @@registry
 
-  def initialize(generator = nil)
+  def initialize(args = {})
     @@registry ||= []
-    @name_generator = generator
+    @name_generator = args[:name_generator]
 
-    @name = @name_generator ? @name_generator.call : generate_name
-    check_name
+    if @name_generator
+      @name = @name_generator.call
+    else
+      generate_char = -> { ('A'..'Z').to_a.sample }
+      generate_num = -> { rand(10) }
 
+      @name = "#{generate_char.call}#{generate_char.call}#{generate_num.call}#{generate_num.call}#{generate_num.call}"
+    end
+
+    raise NameCollisionError, 'There was a problem generating the robot name!' if !(name =~ /[[:alpha:]]{2}[[:digit:]]{3}/) || @@registry.include?(name)
     @@registry << @name
-  end
-
-  def check_name
-    raise NameInvalidError, ERR_INVALID unless valid?(name)
-    raise NameCollisionError, ERR_COLLISION if @@registry.include?(name)
-  end
-
-  def valid?(name)
-    name.match(/[[:alpha:]]{2}[[:digit:]]{3}/)
-  end
-
-  def generate_name
-    "#{generate_char_str(2)}#{generate_num_str(3)}"
-  end
-
-  def generate_char_str(length)
-    str = ""
-
-    length.times do
-      str << ('A'..'Z').to_a.sample
-    end
-
-    str
-  end
-
-  def generate_num_str(length)
-    str = ""
-
-    length.times do
-      str << rand(10).to_s
-    end
-
-    str
   end
 end
 
@@ -55,8 +27,6 @@ robot = Robot.new
 puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
 
 # Errors!
-generator = -> { 'AA111' }
-Robot.new(generator)
-Robot.new(generator)
+# generator = -> { 'AA111' }
 # Robot.new(name_generator: generator)
 # Robot.new(name_generator: generator)

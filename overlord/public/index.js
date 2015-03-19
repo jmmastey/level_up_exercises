fb9f49575b5d4e51ed13e734c8a24906a77fea98
Kevin Shu <kshu@enova.com>
2015-03-19 17:21:55 -0500

    fixing more hound complaints

--- overlord/public/index.js
+++ overlord/public/index.js
@@ -1,38 +1,38 @@
 $(function(){
   $('.bomb-initialization-button').click(function(){
     var bombCodes = {
-      "activation_code": $('.bomb-activation-input').val(),
-      "deactivation_code": $('.bomb-deactivation-input').val()
+      'activation_code': $('.bomb-activation-input').val(),
+      'deactivation_code': $('.bomb-deactivation-input').val()
     };
 
     $.post('/initialize', bombCodes, function(){
-      location.reload()
+      location.reload();
     });
   });
 
   $('.bomb-activation-button').click(function(){
     var activationCode = {
-      "activation_code": $('.bomb-code-input').val()
+      'activation_code': $('.bomb-code-input').val()
     };
 
     $.post('/activate', activationCode, function(){
-      location.reload()
+      location.reload();
     });
   });
 
   $('.bomb-deactivation-button').click(function(){
     var deactivationCode = {
-      "deactivation_code": $('.bomb-code-input').val()
+      'deactivation_code': $('.bomb-code-input').val()
     };
 
     $.post('/deactivate', deactivationCode, function(){
-      location.reload()
+      location.reload();
     });
   });
 
   $('.bomb-button').click(function(){
-    var currentInput = $('.bomb-code-input').val()
-    var currentValue = $(this).html()
-    $('.bomb-code-input').val(currentInput + currentValue)
+    var currentInput = $('.bomb-code-input').val();
+    var currentValue = $(this).html();
+    $('.bomb-code-input').val(currentInput + currentValue);
   });
 });

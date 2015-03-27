1e33ed6af629e3d079c3ddfd71520978fb42a117
avavita <aciccarelli@enova.com>
2015-03-27 11:43:27 -0500

    separated out the interactive bits from the dinodex class

--- dino_catalog/Dinodex_Wizard.rb
+++ dino_catalog/Dinodex_Wizard.rb
@@ -1,9 +1,9 @@
 class Dinodex_Wizard
 
+  # this acts as an interactive interface for the dinodex class
+
 require_relative 'dinodex'
 require 'pp'
-
-@Dinodex = Dinodex.new
 
 def check_args(args)
   if args == []
@@ -22,7 +22,6 @@ def prompt_user_for_constraint
   puts "{examples: name = velociraptor, BIG, SMALL}"
 
   input = STDIN.gets.split("=", 2)
-
   return input if special_commands.include? input[0].strip
 
   if input.count != 2
@@ -36,25 +35,49 @@ def prompt_user_for_constraint
   unless joeFormat.include? input[0]
     abort("That's not a valid field. I bet you don't even like dinosaurs. Get out of here.")
   end
-
   return input
+end
 
+def prompt_user_for_constraints
+
+  filter_further = true
+  constraints = []
+
+  while filter_further
+    puts "add a constraint? (y/n)"
+    input = STDIN.gets.strip.downcase
+    if input == "y" 
+      constraints += prompt_user_for_constraint
+    else
+      filter_further = false
+    end
+  end
+  return constraints
+end
+
+def choose_function
+  puts "Select function to run:\nQUERY\nBIG\nSMALL\n"
+  input = STDIN.gets.strip.downcase
 end
 
 def run(files){
 
-  check_args(args)
-
-
+  check_args(files)
+  dex = Dinodex.new(files)
+  pp dex.array_of_dinos
+  pp "Here's the entire set."
+  choice = choose_function
+  case choice
+  when "query"
+    constraints = prompt_user_for_constraints
+    pp dex.filter_by_strings(constraints[0]. constraints[1])
+  when "big"
+    pp dex.get_big_dinos
+  when "small"
+    pp dex.get_small_dinos
+  end
 }
-
-    dinodex = Dinodex.new
-    dinodex.check_args(ARGV)
-    array_of_dinos = DinoConverter.csv_files_to_dino_array(ARGV)
-    pp array_of_dinos # it's really not that pretty, ruby.
-    p "Here's the entire set."
-    dinodex.filter_loop(array_of_dinos)
 
 end
 
-Dinodex_Wizard.run_via_console
+Dinodex_Wizard.run(ARGV)
--- dino_catalog/dinodex.rb
+++ dino_catalog/dinodex.rb
@@ -3,47 +3,41 @@ class Dinodex
 require_relative 'dinoConverter'
 require 'pp'
 
-@array_of_dinos= []
+attr_reader @array_of_dinos
+
+@array_of_dinos = []
+@big = 4000
 
 # Dinodex should do two things. 1: hold data. 2: return selections of that data. 
 
-def initialize(ARGV)
-  @array_of_dinos = DinoConverter.csv_files_to_dino_array(ARGV)
+def initialize(files)
+  @array_of_dinos = DinoConverter.csv_files_to_dino_array(files)
 end
 
-def check_args(args)
-  if args == []
-    abort("Please include CSV files as arguments. Exiting...")
-  end
+def filter_by_string(field, value)
+  @array_of_dinos.select {|dino| dino.like?(field, value)}
 end
 
-def filter_by_constraint(field, value, array_of_dinos) # this doesn't work exactly yet
-
-  puts field.chomp "SMALL"
-
-  if field.chomp == "BIG"
-    return array_of_dinos.select {|dino| dino.heavier_than?(4000)}
-  elsif field.chomp == "SMALL"
-    return array_of_dinos.select {|dino| 
-      puts "YO IT'S RIGhT HERE" + dino.heavier_than?(4000)
-      dino.heavier_than?(4000)}
-  elsif !value.nil?
-    return array_of_dinos.select {|dino| dino.like?(field, value) }
-  else
-    puts "This query doesn't make sense. Get out of here."
-  end
-  return array_of_dinos
+def filter_by_string_external_dino(field, value, dinos) # b/c ruby doesn't support overloading methods
+  dinos.select {|dino| dino.like?(field, value)}
 end
 
-def self.run_via_console
+def filter_by_strings(fields, values)
+  i = 0
+  filtered_array = @array_of_dinos
+  fields.count.times {
+    filtered_array = filter_by_string_external_dino(fields[i], values[i], filtered_array)
+    i += 1
+  }
+  return filtered_array
+end
 
-  dinodex = Dinodex.new(ARGV)
-  dinodex.check_args
-  array_of_dinos = DinoConverter.csv_files_to_dino_array(ARGV)
-  pp array_of_dinos # it's really not that pretty, ruby.
-  p "Here's the entire set."
-  dinodex.filter_loop(array_of_dinos)
+def get_big_dinos
+  @array_of_dinos.select {|dino| dino.heavier_than?(@big)}
+end
 
+def get_small_dinos
+  @array_of_dinos.select {|dino| !dino.heavier_than?(@big)}
 end
 
 end 

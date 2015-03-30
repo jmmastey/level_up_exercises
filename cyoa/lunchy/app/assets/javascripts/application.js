7629bf0d0990d919953d903ee5dcb2c8b622cea9
Joshua Horvath <jhorvath@clm-jhorvath.enova.com>
2015-03-30 17:34:30 -0500

    Added jquery-turbolinks gem to fix problems with document.ready and rails 4 turbolinks

--- cyoa/lunchy/app/assets/javascripts/application.js
+++ cyoa/lunchy/app/assets/javascripts/application.js
@@ -11,7 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
-//= require turbolinks
 //= require bootstrap-sprockets
+//= require turbolinks
 //= require_tree .

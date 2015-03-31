447fa65e2ef5104919ec958f9c6a0c4b2b65dbaf
Kareem Gan <kareemgan@gmail.com>
2015-03-31 13:19:30 -0500

    Added static pages

--- cyoa/weather/config/routes.rb
+++ cyoa/weather/config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  get 'static_pages/about'
+  get 'static_pages/help'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

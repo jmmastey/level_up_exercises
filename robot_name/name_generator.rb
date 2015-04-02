73d5a2b8d0f169f548c4554e8167ce78740d3b1e
avavita <aciccarelli@enova.com>
2015-04-02 16:56:04 -0500

    hi again hound.

--- robot_name/name_generator.rb
+++ robot_name/name_generator.rb
@@ -17,29 +17,32 @@ class NameGenerator
   end
 
   private
-
   def self.random_letter_sequence(number_of_characters)
-    number_of_characters.times.inject('') { |string, iteration| 
-      string << VALID_LETTERS.sample 
-    }
-  end 
-
-  def self.random_number_sequence(number_of_digits)
-    number_of_digits.times.inject('') { |int, iteration| 
-      int << VALID_NUMBERS.sample.to_s 
+    number_of_characters.times.inject('') { |string, _iteration|
+      string << VALID_LETTERS.sample
     }
   end
 
-  def self.generate_name 
+  private
+  def self.random_number_sequence(number_of_digits)
+    number_of_digits.times.inject('') { |int, _iteration|
+      int << VALID_NUMBERS.sample.to_s
+    }
+  end
+
+  private
+  def self.generate_name
     "#{random_letter_sequence(2)}#{random_number_sequence(3)}"
     # IB "Magic numbers." Cost of changing later is identical to now.
   end
 
+  private
   def self.generate_name_and_register
     name = generate_name
     REGISTRY.add_name(name) ? name : nil # is this weird?
   end
 
+  private
   def self.generate_unique_name(max_attempts)
     attempts = 0
 
@@ -49,9 +52,10 @@ class NameGenerator
       attempts += 1
     end
 
-    raise NameRegistryError,"Unique name cannot be generated."
+    raise NameRegistryError, "Unique name cannot be generated."
   end
 
+  private
   def self.check_name_format(name)
     unless name =~ ROBOT_NAME_FORMAT
       raise NameFormatError, "The robot's name sucks! (#{name})"

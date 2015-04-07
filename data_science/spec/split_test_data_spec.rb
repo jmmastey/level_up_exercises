aedaae70d88a3a204a1da262c355fd44177797f3
avavita <aciccarelli@enova.com>
2015-04-07 14:43:30 -0500

    okay, I see how this is useful. Done with the data import.

--- data_science/spec/split_test_data_spec.rb
+++ data_science/spec/split_test_data_spec.rb
@@ -2,12 +2,14 @@ require 'spec_helper'
 
 describe SplitTestData do 
   before :each do 
-    @data = SplitTestData.new(json)
+    file = 'spec/test_data.json'
+    file_contents = File.open(file, 'r') {|f| f.read }
+    @data = SplitTestData.new(file_contents)
   end
 
   describe "#new" do
     it "takes a json object and returns a SplitTestData object" do
-      @data.should be_an_instance_of SplitTestData
+      expect(@data).instance_of? SplitTestData
     end
   end
 end
--- data_science/split_test_data.rb
+++ data_science/split_test_data.rb
@@ -1,5 +1,9 @@
 require 'json'
 
 class SplitTestData
+  attr_reader :data
 
+  def initialize(json)
+    @data = JSON.parse(json)
+  end
 end

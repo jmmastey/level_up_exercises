0d7f69dea8fbed58741a75185a0cc22fa0a51831
avavita <aciccarelli@enova.com>
2015-04-07 14:54:22 -0500

    test is checking for a test object now. Ain't none. Good failing, test.

--- data_science/spec/split_test_spec.rb
+++ data_science/spec/split_test_spec.rb
@@ -1,1 +1,14 @@
 require 'spec_helper'
+
+describe SplitTest do 
+  before :each do 
+    @data = SplitTestData.new('spec/test_data.json')
+    @test = SplitTest.new(@data)
+  end
+
+  describe "#new" do
+    it "takes a SplitTestData object and returns a SplitTest object" do
+      expect(@test).instance_of? SplitTest
+    end
+  end
+end

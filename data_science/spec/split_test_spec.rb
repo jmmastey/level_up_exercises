ba597664607bd191ff68950d4fec6743aa7278f9
avavita <aciccarelli@enova.com>
2015-04-08 13:32:53 -0500

    made a generic method for countiong values by field in json objects.

--- data_science/spec/split_test_spec.rb
+++ data_science/spec/split_test_spec.rb
@@ -14,13 +14,19 @@ describe SplitTest do
 
   describe "#confidence" do 
     it "finds min/max predictions for a given confidence level" do
-      expect(@test.confidence(1000, 4000, 0.95)) === [0.23658104401082297, 0.26341895598917703]
+      expect(@test.confidence(1000, 4000, 0.95)).to eq([0.23658104401082297, 0.26341895598917703])
     end
   end
 
   describe "#sample_size" do
     it "finds the required sample size for accurate analysis" do 
-      expect(@test.sample_size(0.1, 0.11, 0.05, 0.8))
+      expect(@test.sample_size(0.1, 0.11, 0.05, 0.8)).to eq(14751)
+    end
+  end
+
+  describe "#count" do
+    it "counts how many rows have a specific value in a specific field." do
+      expect(@test.count("cohort", "A")).to eq(60)
     end
   end
 end

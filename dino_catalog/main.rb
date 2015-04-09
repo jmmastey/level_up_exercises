60e6b9f82e6a697374d76d7368dd7a0197068eea
Christine Schwerdtfeger <christine.schwerdtfeger@gmail.com>
2015-04-09 09:42:16 -0500

    Put the CSV reading into a different function.
    main.rb is now a script that calls the function to get
    the needed data and then performs the example queries.

--- dino_catalog/main.rb
+++ dino_catalog/main.rb
@@ -2,51 +2,36 @@ require 'csv'
 load 'dino_class.rb'
 load 'dino_filters.rb'
 
-def main()
+#This script reads the data and runs the queries of interest
+#If you want to run your own queries, you can just call
+#the read_data function and store the output in variables
+#like the following line of code!
+dino_filter_object, dino_objects = read_data()
 
-   #Make an list of dino_class objects
-   dino_objects = []
 
-   #Loop over the csv files.  Create dino object and append them to a list of dino objects
-   CSV.foreach('dino_csv_list') do |new_file|
-      filename = new_file[0]
-      dino_data = CSV.read(filename, :headers => true)
-      dino_data.each do |row| 
-         new_dino = Dinos.new(row.to_hash)
-         dino_objects.push(new_dino)
-      end
-   end
+#Find bipeds
+puts "**********These are the bipeds!************"
+dino_filter_object.find_bipeds.print_list
 
-   #Create Dino_filters object so that all the filter methods can be used
-   dino_filter_object = Dino_filters.new(dino_objects)
- 
+#Find carnivores
+puts "**********These are the carnivores!**********"
+dino_filter_object.find_carnivores.print_list
 
-   #Find bipeds
-   puts '\n **********These are the bipeds!*******************'
-   dino_filter_object.find_bipeds.print_list
-  
-   #Find carnivores
-   puts '\n **********These are the carnivores!*******************'
-   dino_filter_object.find_carnivores.print_list
+#Find dinos from Cretaceous periods
+puts "**********These are the Cretaceous dinos!**********"
+dino_filter_object.find_dinos_specific_period('Cretaceous').print_list
 
-   #Find dinos from Cretaceous periods
-   puts '\n **********These are the Cretaceous dinos!*******************'
-   dino_filter_object.find_dinos_specific_period('Cretaceous').print_list
+#Find big dinos
+puts "**********These are the dinos over 2 tons!******"
+dino_filter_object.find_big_dinos(4000).print_list
 
-   #Find big dinos
-   puts '\n **********These are the dinos over 2 tons!*******************'
-   dino_filter_object.find_big_dinos(4000).print_list
+#Find small dinos
+puts "**********These are the dinos under 2 tons!*****"
+dino_filter_object.find_small_dinos(4000).print_list
 
-   #Find small dinos
-   puts '\n **********These are the dinos under 2 tons!*******************'
-   dino_filter_object.find_small_dinos(4000).print_list
+#Use multiple filters
+puts "**********These are the carnivorous bipeds!*******"
+dino_filter_object.find_carnivores.find_bipeds.print_list
 
-   #Use multiple filters
-   puts '\n **********These are the carnivorous bipeds!*******************'   
-   dino_filter_object.find_carnivores.find_bipeds.print_list
-
-  # return dino_filter_object, dino_objects
-  return dino_filter_object, dino_objects
    
 
-end 

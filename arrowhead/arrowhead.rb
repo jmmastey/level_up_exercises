23f4d9cd3297e19ac7c8b3b86d2d93f32be25b7e
Christine Schwerdtfeger <christine.schwerdtfeger@gmail.com>
2015-04-10 13:30:13 -0500

    Changed the if else nested block so that the function breaks at the point of faulture and returns a print statement specifying the error for the case when the key is not found in the dictionary.

--- arrowhead/arrowhead.rb
+++ arrowhead/arrowhead.rb
@@ -17,18 +17,15 @@ class Arrowhead
 
   # FIXME: I don't have time to deal with this.
   def self.classify(region, shape)
-    if CLASSIFICATIONS.include? region
-      shapes = CLASSIFICATIONS[region]
-      if shapes.include? shape
-        arrowhead = shapes[shape]
-        puts "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
-      else
-        raise "Unknown shape value. Are you sure you know what you're talking about?"
-      end
-    else
-      raise "Unknown region, please provide a valid region."
-    end
+
+    return 'Unknown region.' if not CLASSIFICATIONS.include?(region)
+    shapes = CLASSIFICATIONS[region]
+    return 'Unknown shape.' if not shapes.include?(shape)
+    arrowhead = shapes[shape]
+    puts "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
   end
 end
 
-puts Arrowhead.classify(:northern_plains, :bifurcated)
+puts Arrowhead.classify(:far_west, :bisurcated)
+puts Arrowhead.classify(:far_east, :notched)
+puts Arrowhead.classify(:far_west, :bifurcated)

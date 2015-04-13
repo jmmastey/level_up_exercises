52737cfa4adf2d25f0c1c806c9d82c55c2b71b32
Chaitanya Khadilkar <ckhadilkar@enova.com>
2015-04-13 16:42:37 -0500

    Devise User Model

--- cyoa/movie_review/config/routes.rb
+++ cyoa/movie_review/config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'application#hello'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 

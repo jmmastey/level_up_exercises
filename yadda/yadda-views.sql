ef4705571693a6b75e95c3859760186ff2a8dbcc
rcowan <ryancowan@gmail.com>
2015-04-14 12:03:42 -0500

    rewrite views without subquery

--- yadda/yadda-views.sql
+++ yadda/yadda-views.sql
@@ -1,20 +1,15 @@
 -- Usage: psql -d yadda -f yadda/yadda-views.sql
 
-#try to keep inner joins
-#preferable over subqueries
-
 -- Top beers from a given brewery, according to their total ratings. 
 create or replace view top_beers AS
   select breweries.brewery_id as breweryid,
     breweries.name as breweryname,
     beers.name as beername,
-    ratings.ratings_count
+    COUNT(ratings.overall) as ratings_count
   from breweries 
-  left join beers on beers.brewery_id = breweries.brewery_id 
-  left join (select beer_id,
-               COUNT(ratings.overall) as ratings_count 
-             from ratings 
-             group by beer_id) ratings on ratings.beer_id = beers.beer_id
+  left join beers on beers.brewery_id = breweries.brewery_id
+  left join ratings on ratings.beer_id = beers.beer_id
+  group by breweries.brewery_id, beers.beer_id
   order by ratings_count desc;
 
 -- "Recent score" for a beer, where only ratings within the last six months are counted and the ratings within that period are averaged. 
@@ -22,25 +17,21 @@ create or replace view recent_score AS
   select breweries.brewery_id as breweryid,
     breweries.name as breweryname,
     beers.name as beername,
-    ratings.computed
+    AVG(ratings.overall) as computed
   from breweries 
   left join beers on beers.brewery_id = breweries.brewery_id 
-  left join (select beer_id,
-               AVG(ratings.overall) as computed 
-             from ratings 
-             where created_at >= (NOW() - interval '6 months')
-             group by beer_id) ratings on ratings.beer_id = beers.beer_id
+  left join ratings on ratings.beer_id = beers.beer_id
+  where ratings.created_at >= (NOW() - interval '6 months')
+  group by breweries.brewery_id, beers.beer_id
   order by computed desc;
 
 -- "You might also enjoy", which picks beers of the same style with high average scores, and then sorts them randomly. 
 create or replace view also_enjoy AS
   select beers.name as beername,
     styles.style as beerstyle,
-    ratings.computed
+    AVG(ratings.overall) as computed
   from beers
   left join styles on beers.style_id = styles.style_id
-  left join (select beer_id,
-               AVG(ratings.overall) as computed 
-             from ratings 
-             group by beer_id) ratings on ratings.beer_id = beers.beer_id
+  left join ratings on ratings.beer_id = beers.beer_id
+  group by styles.style_id, beers.beer_id
   order by RANDOM();

2631b96529c7dcc0df1db3973b24ab30f4c87d06
Chaitanya Khadilkar <ckhadilkar@enova.com>
2015-04-14 15:24:04 -0500

    Static Pages

--- cyoa/movie_review/config/routes.rb
+++ cyoa/movie_review/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'application#hello'
+  root             'static_pages#home'
+  get 'help'    => 'static_pages#help'
+  get 'about'   => 'static_pages#about'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

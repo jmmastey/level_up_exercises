2930f0d0135b84c39f6c180c979e435638496ad1
rcowan <ryancowan@gmail.com>
2015-04-16 15:12:16 -0500

    add created_at index for ratings

--- yadda/yadda-indices.sql
+++ yadda/yadda-indices.sql
@@ -5,6 +5,7 @@ CREATE UNIQUE INDEX breweries_id_index ON breweries(brewery_id);
 CREATE INDEX beers_id_to_breweries_id_index ON beers(beer_id, brewery_id);
 CREATE INDEX persons_id_index ON persons(person_id);
 CREATE UNIQUE INDEX ratings_id_index ON ratings(rating_id);
+CREATE INDEX ratings_created_at_index ON ratings(created_at);
 
 -- multicolumn indices
 CREATE INDEX persons_to_beers_index ON ratings(person_id, beer_id);

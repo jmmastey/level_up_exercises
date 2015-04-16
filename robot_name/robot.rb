bc6b092499f3f41345dcacc0b1f8db6fab277179
Christine Schwerdtfeger <christine.schwerdtfeger@gmail.com>
2015-04-16 17:34:17 -0500

    Simplified initialize function and took other suggestions from review.

--- robot_name/robot.rb
+++ robot_name/robot.rb
@@ -1,9 +1,8 @@
-class NameGenerationError < RuntimeError; end
+NameCollisionError = Class.new(RuntimeError)
+NameFormatError = Class.new(RuntimeError)
 
 class Robot
   attr_accessor :robot_name
-
-  @@name_registry
 
   def initialize(name_gen = nil)
     @@name_registry ||= []
@@ -11,34 +10,41 @@ class Robot
 
     attempts = 0
     while attempts < 3
-      if @name_generator
-        @robot_name = @name_generator.call
-        puts "robot name is #{@robot_name}"
-      else
-        @robot_name = generate_name
-      end
+      @robot_name = generate_name
       break if valid_name?
       attempts += 1
     end
 
-    if valid_name?
-      @@name_registry << @robot_name
+    find_error unless valid_name?
+    @@name_registry << @robot_name
+  end
+
+  private
+
+  def generate_name
+    if @name_generator
+      @name_generator.call
     else
-      find_error
+      internal_name_generator
     end
   end
 
-  def generate_name
-    generate_char = -> { ('A'..'Z').to_a.sample } # convert range to array
-    generate_num = -> { rand(10) }
-    robot_name = (1..2).collect { generate_char.call }
-    robot_name += (1..3).collect { generate_num.call }
-    robot_name.join
+  private
+
+  def internal_name_generator
+    char_list = ('A'..'Z').to_a
+    num_list = (0..9).to_a
+    @robot_name = (1..2).inject("") { |name| name + char_list.sample }
+    @robot_name += (1..3).inject("") { |name| name + num_list.sample.to_s }
   end
+
+  private
 
   def name_already_in_list?
     @@name_registry.include?(@robot_name)
   end
+
+  private
 
   def wrong_name_format?
     @robot_name[/^[[:alpha:]]{2}[[:digit:]]{3}$/].nil?
@@ -48,20 +54,19 @@ class Robot
     !(name_already_in_list? || wrong_name_format?)
   end
 
+  private
+
   def find_error
-    if name_already_in_list?
-      raise NameGenerationError, 'Name already in list.'
-    else
-      raise NameGenerationError, 'Name has wrong format.'
-    end
+    raise NameCollisionError, 'Name already in list.' if name_already_in_list?
+    raise NameFormatError, 'Name has wrong format.' if wrong_name_format?
   end
 end
 
-#robot = Robot.new
-#puts "My pet robot's name is #{robot.robot_name}, but we usually call him Rob."
+robot = Robot.new
+puts "My pet robot's name is #{robot.robot_name}, but we usually call him Rob."
 
 # Errors!
 #  generator = -> { 'AA111' }
-#generator = -> { ('A'..'Z').to_a.sample }  # Will give incorrect format error
-#Robot.new(name_generator: generator)
-#Robot.new(name_generator: generator)
+# generator = -> { ('A'..'Z').to_a.sample }  # Will give incorrect format error
+# Robot.new(name_generator: generator)
+# Robot.new(name_generator: generator)

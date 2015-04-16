7befa2ef7cc7e64b1d116c522bdf9a09adff1f94
rcowan <ryancowan@gmail.com>
2015-04-16 15:16:04 -0500

    update view keyword capitalization

--- yadda/yadda-views.sql
+++ yadda/yadda-views.sql
@@ -1,37 +1,37 @@
 -- Usage: psql -d yadda -f yadda/yadda-views.sql
 
 -- Top beers from a given brewery, according to their total ratings. 
-create or replace view top_beers AS
-  select breweries.brewery_id as breweryid,
-    breweries.name as breweryname,
-    beers.name as beername,
-    COUNT(ratings.overall) as ratings_count
-  from breweries 
-  left join beers on beers.brewery_id = breweries.brewery_id
-  left join ratings on ratings.beer_id = beers.beer_id
-  group by breweries.brewery_id, beers.beer_id
-  order by ratings_count desc;
+CREATE OR REPLACE VIEW top_beers AS
+  SELECT breweries.brewery_id AS breweryid,
+    breweries.name AS breweryname,
+    beers.name AS beername,
+    COUNT(ratings.overall) AS ratings_count
+  FROM breweries 
+  LEFT JOIN beers ON beers.brewery_id = breweries.brewery_id
+  LEFT JOIN ratings ON ratings.beer_id = beers.beer_id
+  GROUP BY breweries.brewery_id, beers.beer_id
+  ORDER BY ratings_count DESC;
 
 -- "Recent score" for a beer, where only ratings within the last six months are counted and the ratings within that period are averaged. 
-create or replace view recent_score AS
-  select breweries.brewery_id as breweryid,
-    breweries.name as breweryname,
-    beers.name as beername,
-    AVG(ratings.overall) as computed
-  from breweries 
-  left join beers on beers.brewery_id = breweries.brewery_id 
-  left join ratings on ratings.beer_id = beers.beer_id
-  where ratings.created_at >= (NOW() - interval '6 months')
-  group by breweries.brewery_id, beers.beer_id
-  order by computed desc;
+CREATE OR REPLACE VIEW recent_score AS
+  SELECT breweries.brewery_id AS breweryid,
+    breweries.name AS breweryname,
+    beers.name AS beername,
+    AVG(ratings.overall) AS computed
+  FROM breweries 
+  LEFT JOIN beers ON beers.brewery_id = breweries.brewery_id 
+  LEFT JOIN ratings ON ratings.beer_id = beers.beer_id
+  WHERE ratings.created_at >= (NOW() - interval '6 months')
+  GROUP BY breweries.brewery_id, beers.beer_id
+  ORDER BY computed DESC;
 
 -- "You might also enjoy", which picks beers of the same style with high average scores, and then sorts them randomly. 
-create or replace view also_enjoy AS
-  select beers.name as beername,
-    styles.style as beerstyle,
-    AVG(ratings.overall) as computed
-  from beers
-  left join styles on beers.style_id = styles.style_id
-  left join ratings on ratings.beer_id = beers.beer_id
-  group by styles.style_id, beers.beer_id
-  order by RANDOM();
+CREATE OR REPLACE VIEW also_enjoy AS
+  SELECT beers.name AS beername,
+    styles.style AS beerstyle,
+    AVG(ratings.overall) AS computed
+  FROM beers
+  LEFT JOIN styles ON beers.style_id = styles.style_id
+  LEFT JOIN ratings ON ratings.beer_id = beers.beer_id
+  GROUP BY styles.style_id, beers.beer_id
+  ORDER BY RANDOM();

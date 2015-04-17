24ae042145300601e923f7252ccbda308de070fe
avavita <aciccarelli@enova.com>
2015-04-17 14:50:21 -0500

    whoops

--- data_science/ab_runner.rb
+++ data_science/ab_runner.rb
@@ -4,7 +4,7 @@ require_relative 'cohort_converter'
 
 abort('Please include one json file as an argument.') unless ARGV.count == 1
 
-converter = CohortConverter.new(ARGV[0]) #this runner is very lazy.
+converter = CohortConverter.new(ARGV[0])
 cohorts = converter.cohorts
 test = SplitTest.new(cohorts)
 
@@ -20,17 +20,5 @@ pp test.conversion_rates
 puts '95% of the time, the resulting proportion will be between...'
 pp test.confidence
 puts 'Probability that data at least as surprising as the observed sample ' <<
-         'results would be generated under a model of random chance: '
+  'results would be generated under a model of random chance: '
 pp test.chi_square
-
-# ruby ab_runner.rb data_export_2014_06_20_15_59_02.json 
-# > Successes for each cohort: 
-# > {"B"=>79, "A"=>47}
-# > Attempts for each cohort: 
-# > {"B"=>1543, "A"=>1349}
-# > 95% of the time, the resulting proportion will be between...
-# > {"B"=>[0.04020173369400816, 0.06219619242394388],
-# > "A"=>[0.025055087455844, 0.04462615791109446]}
-# > Probability that data at least as surprising as the observed sample 
-# > results would be generated under a model of random chance: 
-# > 0.03156402546059378

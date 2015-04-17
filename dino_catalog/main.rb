9579aec11330ba90ef71adb390c627ee172a9b72
Christine Schwerdtfeger <christine.schwerdtfeger@gmail.com>
2015-04-17 17:05:45 -0500

    Changed main to have the data reader and then the dino filter object accessed in the same line since I don't need to store a data_reader object.

--- dino_catalog/main.rb
+++ dino_catalog/main.rb
@@ -4,12 +4,10 @@ require_relative './dino_filters.rb'
 require_relative './data_reader.rb'
 require_relative './user_input.rb'
 
-# This script reads the data and runs the queries of interest
-# If you want to run your own queries, you can just need
+# If you want to run your own queries, you just need
 # to get the dino_filter_object
 
-data_reader = DataReader.new('dino_csv_list')
-dino_filter = data_reader.dino_filter
+dino_filter = DataReader.new('dino_csv_list').dino_filter
 
 user_input = UserInput.new
 user_input.query_user

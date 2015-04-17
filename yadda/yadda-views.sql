c9ab3ec736a89fe8aeb8fff945282d9d10b29182
rcowan <ryancowan@gmail.com>
2015-04-17 08:54:15 -0500

    change name of computed to average_rating, add having clause

--- yadda/yadda-views.sql
+++ yadda/yadda-views.sql
@@ -17,21 +17,22 @@ CREATE OR REPLACE VIEW recent_score AS
   SELECT breweries.brewery_id AS breweryid,
     breweries.name AS breweryname,
     beers.name AS beername,
-    AVG(ratings.overall) AS computed
+    AVG(ratings.overall) AS average_rating
   FROM breweries 
   LEFT JOIN beers ON beers.brewery_id = breweries.brewery_id 
   LEFT JOIN ratings ON ratings.beer_id = beers.beer_id
   WHERE ratings.created_at >= (NOW() - interval '6 months')
   GROUP BY breweries.brewery_id, beers.beer_id
-  ORDER BY computed DESC;
+  ORDER BY average_rating DESC;
 
 -- "You might also enjoy", which picks beers of the same style with high average scores, and then sorts them randomly. 
 CREATE OR REPLACE VIEW also_enjoy AS
   SELECT beers.name AS beername,
     styles.style AS beerstyle,
-    AVG(ratings.overall) AS computed
+    AVG(ratings.overall) AS average_rating
   FROM beers
   LEFT JOIN styles ON beers.style_id = styles.style_id
   LEFT JOIN ratings ON ratings.beer_id = beers.beer_id
   GROUP BY styles.style_id, beers.beer_id
+  HAVING AVG(ratings.overall) > 4
   ORDER BY RANDOM();

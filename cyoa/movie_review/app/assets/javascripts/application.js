74c0a11b0f5e4272ebb33db9ff01d304ebb087ff
Chaitanya Khadilkar <ckhadilkar@enova.com>
2015-04-20 15:32:02 -0500

    fixed flash msg issue

--- cyoa/movie_review/app/assets/javascripts/application.js
+++ cyoa/movie_review/app/assets/javascripts/application.js
@@ -12,5 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+
+//= require bootstrap-sprockets
 //= require_tree .
+
--- cyoa/movie_review/app/controllers/reviews_controller.rb
+++ cyoa/movie_review/app/controllers/reviews_controller.rb
@@ -1,6 +1,5 @@
 class ReviewsController < ApplicationController
 	before_action :set_movie
-	#before_action :set_review
 	before_action :authenticate_user!
 	
 	def new
@@ -8,21 +7,20 @@ class ReviewsController < ApplicationController
 	end
 
 	def create
+
 		@review = Review.new(review_params)
 		@review.user_id = current_user.id
 		@review.movie_id = @movie.id
-
 		if @review.save
 			redirect_to @movie
 		else
+			flash.now[:error] = @review.errors.messages[:rating][0] if @review.errors.messages[:rating]
+			flash.now[:error] = @review.errors.messages[:comment][0] if @review.errors.messages[:comment]
 			render 'new'
 		end
 	end
 	
 	private
-		def set_review
-			@review = Review.find(params[:id])
-		end
 		def set_movie
 			@movie = Movie.find(params[:movie_id])
 		end

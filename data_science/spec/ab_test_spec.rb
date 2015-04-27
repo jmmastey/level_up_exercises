593950a9d30b37dcf5dca4cfed4878f7a2ad30f7
Christine Schwerdtfeger <christine.schwerdtfeger@gmail.com>
2015-04-27 10:39:50 -0500

    Changing the rspec files to use "let" blocks and be_within statement.

--- data_science/spec/ab_test_spec.rb
+++ data_science/spec/ab_test_spec.rb
@@ -2,34 +2,38 @@ require_relative '../ab_test'
 
 require 'json'
 
-describe ABtest do
-  before :all do
-    file = File.read('smaller_data_set.json')
-    all_data = JSON.parse(file)
-    pop_a = all_data.select { |record| record["cohort"] == "A" }
-    pop_b = all_data.select { |record| record["cohort"] == "B" }
-    cohort_a = Cohort.new(pop_a)
-    cohort_b = Cohort.new(pop_b)
-    @ab_test = ABtest.new(cohort_a, cohort_b)
-  end
+describe "let" do 
+  leading_cohort_name = :B
+  confidence_in_leader = 0.3640661012729629
 
-  describe("#new") do
-    it "takes 2 cohorts and returns and ABtest object" do
-      expect(@ab_test).to(be_an_instance_of(ABtest))
+  describe ABtest do
+    before :all do
+      file = File.read('smaller_data_set.json')
+      all_data = JSON.parse(file)
+      pop_a = all_data.select { |record| record["cohort"] == "A" }
+      pop_b = all_data.select { |record| record["cohort"] == "B" }
+      cohort_a = Cohort.new(pop_a)
+      cohort_b = Cohort.new(pop_b)
+      @ab_test = ABtest.new(cohort_a, cohort_b)
     end
-  end
-  describe("@new") do
-    it "has 2 instance variables, cohort_A, cohort_B of type Cohort" do
-      expect(@ab_test.cohort_a).to(be_an_instance_of(Cohort))
-      expect(@ab_test.cohort_b).to(be_an_instance_of(Cohort))
+
+    describe("#new") do
+      it "takes 2 cohorts and returns and ABtest object" do
+        expect(@ab_test).to(be_an_instance_of(ABtest))
+      end
+      it "has 2 instance variables, cohort_A, cohort_B of type Cohort" do
+        expect(@ab_test.cohort_a).to(be_an_instance_of(Cohort))
+        expect(@ab_test.cohort_b).to(be_an_instance_of(Cohort))
+      end
     end
-  end
 
-  it "finds the leader in conversion rates" do
-    expect(@ab_test.leader).to(eql(@ab_test.cohort_b.name))
-  end
+    it "finds the leader in conversion rates" do
+      expect(@ab_test.leader).to(eql(leading_cohort_name))
+    end
 
-  it "determines if there is a signifcant leader" do
-    expect(@ab_test.compute_leader_confidence_level).to(eql(0.3640661012729629))
+    describe @ab_test.compute_leader_confidence_level do
+      it "determines if there is a signifcant leader" do
+      it { should_be_within(0.01).of.(confidence_in_leader) }
+    end
   end
 end
--- data_science/spec/cohort_spec.rb
+++ data_science/spec/cohort_spec.rb
@@ -9,52 +9,52 @@ describe "let" do
   conversion_rate = num_conversions.to_f/sample_size
   std_dev = Math.sqrt(conversion_rate*(1-conversion_rate)/sample_size)
   confidence_95pct = 1.96*std_dev
+  all_data = JSON.parse(File.read('smaller_data_set.json')) 
+  pop_data = all_data.select { |record| record["cohort"] == "A" }
+  cohort = Cohort.new(pop_data)
 
   describe Cohort do
-    before :all do
-      file = File.read('smaller_data_set.json')
-      all_data = JSON.parse(file)
-      pop_data = all_data.select { |record| record["cohort"] == "A" }
-      @cohort = Cohort.new(pop_data)
-    end
+    # When would a before :all block be used (if everything is done in let)?
+    # before :all do
+    # end
 
     describe("#new") do
       it "takes a list of dictionaries and returns a Cohort object" do
-        expect(@cohort).to(be_an_instance_of(Cohort))
+        expect(cohort).to(be_an_instance_of(Cohort))
       end
     end
 
     describe("#name") do
       it "returns the correct cohort name" do
-        expect(@cohort.name).to(eql(cohort_name))
+        expect(cohort.name).to(eql(cohort_name))
       end
     end
 
     describe("#sample_size") do
       it "returns the size of the cohort" do
-        expect(@cohort.sample_size).to(eql(sample_size))
+        expect(cohort.sample_size).to(eql(sample_size))
       end
     end
 
     describe("#num_conversions") do
       it "returns the number of successes" do
-        expect(@cohort.num_conversions).to(eql(num_conversions))
+        expect(cohort.num_conversions).to(eql(num_conversions))
       end
     end
 
     it "returns the conversion rate for this cohort" do
-      expect(@cohort.conversion_rate).to(eql(conversion_rate))
+      expect(cohort.conversion_rate).to(eql(conversion_rate))
     end
 
     it "returns the standard deviation for this cohort" do
-      expect(@cohort.std_dev).to(eql(std_dev))
+      expect(cohort.std_dev).to(eql(std_dev))
     end
 
     it "returns the 95% confidence level for this cohort" do
       confidence_interval = []
-      confidence_interval.push(@cohort.conversion_rate - confidence_95pct)
-      confidence_interval.push(@cohort.conversion_rate + confidence_95pct)
-      expect(@cohort.compute_confidence_interval_95pct).to(eql(confidence_interval))
+      confidence_interval.push(cohort.conversion_rate - confidence_95pct)
+      confidence_interval.push(cohort.conversion_rate + confidence_95pct)
+      expect(cohort.compute_confidence_interval_95pct).to(eql(confidence_interval))
     end
   end
 end

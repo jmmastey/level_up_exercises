e206217a1a48d3099a8ceb40078a12f033068fe3
Christine Schwerdtfeger <christine.schwerdtfeger@gmail.com>
2015-04-27 09:26:25 -0500

    Initial testing

--- overlord/overlord.rb
+++ overlord/overlord.rb
@@ -1,6 +1,7 @@
 # run `ruby overlord.rb` to run a webserver for this app
 
 require 'sinatra'
+require 'bomb.rb'
 
 enable :sessions
 
@@ -10,5 +11,5 @@ end
 
 # we can shove stuff into the session cookie YAY!
 def start_time
-  session[:start_time] ||= (Time.now).to_s
+  session[:start_time]  ||= (Time.now).to_s
 end

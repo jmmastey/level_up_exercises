5ce1eaada9b2591f14ef99ad94f08868d77ee13f
dietpop <mwaldorf@clm-mwaldorf.local>
2015-05-04 08:50:27 -0500

    dino class initialized

--- dino_catalog/dinoclass.rb
+++ dino_catalog/dinoclass.rb
@@ -1,13 +1,26 @@
 class DinoDex
 	def initialize(raw_data)
-        dinos=Array.new 
+        @dinos=Array.new 
         raw_data.each_with_index do |row, i|
-           #arry[i]=Dino.new(row)
-            dinos[i]=Dino.new(row[:name], row[:period])
+            if row[:genus]!=nil
+                row[:name]=row[:genus]
+            end
+            if row[:continent]==nil
+                row[:continent]="Africa"
+            end
+            if row[:carnivore]!=nil
+                row[:diet]="Carnivore"
+            end
+            @dinos[i]=Dino.new(row[:name], row[:period], row[:continent], row[:diet], row[:weight], row[:walking], row[:description])
         end           
-        dinos.each do |j| 
+        @dinos.each do |j| 
             p j.name 
             p j.period 
+            p j.continent
+            p j.diet
+            p j.weight
+            p j.walking
+            p j.description
         end
     end
   
@@ -20,16 +33,17 @@ end
 
 
 class Dino
-	#attr_accessor :genus, :period, :continent, :carnivore, :weight, :walking, :description
-    attr_reader :name, :period
-    #def initialize(genus, period, continent, carnivore, weight, walking, description)
+    attr_reader :name, :period, :continent, :diet, :weight, :walking, :description
     
-    def initialize(name_info, period_info )
-        #p all_info
-        #puts "#{all_info[:name]}#{all_info[:genus]}"
+    def initialize(name_info, period_info, continent_info, diet_info, weight_info, walking, description )
         #p all_info
         @name=name_info
         @period=period_info
+        @continent=continent_info
+        @diet=diet_info
+        @weight=weight_info
+        @walking=walking
+        @description=description
     end
 
 end

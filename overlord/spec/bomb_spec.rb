901dcb88aad17996db8ef497dca4f5c2706260f5
Christine Schwerdtfeger <christine.schwerdtfeger@gmail.com>
2015-05-04 16:51:44 -0500

    Changed bomb rspec to expect a raised error rather than nil (because the object was not created).

--- overlord/spec/bomb_spec.rb
+++ overlord/spec/bomb_spec.rb
@@ -4,16 +4,12 @@ VALID_ACT_CODE = 2468
 VALID_DEACT_CODE = 1357
 INVALID_ACT_CODE = "abcd"
 INVALID_DEACT_CODE = "hello"
-TIME_REMAINING = 30
+TIME_REMAINING = 3
 
 describe Bomb do
   let(:newbomb_valid) {Bomb.new(VALID_ACT_CODE, VALID_DEACT_CODE) }
-#  let(:newbomb_invalid) {Bomb.new(INVALID_ACT_CODE, INVALID_DEACT_CODE) }
+  let(:newbomb_invalid) {Bomb.new(INVALID_ACT_CODE, INVALID_DEACT_CODE) }
   
-  it "has #{TIME_REMAINING} seconds after the bomb is created but before activation." do
-    expect(newbomb_valid.time_remaining).to eql(TIME_REMAINING)
-  end
-
   it "is in an inactive state before start_bomb is called" do 
     expect(newbomb_valid.active).to eql(false)
   end
@@ -42,8 +38,12 @@ describe Bomb do
   end
 
   it "explodes after #{TIME_REMAINING} seconds" do
-    newbomb_valid.restart_bomb(VALID_ACT_CODE)
+    newbomb_valid.start_bomb(VALID_ACT_CODE)
     sleep(TIME_REMAINING+2)
     expect(newbomb_valid.exploded).to eql(true)
+  end
+
+  it "complains when the activation code is not four digits" do
+    expect{Bomb.new(INVALID_ACT_CODE,VALID_DEACT_CODE)}.to raise_error(BombCodeError)
   end
 end

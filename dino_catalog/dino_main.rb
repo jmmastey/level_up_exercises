4f2c40a7c86a060c9aae7dbebe40e3331f929b13
dietpop <mwaldorf@clm-mwaldorf.local>
2015-05-05 10:16:53 -0500

    asks user for filters

--- dino_catalog/dino_main.rb
+++ dino_catalog/dino_main.rb
@@ -4,14 +4,16 @@ require './dinodex_class.rb'
 
 dino_input_files = ['./dinodex.csv', './african_dinosaur_export.csv']
 
-csv_array = CsvImporter.new(dino_input_files).create_dinos
+#csv_array = CsvImporter.new(dino_input_files).create_dinos
+# puts csv_array[9].name
 
- # puts csv_array[9].name
+my_dinodex = DinoDex.new(CsvImporter.new(dino_input_files).create_dinos)
+#p my_dinodex.filter_dinos('Biped')
 
-my_dinodex = DinoDex.new(csv_array)
-my_dinodex.get_matching_dinos('Biped')
-
-puts "What kind of dino would you like to view?"
-filter = gets.chomp
-my_dinodex.get_matching_dinos(filter)
+my_dinodex.prompt_user_for_filter("name")
+my_dinodex.prompt_user_for_filter("period")
+my_dinodex.prompt_user_for_filter("continent")
+my_dinodex.prompt_user_for_filter("diet")
+my_dinodex.prompt_user_for_filter("weight")
+my_dinodex.prompt_user_for_filter("walking")
 
--- dino_catalog/dinodex_class.rb
+++ dino_catalog/dinodex_class.rb
@@ -5,13 +5,23 @@ class DinoDex
         @dino_array = dino_arry
     end
 
-    def get_matching_dinos(filter_params)
+    def prompt_user_for_filter(filter_name)
+        puts "Would you like to view dinosaurs by #{filter_name}? Y/N"
+        if gets.chomp.downcase == 'y'
+            puts "Choose the number of the #{filter_name} you wish to view:"
+            get_all_values(filter_name).each_with_index {|val,i| puts "#{i+1}. #{val}\n" }
+        end
+    end
+
+    def filter_dinos(filter_params)
+        matching_dinos = Array.new
         @dino_array.each  do |i|
             #  puts i.all_info
             if i.all_info.include?(filter_params)
-                puts i.name, " is a #{filter_params}"
+                matching_dinos << i.name
+               # puts i.name, " is a #{filter_params}"
             else
-                puts i.name, " is not a #{filter_params}"
+               # puts i.name, " is not a #{filter_params}"
             end
 
           #  if i.walking.include?("Biped") 
@@ -22,8 +32,16 @@ class DinoDex
          #     puts i.name, " not biped"
          #   end
         end
-
+    matching_dinos
     end
+
+    def get_all_values(key)
+        values = Array.new
+        @dino_array.each do |i|
+        values << i.send(key)
+    end
+    values.uniq
+    end 
 end
 
 

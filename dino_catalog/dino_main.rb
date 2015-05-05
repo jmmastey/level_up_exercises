1195f74de95b0ffce74c8497cf077c878b44872a
dietpop <mwaldorf@clm-mwaldorf.local>
2015-05-05 14:32:49 -0500

    input gathered, query built but not run

--- dino_catalog/dino_main.rb
+++ dino_catalog/dino_main.rb
@@ -10,10 +10,4 @@ dino_input_files = ['./dinodex.csv', './african_dinosaur_export.csv']
 my_dinodex = DinoDex.new(CsvImporter.new(dino_input_files).create_dinos)
 #p my_dinodex.filter_dinos('Biped')
 
-my_dinodex.prompt_user_for_filter("name")
-my_dinodex.prompt_user_for_filter("period")
-my_dinodex.prompt_user_for_filter("continent")
-my_dinodex.prompt_user_for_filter("diet")
-my_dinodex.prompt_user_for_filter("weight")
-my_dinodex.prompt_user_for_filter("walking")
-
+my_dinodex.run_user_interface
--- dino_catalog/dinodex_class.rb
+++ dino_catalog/dinodex_class.rb
@@ -5,13 +5,38 @@ class DinoDex
         @dino_array = dino_arry
     end
 
+    def run_user_interface()
+        @filter_query = {:name => "", :period => "" , :continent => "", :diet => "", :weight => "", :walking => ""}
+        keys = ["name", "period", "continent", "diet", "weight", "walking"]
+        keys.each { |k| prompt_user_for_filter(k)}
+        p @filter_query
+        #p @filter_query[0]
+        #filter_dinos(@filter_query[0])
+    end
+
     def prompt_user_for_filter(filter_name)
         puts "Would you like to view dinosaurs by #{filter_name}? Y/N"
+        values = Array.new
         if gets.chomp.downcase == 'y'
             puts "Choose the number of the #{filter_name} you wish to view:"
-            get_all_values(filter_name).each_with_index {|val,i| puts "#{i+1}. #{val}\n" }
+            get_all_values(filter_name).each_with_index do |val,i| 
+                puts "#{i+1}. #{val}\n"
+                values << val
+            end
+            users_selection = values[gets.chomp.to_i - 1]
+            p filter_name.to_sym
+            @filter_query[filter_name.to_sym] = users_selection
+            
+            p users_selection
         end
     end
+
+
+   # def filter_dinos(filter)
+    #    p filter[0]
+    
+
+
 
     def filter_dinos(filter_params)
         matching_dinos = Array.new
@@ -38,12 +63,11 @@ class DinoDex
     def get_all_values(key)
         values = Array.new
         @dino_array.each do |i|
-        values << i.send(key)
-    end
+            values << i.send(key)
+        end
     values.uniq
     end 
 end
-
 
 
 

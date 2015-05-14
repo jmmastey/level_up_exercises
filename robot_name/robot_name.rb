b54d518f92ec6ab9ca0ffe52b14480f1b72a3b8d
dietpop <mwaldorf@clm-mwaldorf.local>
2015-05-14 10:36:31 -0500

    robot name fixed

--- robot_name/robot_name.rb
+++ robot_name/robot_name.rb
@@ -1,32 +1,49 @@
 class NameCollisionError < RuntimeError; end
 
-class Robot
-  attr_accessor :name
-
-  @@registry
-
-  def initialize(args = {})
-    @@registry ||= []
-    @name_generator = args[:name_generator]
-
-    if @name_generator
-      @name = @name_generator.call
-    else
-      generate_char = -> { ('A'..'Z').to_a.sample }
-      generate_num = -> { rand(10) }
-
-      @name = "#{generate_char.call}#{generate_char.call}#{generate_num.call}#{generate_num.call}#{generate_num.call}"
-    end
-
-    raise NameCollisionError, 'There was a problem generating the robot name!' if !(name =~ /[[:alpha:]]{2}[[:digit:]]{3}/) || @@registry.include?(name)
-    @@registry << @name
+class RobotNames
+  attr_accessor :names
+  def initialize
+    @names = []
   end
 end
 
-robot = Robot.new
+class Robot
+  attr_accessor :name
+  NAME_ERROR = "There was a problem generating the robot name!"
+  REG_EX = '[[:alpha:]]{2}[[:digit:]]{3}'
+
+  def initialize(args = {})
+    @registry ||= args[:used_names]
+    @name_generator = args[:name_generator]
+    assign_name
+    bad_name = !(@name =~ /#{REG_EX}/) || @registry.include?(@name)
+    raise NameCollisionError, NAME_ERROR if bad_name
+    @registry << @name
+  end
+
+  def assign_name
+    if @name_generator
+      @name = @name_generator.call
+    else
+      generate_name
+    end
+  end
+
+  def generate_name
+    generate_char = -> { ('A'..'Z').to_a.sample }
+    generate_num = -> { rand(10) }
+    @name = "#{generate_char.call}#{generate_char.call}"
+    @name += "#{generate_num.call}#{generate_num.call}#{generate_num.call}"
+  end
+end
+
+used_names = RobotNames.new
+robot = Robot.new(used_names: used_names.names)
+used_names.names << robot
 puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
 
 # Errors!
-# generator = -> { 'AA111' }
-# Robot.new(name_generator: generator)
-# Robot.new(name_generator: generator)
+generator = -> { 'AA111' }
+robot = Robot.new(name_generator: generator, used_names: used_names.names)
+puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
+robot = Robot.new(name_generator: generator, used_names: used_names.names)

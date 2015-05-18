32373c8d1efafd242ca060a6abb796bc6c856802
Dustin Hansen <dustin@dustinhansen.net>
2015-05-18 15:04:36 -0500

    Overlord
    Issues:
     * Had issues getting hidden elements to work well in Capybara
     * Could probably DRY the test suite a bit more but not sure what’s the
    correct/best procedures for doing so.

---
+++ overlord/public/assets/app/js/main.js
@@ -0,0 +1,26 @@
+$(function()
+{
+  $('form[name="security"]').submit(function(e)
+  {
+    e.preventDefault();
+    $.post('/security', $(this).serialize(), function(data)
+    {
+      response = $.parseJSON(data);
+
+      // status
+      $('.bomb-status').html(response.bomb_status)
+
+      // display failed attempts to deactivate
+      error_text = '';
+      if (typeof response.error_count !== 'undefined' && response.error_count > 0) {
+        error_text = 'Failed attempts: ' + response.error_count;
+      }
+      $('.bomb-errors').html(error_text);
+
+      // destroy bomb
+      if (response.bomb_status == 'exploded') {
+        $('form').remove();
+      }
+    });
+  });
+});

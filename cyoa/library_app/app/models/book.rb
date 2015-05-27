25ad01df0ca29e8415008087141042a498d725ac
Christine Schwerdtfeger <christine.schwerdtfeger@gmail.com>
2015-05-27 12:57:29 -0500

    Added some tests to the Book model and also added gem 'devise' to the Gemfile.  This will be used for creating user accounts.

--- cyoa/library_app/app/models/book.rb
+++ cyoa/library_app/app/models/book.rb
@@ -1,2 +1,3 @@
 class Book < ActiveRecord::Base
+  validates(:title, presence: true)
 end
--- cyoa/library_app/test/models/book_test.rb
+++ cyoa/library_app/test/models/book_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup 
+    @book = Book.new(author: "Erik Larson", title: "Isaac's Storm")
+  end
+
+  test "should be valid" do
+    assert @book.valid?
+  end
+
+  test "title should be present" do
+    @book.title = "   "
+    assert_not @book.valid?
+  end
 end

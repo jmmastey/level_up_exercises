402caf548dc13b0157d758cb328bfcd542e784e7
Ken Hebel <khebel@enova.com>
2015-05-27 11:17:12 -0500

    robot_name now skipping robots that already exist. rubocop complaining refactoring to use smaller methods

--- robot_name/robot_name.rb
+++ robot_name/robot_name.rb
@@ -1,32 +1,41 @@
-class NameCollisionError < RuntimeError; end
-
 class Robot
-  attr_accessor :name
-
-  @@registry
+  attr_reader :name
 
   def initialize(args = {})
     @@registry ||= []
-    @name_generator = args[:name_generator]
-
-    if @name_generator
-      @name = @name_generator.call
-    else
-      generate_char = -> { ('A'..'Z').to_a.sample }
-      generate_num = -> { rand(10) }
-
-      @name = "#{generate_char.call}#{generate_char.call}#{generate_num.call}#{generate_num.call}#{generate_num.call}"
-    end
-
-    raise NameCollisionError, 'There was a problem generating the robot name!' if !(name =~ /[[:alpha:]]{2}[[:digit:]]{3}/) || @@registry.include?(name)
+    self.name = args[:name_generator] ? args[:name_generator] : generate_name
     @@registry << @name
+  end
+
+  def name=(value)
+    @name = value if check_duplicate?(value)
+  end
+
+  def self.registry
+    @@registry
+  end
+
+  def check_duplicate?(str)
+    if !@@registry.include?(str)
+      true
+    else
+      puts "A robot named '#{str}' already exists. Skipping robot."
+      return false
+    end
+  end
+
+  def generate_name
+    generate_char = -> { ('A'..'Z').to_a.sample }
+    generate_num = -> { rand(10) }
+
+    2.times.collect { generate_char.call }.join +
+      3.times.collect { generate_num.call }.join
   end
 end
 
-robot = Robot.new
-puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
+50.times do
+  robot = Robot.new
+  puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
+end
 
-# Errors!
-# generator = -> { 'AA111' }
-# Robot.new(name_generator: generator)
-# Robot.new(name_generator: generator)
+puts Robot.registry

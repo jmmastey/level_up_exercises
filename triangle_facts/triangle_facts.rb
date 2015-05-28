267bc1c28fd5fd9aab24bf34fcca4fa499e82a20
Chi-chi Wang <chichi@wangchiyi.com>
2015-05-28 11:37:18 -0500

    [triangle_facts] Clean up and simplify calculate_angles method

--- triangle_facts/triangle_facts.rb
+++ triangle_facts/triangle_facts.rb
@@ -14,6 +14,15 @@ class Triangle
     [side1, side2, side3].uniq.length == 2
   end
 
+  def scalene?
+    equilateral? || scalene? ? false : true
+  end
+
+  def recite_facts
+    puts type_message
+    puts angle_message
+  end
+
   def type_message
     if equilateral?
       'This triangle is equilateral!'
@@ -32,24 +41,22 @@ class Triangle
     msg + "\n\n"
   end
 
-  def recite_facts
-    puts type_message
-    puts angle_message
+  def calculate_angles(a, b, c)
+    angle_a = calculate_angle(a, b, c)
+    angle_b = calculate_angle(b, c, a)
+    angle_c = calculate_angle(c, a, b)
+
+    [angle_a, angle_b, angle_c]
   end
 
-  def calculate_angles(a,b,c)
-    angleA = radians_to_degrees(Math.acos((b**2 + c**2 - a**2) / (2.0 * b * c)))
-    angleB = radians_to_degrees(Math.acos((a**2 + c**2 - b**2) / (2.0 * a * c)))
-    angleC = radians_to_degrees(Math.acos((a**2 + b**2 - c**2) / (2.0 * a * b)))
-
-    return [angleA, angleB, angleC]
+  def calculate_angle(m, n, o)
+    radians_to_degrees(Math.acos((n**2 + o**2 - m**2) / (2.0 * n * o)))
   end
 
   def radians_to_degrees(rads)
-    return (rads * 180 / Math::PI).round
+    (rads * 180 / Math::PI).round
   end
 end
-
 
 triangles = [[5, 5, 5], [5, 12, 13]]
 

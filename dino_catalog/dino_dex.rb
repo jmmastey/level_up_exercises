851677f512e2acfdb66eb19d7ef300ac92d0bf07
sfoa6558 <fakinyemi@clm-fakinyemi.local>
2015-05-30 19:32:55 -0500

    dino_menu.rb

---
+++ dino_catalog/dino_dex.rb
@@ -0,0 +1,86 @@
+require 'csv'
+require_relative 'dinosaur.rb'
+
+# DinoDex is the main class
+class DinoDex
+  SIZE_VALUE = 4000
+  NAME_CONVERTER = lambda do |field|
+    field = 'NAME' if field == 'Genus'
+    field
+  end
+
+  DIET_CONVERTER = lambda do |field|
+    field = 'DIET' if field == 'Carnivore'
+    field
+  end
+
+  WEIGHT_CONVERTER = lambda do |field|
+    field = 'WEIGHT_IN_LBS' if field == 'Weight'
+    field
+  end
+
+  CONVERTERS = [DIET_CONVERTER, WEIGHT_CONVERTER, NAME_CONVERTER, :downcase]
+  def initialize(name_csv)
+    @dinosaurs = []
+    CSV.foreach(name_csv,
+                headers: true,
+                header_converters: CONVERTERS)do |row|
+      a = Dinosaur.new(row.to_hash)
+      @dinosaurs << a
+    end
+  end
+
+  def find_biped
+    result = @dinosaurs.select(&:biped?)
+    puts result.to_s
+    Dinosaur.new(result.to_s)
+  end
+
+  def find_late_cretaceous
+    result = @dinosaurs.select(&:late_cretaceous?)
+    puts result.to_s
+    Dinosaur.new(result.to_s)
+  end
+
+  def find_early_cretaceous
+    result = @dinosaurs.select(&:early_cretaceous?)
+    puts result.to_s
+    Dinosaur.new(result.to_s)
+  end
+
+  def find_jurassic
+    result = @dinosaurs.select(&:jurassic?)
+    puts result.to_s
+    Dinosaur.new(result.to_s)
+  end
+
+  def find_abrictosaurus
+    result = @dinosaurs.select(&:abrictosaurus?)
+    puts result.to_s
+    Dinosaur.new(result.to_s)
+  end
+
+  def find_albertosaurus
+    result = @dinosaurs.select(&:albertosaurus?)
+    puts result.to_s
+    Dinosaur.new(result.to_s)
+  end
+
+  def find_carnivore
+    result = @dinosaurs.select(&:carnivore?)
+    puts result.to_s
+    Dinosaur.new(result.to_s)
+  end
+
+  def find_big
+    result = @dinosaurs.select(&:big?)
+    puts result.to_s
+    Dinosaur.new(result.to_s)
+  end
+
+  def find_small
+    result = @dinosaurs.select(&:small?)
+    puts result.to_s
+    Dinosaur.new(result.to_s)
+  end
+end

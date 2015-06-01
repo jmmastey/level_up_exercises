2ad87468bd07025128eafcf78c49fe166b9051e3
Ken Hebel <khebel@enova.com>
2015-06-01 11:07:05 -0500

    arrowhead now using one line predicate methods, and constants for puts

--- arrowhead/arrowhead.rb
+++ arrowhead/arrowhead.rb
@@ -1,4 +1,11 @@
 class Arrowhead
+  INVALID_REGION_MSG = "Unknown region, please provide a valid region."
+  INVALID_SHAPE_MSG = "Unknown shape value. Are you sure you know what you're"\
+                      " talking about?"
+  SUCCESS_SHAPE = lambda do |var|
+    "You have a(n) '#{var}' arrowhead. Probably priceless."
+  end
+
   CLASSIFICATIONS = {
     far_west: {
       notched: "Archaic Side Notch",
@@ -15,29 +22,18 @@ class Arrowhead
   }
 
   def self.known_region?(region)
-    if CLASSIFICATIONS.include? region
-      true
-    else
-      puts "Unknown region, please provide a valid region."
-      false
-    end
+    CLASSIFICATIONS.include? region
   end
 
   def self.known_shape?(region, shape)
-    if CLASSIFICATIONS[region].include? shape
-      true
-    else
-      puts "Unknown shape value. Are you sure you know what you're"\
-         " talking about?"
-      false
-    end
+    CLASSIFICATIONS[region].include? shape
   end
 
   def self.classify(region, shape)
-    if known_region?(region) && known_shape?(region, shape)
-      arrowhead = CLASSIFICATIONS[region][shape]
-      puts "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
-    end
+    raise INVALID_REGION_MSG unless known_region?(region)
+    raise INVALID_SHAPE_MSG unless known_shape?(region, shape)
+    arrowhead = CLASSIFICATIONS[region][shape]
+    puts SUCCESS_SHAPE.call arrowhead
   end
 end
 

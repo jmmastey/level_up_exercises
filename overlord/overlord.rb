9aaa8bd4776fb7e6096c05bd70afa732ac4976d8
Pranav <pranav.moktali@gmail.com>
2015-06-03 10:56:46 -0500

    syncing overlord

--- overlord/overlord.rb
+++ overlord/overlord.rb
@@ -1,14 +1,14 @@
--# run `ruby overlord.rb` to run a webserver for this app
--
--require 'sinatra'
--
--enable :sessions
--
--get '/' do
--  "Time to build an app around here. Start time: " + start_time
--end
--
--# we can shove stuff into the session cookie YAY!
--def start_time
--  session[:start_time] ||= (Time.now).to_s
--end
+# run `ruby overlord.rb` to run a webserver for this app
+
+require 'sinatra'
+
+enable :sessions
+
+get '/' do
+  "Time to build an app around here. Start time: " + start_time
+end
+
+# we can shove stuff into the session cookie YAY!
+def start_time
+  session[:start_time] ||= (Time.now).to_s
+end

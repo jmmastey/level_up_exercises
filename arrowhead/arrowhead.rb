93eba39456fa444d1cd89b09949e3ed39c6483a0
Nathan Tornquist <Tornquist@live.com>
2015-06-06 11:25:31 -0500

    Close arrowhead solution.  I'm satisfied with the direction, but am not completely satisfied with the final result

--- arrowhead/arrowhead.rb
+++ arrowhead/arrowhead.rb
@@ -15,19 +15,42 @@ class Arrowhead
     },
   }
 
+  INVALID_REGION = 0
+  INVALID_SHAPE = 1
+  VALID_REGION_SHAPE = 2
+
   # FIXME: I don't have time to deal with this.
   def self.classify(region, shape)
-    if CLASSIFICATIONS.include? region
-      shapes = CLASSIFICATIONS[region]
-      if shapes.include? shape
-        arrowhead = shapes[shape]
-        "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
-      else
-        raise "Unknown shape value. Are you sure you know what you're talking about?"
-      end
-    else
-      raise "Unknown region, please provide a valid region."
+    state = check_status(region, shape)
+    case state
+    when VALID_REGION_SHAPE
+      arrowhead = CLASSIFICATIONS[region][shape]
+      puts "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
+    when INVALID_SHAPE
+      puts "Unknown shape value. Are you sure you know what you're talking about?"
+    when INVALID_REGION
+      puts "Unknown region, please provide a valid region."
     end
+  end
+
+  def check_status(region, shape)
+    if valid_region?(region) && valid_shape?(region, shape)
+      VALID_REGION_SHAPE
+    elsif valid_region?(region)
+      INVALID_SHAPE
+    else
+      INVALID_REGION
+    end
+  end
+
+  def valid_region?(region)
+    CLASSIFICATIONS.include? region
+  end
+
+  def valid_shape?(region, shape)
+    CLASSIFICATIONS[region].include? shape
+  rescue
+    false
   end
 end
 

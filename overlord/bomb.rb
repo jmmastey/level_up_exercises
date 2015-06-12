cc403a769ea161ac7756f0f105fe7278d148a1f2
Ken Hebel <khebel@enova.com>
2015-06-12 13:36:47 -0500

    initial rspec test written, please I'm pretty sure they suck. but I need to write code now before I explode

--- overlord/bomb.rb
+++ overlord/bomb.rb
@@ -1,12 +1,29 @@
 class Bomb
   attr_accessor :status
+  attr_reader :activation_code, :deactivation_code
 
-  def initialize
+  def initialize()
     @status = default_status
+  end
+
+  def boot(args={})
+    @activaiton_code = args.fetch(:activaiton_code, default_activation_code)
+    @deactivaiton_code = args.fetch(:deactivaiton_code, default_deactivation_code)
+  end
+
+  def apply_code(code)
   end
 
   def default_status
     "Inactive"
+  end
+
+  def default_activation_code
+    1234
+  end
+
+  def default_deactivation_code
+    "0000"
   end
 
 end

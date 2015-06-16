4d06e7a5704403aec0949d10aa686d359ba3a6a1
Ken Hebel <khebel@enova.com>
2015-06-16 17:08:54 -0500

    added ability to customize group, and result names

--- data_science/lib/cohort_importer.rb
+++ data_science/lib/cohort_importer.rb
@@ -6,28 +6,29 @@ class InvalidFile < IOError; end
 class CohortImporter
   attr_reader :cohorts, :groups
 
-  GROUP = 'cohort'
-  RESULT = 'result'
-
-  def initialize(filename)
+  def initialize(filename, group: 'cohort', result: 'result')
     raise InvalidFile, "JSON file must be passed to initialize" unless filename
+    @group = group
+    @result = result
     file = File.read(filename)
     @data = JSON.parse(file)
     @groups = transform_conversions(setup_groups)
     @cohorts = make_cohorts(@groups)
   end
 
+  private
+
   def setup_groups(groups = {})
     @data.each do |record|
-      groups[record[GROUP]] = { conversions: 0, non_conversions: 0 }
+      groups[record[@group]] = { conversions: 0, non_conversions: 0 }
     end
     groups
   end
 
   def transform_conversions(groups)
     @data.each do |record|
-      groups[record[GROUP]][:conversions] += 1 if record[RESULT] == 1
-      groups[record[GROUP]][:non_conversions] += 1 if record[RESULT] == 0
+      groups[record[@group]][:conversions] += 1 if record[@result] == 1
+      groups[record[@group]][:non_conversions] += 1 if record[@result] == 0
     end
     groups
   end
--- data_science/spec/ab_tester_spec.rb
+++ data_science/spec/ab_tester_spec.rb
@@ -2,12 +2,11 @@ require 'spec_helper'
 
 describe ABTester do
   let(:group_a) { { "A" => { conversions: 340, non_conversions: 320 } } }
-  let(:cohort_a) { Cohort.new(group_a) }
   let(:group_b) { { "B" => { conversions: 380, non_conversions: 348 } } }
-  let(:cohort_b) { Cohort.new(group_b) }
   let(:group_c) { { "C" => { conversions: 1000, non_conversions: 6 } } }
+  let(:cohort_a) { Cohort.new(group_a) }
+  let(:cohort_b) { Cohort.new(group_b) }
   let(:cohort_c) { Cohort.new(group_c) }
-
   let(:abtest_a) { ABTester.new([cohort_a, cohort_b]) }
   let(:abtest_b) { ABTester.new([cohort_a, cohort_b, cohort_c]) }
 

7088e55921e3288e84909d3212a336ddd57a7f81
Nathan Tornquist <Tornquist@live.com>
2015-06-20 14:14:31 -0500

    Basic printing of dino details

--- dino_catalog/dinosaur.rb
+++ dino_catalog/dinosaur.rb
@@ -11,4 +11,43 @@ class Dinosaur
     @carnivore = dinosaur[:carnivore]
     @period = dinosaur[:period]
   end
+
+  def to_s
+    result = "Dino Details:"
+    if !genus.nil?
+      result += "\nGenus: " + genus
+    end
+
+    if !period.empty?
+      result += "\nPeriod: "
+      period_string = period.map{|p| [p[:modifier].to_s, p[:period]].join(" ")}.join(" or ")
+      result += period_string
+    end
+
+    if !description.nil?
+      result += "\nDescription: " + description
+    end
+
+    if !weight.nil?
+      result += "\nWeight: " + weight
+    end
+
+    if !walking.nil?
+      result += "\nWalking: " + walking
+    end
+
+    if !continent.nil?
+      result += "\nContinent: " + continent
+    end
+
+    if !carnivore.nil?
+      result += "\nCarnivore: " + carnivore
+    end
+
+    if !diet_details.nil?
+      result += "\nDiet Details: " + diet_details
+    end
+
+    result
+  end
 end

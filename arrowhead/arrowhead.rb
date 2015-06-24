325818d943f169ab350b96f79a00d670af6627ec
Yuval Roth <yuvalr@gmail.com>
2015-06-24 11:09:05 -0500

    * update code to use guard where possible * minor additional code cleanup

--- arrowhead/arrowhead.rb
+++ arrowhead/arrowhead.rb
@@ -2,36 +2,34 @@ class Arrowhead
   # This seriously belongs in a database.
   CLASSIFICATIONS = {
     far_west: {
-      notched: "Archaic Side Notch",
-      stemmed: "Archaic Stemmed",
-      lanceolate: "Agate Basin",
-      bifurcated: "Cody",
+      notched: 'Archaic Side Notch',
+      stemmed: 'Archaic Stemmed',
+      lanceolate: 'Agate Basin',
+      bifurcated: 'Cody',
     },
     northern_plains: {
-      notched: "Besant",
-      stemmed: "Archaic Stemmed",
-      lanceolate: "Humboldt Constricted Base",
-      bifurcated: "Oxbow",
+      notched: 'Besant',
+      stemmed: 'Archaic Stemmed',
+      lanceolate: 'Humboldt Constricted Base',
+      bifurcated: 'Oxbow',
     },
   }
 
   def self.classify(region, shape)
-    if CLASSIFICATIONS.include? region
-      classify_shape_for_specfic_region region, shape
-    else
-      raise "Unknown region, please provide a valid region."
+    unless CLASSIFICATIONS.include? region
+      raise 'Unknown region, please provide a valid region.'
     end
+    classify_shape_for_specific_region region, shape
   end
 
   private_class_method :new
-  def self.classify_shape_for_specfic_region(region, shape)
+  def self.classify_shape_for_specific_region(region, shape)
     shapes = CLASSIFICATIONS[region]
-    if shapes.include? shape
-      "You have a(n) '#{shapes[shape]}' arrowhead. Probably priceless."
-    else
-      raise "Unknown shape value."\
-            " Are you sure you know what you're talking about?"
+    unless shapes.include? shape
+      raise 'Unknown shape value.' \
+      " Are you sure you know what you're talking about?"
     end
+    "You have a(n) '#{shapes[shape]}' arrowhead. Probably priceless."
   end
 end
 

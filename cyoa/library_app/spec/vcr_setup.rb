c8a2c72f58dda1bd758c5748e37a7af9b74f9940
Christine Schwerdtfeger <christine.schwerdtfeger@gmail.com>
2015-06-25 14:44:06 -0500

    Fixed some rubocop complaints in rspec tests.

--- cyoa/library_app/spec/vcr_setup.rb
+++ cyoa/library_app/spec/vcr_setup.rb
@@ -4,5 +4,5 @@ require 'webmock'
 VCR.configure do |c|
   c.cassette_library_dir = "vcr_cassettes"
   c.hook_into :webmock
-  c.default_cassette_options = {:record => :once}
+  c.default_cassette_options = { :record => :once }
 end

a0733e66ddd7ce085b3ccc03e0fffcd124b6e0ac
Jeff Nyman <jnyman@enova.com>
2015-06-25 07:36:30 -0500

    Improve performance slightly by optimizing assignment.

--- triangle_facts/triangle_facts.rb
+++ triangle_facts/triangle_facts.rb
@@ -3,7 +3,9 @@ class Triangle
   attr_accessor :side1, :side2, :side3
 
   def initialize(side1, side2, side3)
-    @side1, @side2, @side3 = side1, side2, side3
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
   end
 
   def equilateral()

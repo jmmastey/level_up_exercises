949a59840ae065fbf317a7db72d92489bc115725
Jeff Nyman <jnyman@enova.com>
2015-06-25 07:38:23 -0500

    Improve code clarity by removing redundancies.

--- triangle_facts/triangle_facts.rb
+++ triangle_facts/triangle_facts.rb
@@ -8,15 +8,15 @@ class Triangle
     @side3 = side3
   end
 
-  def equilateral()
-    return side1 == side2 && side2 == side3
+  def equilateral
+    side1 == side2 && side2 == side3
   end
 
-  def isosceles()
-    return [side1, side2, side3].uniq.length == 2
+  def isosceles
+    [side1, side2, side3].uniq.length == 2
   end
 
-  def scalene()
+  def scalene
     unless ! (equilateral || isosceles)
       false
     else
@@ -29,7 +29,7 @@ class Triangle
     puts 'This triangle is isosceles! Also, that word is hard to type.' if isosceles
     puts 'This triangle is scalene and mathematically boring.' if scalene
 
-    angles = self.calculate_angles(side1, side2, side3)
+    angles = calculate_angles(side1, side2, side3)
     puts 'The angles of this triangle are ' + angles.join(',')
 
     puts 'This triangle is also a right triangle!' if angles.include? 90
@@ -41,11 +41,11 @@ class Triangle
     angleB = radians_to_degrees(Math.acos((a**2 + c**2 - b**2) / (2.0 * a * c)))
     angleC = radians_to_degrees(Math.acos((a**2 + b**2 - c**2) / (2.0 * a * b)))
 
-    return [angleA, angleB, angleC]
+    [angleA, angleB, angleC]
   end
 
   def radians_to_degrees(rads)
-    return (rads * 180 / Math::PI).round
+    (rads * 180 / Math::PI).round
   end
 end
 

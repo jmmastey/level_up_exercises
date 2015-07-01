de006ca1b5098583b2f7e174a5ebe8046e19ddcb
dietpop <mwaldorf@enova.com>
2015-07-01 10:49:17 -0500

    added tables to postgres

--- cyoa/workspace/cyoa3/config/routes.rb
+++ cyoa/workspace/cyoa3/config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  resources :microposts
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'application#hello'
+   root 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

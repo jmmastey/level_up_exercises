401b3b192263de5d905d09beffb6b527e3ca3809
Luai Mahmud <lmahmud@enova.com>
2015-07-02 15:08:24 -0500

    Refactor after review

--- robot_name/robot_name.rb
+++ robot_name/robot_name.rb
@@ -1,5 +1,4 @@
-class NameCollisionError < RuntimeError; end
-class InvalidNameError < RuntimeError; end
+class RegistryError < StandardError; end
 
 class RobotRegistry
   attr_accessor :robot_name
@@ -9,11 +8,9 @@ class RobotRegistry
 
   def self.add_name_to_registry(robot_name)
     @robot_name = robot_name
-    raise NameCollisionError, DUP_NAME_ERR_MSG if @registry.include?(robot_name)
-    @registry << robot_name
-    puts "Added #{robot_name} to registry: #{@registry}"
-    rescue NameCollisionError => e
-      puts "Error: #{robot_name}.  #{e}"
+    raise RegistryError, DUP_NAME_ERR_MSG if @registry.include?(robot_name)
+      @registry << robot_name
+      puts "Added #{robot_name} to registry: #{@registry}"
   end
 end
 
@@ -21,20 +18,29 @@ class Robot
   attr_accessor :name
   INVALID_NAME_MSG = "The name is invalid"
 
-  def initialize(args = {})
-    @name = args[:name_generator] || generate_name
+  def initialize(args = nil)
+    args ? @name = args.call : @name = generate_name
+    puts "Robot name is #{name}"
     RobotRegistry.add_name_to_registry(name) if name_valid?
+  rescue RegistryError => e
+    puts "Error: #{name}. #{e}"
   end
 
   def generate_name
-    @name = ('A'..'Z').to_a.sample(2).join + rand(0..999).to_s.rjust(3, "0")
+    generate_name_letters << generate_name_numbers
+  end
+
+  def generate_name_letters
+    ('A'..'Z').to_a.sample(2).join
+  end
+
+  def generate_name_numbers
+    rand(0..999).to_s.rjust(3, "0")
   end
 
   def name_valid?
     return true if name =~ /[[:alpha:]]{2}[[:digit:]]{3}/
-    raise InvalidNameError, INVALID_NAME_MSG
-    rescue InvalidNameError => e
-      puts "Error: #{name}. #{e}"
+    puts "Error: #{name}. #{INVALID_NAME_MSG}"
   end
 end
 
@@ -42,15 +48,18 @@ robot = Robot.new
 puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
 robot = Robot.new
 puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
-# Errors!
 name_generator = -> { 'AA111' }
-Robot.new(name_generator: name_generator.call)
-puts "My pet robot's name is #{name_generator.call}, \
-but we usually call him sparky."
-Robot.new(name_generator: name_generator.call)
+robot = Robot.new(name_generator)
+puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
+robot = Robot.new(name_generator)
+puts "Will show error message above"
 robot = Robot.new
 puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
 name_generator = -> { 'ZZZ' }
-Robot.new(name_generator: name_generator.call)
+puts "Will show error message above"
+robot = Robot.new(name_generator)
 robot = Robot.new
 puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
+name_generator = -> { 'AA111' }
+robot = Robot.new(name_generator)
+puts "--> My pet robot's name is #{robot.name}, but we usually call him sparky."

5c82a688b96efc0cd8883c0ee2274c757475933e
Jeff Nyman <jnyman@enova.com>
2015-07-06 09:02:05 -0500

    Passing tests (written first, of course) for the data loader.

--- data_science/lib/data_loader.rb
+++ data_science/lib/data_loader.rb
@@ -1,7 +1,17 @@
+require 'json'
+
 class DataLoader
   attr_accessor :data_source
 
   def initialize(data_source)
     @data_source = data_source
+  end
+
+  def json?
+    data_source.end_with?('.json')
+  end
+
+  def load_data
+    JSON.parse(File.read(data_source)) if json?
   end
 end
--- data_science/spec/data_loader_spec.rb
+++ data_science/spec/data_loader_spec.rb
@@ -2,6 +2,11 @@ require 'data_loader'
 
 RSpec.describe DataLoader do
   it 'will load a JSON file' do
-    expect(DataLoader.new('test_data.json').data_source).to end_with('.json')
+    expect(DataLoader.new('test_data.json').json?).to be_truthy
+  end
+
+  it 'will load data from the provided data source' do
+    expect { DataLoader.new('data/test_data.json').load_data }.not_to raise_error
+    expect(DataLoader.new('data/test_data.json').load_data).not_to be_empty
   end
 end

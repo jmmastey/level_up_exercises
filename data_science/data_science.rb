50c0b7a16a6defa1977478b7e592e85b691ce3a4
Joseph Klemen <jklemen@enova.com>
2015-07-06 17:02:13 -0500

    added test file for data_science

--- data_science/data_science.rb
+++ data_science/data_science.rb
@@ -1,19 +1,15 @@
 require 'json'
 
 class DataScience
-  attr_accessor :json, :cohorts, :sample
+  attr_accessor :cohorts, :sample_size
 
   def initialize(json_file)
     json_data = open(json_file).read
-    @json = JSON.parse(json_data).to_a
-    @sample = @json.count
+    json = JSON.parse(json_data).to_a
+    @sample_size = json.count
 
     parts = json.partition { |item| item['cohort'] == 'A' }
     @cohorts = Hash[*%w(A B).zip(parts).flatten(1)]
-  end
-
-  def total_sample_size
-    puts "Sample size: #{@sample}"
   end
 
   def cohort_conversions(cohort_name)
@@ -22,26 +18,26 @@ class DataScience
   end
 
   def cohort_stats(cohort_name)
-    cconv = cohort_conversions(cohort_name)
-    csample = @cohorts[cohort_name].count
+    conv = cohort_conversions(cohort_name)
+    sample = @cohorts[cohort_name].count
 
-    [cconv, csample]
+    [conv, sample]
   end
 
   # 1. Total sample size and number of conversions for each cohort.
   def report_sample_sizes
-    @cohorts.keys.each do |cname|
-      cstats = cohort_stats(cname)
+    @cohorts.keys.each do |name|
+      stats = cohort_stats(name)
 
-      puts "Cohort #{cname}: #{cstats[0]} / #{cstats[1]}"
+      puts "Cohort #{name}: #{stats[0]} / #{stats[1]}"
     end
   end
 
   def calc_mean_error(cohort_name)
-    cstats = cohort_stats(cohort_name)
+    stats = cohort_stats(cohort_name)
 
-    p = (cstats[0].to_f / cstats[1].to_f).round(5)
-    se = (2 * Math.sqrt(p * (1 - p) / @sample.to_f)).round(5)
+    p = (stats[0].to_f / stats[1].to_f)
+    se = (2 * Math.sqrt(p * (1 - p) / @sample_size.to_f))
 
     [p, se]
   end
@@ -49,40 +45,37 @@ class DataScience
   # 2. Conversion rate (w/ error bars) for each cohort with a 95% confidence.
   # Square root of (p * (1-p) / n)
   def report_mean_error
-    @cohorts.keys.each do |cname|
-      p, se = calc_mean_error(cname)
+    @cohorts.keys.each do |name|
+      p, se = calc_mean_error(name)
 
-      puts "Conversion Rate #{cname} = #{p}"
-      puts "Standard Error #{cname}  = #{se}"
+      puts "Conversion Rate #{name} = #{p}"
+      puts "Standard Error #{name}  = #{se}"
     end
   end
 
   def calc_confidence(cohort_name)
     p, se = calc_mean_error(cohort_name)
 
-    low = (p - se).round(3)
-    high = (p + se).round(3)
-
-    [low, p, high]
+    [p - se, p, p + se].map{ |num| num.round(3) }
   end
 
   # 3. Confidence level that the current leader is in fact better than random.
   # (done in confidence)
   def report_confidence
-    @cohorts.keys.each do |cname|
-      low, p, high = calc_confidence(cname)
+    @cohorts.keys.each do |name|
+      low, p, high = calc_confidence(name)
 
-      puts "95% Confidence Interval for Cohort #{cname}: "
+      puts "95% Confidence Interval for Cohort #{name}: "
       puts "|(#{low})--- (#{p}) ---(#{high})|"
     end
   end
 
   def report_all
-    total_sample_size
+    puts "Sample size: #{@sample_size}"
     report_sample_sizes
     report_mean_error
     report_confidence
   end
 end
 
-DataScience.new('data_export_2014_06_20_15_59_02.json').report_all
+#DataScience.new('data_export_2014_06_20_15_59_02.json').report_all

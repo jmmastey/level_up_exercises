b9bf93ff3d18b9666140ff44ad63907cbe79bb8c
Jeff Nyman <jnyman@enova.com>
2015-07-06 09:04:28 -0500

    Using a subject to provide better test readability.

--- data_science/spec/data_loader_spec.rb
+++ data_science/spec/data_loader_spec.rb
@@ -1,12 +1,14 @@
 require 'data_loader'
 
 RSpec.describe DataLoader do
+  subject(:data_source) { DataLoader.new('data/test_data.json') }
+
   it 'will load a JSON file' do
-    expect(DataLoader.new('test_data.json').json?).to be_truthy
+    expect(subject.json?).to be_truthy
   end
 
   it 'will load data from the provided data source' do
-    expect { DataLoader.new('data/test_data.json').load_data }.not_to raise_error
-    expect(DataLoader.new('data/test_data.json').load_data).not_to be_empty
+    expect { subject.load_data }.not_to raise_error
+    expect(subject.load_data).not_to be_empty
   end
 end

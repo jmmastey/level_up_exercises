3adca8934c92c0076675860c8b1cef125a28b915
Rachel Caileff <rcaileff@enova.com>
2015-07-09 19:24:10 -0500

    Revert changes from robot_name exercise.

--- robot_name/robot_name.rb
+++ robot_name/robot_name.rb
@@ -1,50 +1,32 @@
-class RobotNameError < RuntimeError; end
+class NameCollisionError < RuntimeError; end
 
 class Robot
-  class << self
-    attr_accessor :registry
-  end
-  @registry = []
-
   attr_accessor :name
 
+  @@registry
+
   def initialize(args = {})
-    self.class.registry |= []
-    @name_generator = args[:name_generator] || method(:create_name)
-    assign_next_valid_name
-    self.class.registry << @name
-  end
+    @@registry ||= []
+    @name_generator = args[:name_generator]
 
-  def create_name
-    generate_char = -> { ('A'..'Z').to_a.sample }
-    generate_digit = -> { rand(10).to_s }
+    if @name_generator
+      @name = @name_generator.call
+    else
+      generate_char = -> { ('A'..'Z').to_a.sample }
+      generate_num = -> { rand(10) }
 
-    name = ""
-    2.times { name << generate_char.call }
-    3.times { name << generate_digit.call }
-    name
-  end
+      @name = "#{generate_char.call}#{generate_char.call}#{generate_num.call}#{generate_num.call}#{generate_num.call}"
+    end
 
-  def assign_next_valid_name
-    @name = @name_generator.call
-    @name = @name_generator.call if invalid_name?
-    raise(RobotNameError, "Name already exists: #{@name}.") if duplicate_name?
-    raise(RobotNameError, "Name has bad format: #{@name}.") if bad_format_name?
-    name
-  end
-
-  def invalid_name?
-    bad_format_name? || duplicate_name?
-  end
-
-  def bad_format_name?
-    !(@name =~ /[[:alpha:]]{2}[[:digit:]]{3}/)
-  end
-
-  def duplicate_name?
-    self.class.registry.include?(@name)
+    raise NameCollisionError, 'There was a problem generating the robot name!' if !(name =~ /[[:alpha:]]{2}[[:digit:]]{3}/) || @@registry.include?(name)
+    @@registry << @name
   end
 end
 
 robot = Robot.new
-puts("My pet robot's name is #{robot.name}, but we usually call him sparky.")
+puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
+
+# Errors!
+# generator = -> { 'AA111' }
+# Robot.new(name_generator: generator)
+# Robot.new(name_generator: generator)

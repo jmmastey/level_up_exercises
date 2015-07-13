35afd4af6504548ad81e7a5b5687ba5bb63924b8
Joseph Klemen <jklemen@enova.com>
2015-07-13 12:01:11 -0500

    significant data_science refactor. new cohort class. expect() syntax in rspec tests.

--- data_science/cohort.rb
+++ data_science/cohort.rb
@@ -1,6 +1,8 @@
+require 'abanalyzer'
+
 class Cohort
-  attr_accessor :conversions, :sample_size, :name
-  attr_accessor :ci_low, :ci_mid, :ci_high
+  attr_accessor :conversions, :conversion_rate, :standard_error, :sample_size
+  attr_accessor :name, :ci_low, :ci_high
 
   def initialize(samples)
     @name = samples[0]['cohort']
@@ -8,13 +10,27 @@ class Cohort
     @conversions = calc_conversions(samples)
 
     @conversion_rate, @standard_error = calc_mean_error(samples)
+    @ci_low, @ci_high = calc_confidence_interval
+  end
 
-    # confidence interval
-    @ci_low, @ci_mid, @ci_high = calc_confidence
+  def conversion_hash
+    { 
+      :converted => @conversions, 
+      :not_converted => @sample_size - @conversions 
+    }
+  end
+
+  def report_conversions
+    puts "Conversions:\t\t#{@conversions}"
+  end
+
+  def report_sample_size
+    puts "Sample Size:\t\t#{@sample_size}"
   end
 
   def report_conversion_rate
-    puts "Conversion Rate:\t#{@conversion_rate}"
+    rate = (@conversion_rate * 100).round(2)
+    puts "Conversion Rate:\t#{rate}%"
   end
 
   def report_sample_error
@@ -23,25 +39,31 @@ class Cohort
 
   def report_confidence
     puts "95% Confidence Interval for Cohort #{@name}: "
-    puts "|(#{@ci_low})--- (#{@ci_mid}) ---(#{@ci_high})|"
+    puts "[#{@ci_low}, #{@ci_high}]"
   end
 
-  def report_all
-    puts
-    puts "======== Cohort #{@name} ========"
+  def report_stats
+    report_conversions
+    report_sample_size
     report_conversion_rate
     report_sample_error
-    puts
     report_confidence
+  end
+
+  def formatted_report
+    puts
+    puts "======== Cohort #{@name} ========"
+    report_stats
     puts
   end
 
   private
 
-  def calc_confidence
+  def calc_confidence_interval
     low = @conversion_rate - 1.96 * @standard_error
     high = @conversion_rate + 1.96 * @standard_error
-    [low, @conversion_rate, high].map { |num| num.round(3) }
+
+    [low, high].map { |num| num.round(3) }
   end
 
   def calc_conversions(samples)
--- data_science/data_science.rb
+++ data_science/data_science.rb
@@ -1,9 +1,9 @@
 require 'json'
-
+require 'abanalyzer'
 require './cohort.rb'
 
 class DataScience
-  attr_accessor :cohorts, :sample_size
+  attr_accessor :cohorts, :sample_size, :winner, :test
 
   def initialize(json_file)
     json_data = open(json_file, &:read)
@@ -12,11 +12,57 @@ class DataScience
 
     raw_cohorts = json.partition { |item| item['cohort'].downcase == 'a' }
     @cohorts = raw_cohorts.map { |cohort_info| Cohort.new(cohort_info) }
+
+    run_chi_square_test
+  end
+
+  def report_chi_square_results
+    print "Better than random? #{@test.different?}"
+
+    p = @test.chisquare_p.round(3)
+    puts " (#{p} #{p < 0.05 ? '<=' : '>='} 0.05)"
+  end
+
+  def report_winner
+    if @winner.nil?
+      puts "The chi square test results are not significant enough."
+    else
+      puts "#{@winner.name} is the winner!"
+    end
+  end
+
+  def report_cohorts
+    @cohorts.each(&:formatted_report)
   end
 
   def report_all
-    @cohorts.each(&:report_all)
+    report_cohorts
+    report_chi_square_results
+    report_winner
+  end
+
+  private
+
+  def chi_square_test
+    values = @cohorts.inject({}) do |vals, cohort|
+      vals[cohort.name] = cohort.conversion_hash
+      vals
+    end
+    ABAnalyzer::ABTest.new(values)
+  end
+
+  def select_winner(test)
+    return nil unless test.different?
+
+    @winner = @cohorts.max do |cohort|
+      cohort.conversion_rate
+    end
+  end
+
+  def run_chi_square_test
+    @test = chi_square_test
+    @winner = select_winner(test)
   end
 end
 
-DataScience.new('data_export_2014_06_20_15_59_02.json').report_all
+# DataScience.new('data_export_2014_06_20_15_59_02.json').report_all

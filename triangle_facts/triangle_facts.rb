99c62f3a7844f0960ab4eb3d45538fe74cac9504
Kyle McDaniel <mcdaniel221@gmail.com>
2015-07-16 13:13:53 -0500

    Refactored triangle_facts again.

--- triangle_facts/triangle_facts.rb
+++ triangle_facts/triangle_facts.rb
@@ -8,6 +8,15 @@ class Triangle
     @side3 = side3
   end
 
+  def recite_facts
+    puts 'This triangle is equilateral!' if equilateral
+    puts 'This triangle is isosceles!' \
+         ' Also, that word is hard to type.' if isosceles
+    puts 'This triangle is scalene and mathematically boring.' if scalene
+
+    puts_angle_info
+  end
+
   def equilateral
     side1 == side2 && side2 == side3
   end
@@ -24,15 +33,6 @@ class Triangle
     end
   end
 
-  def recite_facts
-    puts 'This triangle is equilateral!' if equilateral
-    puts 'This triangle is isosceles!' \
-         ' Also, that word is hard to type.' if isosceles
-    puts 'This triangle is scalene and mathematically boring.' if scalene
-
-    puts_angle_info
-  end
-
   def puts_angle_info
     angles = calculate_angles(side1, side2, side3)
     puts 'The angles of this triangle are ' + angles.join(',')
@@ -42,11 +42,15 @@ class Triangle
   end
 
   def calculate_angles(a, b, c)
-    ang_a = radians_to_degrees(Math.acos((b**2 + c**2 - a**2) / (2.0 * b * c)))
-    ang_b = radians_to_degrees(Math.acos((a**2 + c**2 - b**2) / (2.0 * a * c)))
-    ang_c = radians_to_degrees(Math.acos((a**2 + b**2 - c**2) / (2.0 * a * b)))
+    angle_a_rad = Math.acos((b**2 + c**2 - a**2) / (2.0 * b * c))
+    angle_b_rad = Math.acos((a**2 + c**2 - b**2) / (2.0 * a * c))
+    angle_c_rad = Math.acos((a**2 + b**2 - c**2) / (2.0 * a * b))
 
-    [ang_a, ang_b, ang_c]
+    angle_a = radians_to_degrees(angle_a_rad)
+    angle_b = radians_to_degrees(angle_b_rad)
+    angle_c = radians_to_degrees(angle_c_rad)
+
+    [angle_a, angle_b, angle_c]
   end
 
   def radians_to_degrees(rads)

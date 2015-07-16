1bd9052d1de1236e7a2b7016ba87b8e12e1a742a
Alex Bake <abake@abake-mbp.enova.com>
2015-07-16 13:17:05 -0500

    Fix rubocop offenses.

--- triangle_facts/triangle_facts.rb
+++ triangle_facts/triangle_facts.rb
@@ -19,34 +19,37 @@ class Triangle
     !(equilateral? || isosceles?)
   end
 
-  def recite_facts
-    type =
-      if equilateral?
-        'This triangle is equilateral!'        
-      elsif isosceles?
-        'This triangle is isosceles! Also, that word is hard to type.'
-      elsif scalene?
-         'This triangle is scalene and mathematically boring.'
-      end 
-    puts type
+  def right_triangle?
+    angles.include? 90
+  end
 
+  def triangle_type_message
+    if equilateral?
+      'This triangle is equilateral!'
+    elsif isosceles?
+      'This triangle is isosceles! Also, that word is hard to type.'
+    elsif scalene?
+      'This triangle is scalene and mathematically boring.'
+    end
+  end
+
+  def recite_facts
     angles = calculate_angles(side1, side2, side3)
     puts 'The angles of this triangle are ' + angles.join(',')
 
-    puts 'This triangle is also a right triangle!' if angles.include? 90
+    puts triangle_type_message
+    puts 'This triangle is also a right triangle!' if right_triangle?
     puts ''
   end
 
-  def calculate_angles(a, b, c)
-    rad_a = Math.acos((b ** 2 + c ** 2 - a ** 2) / (2.0 * b * c))
-    angle_a = radians_to_degrees(rad_a)
-    
-    rad_b = Math.acos((a ** 2 + c ** 2 - b ** 2) / (2.0 * a * c))
-    angle_b = angle_b = radians_to_degrees(rad_b)
-    
-    rad_c = Math.acos((a ** 2 + b ** 2 - c ** 2) / (2.0 * a * b))
-    angle_c = radians_to_degrees(rad_c)
+  def calculate_angle(a, b, c)
+    Math.acos((a**2 + b**2 - c**2) / (2.0 * a * b))
+  end
 
+  def calculate_angles(a, b, c)
+    angle_a = radians_to_degrees(calculate_angle(b, c, a))
+    angle_b = radians_to_degrees(calculate_angle(a, c, b))
+    angle_c = radians_to_degrees(calculate_angle(a, b, c))
     [angle_a, angle_b, angle_c]
   end
 
@@ -54,7 +57,6 @@ class Triangle
     (rads * 180 / Math::PI).round
   end
 end
-
 
 triangles = [
   [5, 5, 5],

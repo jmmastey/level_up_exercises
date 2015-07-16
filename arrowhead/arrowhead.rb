32cbb95318c776686888f6ca7159f12d01a0263d
Alex Bake <abake@abake-mbp.local>
2015-07-16 11:59:22 -0500

    Fix rubocop offenses.

--- arrowhead/arrowhead.rb
+++ arrowhead/arrowhead.rb
@@ -5,19 +5,19 @@ class Arrowhead
       notched: "Archaic Side Notch",
       stemmed: "Archaic Stemmed",
       lanceolate: "Agate Basin",
-      bifurcated: "Cody"
+      bifurcated: "Cody",
     },
     northern_plains: {
       notched: "Besant",
       stemmed: "Archaic Stemmed",
       lanceolate: "Humboldt Constricted Base",
-      bifurcated: "Oxbow"
-    }
+      bifurcated: "Oxbow",
+    },
   }
 
   def self.classify(region, shape)
     raise "Must provide valid region." unless CLASSIFICATIONS.include? region
-    
+
     shapes = CLASSIFICATIONS[region]
     raise "Must provide valid shape." unless shapes.include? shape
 

a1af821ee16d2af1347f277e53733ac67b1d98d6
Shaan Sapra <ssapra@uchicago.edu>
2015-07-15 20:46:09 -0500

    Addressing feedback

--- robot_name/robot.rb
+++ robot_name/robot.rb
@@ -1,43 +1,43 @@
 class DuplicateName < RuntimeError; end
 class InvalidName < RuntimeError; end
 
+class Registry
+  @robot_names = []
+
+  def self.check_for_duplicate(name)
+    if @robot_names.include?(name)
+      raise DuplicateName, 'There was a problem generating the robot name!'
+    end
+  end
+
+  def self.add_name(name)
+    check_for_duplicate(name)
+    @robot_names << name
+  end
+end
+
 class Robot
   attr_accessor :name
-
-  @@robot_names = []
 
   CHAR_GENERATOR = -> { ('A'..'Z').to_a.sample }
   NUM_GENERATOR = -> { rand(10) }
 
-  def self.robot_names
-    @@robot_names
+  def initialize(generator = nil)
+    @name = generator ? generator.call : generate_name
+    validate_name
+    Registry.add_name(@name)
   end
 
-  def valid?
-    if !(name =~ /[[:alpha:]]{2}[[:digit:]]{3}/)
-      raise InvalidName, 'Invalid robot name'
-    elsif @@robot_names.include?(name)
-      raise DuplicateName, 'There was a problem generating the robot name!'
-    end
-    true
+  def validate_name
+    is_invalid = !(name =~ /[[:alpha:]]{2}[[:digit:]]{3}/)
+    raise InvalidName, 'Invalid name' if is_invalid
   end
 
   def generate_name
     @name = ""
-    3.times { @name << CHAR_GENERATOR.call }
+    2.times { @name << CHAR_GENERATOR.call }
     3.times { @name << NUM_GENERATOR.call.to_s }
-  end
-
-  def initialize(args = {})
-    name_generator = args[:name_generator]
-
-    if name_generator
-      @name = name_generator.call
-    else
-      generate_name
-    end
-
-    @@robot_names << @name if valid?
+    @name
   end
 end
 
@@ -46,5 +46,8 @@ puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
 
 # Errors!
 # generator = -> { 'AA111' }
-# Robot.new(name_generator: generator)
-# Robot.new(name_generator: generator)
+# Robot.new(generator)
+# Robot.new(generator)
+
+# generator = -> { 'AA11' }
+# Robot.new(generator)

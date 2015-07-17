49e56e387ae3e0c47bde4d9f3dcff63b12008c27
Alex Bake <abake@abake-mbp.enova.com>
2015-07-17 11:51:53 -0500

    Start implementing search features

--- dino_catalog/dinosaur.rb
+++ dino_catalog/dinosaur.rb
@@ -1,15 +1,39 @@
 require_relative 'dino_parser'
 
 class Dinodex
+  SEARCH_NAME = lambda do |res, name|
+    res.select { |dino| dino.name_is(name) }
+  end
+
+  SEARCH_PERIOD = lambda do |res, period|
+    res.select { |dino| dino.from_period(period) }
+  end
+
+  SEARCH_DISPATCHER = {
+    "name" => SEARCH_NAME,
+    "period" => SEARCH_PERIOD,
+  }
+
   attr_accessor :registry
 
   def initialize
-    @registry = {}
+    @registry = []
+  end
+
+  def search(args, results=@registry)
+    return results unless args.length > 0
+    k, v = args.pop
+    results = SEARCH_DISPATCHER[k].call(results, v)
+    search(args, results)
+  end
+
+  def to_s(subset=@registry)
+    max_col_len = subset.
   end
 
   def load_data(data)
     data.each do |row|
-      @registry[row["name"]] = Dinosaur.new(row)
+      @registry << Dinosaur.new(row)
     end
   end
 
@@ -21,20 +45,44 @@ class Dinodex
 end
 
 class Dinosaur
-  attr_accessor :name, :period, :continent, :diet, :weight, :walking, :desc
+  attr_accessor :data
 
-  def initialize(args = {})
-    @name = args[:name]
-    @period = args[:period]
-    @continent = args[:continent]
-    @diet = args[:diet]
-    @weight = args[:weight]
-    @walking = args[:walking]
-    @desc = args[:desc]
+  def initialize(data = {})
+    @data = data
+  end
+
+  def has?(key)
+    @data.include?(key)
+  end
+
+  def name_is(name)
+    @data['name'] == name if has?('name')
+  end
+
+  def from_period(period)
+    @data['period'].include?(period) if has?('period')
+  end
+
+  def to_s
+    max_len = @data.keys.max_by { |k| k.length if @data[k].length > 0 }.length
+    @data.each do |k, v|
+      spaces = ' ' * (max_len - k.length) + ' '
+      puts k + ':' + spaces + v if v.length > 0
+    end
   end
 end
 
-dinodex = Dinodex.new
-ARGV.each do |arg|
-  dinodex.load_csv(arg)
+def main
+  dinodex = Dinodex.new
+  dinodex.load_csv('dinodex.csv')
+
+  args = [
+    %w(period cretaceous),
+  ]
+  dinodex.search(args)
 end
+
+# dinodex = Dinodex.new
+# ARGV.each do |arg|
+#   dinodex.load_csv(arg)
+# end

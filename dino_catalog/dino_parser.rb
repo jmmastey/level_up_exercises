e57671d5b1bd53f9ddf34f1718d11c89a3edb7d2
Alex Bake <abake@abake-mbp.enova.com>
2015-07-17 09:38:41 -0500

    Create DinoParser and DinoTranslator classes.

--- dino_catalog/dino_parser.rb
+++ dino_catalog/dino_parser.rb
@@ -1,16 +1,67 @@
-def split_strip_sanitize(line)
-  line.split(',').map { |d| d.strip.downcase }
+class DinoParser
+  TRANSLATE_MAP = {
+    "genus" => "name",
+    "carnivore" => "diet",
+    "weight_in_lbs" => "weight",
+  }
+  TRANSLATE = -> h { TRANSLATE_MAP.include?(h) ? TRANSLATE_MAP[h] : h }
+
+  def self.split_strip_downcase(line)
+    line.split(',').map { |d| d.strip.downcase }
+  end
+
+  def self.grab_header
+    self.split_strip_downcase(@f.first)
+  end
+
+  def self.grab_data
+    dino_data = []
+    @f.drop(1).each do |line|
+      dino_data << Hash[@header.zip(self.split_strip_downcase(line))]
+    end
+    dino_data
+  end
+
+  def self.parse_csv(filename)
+    @f = open(filename, 'r')
+    @header = self.grab_header
+    self.grab_data
+  end
 end
 
-def parse_dino_csv(filename)
-  headers = []
-  dino_data = []
-  File.open(filename).each_with_index do |line, index|
-    if index == 0
-      headers = split_strip_sanitize(line)
-      next
-    end
-    dino_data << Hash[headers.zip(split_strip_sanitize(line))]
+class DinoTranslator
+  HEADER_MAP = {
+    "genus" => "name",
+    "carnivore" => "diet",
+    "weight_in_lbs" => "weight",
+  }
+
+  def self.translate_key(hash, key)
+    hash[HEADER_MAP[key]] = hash.delete key if HEADER_MAP.include?(key)
   end
-  dino_data
+
+  def self.translate_carnivore_key(h)
+    carnivore_map = {
+      "yes" => "carnivore",
+      "no" => ""
+    }
+    h["carnivore"] = carnivore_map[h["carnivore"]] if h.include? "carnivore"
+  end
+
+  def self.translate_header(data)
+    data.each { |h| h.keys.each { |k| self.translate_key(h, k) } }
+  end
+
+  def self.translate_data(data)
+    data.each { |h| self.translate_carnivore_key(h) }
+  end
+
+  def self.translate(data)
+    self.translate_data(data)
+    self.translate_header(data)
+    data
+  end
+end
+
+class DinoValidator
 end
--- dino_catalog/dinosaur.rb
+++ dino_catalog/dinosaur.rb
@@ -1,44 +1,73 @@
+require_relative 'dino_parser'
+
+class String
+  def alpha?
+    !match(/[^[:alnum:]]/)
+  end
+
+  def number?
+    true if Float(self) rescue false
+  end
+end
+
+class Dinodex
+  @registry = []
+end
+
 class Dinosaur
   attr_accessor :name, :period, :continent, :diet, :weight, :walking, :desc
 
   def initialize(args = {})
-     @name = args[:name]
-     @period = args[:period]
-     @continent = args[:continent]
-     @diet = args[:diet]
-     @weight = args[:weight]
-     @walking = args[:walking]
-     @desc = args[:desc]
+    @name = args[:name]
+    @period = args[:period]
+    @continent = args[:continent]
+    @diet = args[:diet]
+    @weight = args[:weight]
+    @walking = args[:walking]
+    @desc = args[:desc]
   end
 
-  def validate_input(args)
-    args[:name]
-    args[:period]
-    args[:continent]
-    args[:diet]
-    args[:weight]
-    args[:walking]
-    args[:desc]
+  def valid_input?(args)
+    args[:name] &&
+      args[:period] &&
+      args[:continent] &&
+      args[:diet] &&
+      args[:weight] &&
+      args[:walking] &&
+      args[:desc]
   end
 
-  def validate_name(name)
+  def valid_name?(name)
+    name.alpha?
   end
 
-  def validate_period(period)
+  def valid_period?(period)
+    period.alpha?
   end
 
-  def validate_continent(continent)
+  def valid_continent?(continent)
+    continent.alpha?
   end
 
-  def validate_diet(diet)
+  def valid_diet?(diet)
+    diet.alpha?
   end
 
-  def validate_weight(weight)
+  def valid_weight?(weight)
+    weight.number?
   end
 
-  def validate_walking(walking)
+  def valid_walking?(walking)
+    walking.alpha?
   end
 
-  def validate_desc(desc)
+  def valid_desc?(desc)
+    true
   end
 end
+
+dinosaurs = []
+ARGV.each do |arg|
+  dinosaurs += DinoParser.parse_csv(arg) if File.file?(arg)
+end
+puts dinosaurs

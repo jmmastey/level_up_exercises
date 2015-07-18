189094a437ec0b675ae7d44c65a1e78a9ead8e22
Zshawn Syed <zsyed1@enova.com>
2015-07-18 14:08:43 -0500

    Add bomb delete rest api path

--- overlord/overlord.rb
+++ overlord/overlord.rb
@@ -6,14 +6,22 @@ require 'json'
 class Overlord < Sinatra::Base
   enable :sessions
 
+  set :public_folder, File.dirname(__FILE__) + '/public'
+
   get '/' do
     session.delete(:bomb)
-    erb :home, layout: 'layouts/main'.to_sym
+    erb :home,
+        layout: 'layouts/main'.to_sym,
+        locals: { bomb: session[:bomb] }
   end
 
   get '/bomb' do
     content_type :json
     bomb_count = session[:bomb].nil? ? 0 : 1
     { count: bomb_count }.to_json
+  end
+
+  delete '/bomb/delete' do
+    session.delete(:bomb)
   end
 end

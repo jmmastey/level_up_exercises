bae2cdbe6b9913978efab92762c860cb79f27700
Jeff Nyman <jeffnyman@gmail.com>
2015-07-20 07:26:56 -0500

    Coverage metrics generated for unit testing.

--- overlord/spec/spec_helper.rb
+++ overlord/spec/spec_helper.rb
@@ -1,5 +1,14 @@
 $LOAD_PATH.unshift File.expand_path('../../app', __FILE__)
 
+require 'simplecov'
+
+SimpleCov.start do
+  add_filter '/spec'
+  coverage_dir "#{SimpleCov.root}/spec/reports/coverage"
+  minimum_coverage 90
+  maximum_coverage_drop 5
+end
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
@@ -16,7 +25,7 @@ RSpec.configure do |config|
 
   config.disable_monkey_patching!
 
-  config.warnings = true
+  config.warnings = false
 
   config.default_formatter = 'doc' if config.files_to_run.one?
 

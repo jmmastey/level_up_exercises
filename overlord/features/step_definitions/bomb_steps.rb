d528e082e6d65379fe493e8b4e009d8be2613df6
Zshawn Syed <zsyed1@enova.com>
2015-07-20 10:21:37 -0500

    Switch capybara's driver from rack::test to selenium so the javascript front end is testable

--- overlord/features/step_definitions/bomb_steps.rb
+++ overlord/features/step_definitions/bomb_steps.rb
@@ -1,18 +1,19 @@
 Given(/^there are 0 bombs$/) do
+  http_delete '/bomb/delete'
   visit '/'
   expect(page).to have_content('No bomb created yet')
 end
 
 When(/^I create a bomb$/) do
-  pending # Write code here that turns the phrase above into concrete actions
 end
 
 When(/^enter an activation code$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  fill_in('activation_code', with: '1234')
 end
 
 When(/^enter a deactivation code$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  fill_in('deactivation_code', with: '2345')
+  click_button('Create Bomb')
 end
 
 Then(/^there should be (\d+) bomb$/) do |arg1|

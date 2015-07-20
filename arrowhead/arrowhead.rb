fed5e762585fdddc1b5c302d41370cead23793d8
Kyle McDaniel <mcdaniel221@gmail.com>
2015-07-20 11:30:17 -0500

    Reset the modified directories

--- arrowhead/arrowhead.rb
+++ arrowhead/arrowhead.rb
@@ -2,34 +2,32 @@ class Arrowhead
   # This seriously belongs in a database.
   CLASSIFICATIONS = {
     far_west: {
-      notched: 'Archaic Side Notch',
-      stemmed: 'Archaic Stemmed',
-      lanceolate: 'Agate Basin',
-      bifurcated: 'Cody',
+      notched: "Archaic Side Notch",
+      stemmed: "Archaic Stemmed",
+      lanceolate: "Agate Basin",
+      bifurcated: "Cody",
     },
     northern_plains: {
-      notched: 'Besant',
-      stemmed: 'Archaic Stemmed',
-      lanceolate: 'Humboldt Constricted Base',
-      bifurcated: 'Oxbow',
+      notched: "Besant",
+      stemmed: "Archaic Stemmed",
+      lanceolate: "Humboldt Constricted Base",
+      bifurcated: "Oxbow",
     },
   }
 
-  def region_error
-    raise 'Unknown region, please provide a valid region.'
-  end
-
-  def shape_error
-    raise "Unknown shape value. Are you " \
-          "sure you know what you're talking about?"
-  end
-
+  # FIXME: I don't have time to deal with this.
   def self.classify(region, shape)
-    region_error unless CLASSIFICATIONS.include? region
-    shapes = CLASSIFICATIONS[region]
-    shape_error unless shapes.include? shape
-    arrowhead = shapes[shape]
-    "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
+    if CLASSIFICATIONS.include? region
+      shapes = CLASSIFICATIONS[region]
+      if shapes.include? shape
+        arrowhead = shapes[shape]
+        "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
+      else
+        raise "Unknown shape value. Are you sure you know what you're talking about?"
+      end
+    else
+      raise "Unknown region, please provide a valid region."
+    end
   end
 end
 

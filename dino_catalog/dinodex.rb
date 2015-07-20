98734067d6482e081ec3b36aa771d66948d417eb
Alex Bake <abake@enova.com>
2015-07-20 10:00:37 -0500

    Satisfy rubocop.

--- dino_catalog/dinodex.rb
+++ dino_catalog/dinodex.rb
@@ -117,6 +117,18 @@ class Dinodex
 end
 
 module DinodexPrinter
+  COLUMN_MAX_LENGTHS = lambda do
+    {
+      'name' => 0,
+      'period' => 0,
+      'continent' => 0,
+      'diet' => 0,
+      'weight' => 0,
+      'walking' => 0,
+      'description' => 0,
+    }
+  end
+
   def self.build_table_header(columns)
     header = "|"
     columns.each do |key, max_len|
@@ -126,37 +138,31 @@ module DinodexPrinter
     header
   end
 
+  def self.build_cell(dino, key, max_len)
+    data = dino.has?(key) ? dino.data[key] : ""
+    data_len = dino.has?(key) ? dino.data[key].length : 0
+    spaces = "." * (max_len - data_len) + " |"
+    " " + data + spaces
+  end
+
+  def self.build_table_row(dino, columns)
+    row = "|"
+    columns.each { |key, max_len| row += build_cell(dino, key, max_len) }
+    row += "\n"
+  end
+
   def self.build_table_body(subset, columns)
     body = ""
-    subset.each do |dino|
-      row = "|"
-      columns.each do |key, max_len|
-        data = dino.has?(key) ? dino.data[key] : ""
-        data_len = dino.has?(key) ? dino.data[key].length : 0
-        spaces = "." * (max_len - data_len) + " |"
-        row += " " + data + spaces
-      end
-      row += "\n"
-      body += row
-    end
+    subset.each { |dino| body += build_table_row(dino, columns) }
     body
   end
 
   def self.calc_max_column_lengths(subset)
-    columns_max_len = {
-      'name' => 0,
-      'period' => 0,
-      'continent' => 0,
-      'diet' => 0,
-      'weight' => 0,
-      'walking' => 0,
-      'description' => 0,
-    }
+    columns_max_len = COLUMN_MAX_LENGTHS.call
     columns_max_len.keys.each do |key|
       dino = subset.max_by { |d| d.has?(key) ? d.data[key].length : 0 }
       next unless dino.has?(key)
-      max_col_len = dino.data[key].length
-      columns_max_len[key] = max_col_len
+      columns_max_len[key] = dino.data[key].length
     end
     columns_max_len
   end
@@ -168,10 +174,8 @@ module DinodexPrinter
 
   def self.remove_blank_columns(columns_max_len)
     columns_max_len.keys.each do |key|
-      if columns_max_len[key] == 0
-        columns_max_len.delete key
-        next
-      end
+      columns_max_len.delete key if columns_max_len[key] == 0
+      next if columns_max_len[key] == 0
       v = columns_max_len[key]
       columns_max_len[key] = v > key.length ? v : key.length
     end

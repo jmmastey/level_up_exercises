7c54b883bb668008bf746e26a47ad1705eb15a29
Kyle McDaniel <mcdaniel221@gmail.com>
2015-07-20 11:45:02 -0500

    Robot Name V2
    Refactored the errors from the first iteration of Robot Name

--- robot_name/robot_name.rb
+++ robot_name/robot_name.rb
@@ -7,29 +7,42 @@ class Robot
     @@registry ||= []
     @name_generator = args[:name_generator]
 
-    check_name_generator_arg
+    check_name_instance_and_generate_name
 
-    raise_name_error unless name_ok
+    raise_name_error unless name_valid?
     @@registry << @name
   end
 
-  def check_name_generator_arg
+  def check_name_instance_and_generate_name
     if @name_generator
       @name = @name_generator.call
-      generate_name if already_registered(@name)
+      generate_new_name if already_registered?(@name)
     else
       generate_name
     end
   end
 
   def generate_name
-    generate_char = -> { ('A'..'Z').to_a.sample }
-    generate_num = -> { rand(10) }
-    @name = "#{generate_char.call}#{generate_char.call}" \
-      "#{generate_num.call}#{generate_num.call}#{generate_num.call}"
+    @name = "#{generate_char}" \
+            "#{generate_char}" \
+            "#{generate_num}" \
+            "#{generate_num}"\
+            "#{generate_num}"
   end
 
-  def already_registered(name)
+  def generate_new_name
+    generate_name
+  end
+
+  def generate_char
+    ('A'..'Z').to_a.sample
+  end
+
+  def generate_num
+    rand(10)
+  end
+
+  def already_registered?(name)
     @@registry.include?(name)
   end
 
@@ -37,7 +50,7 @@ class Robot
     raise NameCollisionError, 'There was a problem generating the robot name!'
   end
 
-  def name_ok
+  def name_valid?
     (name =~ /[[:alpha:]]{2}[[:digit:]]{3}/) || @@registry.include?(name)
   end
 end

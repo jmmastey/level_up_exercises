4ca0c9f5543fe90712dbbd8fc97aece892d9fc88
Zshawn Syed <zsyed1@enova.com>
2015-07-20 16:04:53 -0500

    Create a bomb model for the front end, and update the submission event to POST a new bomb object to the rest api

---
+++ overlord/public/javascript/models/bomb.js
@@ -0,0 +1,32 @@
+define(["app", "jquery", "underscore"],
+function(App, $, _){
+
+  var BombModel = App.Model.extend({
+    url: 'bomb',
+    
+    defaults: {
+      activation_code: '0000',
+      deactivation_code: '0000'
+    },
+
+    validate: function(attributes, options) {
+      var errors = [];
+      if (_.isUndefined(attributes.activation_code)
+          || !attributes.activation_code.match(/^\d{4}$/)) {
+        errors.push("Activation code is required and must be 4 numbers");
+      }
+
+      if (_.isUndefined(attributes.deactivation_code)
+          || !attributes.deactivation_code.match(/^\d{4}$/)) {
+        errors.push("Deactivation code is required and must be 4 numbers");
+      }
+      
+      if (errors.length) {
+        return "- " + errors.join("\n- ");
+      }
+    }
+  });
+
+
+  return BombModel;
+});
--- overlord/public/javascript/views/new-bomb.js
+++ overlord/public/javascript/views/new-bomb.js
@@ -1,12 +1,43 @@
-define(["app", "jquery", "underscore"],
-function(App, $, _){
+define(["app", "jquery", "underscore", "lib/forms", "models/bomb"],
+function(App, $, _, FormHelper, BombModel){
   var NewBombView = App.View.extend({
     template: App.loadTemplate('new-bomb-form'),
+
+    events: {
+      "click :submit": "create_bomb"
+    },
 
     render: function(){
       this.setElement(this.template());
 
       return this;
+    },
+
+    create_bomb: function(event) {
+      event.preventDefault();
+      if (this.processing === true) {
+        return;
+      }
+      var data = FormHelper.serialize("form");
+      console.log(data);
+
+      var bomb = new BombModel(data);
+
+      if (!bomb.isValid()) {
+        alert(bomb.validationError);
+        return;
+      }
+      var self = this;
+
+      bomb.save(bomb.attributes, {
+        success: function(model) {
+          self.$(".no-bomb-alert").remove();
+        },
+        error: function(response) {
+          alert("Could not create bomb.");
+        }
+      });
+
     }
   });
 

9e7b3eecafc8154576dbc09c35281ba73bbb00d7
Alex Bake <abake@enova.com>
2015-07-21 15:12:40 -0500

    Implement bomb codes. Rewrite rspec tests.

--- overlord/bomb.rb
+++ overlord/bomb.rb
@@ -1,32 +1,55 @@
+InvalidCodeError = Class.new(RuntimeError)
+
 class Bomb
   OFF = 0
   ON = 1
   ACTIVATED = 2
   DEACTIVATED = 3
   DESTROYED = 4
-  
+
   attr_accessor :start_time, :state, :activation_code, :deactivation_code
-  
-  def initialize(activation_code = 1234, deactivation_code = 0000)
+  attr_accessor :deactivation_attempts
+
+  def initialize
     @state = OFF
+    @deactivation_attempts = 0
+  end
+
+  def valid_code?(code)
+    code =~ /[0-9]+/
+  end
+
+  def set_codes(activation_code, deactivation_code)
+    unless valid_code?(activation_code) && valid_code?(deactivation_code)
+      raise InvalidCodeError, "Bomb attempted to boot up with invalid codes"
+    end
     @activation_code = activation_code
     @deactivation_code = deactivation_code
   end
 
-  def boot_up
+  def boot_up(activation_code = "1234", deactivation_code = "0000")
+    set_codes(activation_code, deactivation_code)
     @state = ON
   end
 
-  def activate
-    @state = ACTIVATED if on?
+  def activate(code)
+    return unless on?
+    @state = ACTIVATED if code == @activation_code
   end
 
-  def deactivate
-    @state = DEACTIVATED if activated?
+  def deactivate(code)
+    return unless activated?
+    if code == @deactivation_code
+      @state = DEACTIVATED
+    else
+      @deactivation_attempts += 1
+      detonate if @deactivation_attempts >= 3
+    end
   end
 
   def detonate
-    @state = DESTROYED if activated?
+    return unless activated? && @deactivation_attempts >= 3
+    @state = DESTROYED
   end
 
   def on?
--- overlord/spec/bomb_spec.rb
+++ overlord/spec/bomb_spec.rb
@@ -3,6 +3,18 @@ require_relative '../bomb'
 describe Bomb do
   before(:each) do
     @bomb = Bomb.new
+    @activate = lambda do |code|
+      @bomb.boot_up
+      @bomb.activate(code)
+    end
+    @deactivate = lambda do |code|
+      @activate.call("1234")
+      @bomb.deactivate(code)
+    end
+    @detonate = lambda do
+      @activate.call("1234")
+      3.times { @bomb.deactivate("9999") }
+    end
   end
 
   describe "#initialize" do
@@ -13,74 +25,113 @@ describe Bomb do
   end
 
   describe "#boot_up" do
-    it "should set the Bomb's state to ON" do
-      @bomb.boot_up
-      expect(@bomb.state).to eq(1)
+    context "no arguments" do
+      before(:each) do
+        @bomb.boot_up
+      end
+
+      it "should boot up with default codes" do
+        expect(@bomb.activation_code).to eq("1234")
+        expect(@bomb.deactivation_code).to eq("0000")
+      end
+
+      it "should set the Bomb's state to ON" do
+        expect(@bomb.state).to eq(1)
+      end
+    end
+
+    context "activation and deactivation codes supplied" do
+      before(:each) do
+        @bomb.boot_up("2345", "1111")
+      end
+
+      it "should boot up with supplied codes" do
+        expect(@bomb.activation_code).to eq("2345")
+        expect(@bomb.deactivation_code).to eq("1111")
+      end
+
+      it "should set the Bomb's state to ON" do
+        expect(@bomb.state).to eq(1)
+      end
     end
   end
 
   describe "#activate" do
+    before(:each) do
+      @activate.call("1234")
+    end
+
     it "should set the Bomb's state to ACTIVATED" do
-      @bomb.boot_up
-      @bomb.activate
       expect(@bomb.state).to eq(2)
     end
   end
 
   describe "#deactivate" do
+    before(:each) do
+      @deactivate.call("0000")
+    end
+
     it "should set the Bomb's state to DEACTIVATED" do
-      @bomb.boot_up
-      @bomb.activate
-      @bomb.deactivate
       expect(@bomb.state).to eq(3)
     end
   end
 
   describe "#detonate" do
+    before(:each) do
+      @detonate.call
+    end
+
     it "should set the Bomb's state to DESTROYED" do
-      @bomb.boot_up
-      @bomb.activate
-      @bomb.detonate
       expect(@bomb.state).to eq(4)
     end
   end
 
   describe "#on?" do
-    it "should check if the Bomb's state to ON" do
+    before(:each) do
       @bomb.boot_up
+    end
+
+    it "should check if the Bomb's state to ON" do
       expect(@bomb.on?).to eq(true)
     end
   end
 
   describe "#off?" do
-    it "should check if the Bomb's state to OFF" do
+    before(:each) do
       @bomb.boot_up
+    end
+
+    it "should check if the Bomb's state to OFF" do
       expect(@bomb.off?).to eq(false)
     end
   end
 
   describe "#activated?" do
+    before(:each) do
+      @activate.call("1234")
+    end
+
     it "should check if the Bomb's state to ACTIVATED" do
-      @bomb.boot_up
-      @bomb.activate
       expect(@bomb.activated?).to eq(true)
     end
   end
 
   describe "#deactivated?" do
+    before(:each) do
+      @deactivate.call("0000")
+    end
+
     it "should check if the Bomb's state to DEACTIVATED" do
-      @bomb.boot_up
-      @bomb.activate
-      @bomb.deactivate
       expect(@bomb.deactivated?).to eq(true)
     end
   end
 
   describe "#destroyed?" do
+    before(:each) do
+      @detonate.call
+    end
+
     it "should check if the Bomb's state to DESTROYED" do
-      @bomb.boot_up
-      @bomb.activate
-      @bomb.detonate
       expect(@bomb.destroyed?).to eq(true)
     end
   end

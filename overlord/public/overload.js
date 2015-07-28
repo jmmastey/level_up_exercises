86ad818655883a46a66e050af7c08b503fe49b0c
Shaan Sapra <shaanfire@gmail.com>
2015-07-28 16:06:38 -0500

    Refactored javascript to be more organized

--- overlord/public/overload.js
+++ overlord/public/overload.js
@@ -1,19 +1,101 @@
-var timer = null;
-var dangerFlashing = null;
+var bomb = {
+    timer:              null,
+    dangerFlashing:     null,
+
+    startTimer: function () {
+        $('.timer').show();
+        var c = $('.timer').attr('id');
+        $('.timer').text(c);
+        bomb.timer = setInterval(function() {
+            c--;
+            if (c >= 0) {
+                $('.timer').text(c + ' seconds left');
+            }
+            if (c == 10) {
+                bomb.danger();
+            }
+            if (c === 0) {
+                bomb.explode();
+                clearInterval(bomb.dangerFlashing);
+            }
+        }, 1000);
+    },
+
+    explode: function () {
+        $('.interface').hide();
+        bomb.changePanel('panel-danger');
+        $('.bomb_status').html('Exploded!');
+        $('.attempts').hide();
+        $('.timer').hide();
+        $('.exploded').show();
+    },
+
+    deactivate: function() {
+        $('.interface').hide();
+        bomb.changePanel('panel-success');
+        $('.bomb_status').html('Deactivated!');
+        $('.safe').show();
+        $('.timer').hide();
+        clearInterval(bomb.timer);
+        clearInterval(bomb.dangerFlashing);
+    },
+
+    activate: function() {
+        $('.activate').hide();
+        $('.bomb_status').html('Activated!');
+        bomb.changePanel('panel-warning');
+        $('.deactivate').show();
+        $('.attempts').show();
+        bomb.startTimer();
+    },
+
+    boot: function() {
+        $('.interface').show();
+        bomb.changePanel('panel-info');
+        $('.bomb_status').html('Booted up!');
+        $('.configuration').hide();
+    },
+
+    danger: function() {
+        $('.countdown').css('color', 'red');
+        bomb.changePanel('panel-danger');
+        bomb.dangerFlashing = setInterval(function() {
+           $('body').toggleClass('backgroundRed');
+        }, 500);
+    },
+    changePanel: function(newClass) {
+        var lastClass = $('.panel').attr('class').split(' ').pop();
+        $('.panel').removeClass(lastClass);
+        $('.panel').addClass(newClass);
+    },
+
+    insert_errors: function(selector, errors) {
+        $('.errors').html('');
+        for(var i = 0; i < errors.length; i++) {
+            error_div = '<div class="alert alert-danger" style="display: block;">'
+                        + errors[i] + '</div>';
+            $(selector + ' .errors').append(error_div);
+        }
+        $(selector + ' .alert').show();
+    }
+}
 
 $('.configuration form').on('submit', function (event) {
     event.preventDefault();
+    var activationCode = $('input[name="configure_activation_code"]').val();
+    var deactivationCode = $('input[name="configure_deactivation_code"]').val();
+
     $.ajax({
         url: '/configure',
         type: 'POST',
         data: {
-            'activation_code': $('input[name="configure_activation_code"]').val(),
-            'deactivation_code': $('input[name="configure_deactivation_code"]').val()
+            'activation_code':  activationCode,
+            'deactivation_code': deactivationCode
         },
         dataType: 'json',
-        success: function(data) { boot(); },
+        success: function(data) { bomb.boot(); },
         error: function (data) {
-            insert_errors('.configuration', data.responseJSON['errors'])
+            bomb.insert_errors('.configuration', data.responseJSON['errors'])
         }
     });
 });
@@ -25,9 +107,9 @@ $('.activate form').on('submit', function(event) {
         type: 'POST',
         data: { 'activation_code': $('.activate input').val()},
         dataType: 'json',
-        success: function(data) { activate(); },
+        success: function(data) { bomb.activate(); },
         error: function (data) {
-          insert_errors('.activate', data.responseJSON['errors'])
+          bomb.insert_errors('.activate', data.responseJSON['errors'])
         }
     });
 });
@@ -39,90 +121,13 @@ $('.deactivate form').on('submit', function(event) {
         type: 'POST',
         data: { 'deactivation_code': $('.deactivate input').val()},
         dataType: 'json',
-        success: function(data) { deactivate(); },
+        success: function(data) { bomb.deactivate(); },
         statusCode: {
-            400: function() { explode(); },
+            400: function() { bomb.explode(); },
             422: function (data) {
-                insert_errors('.deactivate', data.responseJSON['errors'])
+                bomb.insert_errors('.deactivate', data.responseJSON['errors']);
                 $('.attempts').html(data.responseJSON['attempts']);
             }
         }
     });
-})
-
-function startTimer() {
-    $('.timer').show();
-    var c = $('.timer').attr('id');
-    $('.timer').text(c);
-    timer = setInterval(function() {
-        c--;
-        if (c >= 0) {
-            $('.timer').text(c + ' seconds left');
-        }
-        if (c == 10) {
-            danger();
-        }
-        if (c === 0) {
-            explode();
-            clearInterval(dangerFlashing);
-        }
-    }, 1000);
-}
-
-function explode() {
-    $('.interface').hide();
-    changePanel('panel-danger');
-    $('.bomb_status').html('Exploded!');
-    $('.attempts').hide();
-    $('.timer').hide();
-    $('.exploded').show();
-}
-
-function deactivate() {
-    $('.interface').hide();
-    changePanel('panel-success');
-    $('.bomb_status').html('Deactivated!');
-    $('.safe').show();
-    $('.timer').hide();
-    clearInterval(timer);
-    clearInterval(dangerFlashing);
-}
-
-function activate() {
-    $('.activate').hide();
-    $('.bomb_status').html('Activated!');
-    changePanel('panel-warning');
-    $('.deactivate').show();
-    $('.attempts').show();
-    startTimer();
-}
-
-function boot() {
-    $('.interface').show();
-    changePanel('panel-info');
-    $('.bomb_status').html('Booted up!');
-    $('.configuration').hide();
-}
-
-function danger() {
-    $('.countdown').css('color', 'red');
-    changePanel('panel-danger');
-    dangerFlashing = setInterval(function(){
-       $('body').toggleClass('backgroundRed');
-    }, 500);
-}
-
-function changePanel(newClass) {
-    var lastClass = $('.panel').attr('class').split(' ').pop();
-    $('.panel').removeClass(lastClass);
-    $('.panel').addClass(newClass);
-}
-
-function insert_errors(selector, errors) {
-    $('.errors').html('')
-    for(var i = 0; i < errors.length; i++){
-        error_div = '<div class="alert alert-danger" role="alert" style="display: block;"><span aria-hidden="true" class="glyphicon glyphicon-exclamation-sign"></span>' + errors[i] + '</div>'
-        $(selector + ' .errors').append(error_div)
-    }
-    $(selector + ' .alert').show();
-}
+});

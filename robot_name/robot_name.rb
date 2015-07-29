268d2bffb77160def6932711bb1b1d119f3b3b23
Jennifer Ma <jma1@clm-jma1.local>
2015-07-28 22:49:23 -0500

    fixed some rubocop issues

--- robot_name/robot_name.rb
+++ robot_name/robot_name.rb
@@ -1,6 +1,6 @@
 require_relative 'exceptions'
 
-class Robot 
+class Robot
   attr_accessor :registry
 
   @@registry ||= []
@@ -9,26 +9,32 @@ class Robot
 
     def initialize(args = {})
       @name_generator = args[:name_generator]
-      @name = @name_generator.call if @name_generator != nil
+      @name = @name_generator.call unless @name_generator.nil?
       create_name(2, 3) unless @name
       check_name
     end
-  
+
+    def random_ascii(string = "", num, char)
+      num.times do
+        string += ('A'..'Z').to_a.sample if char
+        string += rand(10).to_s unless char
+      end
+      string
+    end
+
     def create_name(num_char, num_dig)
-      char = ""
-      digit = ""
-      num_char.times do 
-        char += ('A'..'Z').to_a.sample
-      end
-      num_dig.times do 
-        digit += rand(10).to_s
-      end
+      char = random_ascii(num_char, true)
+      digit = random_ascii(num_dig, false)
       @name = char + digit
     end
-  
+
+    def name_structure?
+      @name =~ /[[:alpha:]]{2}[[:digit:]]{3}/
+    end
+
     def check_name
-      if !(@name =~ /[[:alpha:]]{2}[[:digit:]]{3}/) || @@registry.include?(@name)
-        raise NameCollisionError, 'There was a problem generating the robot name #!'
+      if !(name_structure?) || @@registry.include?(@name)
+        raise NameCollisionError, 'There was a problem generating the name'
       end
       @@registry << @name
     end
@@ -37,7 +43,7 @@ end
 
 robot = Robot::RobotName.new
 puts "My pet robot's name is #{robot.name}, but we usually call him sparky"
-generator = -> {'AA111'}
+generator = -> { 'AA111' }
 robot1 = Robot::RobotName.new(name_generator: generator)
 robot1 = Robot::RobotName.new(name_generator: generator)
 

3d1b965cf2f1676a98e02005e568a42beb9d1d24
Zshawn Syed <zsyed1@enova.com>
2015-08-01 16:04:09 -0500

    Fixed error in gravatar_for method

--- cyoa/animetrackr/app/helpers/profile_helper.rb
+++ cyoa/animetrackr/app/helpers/profile_helper.rb
@@ -2,7 +2,7 @@ require 'digest'
 
 module ProfileHelper
   def gravatar_for(email)
-    email.downcase!.strip!
+    email.downcase!
     email_digest = Digest::MD5.hexdigest(email)
     "<img src='http://www.gravatar.com/avatar/#{email_digest}' />".html_safe
   end

88abe1bfac034f0a6713ba8db16ca3ba153e80a6
Alex Bake <abake@enova.com>
2015-08-03 10:04:50 -0500

    Add static pages.

--- cyoa/mtg-deck-builder/config/routes.rb
+++ cyoa/mtg-deck-builder/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  get 'static_pages/help'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

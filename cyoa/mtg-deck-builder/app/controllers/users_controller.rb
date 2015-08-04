ec21eff26e093b619483837002aaf1db86646fe4
Alex Bake <abake@enova.com>
2015-08-04 16:40:31 -0500

    Implement log in, log out.

--- cyoa/mtg-deck-builder/app/controllers/users_controller.rb
+++ cyoa/mtg-deck-builder/app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Woohoo! Welcome to MTG Deck builder!"
       redirect_to @user
     else
--- cyoa/mtg-deck-builder/config/routes.rb
+++ cyoa/mtg-deck-builder/config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get 'help'      => 'static_pages#help'
   get 'about'     => 'static_pages#about'
   get 'signup'    => 'users#new'
+  get 'login'     => 'sessions#new'
+  post 'login'    => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.

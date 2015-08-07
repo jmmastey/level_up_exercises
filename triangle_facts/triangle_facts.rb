dd5b156dda805d65cc3e672a22b97c9241fb2ebb
Deja Augustine <daugustine@enova.com>
2015-08-07 12:18:48 -0500

    Adjusting line length on triangle_facts.rb

--- triangle_facts/triangle_facts.rb
+++ triangle_facts/triangle_facts.rb
@@ -47,13 +47,14 @@ class Triangle
   #
   # Returns nothing.
   def recite_facts
-    puts "Facts about a triangle described by side lengths of #{side_a}, #{side_b}, and #{side_c} units"
+    puts "Facts about a triangle described by side lengths of " \
+         "#{side_a}, #{side_b}, and #{side_c} units"
     puts "This triangle is equilateral!" if equilateral?
-    puts "This triangle is isosceles! Also, that word is hard to type." if isosceles?
-    puts "This triangle is scalene and mathematically boring." if scalene?
+    puts "This triangle is isosceles!" if isosceles?
+    puts "This triangle is scalene!" if scalene?
 
     angles = calculate_angles
-    puts "The angles of this triangle are #{ angles.join("\xC2\xB0, ") }\xC2\xB0"
+    puts "The angles of this triangle are #{angles.join("\xC2\xB0, ")}\xC2\xB0"
 
     puts "This triangle is also a right triangle!" if angles.include? 90
     puts ""
@@ -63,11 +64,22 @@ class Triangle
   #
   # Returns the three angles in an Array
   def calculate_angles
-    angle_a = radians_to_degrees(Math.acos((side_b**2 + side_c**2 - side_a**2) / (2.0 * side_b * side_c)))
-    angle_b = radians_to_degrees(Math.acos((side_a**2 + side_c**2 - side_b**2) / (2.0 * side_a * side_c)))
-    angle_c = radians_to_degrees(Math.acos((side_a**2 + side_b**2 - side_c**2) / (2.0 * side_a * side_b)))
+    angle_a = radians_to_degrees(calculate_angle(side_b, side_c, side_a))
+    angle_b = radians_to_degrees(calculate_angle(side_a, side_c, side_b))
+    angle_c = radians_to_degrees(calculate_angle(side_a, side_b, side_c))
 
     [angle_a, angle_b, angle_c]
+  end
+
+  # Internal: Calculate angle in radians
+  #
+  # left  - The Integer length of the side adjacent to the left of the angle
+  # right - The Integer length of the side adjacent to the right of the angle
+  # trans - The Integer length of the transversal side opposite the angle
+  #
+  # Returns the angle in degrees as a Float
+  def calculate_angle(left, right, trans)
+    Math.acos((left**2 + right**2 - trans**2) /( 2.0 * left * right))
   end
 
   # Internal: Converts radians to degrees

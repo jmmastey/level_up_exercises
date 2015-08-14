38810990c00322710fa71cdc8dadd2747811aaca
Alex Bake <abake@enova.com>
2015-08-14 13:16:16 -0500

    Write card search steps.

--- cyoa/mtg-deck-builder/features/step_definitions/card_steps.rb
+++ cyoa/mtg-deck-builder/features/step_definitions/card_steps.rb
@@ -5,3 +5,15 @@ end
 Given(/^there is a card named (.*) in the database$/) do |card_name|
   @card = create(:card, name: card_name)
 end
+
+When(/^I visit the card search page$/) do
+  visit(cards_path)
+end
+
+When(/^I search for the card named (.*)$/) do |card_name|
+  fill_in 'cardname', with: card_name
+end
+
+Then(/^I should see the card named (.*)$/) do |card_name|
+  expect(page).to have_content(card_name)
+end

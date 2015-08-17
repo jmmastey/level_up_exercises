71713446f73f86d2ddba3d070dea5324d5a218cd
Jennifer Ma <jma1@clm-jma1.enova.com>
2015-08-17 15:06:20 -0500

    removed files

--- overlord/features/step_definitions/deactivate_steps.rb
+++
@@ -1,7 +0,0 @@
-Given(/^I wait for (\d+) seconds?$/) do |n|
-  sleep(n.to_i)
-end
-
-And(/^I cannot enter "([^\"]*)" code$/) do |field|
-  page.has_css?(field, visible: false)
-end
--- overlord/spec/capybara_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rspec/expectations'
-
-RSpec.describe Capybara do
-  describe 'default_max_wait_time' do
-    after do
-      Capybara.default_max_wait_time = 4
-    end
-  end
-end

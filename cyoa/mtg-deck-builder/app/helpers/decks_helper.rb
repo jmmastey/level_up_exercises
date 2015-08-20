5a2a2cdcebc07caa30948eb7e0dc5b471a819556
Alex Bake <abake@enova.com>
2015-08-20 11:16:47 -0500

    Remove unused function

--- cyoa/mtg-deck-builder/app/helpers/decks_helper.rb
+++ cyoa/mtg-deck-builder/app/helpers/decks_helper.rb
@@ -1,9 +1,4 @@
 module DecksHelper
-  def cards_in_deck_with_id(deck, card_id)
-    card = deck.cards.find_by_id(card_id)
-    card ? cards : []
-  end
-
   def find_number_of_card_in_deck(deck_id, card_id)
     cards_deck = find_cards_deck(deck_id, card_id)
     return cards_deck ? cards_deck.number_in_deck : 0

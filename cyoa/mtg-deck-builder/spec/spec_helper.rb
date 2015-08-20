5d7a0da04d3c9dfafebe9118d76204e3110ce8fa
Alex Bake <abake@enova.com>
2015-08-20 15:06:11 -0500

    Change location of factories.

--- cyoa/mtg-deck-builder/spec/spec_helper.rb
+++ cyoa/mtg-deck-builder/spec/spec_helper.rb
@@ -17,6 +17,7 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  require "capybara/rspec"
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.

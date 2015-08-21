dc1e441cc3f5403734d09d29dd308f04aca56bfe
Vaughn Bullock <vbullock@clm-vbullock.enova.com>
2015-08-21 11:17:14 -0500

    Refactored code, applied new design patterns for certain actions in Ruby

--- robot_name/robot_name.rb
+++ robot_name/robot_name.rb
@@ -1,35 +1,38 @@
 class NameCollisionError < RuntimeError; end
 
 class Robot
+  
   attr_accessor :name
 
   @@registry
 
   def initialize(args = {})
     @@registry ||= []
-    @name_generator = args[:name_generator]
-
-    if @name_generator
-      add_robot_to_registry(@name_generator.call)
-    else
-      add_robot_to_registry
-    end
+    @name = (args[:name_generator] && args[:name_generator].call) || generate_robot_name
+    add_robot_to_registry
   end
 
-  def self.show_registered
-    puts @@registry
+  def self.get_registered
+    @@registry
   end
 
-  def add_robot_to_registry(robotName="")
-    if robotName.empty?
-      robotName = "#{generate_char(2)}#{generate_num(3)}"
-    end
+  private
 
-    @name = robotName
-    unless robot_registered?(@name)
-      raise NameCollisionError, "Improper robot name!" unless proper_name?(@name)
-      @@registry << @name
-    end
+  def add_robot_to_registry
+    raise NameCollisionError, "Improper robot name!" unless proper_name?
+    @@registry << @name unless robot_registered?
+  end
+
+  def proper_name?
+    @name =~ /[[:alpha:]]{2}[[:digit:]]{3}/
+  end
+
+  def robot_registered?
+    @@registry.include?(@name)
+  end
+
+  def generate_robot_name
+    "#{generate_char(2)}#{generate_num(3)}"
   end
 
   def generate_char(length=1)
@@ -39,22 +42,14 @@ class Robot
   def generate_num(length=1)
     length.times.map { rand(10) }.join("")
   end
-
-  def proper_name?(name) 
-    name =~ /[[:alpha:]]{2}[[:digit:]]{3}/
-  end
-
-  def robot_registered?(name)
-    @@registry.include?(name)
-  end
 end
 
 robot = Robot.new
 puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
 
 generator = -> { 'AA111' }
-Robot.new(name_generator: generator) 
+Robot.new(name_generator: generator)
 Robot.new(name_generator: generator)
 
 puts "Robots Registered:"
-Robot.show_registered
+puts Robot.get_registered

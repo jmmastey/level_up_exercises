a947537aa02defc4dc07efa303b8446c4cc72677
Daniel Nunez <reachdanielnunez@gmail.com>
2015-08-26 12:36:52 -0500

    V2 refactor triangle_facts

--- triangle_facts/triangle_facts.rb
+++ triangle_facts/triangle_facts.rb
@@ -3,49 +3,50 @@ class Triangle
 	attr_accessor :side1, :side2, :side3
 
 	def initialize(side1, side2, side3)
-		@side1, @side2, @side3 = side1, side2, side3
+		@side1 = side1
+    @side2 = side2
+    @side3 = side3
 	end
 
-	def equilateral()
+	def equilateral?
 		side1 == side2 && side2 == side3
 	end
 
-	def isosceles()
+	def isosceles?
 		[side1, side2, side3].uniq.length == 2
 	end
 
-	def scalene()
-		equilateral || isosceles ? true : false
+	def scalene?
+		(equilateral? || isosceles?)
 	end
 
+  def right_triangle?
+    calculate_angles(side1, side2, side3).include? 90
+  end
+
 	def recite_facts
-		if equilateral
-      puts "This triangle is equilateral!"
-    elsif isosceles
-		  puts "This triangle is isosceles! Also, that word is hard to type."
-    else
-      puts "This triangle is scalene and mathematically boring."
-    end
+    puts "This triangle is equilateral!" if equilateral?
+    puts "This triangle is isosceles! Also, that word is hard to type." if isosceles?
+    puts "This triangle is scalene and mathematically boring." if scalene?
 
 		angles = calculate_angles(side1, side2, side3)
 
     puts "The angles of this triangle are #{angles.join(', ')}" 
-
-    puts "This triangle is also a right triangle!" if angles.include? 90
-
+    puts "This triangle is also a right triangle!" if right_triangle?
 		puts ''
 	end
+  
+  def radians_to_degrees(rads)
+		(rads * 180 / Math::PI).round
+	end
 
+  private
 	def calculate_angles(a, b, c)
 		angleA = radians_to_degrees(Math.acos((b**2 + c**2 - a**2) / (2.0 * b * c)))
 		angleB = radians_to_degrees(Math.acos((a**2 + c**2 - b**2) / (2.0 * a * c)))
 		angleC = radians_to_degrees(Math.acos((a**2 + b**2 - c**2) / (2.0 * a * b)))
 
 		[angleA, angleB, angleC]
-	end
-
-	def radians_to_degrees(rads)
-		(rads * 180 / Math::PI).round
 	end
 end
 

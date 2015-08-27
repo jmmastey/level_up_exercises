c43a5abc2a3138c208b6b85ee0a3777909e54cab
Kyle McDaniel <mcdaniel221@gmail.com>
2015-08-27 19:33:27 +0000

    Forgot to let rubymine clean my code before submitting

--- yadda/yadda-schema.sql
+++ yadda/yadda-schema.sql
@@ -1,7 +1,7 @@
 DROP DATABASE IF EXISTS yadda;
 CREATE DATABASE yadda;
 
-\connect yadda;
+\CONNECT yadda;
 
 CREATE TABLE breweries (
   brewery_id     SERIAL PRIMARY KEY,
@@ -12,7 +12,7 @@ CREATE TABLE breweries (
   description    TEXT,
   founding_year  INTEGER,
   modified_on    TIMESTAMP DEFAULT current_timestamp,
-  modified_by    INTEGER NOT NULL REFERENCES users(user_id) ON DELETE SET NULL
+  modified_by    INTEGER      NOT NULL REFERENCES users (user_id) ON DELETE SET NULL
 );
 COMMENT ON TABLE breweries IS 'Information about each brewery';
 
@@ -23,32 +23,32 @@ CREATE TABLE beers (
   description TEXT,
   brew_year   INTEGER,
   brewery_id  INTEGER NOT NULL REFERENCES breweries ON DELETE CASCADE,
-  modified_on    TIMESTAMP DEFAULT current_timestamp,
-  modified_by    INTEGER NOT NULL REFERENCES users(user_id)
+  modified_on TIMESTAMP DEFAULT current_timestamp,
+  modified_by INTEGER NOT NULL REFERENCES users (user_id)
 );
 COMMENT ON TABLE beers IS 'Information about each beer';
 
 CREATE TABLE ratings (
-  rating_id SERIAL PRIMARY KEY,
-  user_id   INTEGER REFERENCES users (user_id) ON DELETE CASCADE,
-  beer_id   INTEGER REFERENCES beers (beer_id) ON DELETE CASCADE,
-  look      SMALLINT,
-  smell     SMALLINT,
-  taste     SMALLINT,
-  feel      SMALLINT,
-  overall   SMALLINT,
-  modified_on    TIMESTAMP DEFAULT current_timestamp,
-  modified_by    INTEGER REFERENCES users
+  rating_id   SERIAL PRIMARY KEY,
+  user_id     INTEGER REFERENCES users (user_id) ON DELETE CASCADE,
+  beer_id     INTEGER REFERENCES beers (beer_id) ON DELETE CASCADE,
+  look        SMALLINT,
+  smell       SMALLINT,
+  taste       SMALLINT,
+  feel        SMALLINT,
+  overall     SMALLINT,
+  modified_on TIMESTAMP DEFAULT current_timestamp,
+  modified_by INTEGER REFERENCES users
 );
 COMMENT ON TABLE ratings IS 'Ratings for each beer';
 
 CREATE TABLE users (
-  user_id  SERIAL PRIMARY KEY,
-  name     VARCHAR(50)  NOT NULL,
-  email    VARCHAR(100) NOT NULL,
-  birthday DATE         NOT NULL,
-  modified_on    TIMESTAMP DEFAULT current_timestamp,
-  modified_by    INTEGER REFERENCES users
+  user_id     SERIAL PRIMARY KEY,
+  name        VARCHAR(50)  NOT NULL,
+  email       VARCHAR(100) NOT NULL,
+  birthday    DATE         NOT NULL,
+  modified_on TIMESTAMP DEFAULT current_timestamp,
+  modified_by INTEGER REFERENCES users
 );
 COMMENT ON TABLE persons IS 'Information about each person';
 

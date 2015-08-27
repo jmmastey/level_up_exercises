a17f8a1714504462757d9c592b6dffb8464e93c5
Shaan Sapra <ssapra@enova.com>
2015-08-27 15:49:47 -0500

    Using constants and create_list

--- yadda/yadda_generator.rb
+++ yadda/yadda_generator.rb
@@ -1,11 +1,14 @@
 require 'active_record'
 require 'factory_girl'
 require 'faker'
-Dir['models/*.rb'].each { |f| require_relative f }
-Dir['factories/*.rb'].each { |f| require_relative f }
+require_relative 'factories'
+require_relative 'models'
 
 module YaddaGenerator
   CONFIG = { adapter: 'postgresql', database: 'yadda' }
+  BREWERY_COUNT = 100
+  USER_COUNT = 1_000
+  RATING_COUNT = 100_000
 
   def self.reload_schema
     `psql -f sql/schema.sql`
@@ -23,32 +26,21 @@ module YaddaGenerator
   end
 
   def self.create_beer_styles
-    beer_style_names = File.readlines('data/beer_styles.txt').map(&:chomp)
-    beer_style_names.each { |name| FactoryGirl.create(:beer_style, name: name) }
+    File.foreach('data/beer_styles.txt') do |beer_style|
+      FactoryGirl.create(:beer_style, name: beer_style.chomp)
+    end
   end
 
   def self.create_breweries
-    100.times do |i|
-      FactoryGirl.create(:brewery)
-      puts "#{i}/100 breweries" if i % 10 == 0
-    end
+    FactoryGirl.create_list(:brewery, BREWERY_COUNT)
   end
 
   def self.create_users
-    100_000.times do |i|
-      FactoryGirl.create(:user)
-      puts "#{i}/100,000 drinkers" if i % 10_000 == 0
-    end
+    FactoryGirl.create_list(:user, USER_COUNT)
   end
 
   def self.create_ratings
-    all_user_ids = User.pluck(:id)
-    all_beer_ids = Beer.pluck(:id)
-    200_000.times do |i|
-      FactoryGirl.create(:rating, user_id: all_user_ids.sample,
-                                  beer_id: all_beer_ids.sample)
-      puts "#{i}/200,000 ratings" if i % 10_000 == 0
-    end
+    FactoryGirl.create_list(:rating, RATING_COUNT)
   end
 end
 

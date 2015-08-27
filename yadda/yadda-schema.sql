7eabbfc8abb5747fa67b48045f12a2aec8d16c31
Kyle McDaniel <mcdaniel221@gmail.com>
2015-08-27 20:30:14 +0000

    Fixed not null on delete set null problem.

--- yadda/yadda-schema.sql
+++ yadda/yadda-schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE breweries (
   description    TEXT,
   founding_year  INTEGER,
   modified_on    TIMESTAMP DEFAULT current_timestamp,
-  modified_by    INTEGER      NOT NULL REFERENCES users (user_id) ON DELETE SET NULL
+  modified_by    INTEGER REFERENCES users (user_id) ON DELETE SET NULL
 );
 COMMENT ON TABLE breweries IS 'Information about each brewery';
 
@@ -25,6 +25,7 @@ CREATE TABLE beers (
   brewery_id  INTEGER NOT NULL REFERENCES breweries ON DELETE CASCADE,
   modified_on TIMESTAMP DEFAULT current_timestamp,
   modified_by INTEGER NOT NULL REFERENCES users (user_id)
+  CHECK (brew_year < date_trunc('year', now()))
 );
 COMMENT ON TABLE beers IS 'Information about each beer';
 
@@ -38,7 +39,12 @@ CREATE TABLE ratings (
   feel        SMALLINT,
   overall     SMALLINT,
   modified_on TIMESTAMP DEFAULT current_timestamp,
-  modified_by INTEGER REFERENCES users
+  modified_by INTEGER REFERENCES users,
+  CHECK (look > 0 AND look < 11),
+  CHECK (smell > 0 AND smell < 11),
+  CHECK (taste > 0 AND taste < 11),
+  CHECK (feel > 0 AND feel < 11),
+  CHECK (overall > 0 AND overall < 11)
 );
 COMMENT ON TABLE ratings IS 'Ratings for each beer';
 
@@ -49,6 +55,7 @@ CREATE TABLE users (
   birthday    DATE         NOT NULL,
   modified_on TIMESTAMP DEFAULT current_timestamp,
   modified_by INTEGER REFERENCES users
+  CHECK a
 );
 COMMENT ON TABLE persons IS 'Information about each person';
 

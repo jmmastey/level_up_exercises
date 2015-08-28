4f2d79b62b85eec3c25b31de6b688de91051a877
Kyle McDaniel <mcdaniel221@gmail.com>
2015-08-28 14:12:13 +0000

    created_on created_at

--- yadda/yadda-schema.sql
+++ yadda/yadda-schema.sql
@@ -11,11 +11,13 @@ CREATE TABLE users (
   birthday    DATE         NOT NULL,
   password    VARCHAR(50)  NOT NULL,
   modified_on TIMESTAMP DEFAULT current_timestamp,
-  modified_by INTEGER REFERENCES users (user_id) ON DELETE SET NULL
+  modified_by INTEGER REFERENCES users (user_id) ON DELETE SET NULL,
+  created_on TIMESTAMP DEFAULT current_timestamp,
+  created_by INTEGER REFERENCES users (user_id) ON DELETE SET NULL
 );
 COMMENT ON TABLE users IS 'Information about each person';
 -- Store information on users. name, email, birthday, and password can not be set to null.
--- modified_by can be set to null if the corresponding user is deleted
+-- modified_by and created_by can be set to null if the corresponding user is deleted
 -- A user has many ratings, and may have many modifications.
 
 CREATE TABLE breweries (
@@ -28,11 +30,13 @@ CREATE TABLE breweries (
   description    TEXT,
   founding_year  INTEGER,
   modified_on    TIMESTAMP DEFAULT current_timestamp,
-  modified_by    INTEGER REFERENCES users (user_id) ON DELETE SET NULL
+  modified_by    INTEGER REFERENCES users (user_id) ON DELETE SET NULL,
+  created_on TIMESTAMP DEFAULT current_timestamp,
+  created_by INTEGER REFERENCES users (user_id) ON DELETE SET NULL
 );
 COMMENT ON TABLE breweries IS 'Information about each brewery';
--- Store brewery information. Name and city must not be null. modified_by
--- may be set to null if the user that modified the brewery has been deleted.
+-- Store brewery information. Name and city must not be null. modified_by and created_by
+-- may be set to null if the corresponding user has been deleted.
 -- Zip is varchar in order to support hyphens. Brewery to beers is one to many.
 
 CREATE TABLE beers (
@@ -43,13 +47,15 @@ CREATE TABLE beers (
   brew_year   INTEGER,
   brewery_id  INTEGER NOT NULL REFERENCES breweries ON DELETE CASCADE,
   modified_on TIMESTAMP DEFAULT current_timestamp,
-  modified_by INTEGER NOT NULL REFERENCES users (user_id) ON DELETE SET NULL
+  modified_by INTEGER NOT NULL REFERENCES users (user_id) ON DELETE SET NULL,
+  created_on TIMESTAMP DEFAULT current_timestamp,
+  created_by INTEGER REFERENCES users (user_id) ON DELETE SET NULL
 );
 COMMENT ON TABLE beers IS 'Information about each beer';
 -- Store beer information. name and style must be under 100 characters.
 -- Brewery for a specific beer can not be null, and when a brewery is deleted, all
 -- corresponding beers created by that brewery are deleted as well.
--- When modified_by may be set to null if the user that modified the beer is deleted.
+-- modified_by and created_by may be set to null if the corresponding user is deleted.
 -- beer (singular) to brewery is one to one, though a brewery has many beers.
 
 CREATE TABLE ratings (
@@ -62,6 +68,8 @@ CREATE TABLE ratings (
   overall     SMALLINT,
   modified_on TIMESTAMP DEFAULT current_timestamp,
   modified_by INTEGER REFERENCES users (user_id) ON DELETE SET NULL,
+  created_on TIMESTAMP DEFAULT current_timestamp,
+  created_by INTEGER REFERENCES users (user_id) ON DELETE SET NULL,
   CHECK (look > 0 AND look < 11),
   CHECK (smell > 0 AND smell < 11),
   CHECK (taste > 0 AND taste < 11),
@@ -70,7 +78,7 @@ CREATE TABLE ratings (
 COMMENT ON TABLE ratings IS 'Ratings for each beer';
 -- Store rating information. When a user is deleted, the user's ratings are too.
 -- When a beer is deleted, it's ratings are deleted too. Look, smell, taste, and
--- overall are integers between 1 and 10. If a user that has modified a
--- rating has been deleted modified_by will be set to null.
+-- overall are integers between 1 and 10. modified_by and created_by may be set
+-- to null if the corresponding user has been deleted.
 -- A user has many ratings, a beer has many ratings.
 

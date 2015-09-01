ca8761239fd1d944d4b21c91d3326e94e0114d05
Vaughn Bullock <vbullock@clm-vbullock.enova.com>
2015-09-01 10:45:39 -0500

    Refactored, modified class name, added better exception calls

--- robot_name/print_test.rb
+++ robot_name/print_test.rb
@@ -1,11 +1,20 @@
 require_relative "Robot"
 
-robot = Robot.new
+registry  = RobotRegistry.new
+robot     = Robot.new
+
+registry.add_robot_to_registry(robot.name)
 puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
 
-generator = -> { 'AA111' }
-Robot.new(name_generator: generator)
-Robot.new(name_generator: generator)
-
 puts "Robots Registered:"
-puts RobotRegistry.get_registered_robots.inspect
+puts registry.get_registered_robots.inspect
+
+generator = -> { 'AA111' }
+puts "\n!! Assembling AutoBots !!\n"
+(1..2).each do |robot|
+  auto_bot = Robot.new(name_generator: generator)
+  registry.add_robot_to_registry(auto_bot.name)
+
+  puts "Robots Registered:"
+  puts registry.get_registered_robots.inspect
+end

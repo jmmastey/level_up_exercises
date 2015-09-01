93f696d9ebf32c1b30afd10896f248037bc5b938
Kyle McDaniel <mcdaniel221@gmail.com>
2015-09-01 16:47:33 +0000

    Working through the v1.5 suggestions

--- spotigraph/app/assets/javascripts/graph.js
+++ spotigraph/app/assets/javascripts/graph.js
@@ -1,6 +1,10 @@
 function show_graph(graph_nodes, graph_edges){
-    var nodes = new vis.DataSet(graph_nodes);
-    var edges = new vis.DataSet(graph_edges);
+    /** give nodes/edges global scope for testing later (makes it easier to
+      * ensure we handed off the correct information to vis.js. Also makes it
+      * easier to ensure that we have the correct relationships between artists.
+      */
+    nodes = new vis.DataSet(graph_nodes);
+    edges = new vis.DataSet(graph_edges);
     var container = document.getElementById('mynetwork');
     var data = { nodes: nodes, edges: edges};
     var options = {
@@ -10,5 +14,4 @@ function show_graph(graph_nodes, graph_edges){
         }
     };
     var network = new vis.Network(container, data, options);
-    return { nodes: nodes, edges: edges }
 }
--- spotigraph/spec/controllers/graph_controller_spec.rb
+++ spotigraph/spec/controllers/graph_controller_spec.rb
@@ -1,18 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe GraphController, type: :controller do
-  describe 'GET', '#index' do
-    it 'returns http success' do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
-
-    it 'loads the index erb file' do
-      get :index
-      expect(response).to render_template(:index)
-    end
-  end
-
   describe 'POST', '#generate_graph' do
     let(:valid_artists) do
       [

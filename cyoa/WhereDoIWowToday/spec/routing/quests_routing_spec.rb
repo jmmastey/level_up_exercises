acd8532c56db62b5527b2a27ba45afc35d100a2d
Rachel Caileff <rcaileff@enova.com>
2015-09-17 14:48:30 -0500

    Fix quest routing specs.

--- cyoa/WhereDoIWowToday/spec/routing/quests_routing_spec.rb
+++ cyoa/WhereDoIWowToday/spec/routing/quests_routing_spec.rb
@@ -10,8 +10,12 @@ RSpec.describe QuestsController, type: :routing do
       expect(get: "/quests/1/edit").to route_to("quests#edit", id: "1")
     end
 
-    it "routes to #update" do
-      expect(get: "/quests/1/update").to route_to("quests#update", id: "1")
+    it "routes to #update via PUT" do
+      expect(put: "/quests/1").to route_to("quests#update", id: "1")
+    end
+
+    it "routes to #update via PATCH" do
+      expect(patch: "/quests/1").to route_to("quests#update", id: "1")
     end
   end
 end

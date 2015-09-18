478e177b1a33943d27de99240279d82da6b21a0e
Rachel Caileff <rcaileff@enova.com>
2015-09-18 15:42:09 -0500

    Make rubocop happy with remaining category files.

--- cyoa/WhereDoIWowToday/spec/routing/categories_routing_spec.rb
+++ cyoa/WhereDoIWowToday/spec/routing/categories_routing_spec.rb
@@ -2,7 +2,6 @@ require "rails_helper"
 
 RSpec.describe CategoriesController, type: :routing do
   describe "routing" do
-
     it "routes to #index" do
       expect(get: "/categories").to route_to("categories#index")
     end

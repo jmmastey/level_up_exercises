b72e4fa97d3273fcb19dbe642d9ebd534d2ac8a7
Rachel Caileff <rcaileff@enova.com>
2015-09-18 15:31:20 -0500

    Make rubocop happy with character files.

--- cyoa/WhereDoIWowToday/spec/routing/characters_routing_spec.rb
+++ cyoa/WhereDoIWowToday/spec/routing/characters_routing_spec.rb
@@ -2,7 +2,6 @@ require "rails_helper"
 
 RSpec.describe CharactersController, type: :routing do
   describe "routing" do
-
     it "routes to #index" do
       expect(get: "/characters").to route_to("characters#index")
     end

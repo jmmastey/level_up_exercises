9a6ecdb07a44044b54e69d69c05b177fdadfc406
Arpan Patel <apatel@apatel1.local>
2015-10-05 17:08:56 -0500

    spacing

--- robot_name/robot_name.rb
+++ robot_name/robot_name.rb
@@ -16,8 +16,8 @@ class Robot
 
   def generate_name
     name = ""
-    2.times {name << generate_char}
-    3.times {name << generate_num.to_s}
+    2.times { name << generate_char }
+    3.times { name << generate_num.to_s }
     name
   end
 
@@ -49,4 +49,4 @@ puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
 generator = -> { 'AA011' }
 robot1 = Robot.new(generator)
 puts "My pet robot's name is #{robot1.name}, but we usually call him sparky."
-#robot2 = Robot.new(generator)
+# robot2 = Robot.new(generator)

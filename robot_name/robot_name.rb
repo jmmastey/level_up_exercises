f5b9d6256426150d9089fa0290cd421cb922a929
Arpan Patel <apatel@apatel1.local>
2015-10-05 16:04:35 -0500

    code cleanup

--- robot_name/robot_name.rb
+++ robot_name/robot_name.rb
@@ -1,25 +1,27 @@
 require 'pry'
 class NameCollisionError < RuntimeError; end
 
-Error = 'There was a problem generating the robot name!'
+ERROR = 'There was a problem generating the robot name!'
 
 class Robot
   attr_accessor :name
+
   @@registry
-  def initialize(args = {})
+
+  def initialize(name_generator = nil)
     @@registry ||= []
-    args[:name_generator].nil? ? generate_name : @name = args[:name_generator].call
+    @name = name_generator ? name_generator.call : generate_name
     duplication_check
   end
 
   def generate_name
       generate_char = -> { ('A'..'Z').to_a.sample }
       generate_num = -> { rand(10) }
-      @name = "#{generate_char.call}#{generate_char.call}#{generate_num.call}#{generate_num.call}#{generate_num.call}"
+      "#{generate_char.call}#{generate_char.call}#{generate_num.call}#{generate_num.call}#{generate_num.call}"
   end
 
   def duplication_check
-    raise NameCollisionError, Error if incorrect_name_pattern? || name_taken?
+    raise NameCollisionError, ERROR if incorrect_name_pattern? || name_taken?
     @@registry << @name
   end
 
@@ -36,6 +38,6 @@ robot = Robot.new
 puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
 
 generator = -> { 'AA011' }
-robot1 = Robot.new(name_generator: generator)
+robot1 = Robot.new(generator)
 puts "My pet robot's name is #{robot1.name}, but we usually call him sparky."
-robot = Robot.new(name_generator: generator)
+robot1 = Robot.new(generator)

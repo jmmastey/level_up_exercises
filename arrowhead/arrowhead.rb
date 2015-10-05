c7b31a686921aed8565a47fea8e7c801017f50e6
Arpan Patel <apatel@apatel1.local>
2015-10-05 15:32:30 -0500

    Arrowhead

--- arrowhead/arrowhead.rb
+++ arrowhead/arrowhead.rb
@@ -15,20 +15,32 @@ class Arrowhead
     },
   }
 
+  REGION_ERROR = "Unknown region, please provide a valid region.".freeze
+
   # FIXME: I don't have time to deal with this.
   def self.classify(region, shape)
+    shapes = self.find_region(region)
+    self.find_shape(shapes, shape)
+  end
+
+  def self.find_region(region)
     if CLASSIFICATIONS.include? region
-      shapes = CLASSIFICATIONS[region]
-      if shapes.include? shape
-        arrowhead = shapes[shape]
-        "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
-      else
-        raise "Unknown shape value. Are you sure you know what you're talking about?"
-      end
+      CLASSIFICATIONS[region]
     else
-      raise "Unknown region, please provide a valid region."
+      raise REGION_ERROR
+    end
+  end
+
+  def self.find_shape(shapes, shape)
+    if shapes.include? shape
+      arrowhead = shapes[shape]
+      "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
+    else
+      raise "Unknown shape value. Are you sure you know what you're talking about?"
     end
   end
 end
+
+
 
 puts Arrowhead.classify(:northern_plains, :bifurcated)

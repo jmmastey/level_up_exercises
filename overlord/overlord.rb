02b796c53f6aa0382998ceafe60f0f8f630c0f14
Arnaud Thiercelin <athiercelin@Arnauds-MBP.local>
2015-10-07 16:27:49 -0500

    Overlord work in progress
    Started to implem the UI form.

--- overlord/overlord.rb
+++ overlord/overlord.rb
@@ -1,11 +1,31 @@
 # run `ruby overlord.rb` to run a webserver for this app
 
 require 'sinatra'
+require './lib/bomb'
 
 enable :sessions
 
+bomb = Bomb.new("1234", "0000", "bad code")
+
 get '/' do
-  "Time to build an app around here. Start time: " + start_time
+
+	@bombStatus = bomb.active? ? "live" : "inactive"
+	@tries = bomb.failed_deactivations
+	@maxTries = bomb.max_failed_deactivations
+	erb :index
+  # "Time to build an app around here. Start time: " + start_time
+end
+
+post '/' do
+	code = params['code']
+
+	bomb.enter_code(code)
+	@bombStatus = bomb.active? ? "live" : "inactive"
+
+	if bomb.active?
+
+	end
+	erb :index
 end
 
 # we can shove stuff into the session cookie YAY!

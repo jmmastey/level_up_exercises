0532c30bf9c165536c09d713e35167a5fd4f2811
Arnaud Thiercelin <athiercelin@Arnauds-MBP.local>
2015-10-12 09:56:32 -0500

    Rubocop fixes

--- overlord/overlord.rb
+++ overlord/overlord.rb
@@ -1,11 +1,45 @@
 # run `ruby overlord.rb` to run a webserver for this app
 
 require 'sinatra'
+require './lib/bomb'
 
 enable :sessions
 
+bomb = Bomb.new("1234", "0000", 3)
+
+dummy = ["We said number dummy!",
+         "Next time, I'm blowing up!",
+         "Are you drunk?",
+         "How could 0 to 9 confuse you?",
+         "I'm going to start counting these as tries..."]
+
 get '/' do
-  "Time to build an app around here. Start time: " + start_time
+  redirect to('/exploded') if bomb.exploded?
+
+  @dummy = nil
+  if params['dummy']
+    index = rand(3)
+    @dummy = dummy[index]
+  end
+
+  @bomb_active = bomb.active?
+  @tries = bomb.failed_deactivations
+  @max_tries = bomb.max_failed_deactivations
+  erb :index
+  # "Time to build an app around here. Start time: " + start_time
+end
+
+post '/' do
+  code = params['code']
+
+  redirect to('/?dummy=duh') unless code =~ /\A[-+]?[0-9]*\.?[0-9]+\Z/
+  bomb.enter_code(code)
+
+  redirect to('/')
+end
+
+get '/exploded' do
+  erb :exploded
 end
 
 # we can shove stuff into the session cookie YAY!

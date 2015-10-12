3dc2a5daeff3761c2b2cf83bd5c6dd946ea81f92
Arnaud Thiercelin <athiercelin@Arnauds-MBP.local>
2015-10-12 13:02:20 -0500

    Edits based on Dan's comments.

--- robot_name/robot_name.rb
+++ robot_name/robot_name.rb
@@ -1,9 +1,7 @@
 class NameCollisionError < RuntimeError; end
 
-class Robot
-  attr_accessor :name
-
-  def new_name_using_default_generator
+class DefaultNameGenerator
+  def call
     generate_char = -> { ('A'..'Z').to_a.sample }
     generate_num = -> { rand(10) }
 
@@ -11,39 +9,68 @@ class Robot
     "#{generate_char.call}#{generate_char.call}"\
     "#{generate_num.call}#{generate_num.call}#{generate_num.call}"
   end
+end
 
-  def new_name_is_invalid(new_name)
-    # garbage sorting
-    if new_name == '' || new_name.nil?
-      return true
-    # pattern matching
-    elsif !(new_name =~ /[[:alpha:]]{2}[[:digit:]]{3}/)
-      return true
-    # redundancy matching
-    elsif @registry.include?(new_name)
-      return true
-    end
-    false
+class Registry
+  attr_reader :registry
+
+  def initialize(_args = {})
+    @registry = []
   end
 
-  def initialize(args = {})
-    @registry ||= []
-    @name_generator = args[:name_generator]
-    @name = ''
+  def add_entry(new_entry)
+    validate_entry(new_entry)
+    @registry.push(new_entry)
+  end
 
-    while new_name_is_invalid(@name)
-      if @name_generator
-        @name = @name_generator.call
-      else
-        @name = new_name_using_default_generator
-      end
+  def validate_entry(new_entry)
+    # garbage sorting
+    if new_entry == '' || new_entry.nil?
+      raise "GarbageError"
+    # pattern matching
+    elsif !(new_entry =~ /[[:alpha:]]{2}[[:digit:]]{3}/)
+      raise "PatternMatchingError"
+    # redundancy matching
+    elsif @registry.include?(new_entry)
+      raise NameCollisionError
     end
-
-    @registry << @name
   end
 end
 
-robot = Robot.new
+class Robot
+  attr_accessor :name
+
+  def initialize(args = {})
+    @name_generator = assign_name_generator(args[:name_generator])
+    @registry = args[:registry]
+    @max_attempts = 10
+
+    produce_valid_name
+  end
+
+  def assign_name_generator(name_generator)
+    name_generator.nil? ? DefaultNameGenerator.new : name_generator
+  end
+
+  def generate_self_name
+    @name = @name_generator.call
+  end
+
+  def produce_valid_name
+    begin
+      generate_self_name
+      @registry.add_entry(@name)
+    rescue NameCollisionError
+      max_attempts -= 1
+      retry unless @max_attempts == 0
+    rescue StandardError => error
+      puts "Error: " << error.to_s
+    end
+  end
+end
+
+registry = Registry.new
+robot = Robot.new(registry: registry)
 puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
 
 # Errors!

a16377766f329947026a6323c09be0edf881cc37
Arnaud Thiercelin <athiercelin@Arnauds-MBP.local>
2015-10-12 09:46:19 -0500

    More tweaks

--- data_science/data_science_rspec.rb
+++ data_science/data_science_rspec.rb
@@ -30,17 +30,17 @@ describe data_science do
     expect { data_science.parse_json_file(f) }.to raise_error(JSON::ParserError)
   end
 
-   it "should find visits and conversions count for each cohort", :happy do
-     data_science = data_science.new
+  it "should find visits and conversions count for each cohort", :happy do
+    data_science = data_science.new
 
     data_science.parse_json_file('./data_export_2014_06_20_15_59_02.json')
-     data_science.find_counts_per_cohort
+    data_science.find_counts_per_cohort
 
-     expect(data_science).to be_able_to_find_cohort_counts
-     data_science
-   end
+    expect(data_science).to be_able_to_find_cohort_counts
+    data_science
+  end
 
-   it "should give the conversion rate for each cohort", :happy do
+  it "should give the conversion rate for each cohort", :happy do
     data_science = data_science.new
 
     data_science.parse_json_file('./data_export_2014_06_20_15_59_02.json')
@@ -51,20 +51,20 @@ describe data_science do
     data_science.calculate_95_perc_conversion_rate_range("B")
     expect(data_science).to be_able_to_calculate_avg_conv_rate
     expect(data_science).to be_able_to_calculate_95_conv_rate_ranges
-   end
+  end
 
-   it "should give the confidence level ", :happy do
-     data_science = data_science.new
+  it "should give the confidence level ", :happy do
+    data_science = data_science.new
 
-     data_science.parse_json_file('./data_export_2014_06_20_15_59_02.json')
-     data_science.find_counts_per_cohort
+    data_science.parse_json_file('./data_export_2014_06_20_15_59_02.json')
+    data_science.find_counts_per_cohort
 
-     data_science.calculate_average_conversion_rate
-     data_science.calculate_95_perc_conversion_rate_range("A")
-     data_science.calculate_95_perc_conversion_rate_range("B")
+    data_science.calculate_average_conversion_rate
+    data_science.calculate_95_perc_conversion_rate_range("A")
+    data_science.calculate_95_perc_conversion_rate_range("B")
 
-     data_science.calculate_confidence_level
+    data_science.calculate_confidence_level
 
-     expect(data_science).to be_able_to_produce_confidence_level
-   end
+    expect(data_science).to be_able_to_produce_confidence_level
+  end
 end

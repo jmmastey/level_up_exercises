fc88e532c42ca07cbabcf83b074e51b570473ca1
David Rosholt <drosholt@enova.com>
2015-10-15 13:05:38 -0500

    Clean up formatting and simplify error handling in Arrowhead.

--- arrowhead/arrowhead.rb
+++ arrowhead/arrowhead.rb
@@ -1,33 +1,28 @@
 class Arrowhead
-  # This seriously belongs in a database.
+  # TODO: Move this to a database.
   CLASSIFICATIONS = {
-    far_west: {
-      notched: "Archaic Side Notch",
-      stemmed: "Archaic Stemmed",
-      lanceolate: "Agate Basin",
-      bifurcated: "Cody",
+    far_west:        {
+      notched:    'Archaic Side Notch',
+      stemmed:    'Archaic Stemmed',
+      lanceolate: 'Agate Basin',
+      bifurcated: 'Cody',
     },
     northern_plains: {
-      notched: "Besant",
-      stemmed: "Archaic Stemmed",
-      lanceolate: "Humboldt Constricted Base",
-      bifurcated: "Oxbow",
+      notched:    'Besant',
+      stemmed:    'Archaic Stemmed',
+      lanceolate: 'Humboldt Constricted Base',
+      bifurcated: 'Oxbow',
     },
   }
 
-  # FIXME: I don't have time to deal with this.
   def self.classify(region, shape)
-    if CLASSIFICATIONS.include? region
-      shapes = CLASSIFICATIONS[region]
-      if shapes.include? shape
-        arrowhead = shapes[shape]
-        "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
-      else
-        raise "Unknown shape value. Are you sure you know what you're talking about?"
-      end
-    else
-      raise "Unknown region, please provide a valid region."
-    end
+    raise 'Unknown region, please provide a valid region.' unless CLASSIFICATIONS.include?(region)
+
+    shapes = CLASSIFICATIONS[region]
+    raise "Unknown shape value. Are you sure you know what you're talking about?" unless shapes.include?(shape)
+
+    arrowhead = shapes[shape]
+    "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
   end
 end
 

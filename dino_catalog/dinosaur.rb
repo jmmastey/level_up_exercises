48b7b83e8435c31ec5a3f44df5f3cbf2269ac1d8
Arnaud Thiercelin <athiercelin@Arnauds-MBP.local>
2015-10-15 09:14:03 -0500

    New classes for more separation.

---
+++ dino_catalog/dinosaur.rb
@@ -0,0 +1,13 @@
+class Dinosaur
+  attr_reader :name
+  attr_reader :period
+  attr_reader :continent
+  attr_reader :diet
+  attr_reader :weight
+  attr_reader :walking
+  attr_reader :description
+
+  def initialize(name:, period:, continent:, diet:, weight:, walking:, description:)
+    
+  end
+end

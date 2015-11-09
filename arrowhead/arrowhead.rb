f93f91d9e8c6808c8eb70542042827a5c4c1c9b5
Chris Stavitsky <cstavitsky@clm-cstavitsky.enova.com>
2015-11-09 11:51:09 -0600

    Refactor Arrowhead exercise to consolidate error conditions

--- arrowhead/arrowhead.rb
+++ arrowhead/arrowhead.rb
@@ -17,14 +17,10 @@ class Arrowhead
 
   # FIXME: I don't have time to deal with this.
   def self.classify(region, shape)
-    if CLASSIFICATIONS.include? region
+    if CLASSIFICATIONS[region]
       shapes = CLASSIFICATIONS[region]
-      if shapes.include? shape
-        arrowhead = shapes[shape]
-        "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
-      else
-        raise "Unknown shape value. Are you sure you know what you're talking about?"
-      end
+      return "You have a(n) '#{shapes[shape]}' arrowhead. Probably priceless." if shapes[shape]
+      raise "Unknown shape value. Are you sure you know what you're talking about?"
     else
       raise "Unknown region, please provide a valid region."
     end
@@ -32,3 +28,9 @@ class Arrowhead
 end
 
 puts Arrowhead.classify(:northern_plains, :bifurcated)
+
+# Nonexistent region
+# puts Arrowhead.classify(:tundra, :bifurcated)
+
+# Nonexistent shape
+# puts Arrowhead.classify(:far_west, :stoney)

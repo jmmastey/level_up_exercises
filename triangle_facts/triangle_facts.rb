8c006ddf384e22a50e6f7b7cad18403196f8db69
Jeff Rade <jrade@enova.com>
2015-11-09 15:35:30 -0600

    Resubmission of changes to from PR #1046

--- triangle_facts/triangle_facts.rb
+++ triangle_facts/triangle_facts.rb
@@ -1,74 +1,67 @@
 # Killer facts about triangles AWW YEAH
 class Triangle
 
-	def initialize(side1,side2,side3)
-		@side1 = side1
-		@side2 = side2
-		@side3 = side3
-	end
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
 
-	def equilateral()
-		side1 == side2 && side2 == side3
-	end
+  def equilateral?
+    @side1 == @side2 && @side2 == @side3
+  end
 
-	def isosceles()
-		[side1,side2,side3].uniq.length == 2
-	end
+  def isosceles?
+    [@side1, @side2, @side3].uniq.length == 2
+  end
 
-	def scalene()
-		if !equilateral && !isosceles
-	    true
-	  else
-	    false
-	  end
-	end
+  def scalene?
+    !equilateral? && !isosceles?
+  end
 
-	def recite_facts
-		if equilateral
-			puts "This triangle is equilateral!"
-		elsif isosceles
-			puts "This triangle is isosceles! Also, that word is hard to type." 
-		elsif scalene 
-			puts "This triangle is scalene and mathematically boring."
-		end
+  def recite_facts
+    case true
+    when equilateral?
+      puts "This triangle is equilateral!"
+    when isosceles?
+      puts "This triangle is isosceles! Also, that word is hard to type."
+    when scalene?
+      puts "This triangle is scalene and mathematically boring."
+    end
 
-		angles = self.calculate_angles(side1,side2,side3)
-		angles_prettified = angles.join(",")
-		puts "The angles of this triangle are #{angles_prettified}"
+    angles = calculate_angles(@side1, @side2, @side3)
+    angles_prettified = angles.join(",")
+    puts "The angles of this triangle are #{angles_prettified}"
 
-		if angles.include? 90
-				puts "This triangle is also a right triangle!"
-		end
-		puts ""
-	end
+    if angles.include? 90
+      puts "This triangle is also a right triangle!"
+    end
+    puts ""
+    angles_prettified
+  end
 
-	def calculate_angles(a,b,c)
-		angleA = radians_to_degrees(Math.acos((b**2 + c**2 - a**2) / (2.0 * b * c)))
-		angleB = radians_to_degrees(Math.acos((a**2 + c**2 - b**2) / (2.0 * a * c)))
-		angleC = radians_to_degrees(Math.acos((a**2 + b**2 - c**2) / (2.0 * a * b)))
+  def calculate_angles(adjacent, opposite, hypotenuse)
+    angleA = solve_for_angle((opposite**2 + hypotenuse**2 - adjacent**2) / (2.0 * opposite * hypotenuse))
+    angleB = solve_for_angle((adjacent**2 + hypotenuse**2 - opposite**2) / (2.0 * adjacent * hypotenuse))
+    angleC = solve_for_angle((adjacent**2 + opposite**2 - hypotenuse**2) / (2.0 * adjacent * opposite))
 
-		[angleA, angleB, angleC]
-	end
+    [angleA, angleB, angleC]
+  end
 	
-	protected
+  attr_reader :side1, :side2, :side3
 	
-	attr_accessor :side1
-	attr_accessor :side2
-	attr_accessor :side3
+  protected
 	
-	private
+  attr_writer :side1, :side2, :side3
+	
+  private
+	
+  def solve_for_angle(distance)
+    radians = Math.acos(distance)
+    radians_to_degrees(radians)
+  end
 
-	def radians_to_degrees(rads)
-		(rads * 180 / Math::PI).round
-	end
-end
-
-
-triangles = [
-	[5,5,5],
-	[5,12,13],
-]
-triangles.each do |sides|
-	tri = Triangle.new(*sides)
-	tri.recite_facts
+  def radians_to_degrees(rads)
+    (rads * 180 / Math::PI).round
+  end
 end

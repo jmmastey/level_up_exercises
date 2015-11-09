169da7bf0d4d3339caaacca35b78083c4faa3783
Jeff Rade <jrade@enova.com>
2015-11-09 17:02:46 -0600

    Fixing trailing white space and new line notified from hound

--- arrowhead/test_arrowhead.rb
+++ arrowhead/test_arrowhead.rb
@@ -7,5 +7,5 @@ class TestArrowhead < Test::Unit::TestCase
     assert_equal("You have a(n) 'Oxbow' arrowhead. Probably priceless.", Arrowhead.classify(:northern_plains, :bifurcated))
     assert_equal("You have a(n) 'Archaic Stemmed' arrowhead. Probably priceless.", Arrowhead.classify(:far_west, :stemmed))
   end
-  
+
 end

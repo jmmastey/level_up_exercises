0dd4bb3566af6130c1e9a861618ce2e92cfb4057
Jeff Rade <jrade@enova.com>
2015-11-10 09:08:35 -0600

    Arrowhead - Interation 1

---
+++ arrowhead/test_arrowhead.rb
@@ -0,0 +1,12 @@
+require "./arrowhead"
+require "test/unit"
+
+class TestArrowhead < Test::Unit::TestCase
+  def test_arrowhead
+    expected = "You have a(n) 'Oxbow' arrowhead. Probably priceless."
+    assert_equal(expected, Arrowhead.classify(:northern_plains, :bifurcated))
+
+    expected = "You have a(n) 'Archaic Stemmed' arrowhead. Probably priceless."
+    assert_equal(expected, Arrowhead.classify(:far_west, :stemmed))
+  end
+end

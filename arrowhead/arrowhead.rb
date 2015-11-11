bbb71c7f31f46840c75ac53de673eceebe9663a1
creativisionist <creativisionist@gmail.com>
2015-11-11 15:39:51 -0600

    remove pry; compromise hash spacing style

--- arrowhead/arrowhead.rb
+++ arrowhead/arrowhead.rb
@@ -1,19 +1,17 @@
-require 'pry'
-
 # This seriously belongs in a database.
 ## CSV or JSON -- overkill?
 CLASSIFICATIONS = {
   far_west: {
-    notched: "Archaic Side Notch",
-    stemmed: "Archaic Stemmed",
-    lanceolate: "Agate Basin",
-    bifurcated: "Cody",
+              notched:    "Archaic Side Notch",
+              stemmed:    "Archaic Stemmed",
+              lanceolate: "Agate Basin",
+              bifurcated: "Cody",
   },
   northern_plains: {
-    notched: "Besant",
-    stemmed: "Archaic Stemmed",
-    lanceolate: "Humboldt Constricted Base",
-    bifurcated: "Oxbow",
+              notched:    "Besant",
+              stemmed:    "Archaic Stemmed",
+              lanceolate: "Humboldt Constricted Base",
+              bifurcated: "Oxbow",
   },
 }
 

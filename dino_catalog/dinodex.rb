30ae7884ff9ee5f6bc0f917c2b34141a41938b46
creativisionist <creativisionist@gmail.com>
2015-11-19 12:18:06 -0600

    create dinodex class

--- dino_catalog/dinodex.rb
+++ dino_catalog/dinodex.rb
@@ -1,48 +1,91 @@
-require 'pry'
 require 'csv'
+require 'json'
+require_relative 'dinosaur.rb'
 
 CSV::Converters[:blank_to_nil] = lambda do |field|
   field && field.empty? ? nil : field
 end
 
 class Dinodex
-  attr_accessor :african_dinos, :dinos
+  attr_reader :master_dinosaur_list
 
-  def initialize
-    @african_dinos = import_file("african_dinosaur_export.csv")
-    @dinos = import_file("dinodex.csv")
+  def initialize(master_dinosaur_list)
+    @master_dinosaur_list ||= []
+    @master_dinosaur_list = master_dinosaur_list || []
   end
 
-  def import_file(filename)
-    # csv = CSV.read(filename, headers: true, header_converters: lambda { |h| header_translator(h) })
-    # puts csv.inspect
-    # binding.pry
-    csv = CSV.foreach(filename, headers: true, header_converters: :symbol, converters: [:all, :blank_to_nil])
-    csv.to_a.map { |row| row.to_hash }
+  def csv_header_converters
+    {
+      headers: true,
+      header_converters: :symbol,
+      converters: [:all, :blank_to_nil],
+    }
   end
 
-  def translate_files
-
+  def african_dino_import
+    CSV.foreach("african_dinosaur_export.csv", csv_header_converters) do |dino|
+      african_dino = {
+        name: dino[:genus],
+        period: dino[:period],
+        continent: "Africa",
+        diet: carnivore_to_diet(dino[:carnivore]),
+        weight_in_lbs: dino[:weight],
+        walking: dino[:walking],
+      }
+      @master_dinosaur_list << Dinosaur.new(african_dino)
+    end
   end
 
-  # def header_translator(h)
-  #   new_h = h.to_sym
-  #   translate = {
-  #     genus: :name,
-  #     period: :period,
-  #     continent: :continent,
-  #     carnivore: :diet,
-  #     diet: :diet,
-  #     weight: :pounds,
-  #     weight_in_lbs: :pounds,
-  #     walking: :walking,
-  #     description: :description,
-  #   }
-  #   translate[new_h]
-  # end
+  def carnivore_to_diet(diet)
+    diet == "Yes" ? "Carnivore" : "Herbivore"
+  end
+
+  def dino_import
+    CSV.foreach("dinodex.csv", csv_header_converters) do |dino|
+      dinosaur = {
+        name: dino[:name],
+        period: dino[:period],
+        continent: dino[:continent],
+        diet: dino[:diet],
+        weight_in_lbs: dino[:weight_in_lbs],
+        walking: dino[:walking],
+        description: dino[:description],
+      }
+      @master_dinosaur_list << Dinosaur.new(dinosaur)
+    end
+  end
+
+  def biped_search
+    Dinodex.new(@master_dinosaur_list.select(&:biped?))
+  end
+
+  def carnivore_search
+    Dinodex.new(@master_dinosaur_list.select(&:carnivore?))
+  end
+
+  def period_search(period)
+    Dinodex.new(@master_dinosaur_list.select { |dino| dino.period_name(period) })
+  end
+
+  def big
+    Dinodex.new(@master_dinosaur_list.select(&:big?))
+  end
+
+  def small
+    Dinodex.new(@master_dinosaur_list.select(&:small?))
+  end
+
+  def to_s
+    array_of_strings = @master_dinosaur_list.map do |dinosaur|
+      dinosaur.to_s
+    end
+    array_of_strings.join("\n")
+  end
+
+  def export_to_json(filename)
+    dinosaur_data = @master_dinosaur_list.map(&:to_hash)
+    File.open(filename, "w") do |f|
+      f.write(JSON.pretty_generate dinosaur_data)
+    end
+  end
 end
-
-d = Dinodex.new
-puts d.african_dinos
-puts d.dinos
-

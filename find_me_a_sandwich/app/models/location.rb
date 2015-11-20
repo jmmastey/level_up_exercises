830f89226a5b9830f4dd53479a5794ee7078d54e
Dan Kotowski <djkotowski@gmail.com>
2015-11-20 13:52:11 -0600

    Formatting Location#to_s to include street address, city, state and ZIP.

--- find_me_a_sandwich/app/models/location.rb
+++ find_me_a_sandwich/app/models/location.rb
@@ -1,4 +1,8 @@
 class Location < ActiveRecord::Base
   scope :with_city_state, -> (city, state) { where(city: city, state: state) }
   scope :with_zip, -> (zip) { where(zip: zip) }
+
+  def to_s
+    [street, "#{city}, #{state}  #{zip}"].reject(&:blank?).join("\n")
+  end
 end

de4170fa4845c248275b8589be6f1464c0b1155d
Jeff Rade <jrade@enova.com>
2015-11-23 17:10:16 -0600

    Removing parentheses for no arg method call

--- dino_catalog/dinodex/data/hbase_service.rb
+++ dino_catalog/dinodex/data/hbase_service.rb
@@ -14,25 +14,25 @@ module HbaseService
 
   def self.put(key, data)
     raise "You must provide key and data" if key.nil? || data.nil?
-    client = init()
+    client = init
     client.create_row(TABLE, key, Time.now.to_i, name: "#{CF}:#{QUAL}", value: Marshal.dump(data))
   end
 
   def self.get(key)
-    client = init()
+    client = init
     row = client.show_row(TABLE, key)
     value = row.columns[0].value
     Marshal.load(value)
   end
 
   def self.delete(key)
-    client = init()
+    client = init
     client.delete_row(TABLE, key, nil, "#{CF}:#{QUAL}")
   end
 
   def self.scan_rowkeys
     results = []
-    client = init()
+    client = init
     scanner = client.open_scanner(TABLE, columns: ["#{CF}:#{QUAL}"])
     rows = client.get_rows(scanner)
     rows.each { |row| results << row.name }

c674660adc5d34311d776fad7a45a6fd3b13b4e2
Jeff Rade <jrade@enova.com>
2015-11-23 17:01:26 -0600

    Updating hbase_service to use attr_accessor and moved declaration to init() method

--- dino_catalog/dinodex/data/hbase_service.rb
+++ dino_catalog/dinodex/data/hbase_service.rb
@@ -6,33 +6,37 @@ module HbaseService
   QUAL = "data"
   URL = "http://localhost:8777"
 
-  @@client = Stargate::Client.new(URL, proxy: URL)
+  attr_accessor :hbase_client
 
   def self.init
-    @@client.create_table(TABLE, CF)
+    @hbase_client ||= Stargate::Client.new(URL, proxy: URL)
   end
 
   def self.put(key, data)
     raise "You must provide key and data" if key.nil? || data.nil?
-    @@client.create_row(TABLE, key, Time.now.to_i, name: "#{CF}:#{QUAL}", value: Marshal.dump(data))
+    client = init()
+    client.create_row(TABLE, key, Time.now.to_i, name: "#{CF}:#{QUAL}", value: Marshal.dump(data))
   end
 
   def self.get(key)
-    row = @@client.show_row(TABLE, key)
+    client = init()
+    row = client.show_row(TABLE, key)
     value = row.columns[0].value
     Marshal.load(value)
   end
 
   def self.delete(key)
-    @@client.delete_row(TABLE, key, nil, "#{CF}:#{QUAL}")
+    client = init()
+    client.delete_row(TABLE, key, nil, "#{CF}:#{QUAL}")
   end
 
   def self.scan_rowkeys
     results = []
-    scanner = @@client.open_scanner(TABLE, columns: ["#{CF}:#{QUAL}"])
-    rows = @@client.get_rows(scanner)
+    client = init()
+    scanner = client.open_scanner(TABLE, columns: ["#{CF}:#{QUAL}"])
+    rows = client.get_rows(scanner)
     rows.each { |row| results << row.name }
-    @@client.close_scanner(scanner)
+    client.close_scanner(scanner)
     results
   end
 end

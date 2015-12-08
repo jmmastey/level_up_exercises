3d5f1fad9e982b866ac30580a79b44ef2369eadd
Eli Albert <ealbert@enova.com>
2015-12-08 16:22:37 -0600

    refactored for shorter methods. took out internal use of call and anon fns

--- robot_name/robot_name.rb
+++ robot_name/robot_name.rb
@@ -8,21 +8,33 @@ class Robot
   def initialize(args = {})
     @@registry ||= []
     @name_generator = args[:name_generator]
+    @name = @name_generator ? @name_generator.call : generate_new_name
+    check_name
 
-    if @name_generator
-      @name = @name_generator.call
-    else
-      generate_char = -> { ('A'..'Z').to_a.sample }
-      generate_num = -> { rand(10) }
-
-      @name = "#{generate_char.call}#{generate_char.call}#{generate_num.call}#{generate_num.call}#{generate_num.call}"
-    end
-
-    raise NameCollisionError, 'There was a problem generating the robot name!' if !(name =~ /[[:alpha:]]{2}[[:digit:]]{3}/) || @@registry.include?(name)
     @@registry << @name
+  end
+
+  def generate_char 
+    ('A'..'Z').to_a.sample
+  end
+
+  def generate_num
+    rand(10)
+  end
+
+  def generate_new_name
+      "#{generate_char}#{generate_char}#{generate_num}#{generate_num}#{generate_num}"
+  end
+
+  def check_name
+    if !(name =~ /[[:alpha:]]{2}[[:digit:]]{3}/) || @@registry.include?(name)
+      raise NameCollisionError, 'There was a problem generating the robot name!' 
+    end
   end
 end
 
+# generator = -> {'bb121'}
+# robot = Robot.new(name_generator: generator)
 robot = Robot.new
 puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
 

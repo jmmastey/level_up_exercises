9419adfe8173730407b2c6c66199f58c74f20266
Eli Albert <ealbert@enova.com>
2015-12-09 13:52:20 -0600

    step stubs

--- overlord/features/step_definitions/bomb_steps.rb
+++ overlord/features/step_definitions/bomb_steps.rb
@@ -1,1 +1,80 @@
-Given
+
+Given(/^the bomb page is loaded$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I look at the bomb$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^it is not activated$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I input the default activation code$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the bomb is active$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the status informs us that the bomb is active$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^the bomb page is loaded with specified$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I input the specified activation code$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I input the wrong code$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^the bomb page is loaded with non numeric$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I get an error$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I input the default activation code$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^the bomb is active$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I input the default deactivation code$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the bomb is not activated$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^the bomb page is loaded with specified deactivation$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I input the specified deactivation code$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I input an incorrect deactivation code$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the number of attempts remaining is (\d+)$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the bomb explodes$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end

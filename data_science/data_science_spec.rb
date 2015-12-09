02376cb52d5a049b944ce75a6cc63a7de9743e36
Eli Albert <ealbert@enova.com>
2015-12-09 11:56:05 -0600

    testing first

---
+++ data_science/data_science_spec.rb
@@ -0,0 +1,16 @@
+require_relative 'data_science'
+
+describe Optimizer do
+  fake_data = [{},{}]
+  before :each do
+    @optimizer = Optimizer.new fake_data
+  end
+
+  describe it "counts total sample size per cohort" do
+
+  end
+
+  describe it "counds number of conversions per cohort" do
+
+  end 
+end

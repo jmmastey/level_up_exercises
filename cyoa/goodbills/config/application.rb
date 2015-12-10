f718f6caf67afaecc12eeecf105568d15f4edd02
Eli Albert <ealbert@enova.com>
2015-12-10 14:02:21 -0600

    working angular and bower setup

--- cyoa/goodbills/config/application.rb
+++ cyoa/goodbills/config/application.rb
@@ -19,6 +19,10 @@ module Goodbills
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.paths << Rails.root.join("vendor","assets","bower_components")
+    config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","fonts")
+
+    config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff|woff2)$)
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true

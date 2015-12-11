06be693c67ad6f9f8c9c64d8561c192257b5d2a9
Eli Albert <ealbert@enova.com>
2015-12-11 11:38:19 -0600

    little rspec test for the bills api

--- cyoa/goodbills/lib/bills_api.rb
+++ cyoa/goodbills/lib/bills_api.rb
@@ -1,21 +1,5 @@
 require 'congress'
 
-# <Hashie::Mash bill_id="hr4236-114" bill_type="hr" chamber="house" 
-# committee_ids=["HSWM"] congress=114 cosponsors_count=16 
-# enacted_as=nil history=<Hashie::Mash active=false awaiting_signature=false 
-# enacted=false vetoed=false> introduced_on="2015-12-10" last_action_at="2015-12-10" 
-# last_version_on="2015-12-10" last_vote_at=nil number=4236
-#  official_title="To promote savings by providing a tax credit for eligible 
-#  taxpayers who contribute to savings products and to facilitate taxpayers 
-#  receiving this credit and open a designated savings product when they file 
-#  their Federal income tax returns." popular_title=nil related_bill_ids=[]
-#   short_title=nil sponsor=<Hashie::Mash first_name="José" last_name="Serrano" 
-#   middle_name="E." name_suffix=nil nickname=nil title="Rep"> sponsor_id="S000248" 
-#   urls=<Hashie::Mash congress="http://beta.congress.gov/bill/114th/house-bill/4236"
-#    govtrack="https://www.govtrack.us/congress/bills/114/hr4236"
-#     opencongress="https://www.opencongress.org/bill/hr4236-114"> 
-#     withdrawn_cosponsors_count=0>
-
 class BillsAPI
   def self.repull_bills
     puts "repulling bills"

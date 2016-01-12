81241c3774b549e8221fb81c5aaa95486a4cdd49
Jonathan Locker <jlocker@enova.com>
2016-01-12 14:40:48 -0600

    Refactoring previous exercises

--- arrowhead/arrowhead.rb
+++ arrowhead/arrowhead.rb
@@ -15,20 +15,32 @@ class Arrowhead
     },
   }
 
-  # FIXME: I don't have time to deal with this.
   def self.classify(region, shape)
-    if CLASSIFICATIONS.include? region
-      shapes = CLASSIFICATIONS[region]
-      if shapes.include? shape
-        arrowhead = shapes[shape]
-        "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
-      else
-        raise "Unknown shape value. Are you sure you know what you're talking about?"
-      end
-    else
+    validate_region(region)
+    validate_shape(region, shape)
+    arrowhead_info = shape_lookup(region, shape)
+    "You have a(n) '#{arrowhead_info}' arrowhead. Probably priceless."
+  end
+
+  def self.shape_lookup(region, shape)
+    CLASSIFICATIONS[region][shape]
+  end
+
+  def self.validate_region(region)
+    unless CLASSIFICATIONS.include? region
       raise "Unknown region, please provide a valid region."
+    end
+  end
+
+  def self.validate_shape(region, shape)
+    unless CLASSIFICATIONS[region].include? shape
+      error_msg = "Unkonwn shape value. Are you sure you know what"
+      error_msg = error_msg << " you are talking about?"
+      raise error_msg
     end
   end
 end
 
 puts Arrowhead.classify(:northern_plains, :bifurcated)
+puts Arrowhead.classify(:far_west, :lanceolate)
+puts Arrowhead.classify(:far_west, :pretty)
--- triangle_facts/triangle_facts.rb
+++ triangle_facts/triangle_facts.rb
@@ -1,58 +1,74 @@
 # Killer facts about triangles AWW YEAH
 class Triangle
-	attr_accessor :side1,:side2,:side3
+  attr_accessor :side1, :side2, :side3
 
-	def initialize(side1,side2,side3)
-		@side1,@side2,@side3 = side1,side2,side3
-	end
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
 
-	def equilateral()
-		return side1 == side2 && side2 == side3
-	end
+  def facts
+    puts "\nFacts of a triangle with sides #{side1}, #{side2}, #{side3}"
+    triangle_type_facts
+    angle_facts
+    puts "\n"
+  end
 
-	def isosceles()
-		return [side1,side2,side3].uniq.length == 2
-	end
+  def equilateral?
+    side1 == side2 && side2 == side3
+  end
 
-	def scalene()
-		unless ! (equilateral || isosceles)
-	    false
-	  else
-	    true
-	  end
-	end
+  def isosceles?
+    [side1, side2, side3].uniq.length == 2
+  end
 
-	def recite_facts
-		puts 'This triangle is equilateral!' if equilateral
-		puts 'This triangle is isosceles! Also, that word is hard to type.' if isosceles 
-		puts 'This triangle is scalene and mathematically boring.' if scalene 
+  def triangle_type
+    return :equilateral if equilateral?
+    return :isosceles if isosceles?
+    :scalene
+  end
 
-		angles = self.calculate_angles(side1,side2,side3)
-		puts 'The angles of this triangle are ' + angles.join(',')
+  def triangle_type_facts
+    case triangle_type
+      when :equilateral then puts 'This triangle is equilateral!'
+      when :isosceles then puts 'This triangle is isosceles!
+                               Also, that word is hard to type.'
+      when :scalene then puts 'This triangle is scalene and mathematically
+                               boring.'
+    end
+  end
 
-		puts 'This triangle is also a right triangle!' if angles.include? 90
-		puts ''
-	end
+  def angle_facts
+    angles = calculate_angles
+    puts 'The angles of this triangle are ' + angles.join(',')
+    puts 'This triangle is also a right triangle!' if angles.include? 90
+  end
 
-	def calculate_angles(a,b,c)
-		angleA = radians_to_degrees(Math.acos((b**2 + c**2 - a**2) / (2.0 * b * c)))
-		angleB = radians_to_degrees(Math.acos((a**2 + c**2 - b**2) / (2.0 * a * c)))
-		angleC = radians_to_degrees(Math.acos((a**2 + b**2 - c**2) / (2.0 * a * b)))
+  def calculate_angles
+    angle_a = single_angle(side1, side2, side3)
+    angle_b = single_angle(side2, side1, side3)
+    angle_c = single_angle(side3, side1, side2)
 
-		return [angleA, angleB, angleC]
-	end
+    [angle_a, angle_b, angle_c]
+  end
 
-	def radians_to_degrees(rads)
-		return (rads * 180 / Math::PI).round
-	end
+  def single_angle(angle_side, other_side_1, other_side_2)
+    numerator = (other_side_1**2 + other_side_2**2 - angle_side**2)
+    denominator = (2.0 * other_side_1 * other_side_2)
+    radians_to_degrees(Math.acos(numerator / denominator))
+  end
+
+  def radians_to_degrees(rads)
+    (rads * 180 / Math::PI).round
+  end
 end
 
-
 triangles = [
-	[5,5,5],
-	[5,12,13],
+  [5, 5,  5],
+  [5, 12, 13],
 ]
-triangles.each { |sides|
-	tri = Triangle.new(*sides)
-	tri.recite_facts
-}
+triangles.each do |sides|
+  tri = Triangle.new(*sides)
+  tri.facts
+end

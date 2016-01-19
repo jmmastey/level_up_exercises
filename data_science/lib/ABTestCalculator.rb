72b353ee93df3774501c11f40dd68e720740fec9
Joe Curlee <jcurlee@enova.com>
2016-01-19 10:25:06 -0600

    removed useless methods and tests

--- data_science/lib/ABTestCalculator.rb
+++ data_science/lib/ABTestCalculator.rb
@@ -9,16 +9,4 @@ class ABTestCalculator
   def initialize(attrs = {})
     @data = DataLoader.load_data(attrs[:json_file])
   end
-
-  def total_sample_size
-    0
-  end
-
-  def total_conversions
-    0
-  end
-
-  def conversion_rate
-    0.0
-  end
 end
--- data_science/spec/ABTestCalculator_tests.rb
+++ data_science/spec/ABTestCalculator_tests.rb
@@ -14,18 +14,4 @@ RSpec.describe ABTestCalculator, "#read_data" do
       expect(@abtc.data.is_a?(Hash)).to eq true
     end
   end
-
-  context "for a given experiment" do
-    it "returns total sample size as an integer" do
-      expect(@abtc.total_sample_size.is_a?(Integer)).to eq true
-    end
-
-    it "returns total number of conversions as an integer" do
-      expect(@abtc.total_conversions.is_a?(Integer)).to eq true
-    end
-
-    it "returns conversion rate as a decimal" do
-      expect(@abtc.conversion_rate % 1 == 1).to eq false
-    end
-  end
 end

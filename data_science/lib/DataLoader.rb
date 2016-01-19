886e518ba8bd368f8465c0456658cec5ede909cf
Joe Curlee <jcurlee@enova.com>
2016-01-19 10:29:45 -0600

    refactored dataloader

--- data_science/lib/DataLoader.rb
+++ data_science/lib/DataLoader.rb
@@ -2,23 +2,20 @@ require "JSON"
 
 class DataLoader
   def self.load_data(json_file)
-    data = {}
-
     File.open(json_file, "r") do |f|
       parse_data(JSON.parse(f.read))
     end
   end
 
   def self.parse_data(raw_data)
-    parsed_data = {}
+    parsed_data = { sample_size: raw_data.length }
     raw_data.each do |res|
-      cohort = res["cohort"].to_sym
-      parsed_data[cohort] ||= {conversion: 0, nonconversion: 0}
-      parsed_data[cohort][:conversion] += 1 if res["result"] > 0
-      parsed_data[cohort][:nonconversion] += 1 if res["result"] == 0
-    end
+      parsed_data[res["cohort"].to_sym] ||= { conversion: 0, nonconversion: 0 }
 
-    parsed_data[:sample_size] = raw_data.length
+      result = res["result"] == 1 ? :conversion : :nonconversion
+
+      parsed_data[res["cohort"].to_sym][result] += 1
+    end
 
     parsed_data
   end

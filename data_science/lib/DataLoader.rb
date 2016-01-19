f9da443cf791a276d6bf68da2e15b318ddb13bea
Joe Curlee <jcurlee@enova.com>
2016-01-19 09:54:02 -0600

    added a data loader class to load and parse json data

---
+++ data_science/lib/DataLoader.rb
@@ -0,0 +1,23 @@
+require "JSON"
+
+class DataLoader
+  def self.load_data(json_file)
+    data = {}
+
+    File.open(json_file, "r") do |f|
+      parse_data(JSON.parse(f.read))
+    end
+  end
+
+  def self.parse_data(raw_data)
+    parsed_data = {}
+    raw_data.each do |res|
+      parsed_data[res["date"]] ||= {}
+
+      parsed_data[res["date"]][res["cohort"]] ||= 0
+      parsed_data[res["date"]][res["cohort"]] += res["result"]
+    end
+
+    parsed_data
+  end
+end

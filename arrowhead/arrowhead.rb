05fd8019d12345a39718b6198b72b36dbd4b5f86
Balaji Subramanian <bsubramanian@enova.com>
2016-01-20 23:43:04 -0600

    Retaining overlord files while deleting others

--- arrowhead/arrowhead.rb
+++ arrowhead/arrowhead.rb
@@ -17,27 +17,18 @@ class Arrowhead
 
   # FIXME: I don't have time to deal with this.
   def self.classify(region, shape)
-      shapes = find_shapes(region)
-      find_arrowhead(shapes, shape)
-  end
-
-  def self.find_shapes(region)
     if CLASSIFICATIONS.include? region
-      CLASSIFICATIONS[region]
+      shapes = CLASSIFICATIONS[region]
+      if shapes.include? shape
+        arrowhead = shapes[shape]
+        "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
+      else
+        raise "Unknown shape value. Are you sure you know what you're talking about?"
+      end
     else
       raise "Unknown region, please provide a valid region."
-    end 
+    end
   end
-
-  def self.find_arrowhead(shapes,shape)
-     if shapes.include? shape
-       arrowhead = shapes[shape]
-       "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
-     else
-       raise "Unknown shape value. Are you sure you know what you're talking about?"
-     end 
-  end
-
 end
 
-puts Arrowhead.classify(:northern_plaains, :bifurcated)
+puts Arrowhead.classify(:northern_plains, :bifurcated)

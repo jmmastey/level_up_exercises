73ad543cc319503bb2f93c1b7759dc4f3ff18197
Balaji Subramanian <bsubramanian@enova.com>
2016-01-20 23:57:31 -0600

    reverting changes to robot name and triangle facts

--- robot_name/robot_name.rb
+++ robot_name/robot_name.rb
@@ -1,9 +1,4 @@
-require 'pry'
-class NameCollisionError < RuntimeError; 
-  def self.message()
-    'There was a problem generating the robot name!'
-  end
-end
+class NameCollisionError < RuntimeError; end
 
 class Robot
   attr_accessor :name
@@ -11,37 +6,27 @@ class Robot
   @@registry
 
   def initialize(args = {})
-    @name = ''
     @@registry ||= []
     @name_generator = args[:name_generator]
-    generate_name
+
+    if @name_generator
+      @name = @name_generator.call
+    else
+      generate_char = -> { ('A'..'Z').to_a.sample }
+      generate_num = -> { rand(10) }
+
+      @name = "#{generate_char.call}#{generate_char.call}#{generate_num.call}#{generate_num.call}#{generate_num.call}"
+    end
+
+    raise NameCollisionError, 'There was a problem generating the robot name!' if !(name =~ /[[:alpha:]]{2}[[:digit:]]{3}/) || @@registry.include?(name)
     @@registry << @name
   end
-
-  def generate_name
-    @name = @name_generator.call if @name_generator
-    default_name_generator if @name.empty?
-    if !(name =~ /[[:alpha:]]{2}[[:digit:]]{3}/) || @@registry.include?(name)
-      raise NameCollisionError, NameCollisionError.message 
-    end
-  end
-  
-  def default_name_generator
-    generate_char = -> { ('A'..'Z').to_a.sample }
-    generate_num = -> { rand(10) }
-    @name = "#{generate_char.call}#{generate_char.call}"\
-      + "#{generate_num.call}#{generate_num.call}#{generate_num.call}"
-  end
 end
- 
 
 robot = Robot.new
 puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
 
 # Errors!
-=begin
-generator = -> { 'AA111' }
-robot = Robot.new(name_generator: generator)
-puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
-Robot.new(name_generator: generator)
-=end
+# generator = -> { 'AA111' }
+# Robot.new(name_generator: generator)
+# Robot.new(name_generator: generator)
--- triangle_facts/triangle_facts.rb
+++ triangle_facts/triangle_facts.rb
@@ -2,57 +2,48 @@
 class Triangle
 	attr_accessor :side1,:side2,:side3
 
-	def initialize(sides)
-		@side1 = side1
-    @side2 = side2
-    @side3 = side3
+	def initialize(side1,side2,side3)
+		@side1,@side2,@side3 = side1,side2,side3
 	end
 
-  def sides
-    [@side1, @side2, @side3]
-  end
 	def equilateral()
-	  side1 == side2 && side2 == side3
+		return side1 == side2 && side2 == side3
 	end
 
 	def isosceles()
-		[side1, side2, side3].uniq.length == 2
+		return [side1,side2,side3].uniq.length == 2
 	end
 
 	def scalene()
-		!(equilateral || isosceles)
+		unless ! (equilateral || isosceles)
+	    false
+	  else
+	    true
+	  end
 	end
 
 	def recite_facts
-    if(equilateral)
-      puts 'This triangle is equilateral!'
-    elsif(isosceles)
-      puts 'This triangle is isosceles! Also, that word is hard to type.'
-    else
-      puts 'This triangle is scalene and mathematically boring.' 
-    end
+		puts 'This triangle is equilateral!' if equilateral
+		puts 'This triangle is isosceles! Also, that word is hard to type.' if isosceles 
+		puts 'This triangle is scalene and mathematically boring.' if scalene 
 
 		angles = self.calculate_angles(side1,side2,side3)
 		puts 'The angles of this triangle are ' + angles.join(',')
 
 		puts 'This triangle is also a right triangle!' if angles.include? 90
+		puts ''
 	end
 
 	def calculate_angles(a,b,c)
-		angleA = calculate_angle(a,b) 
-		angleB = calculate_angle(b,c) 
-		angleC = calculate_angle(c,a) 
+		angleA = radians_to_degrees(Math.acos((b**2 + c**2 - a**2) / (2.0 * b * c)))
+		angleB = radians_to_degrees(Math.acos((a**2 + c**2 - b**2) / (2.0 * a * c)))
+		angleC = radians_to_degrees(Math.acos((a**2 + b**2 - c**2) / (2.0 * a * b)))
 
 		return [angleA, angleB, angleC]
 	end
 
-  def calculate_angle(side1, side2)
-    radians_to_degrees(Math.cos((side1**2 + side2**2 - 
-      (sides-[side1, side2])**2) / (2.0 * side1 * side2))) 
-  end
-
 	def radians_to_degrees(rads)
-		(rads * 180 / Math::PI).round
+		return (rads * 180 / Math::PI).round
 	end
 end
 

8113f1520aba2fcbd38c43dde2832be084f0b493
Brett Peters <brett.peters1@gmail.com>
2016-01-25 21:59:12 -0700

    Arrowhead - Iteration 1

--- arrowhead/arrowhead.rb
+++ arrowhead/arrowhead.rb
@@ -1,33 +1,19 @@
-class Arrowhead
-  # This seriously belongs in a database.
-  CLASSIFICATIONS = {
-    far_west: {
-      notched: "Archaic Side Notch",
-      stemmed: "Archaic Stemmed",
-      lanceolate: "Agate Basin",
-      bifurcated: "Cody",
-    },
-    northern_plains: {
-      notched: "Besant",
-      stemmed: "Archaic Stemmed",
-      lanceolate: "Humboldt Constricted Base",
-      bifurcated: "Oxbow",
-    },
-  }
+require 'json'
 
-  # FIXME: I don't have time to deal with this.
+data = File.read("arrowhead_data.json")
+CLASSIFICATIONS = JSON.parse(data, :symbolize_names => true)
+
+class Arrowhead
   def self.classify(region, shape)
-    if CLASSIFICATIONS.include? region
-      shapes = CLASSIFICATIONS[region]
-      if shapes.include? shape
-        arrowhead = shapes[shape]
-        "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
-      else
-        raise "Unknown shape value. Are you sure you know what you're talking about?"
-      end
-    else
+    unless shapes = CLASSIFICATIONS[region]
       raise "Unknown region, please provide a valid region."
     end
+
+    unless arrowhead = shapes[shape]
+      raise "Unknown shape value. Are you sure you know what you're talking about?"
+    end
+
+    "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
   end
 end
 

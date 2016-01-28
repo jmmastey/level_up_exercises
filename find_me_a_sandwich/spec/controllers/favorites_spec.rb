ce470c2ca90bb75953437c0014aa8919f9e688b4
Joe Curlee <jcurlee@enova.com>
2016-01-28 10:05:44 -0600

    moved logged in / add favorite spec to describe #new block

--- find_me_a_sandwich/spec/controllers/favorites_spec.rb
+++ find_me_a_sandwich/spec/controllers/favorites_spec.rb
@@ -16,6 +16,24 @@ describe FavoritesController do
   end
 
   describe "#new" do
+    context "When logged in and adding a legit item to favorites" do
+      it "saves the item as a favorite" do
+        user = FactoryGirl.create(:user, password: "1234asdf")
+        merchant = FactoryGirl.create(:merchant, name: "FOO")
+        menu = FactoryGirl.create(:menu, name: "foobar", merchant_id: merchant.id)
+        menu_item = FactoryGirl.create(
+          :menu_item, name: "barbaz", menu_id: menu.id, menu_group: "foogroup"
+        )
+        request.env["HTTP_REFERER"] = "http://test.host/foo"
+
+        sign_in(user)
+        get(:new, id: menu_item.id)
+        fave = Favorite.find_by(user: user)
+
+        expect(fave.menu_item).to eq(menu_item)
+      end
+    end
+
     it "redirects to :back" do
       request.env["HTTP_REFERER"] = "http://test.host/foo"
       get :new
@@ -28,25 +46,6 @@ describe FavoritesController do
       request.env["HTTP_REFERER"] = "http://test.host/foo"
       get :destroy
       expect(response).to redirect_to(:back)
-    end
-  end
-
-  context "When logged in and adding a legit item to favorites" do
-    it "saves the item as a favorite" do
-      # login
-      user = FactoryGirl.create(:user, password: "1234asdf")
-      merchant = FactoryGirl.create(:merchant, name: "FOO")
-      menu = FactoryGirl.create(:menu, name: "foobar", merchant_id: merchant.id)
-      menu_item = FactoryGirl.create(
-        :menu_item, name: "barbaz", menu_id: menu.id, menu_group: "foogroup"
-      )
-      request.env["HTTP_REFERER"] = "http://test.host/foo"
-
-      sign_in(user)
-      get(:new, id: menu_item.id)
-      fave = Favorite.find_by(user: user)
-
-      expect(fave.menu_item).to eq(menu_item)
     end
   end
 end

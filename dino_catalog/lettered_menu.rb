9185b3d7cece5f2b777416fc0ee16a81748058df
Joe Curlee <jcurlee@enova.com>
2016-01-29 15:35:09 -0600

    renamed menus to menu and put LetteredMenu class in a separate file

--- dino_catalog/lettered_menu.rb
+++ dino_catalog/lettered_menu.rb
@@ -1,26 +1,4 @@
-class Menu
-  def initialize(options)
-    @menu_options = Array(options)
-  end
-
-  def show
-    if @menu_options.length == 1
-      puts @menu_options[0]
-    else
-      show_options
-    end
-  end
-
-  private
-
-  def show_options
-    menu_text = ""
-    @menu_options.each_with_index do |option, index|
-      menu_text += "#{index + 1}) #{option} "
-    end
-    puts menu_text
-  end
-end
+require_relative './menu'
 
 class LetteredMenu < Menu
   def initialize(attrs = {})

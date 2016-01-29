9866996f697b336a362e5f4f5115b77c6acb4b45
Eric Brooke <ericbrooke@mac.com>
2016-01-29 16:49:10 -0600

    rename files and start next steps

--- overlord/features/bomb_usage.feature
+++ overlord/features/bomb_usage.feature
@@ -4,6 +4,11 @@ Feature: The bomb be able to be activated and deactivated
   I want to activate and deactivate the bomb
 
   Scenario: Activate a Bomb
-    Given The correct code
-    When I start the application
-    Then the game should say “Super Villain's Detonation Device”
+    Given I have entered the correct activation code
+    When I submit the code
+    Then the bomb will be active
+
+  Scenario: De-activate a Bomb
+    Given I have entered the correct de-activation code
+    When I submit the code
+    Then the bomb will be Inactive
---
+++ overlord/features/step_definitions/bomb_usage.rb
@@ -0,0 +1,23 @@
+$LOAD_PATH << 'lib'
+
+require 'bomb'
+
+Given(/^I have entered the correct activation code$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I submit the code$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the bomb will be active$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I have entered the correct de\-activation code$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the bomb will be Inactive$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end

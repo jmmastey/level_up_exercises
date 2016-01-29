a6c6d8f5142b05df9b4cd95c6dc3586481411fbf
Joe Curlee <jcurlee@enova.com>
2016-01-29 15:24:57 -0600

    added specs and changed dinodex variable and method names to dinosaur_catalog

--- dino_catalog/dinosaur_catalog.rb
+++ dino_catalog/dinosaur_catalog.rb
@@ -12,7 +12,7 @@ class DinosaurCatalog
   end
 
   def initialize(attrs = {})
-    @dinodex = []
+    @dinosaur_catalog = []
 
     @json_file_name = 'dinodex.json'
 
@@ -21,8 +21,8 @@ class DinosaurCatalog
     parse_keys
   end
 
-  def dinodex_periods
-    @dinodex.map { |row| row[:period] }.uniq
+  def dinosaur_catalog_periods
+    @dinosaur_catalog.map { |row| row[:period] }.uniq
   end
 
   def find_large
@@ -31,14 +31,6 @@ class DinosaurCatalog
 
   def find_small
     find_by_weight('small')
-  end
-
-  def find_by_weight(size)
-    @dinodex.select do |row|
-      weight = row[:weight].to_i
-
-      size == 'large' ? weight >= ONE_TON : weight < ONE_TON
-    end
   end
 
   def find_bipeds
@@ -51,33 +43,41 @@ class DinosaurCatalog
       .concat find_dinos(:diet, "piscivore")
   end
 
-  def find_by_period
+  def find_by_period(time_period)
     find_dinos(:period, time_period)
   end
 
-  def find_dinos(key, value, catalog = nil)
-    current_catalog = catalog ? catalog : @dinodex
-    current_catalog.select { |row| row[key] && row[key].downcase == value }
-  end
-
   def export_to_json
-    File.open(@json_file_name, "w") { |f| f.write JSON.dump @dinodex }
+    File.open(@json_file_name, "w") { |f| f.write JSON.dump @dinosaur_catalog }
   end
 
   private
+
+  def find_by_weight(size)
+    @dinosaur_catalog.select do |row|
+      weight = row[:weight].to_i
+
+      size == 'large' ? weight >= ONE_TON : weight < ONE_TON
+    end
+  end
+
+  def find_dinos(key, value, catalog = nil)
+    current_catalog = catalog ? catalog : @dinosaur_catalog
+    current_catalog.select { |row| row[key] && row[key].downcase == value }
+  end
 
   def import_csv_files(files)
     files.map do |file_name|
       CSV.foreach(
         file_name, headers: true, header_converters: :symbol
       ) do |row|
-        @dinodex << row.to_hash
+        @dinosaur_catalog << row.to_hash
       end
     end
   end
 
   def parse_keys
-    @dinodex = @dinodex.map do |row|
+    @dinosaur_catalog = @dinosaur_catalog.map do |row|
       row[:name] = row[:genus] unless row[:name]
       row[:weight] = row[:weight_in_lbs] unless row[:weight]
       row[:diet] = 'Carnivore' if !row[:diet] && row[:carnivore] == 'Yes'
---
+++ dino_catalog/specs/dinosaur_catalog_spec.rb
@@ -0,0 +1,89 @@
+require_relative '../dinosaur_catalog.rb'
+
+describe DinosaurCatalog do
+  fooasaurus = {
+    name: "Fooasaurus",
+    period: "Late Fooeous",
+    continent: "North Amerifoo",
+    diet: "Carnivore",
+    weight_in_lbs: "2000",
+    walking: "Fooped",
+    description: "Like a Foo-Rex but smaller.",
+    weight: "2000",
+  }
+
+  barosaurus = {
+    genus: "Barosaurus",
+    period: "Barassic",
+    carnivore: "No",
+    weight: "100",
+    walking: "Biped",
+    name: "Barosaurus",
+  }
+
+  context "When creating a new DinosaurCatalog with CSV files" do
+    before(:each) do
+      allow(File).to receive(:open).and_return(
+        StringIO.new(
+          "NAME,PERIOD,CONTINENT,DIET,WEIGHT_IN_LBS,WALKING,DESCRIPTION\n"\
+          "Fooasaurus,Late Fooeous,North Amerifoo,Carnivore,2000,Fooped,"\
+            "Like a Foo-Rex but smaller."
+        ),
+        StringIO.new(
+          "Genus,Period,Carnivore,Weight,Walking\n"\
+          "Barosaurus,Barassic,No,100,Biped"
+        ),
+      )
+
+      @dinocat = DinosaurCatalog.new(csv_files: [
+        "foo.csv",
+        "bar.csv",
+      ])
+    end
+
+    describe "#new" do
+      it "sets a default json_file_name" do
+        expect(@dinocat.json_file_name).to eq "dinodex.json"
+      end
+    end
+
+    describe "#dinosaur_catalog_periods" do
+      it "returns a list of dinosaur time periods" do
+        expect(@dinocat.dinosaur_catalog_periods).to eq [
+          "Late Fooeous",
+          "Barassic",
+        ]
+      end
+    end
+
+    describe "#find_large" do
+      it "returns a list of dinosaurs over 1 ton" do
+        expect(@dinocat.find_large).to eq [fooasaurus]
+      end
+    end
+
+    describe "#find_small" do
+      it "returns a list of dinosaurs over 1 ton" do
+        expect(@dinocat.find_small).to eq [barosaurus]
+      end
+    end
+
+    describe "#find_bipeds" do
+      it "returns a list of only bipeds" do
+        expect(@dinocat.find_bipeds).to eq [barosaurus]
+      end
+    end
+
+    describe "#find_carnivores" do
+      it "returns a list of only carnivores" do
+        expect(@dinocat.find_carnivores).to eq [fooasaurus]
+      end
+    end
+
+    describe "#find_by_period" do
+      it "returns a list of dinos by period" do
+        expect(@dinocat.find_by_period("late fooeous")).to eq [fooasaurus]
+      end
+    end
+  end
+end

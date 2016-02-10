0d30e1a3ed91b285e4a1c7d0535df61cadbe31aa
dwin357 <e.r.steinmetz@gmail.com>
2016-02-10 13:02:23 -0600

    switching gears, parser up

--- dino_catalog/third_solution/dino_csv_parser.rb
+++ dino_catalog/third_solution/dino_csv_parser.rb
@@ -1,28 +1,92 @@
 require 'csv'
 
 class DinoCsvParser
+  SPECIAL_CASE_CSV_COLUMN_HEADERS = ["carnivore"]
+  RECOGNIZED_CSV_COLUMN_HEADERS = ["name", "genus", "weight", "walking", "period", "diet", "weight_in_lbs", "locomotion"]
 
-  def self.from_directory(dir_path = "../dino_fact_csvs")
+  def self.from_csv_directory(dir_path = "../dino_fact_csvs")
     ary = [] # each_with_object([]) doesn't work w/ += ary
     Dir["#{dir_path}/*"].each do |file_path|
-      ary += DinoCsvParser.from_file(file_path)
+      ary += DinoCsvParser.from_csv_file(file_path)
     end
     ary
   end
 
-  def self.from_file(file_path)
+  def self.from_csv_file(file_path)
     ary = []
     CSV.foreach(file_path,
                 converters: :numeric,
                 headers: true,
                 :header_converters => lambda {|h| h.downcase}
                 ) do |row|
-                  ary << row.to_h
+                  ary << DinoCsvParser.dino_args_from_csv_row(row)
     end
     ary
+  end
+
+  # def self.from_json(json)
+  # end
+
+  private
+  def self.dino_args_from_csv_row(csv_row)
+    # I kind of want to refactor this so it can also parse a json
+    csv_row.each_with_object({}) do |data, arg|
+      column_header = data[0]
+      cell_data = data[1]
+      if SPECIAL_CASE_CSV_COLUMN_HEADERS.include?(column_header)
+        DinoCsvParser.add_special_case_data_to_arg(column_header, cell_data, arg)
+      elsif RECOGNIZED_CSV_COLUMN_HEADERS.include?(column_header)
+        DinoCsvParser.add_recognized_data_to_arg(column_header, cell_data, arg)
+      else
+        DinoCsvParser.add_unrecognized_data_to_arg(column_header, cell_data, arg)
+      end
+    end
+  end
+
+  def self.add_special_case_data_to_arg(column_header, cell_data, arg)
+    case column_header
+    when "carnivore"
+      recognized_header = "diet"
+      recognized_data = DinoCsvParser.special_case_carnivore_data(cell_data)
+    end
+    DinoCsvParser.add_recognized_data_to_arg(recognized_header, recognized_data, arg)
+  end
+
+  def self.add_recognized_data_to_arg(column_header, cell_data, arg)
+    arg[DinoCsvParser.attribute_from_recognized_csv_header(column_header)] = cell_data
+  end
+
+  def self.add_unrecognized_data_to_arg(column_header, cell_data, arg)
+    arg[:additional_info] = {} unless arg[:additional_info]
+    arg[:additional_info][column_header] = cell_data
+  end
+
+  def self.special_case_carnivore_data(cell_data)
+    if cell_data.downcase == "no"
+      "herbivore"
+    else      
+      "carnivore"
+    end
+  end
+
+  def self.attribute_from_recognized_csv_header(column_header)
+    case column_header
+    when "name", "genus"
+      :name
+    when "weight", "weight_in_lbs"
+      :weight
+    when "locomotion", "walking"
+      :locomotion
+    when "period"
+      :period
+    when "diet"
+      :diet
+    end
   end
 
 end
 
 # test = DinoCsvParser.from_directory
 # puts test.count
+# puts test.first
+# puts test.last
--- dino_catalog/third_solution/spec/dino_csv_parser_spec.rb
+++ dino_catalog/third_solution/spec/dino_csv_parser_spec.rb
@@ -2,21 +2,38 @@ require_relative "../dino_csv_parser"
 
 describe DinoCsvParser do
 
-  let(:sample_output) { DinoCsvParser.from_directory("spec/test")}
+  let(:sample_output) { DinoCsvParser.from_csv_directory("spec/test")}
 
 
-  describe ".from_directory" do
-    it "it pulls from 'dino_catalog/dino_fact_csv' if no other directory is provided"do
-      sbj = DinoCsvParser.from_directory
+  describe ".from_csv_directory" do
+  #   it "should call 'bar' with appropriate arguments" do
+  # expect(subject).to receive(:bar).with("an argument I want")
+  # subject.foo
+  #   end
+
+
+
+    it "pulls from 'dino_catalog/dino_fact_csv' if no other directory is provided"do
+      sbj = DinoCsvParser.from_csv_directory
       expect(sbj.count).to eq(17)
-      expect(sbj.first).to eq({"genus"=>"Abrictosaurus", "period"=>"Jurassic", "carnivore"=>"No", "weight"=>100, "walking"=>"Biped"})
-      expect(sbj.last).to eq({"name"=>"Dracopelta", "period"=>"Early Cretaceous or Late Jurassic", "continent"=>"South America", "diet"=>"Herbivore", "weight_in_lbs"=>nil, "walking"=>"Quadruped", "description"=>"One of the most primitive known Ankylosauria."})
+
+      expect(sbj.first).to eq({:name=>"Abrictosaurus", :period=>"Jurassic", :diet=>"herbivore", :weight=>100, :locomotion=>"Biped"})
+      expect(sbj.last).to eq({:name=>"Dracopelta", :period=>"Early Cretaceous or Late Jurassic", :additional_info=>{"continent"=>"South America", "description"=>"One of the most primitive known Ankylosauria."}, :diet=>"Herbivore", :weight=>nil, :locomotion=>"Quadruped"})
     end
 
     it "returns hashes of csv data in provided directory" do
       expect(sample_output.count).to eq(10)
-      expect(sample_output.first).to eq({"name"=>"Albertosaurus", "period"=>"Late Cretaceous", "continent"=>"North America", "diet"=>"Carnivore", "weight_in_lbs"=>2000, "walking"=>"Biped", "description"=>"Like a T-Rex but smaller."})
-      expect(sample_output.last).to eq({"genus"=>"Suchomimus", "period"=>"Cretaceous", "carnivore"=>"Yes", "weight"=>10400, "walking"=>"Biped"})
+      expect(sample_output.first).to eq({:name=>"Albertosaurus", :period=>"Late Cretaceous", :additional_info=>{"continent"=>"North America", "description"=>"Like a T-Rex but smaller."}, :diet=>"Carnivore", :weight=>2000, :locomotion=>"Biped"})
+      expect(sample_output.last).to eq({:name=>"Suchomimus", :period=>"Cretaceous", :diet=>"carnivore", :weight=>10400, :locomotion=>"Biped"})
+    end
+  end
+
+  describe ".from_csv_file" do
+    it "returns the csv data in argument hashes for a dinosaur" do
+      subject = DinoCsvParser.from_csv_file("spec/test/file_one.csv")
+      expect(subject.count).to eq(5)
+      expect(subject.first).to eq({:name=>"Albertosaurus", :period=>"Late Cretaceous", :additional_info=>{"continent"=>"North America", "description"=>"Like a T-Rex but smaller."}, :diet=>"Carnivore", :weight=>2000, :locomotion=>"Biped"})
+      expect(subject.last).to eq( {:name=>"Diplocaulus", :period=>"Late Permian", :additional_info=>{"continent"=>"North America", "description"=>"They actually had fins on the side of their body."}, :diet=>"Carnivore", :weight=>nil, :locomotion=>"Quadruped"})
     end
   end
 end

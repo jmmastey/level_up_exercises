8c98c90b704d3a3b3fb2a00825a3cb45ddd27f83
Brett Peters <brett.peters1@gmail.com>
2016-02-09 20:41:43 -0700

    Remove dependencies from Dinosaur class and rewrite some sheisty code

--- dino_catalog/dinosaur.rb
+++ dino_catalog/dinosaur.rb
@@ -1,15 +1,33 @@
 class Dinosaur
-  attr_reader :name, :period, :continent, :diet, :weight, :walking,
-              :description, :size, :to_h
-
   def initialize(args = {})
-    @to_h = args
-    args.each { |k,v| instance_variable_set("@#{k}", v) }
+    create_instance_variables(args)
+    create_getters
   end
 
   def to_s
-    Array(to_h).reject { |pair|
-      pair.include?(nil) }.map { |pair|
-        pair.join(": ") }.join("\n")
+    valid_fields = all_info.reject { |_k, v| v.nil? }
+    valid_fields.each_with_object("") do |(k, v), output|
+      output << "#{k.capitalize}: #{v}\n"
+    end
+  end
+
+  private
+
+  def create_instance_variables(args)
+    args.each { |k, v| instance_variable_set("@#{k}", v) }
+  end
+
+  def create_getters
+    instance_variables.each do |var|
+      self.class.send(:define_method, "#{var[1..-1]}") do
+        instance_variable_get(var)
+      end
+    end
+  end
+
+  def all_info
+    instance_variables.each_with_object({}) do |var, info|
+      info[var[1..-1].to_sym] = instance_variable_get(var)
+    end
   end
 end

174c6f3aa4ae75d99640640eb896c21ad7a4bbb2
Alan Alcesto <AlanAlcesto@Alans-MacBook-Pro.local>
2016-05-25 13:13:15 -0500

    Refactor triangle code. Remove explicite returns. Use standard if statement formats for readability.

--- triangle_facts/triangle_facts.rb
+++ triangle_facts/triangle_facts.rb
@@ -3,34 +3,40 @@ class Triangle
 	attr_accessor :side1,:side2,:side3
 
 	def initialize(side1,side2,side3)
-		@side1,@side2,@side3 = side1,side2,side3
+		@side1 = side1
+		@side2 = side2
+		@side3 = side3
 	end
 
-	def equilateral()
-		return side1 == side2 && side2 == side3
+	def equilateral
+		side1 == side2 && side2 == side3
 	end
 
-	def isosceles()
-		return [side1,side2,side3].uniq.length == 2
+	def isosceles
+		[side1,side2,side3].uniq.length == 2
 	end
 
-	def scalene()
-		unless ! (equilateral || isosceles)
-	    false
-	  else
-	    true
-	  end
+	def scalene
+		if !(equilateral || isosceles)
+			false
+		end
 	end
 
 	def recite_facts
-		puts 'This triangle is equilateral!' if equilateral
-		puts 'This triangle is isosceles! Also, that word is hard to type.' if isosceles 
-		puts 'This triangle is scalene and mathematically boring.' if scalene 
+		if equilateral
+			puts 'This triangle is equilateral!'
+		elsif isosceles
+			puts 'This triangle is isosceles! Also, that word is hard to type.'
+		else
+			puts 'This triangle is scalene and mathematically boring.'
+		end
 
 		angles = self.calculate_angles(side1,side2,side3)
 		puts 'The angles of this triangle are ' + angles.join(',')
 
-		puts 'This triangle is also a right triangle!' if angles.include? 90
+		if angles.include? 90
+			puts 'This triangle is also a right triangle!'
+		end
 		puts ''
 	end
 
@@ -39,11 +45,11 @@ class Triangle
 		angleB = radians_to_degrees(Math.acos((a**2 + c**2 - b**2) / (2.0 * a * c)))
 		angleC = radians_to_degrees(Math.acos((a**2 + b**2 - c**2) / (2.0 * a * b)))
 
-		return [angleA, angleB, angleC]
+		[angleA, angleB, angleC]
 	end
 
 	def radians_to_degrees(rads)
-		return (rads * 180 / Math::PI).round
+		(rads * 180 / Math::PI).round
 	end
 end
 

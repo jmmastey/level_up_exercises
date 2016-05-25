63fc4a72427c0699cb27b4b0a38b54c004b96b36
Alan Alcesto <AlanAlcesto@Alans-MacBook-Pro.local>
2016-05-25 15:32:50 -0500

    Separate classify method into two methods.

--- arrowhead/arrowhead.rb
+++ arrowhead/arrowhead.rb
@@ -4,15 +4,19 @@ class Arrowhead
 
   def self.classify(region, shape)
     if Classifications::CLASSIFICATIONS.include? region
-      shapes = Classifications::CLASSIFICATIONS[region]
-      if shapes.include? shape
-        arrowhead = shapes[shape]
-        "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
-      else
-        raise "Unknown shape value. Are you sure you know what you're talking about?"
-      end
+      self.something(region, shape)
     else
       raise "Unknown region, please provide a valid region."
+    end
+  end
+
+  def self.something(region, shape)
+    shapes = Classifications::CLASSIFICATIONS[region]
+    if shapes.include? shape
+      arrowhead = shapes[shape]
+      "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
+    else
+      raise "Unknown shape value. Are you sure you know what you're talking about?"
     end
   end
 

e3fa931551fed3f53fb2dea5e1ee231e8453033d
Arman Jon Villalobos <me@armanjon.com>
2016-06-14 21:36:50 +0800

    Remove arrowhead anti pattern & load database from json file

--- arrowhead/arrowhead.rb
+++ arrowhead/arrowhead.rb
@@ -1,34 +1,20 @@
-class Arrowhead
-  # This seriously belongs in a database.
-  CLASSIFICATIONS = {
-    far_west: {
-      notched: "Archaic Side Notch",
-      stemmed: "Archaic Stemmed",
-      lanceolate: "Agate Basin",
-      bifurcated: "Cody",
-    },
-    northern_plains: {
-      notched: "Besant",
-      stemmed: "Archaic Stemmed",
-      lanceolate: "Humboldt Constricted Base",
-      bifurcated: "Oxbow",
-    },
-  }
+require 'json'
 
-  # FIXME: I don't have time to deal with this.
+class Arrowhead
+  REGION_ERROR = "Unknown region, please provide a valid region.".freeze
+  SHAPE_ERROR =  "Unknown shape value. Are you sure you know what
+  you're talking about?".freeze
+  CLASSIFICATIONS = JSON.parse(File.read("arrowhead/database.json"))
+
   def self.classify(region, shape)
-    if CLASSIFICATIONS.include? region
-      shapes = CLASSIFICATIONS[region]
-      if shapes.include? shape
-        arrowhead = shapes[shape]
-        "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
-      else
-        raise "Unknown shape value. Are you sure you know what you're talking about?"
-      end
-    else
-      raise "Unknown region, please provide a valid region."
-    end
+    raise REGION_ERROR unless CLASSIFICATIONS.include?(region)
+
+    shapes = CLASSIFICATIONS[region]
+    raise SHAPE_ERROR unless shapes.include?(shape)
+
+    arrowhead = shapes[shape]
+    "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
   end
 end
 
-puts Arrowhead.classify(:northern_plains, :bifurcated)
+puts Arrowhead.classify('northern_plains', 'bifurcated')

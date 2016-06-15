0ef9c8726867672bb012c1211d4091250ae2eefe
Arman Jon Villalobos <me@armanjon.com>
2016-06-15 16:15:20 +0800

    Refactor RobotName to short methods
    I retain the `NameCollisionError` class and `@@registry`. I want the
    `initialize` method to only accept a var instead of hash but ruby
    doesn't allow an optional parameter. I THINK this is better than my
    previous attempt, all functions are still the same. I'm not sure
    about my `not_in_the_registry?` method since I use negation there.
    
    Thank you for your awesome feedback!

--- robot_name/robot_name.rb
+++ robot_name/robot_name.rb
@@ -1,24 +1,42 @@
 class NameCollisionError < RuntimeError; end
 
 class Robot
-  attr_accessor :name
+  CHARS = ('A'..'Z').to_a
+  NUMS = (1..10).to_a
 
   @@registry
+  attr_accessor :name
 
   def initialize(args = {})
     @@registry ||= []
     @name_generator = args[:name_generator]
+    @name = get_name
+    add_name_to_registry
+  end
 
-    if @name_generator
-      @name = @name_generator.call
-    else
-      generate_char = -> { ('A'..'Z').to_a.sample }
-      generate_num = -> { rand(10) }
+  def get_name
+    return @name_generator.call if @name_generator
+    generate_name
+  end
 
-      @name = "#{generate_char.call}#{generate_char.call}#{generate_num.call}#{generate_num.call}#{generate_num.call}"
-    end
+  def generate_name
+    name = CHARS.sample(2).join + NUMS.shuffle.take(3).join
+  end
 
-    raise NameCollisionError, 'There was a problem generating the robot name!' if !(name =~ /[[:alpha:]]{2}[[:digit:]]{3}/) || @@registry.include?(name)
+  def valid_name?
+    valid_format? && not_in_the_registry?
+  end
+
+  def valid_format?
+    @name =~ /[[:alpha:]]{2}[[:digit:]]{3}/
+  end
+
+  def not_in_the_registry?
+    !@@registry.include?(@name)
+  end
+
+  def add_name_to_registry
+    raise NameCollisionError, 'There was a problem generating the robot name!' unless valid_name?
     @@registry << @name
   end
 end
